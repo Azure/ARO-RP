72fa3820db38472193b64fd5058dcb179b994112
Jim Minter <jminter@redhat.com>
2019-10-18 14:11:11 -0500

    combine logic of getOpenShiftCluster and getOpenShiftClusterCredentials

--- pkg/frontend/get.go
+++ pkg/frontend/get.go
@@ -13,17 +13,25 @@ import (
 )
 
 func (f *frontend) getOpenShiftCluster(w http.ResponseWriter, r *http.Request) {
+	f.get(w, r, r.URL.Path, "OpenShiftCluster")
+}
+
+func (f *frontend) getOpenShiftClusterCredentials(w http.ResponseWriter, r *http.Request) {
+	f.get(w, r, filepath.Dir(r.URL.Path), "OpenShiftClusterCredentials")
+}
+
+func (f *frontend) get(w http.ResponseWriter, r *http.Request, resourceID, typ string) {
 	log := r.Context().Value(contextKeyLog).(*logrus.Entry)
 	vars := mux.Vars(r)
 
-	toExternal, found := api.APIs[api.APIVersionType{APIVersion: r.URL.Query().Get("api-version"), Type: "OpenShiftCluster"}]
+	toExternal, found := api.APIs[api.APIVersionType{APIVersion: r.URL.Query().Get("api-version"), Type: typ}]
 	if !found {
 		f.error(w, http.StatusNotFound, api.CloudErrorCodeInvalidResourceType, "", "The resource type '%s' could not be found in the namespace '%s' for api version '%s'.", vars["resourceType"], vars["resourceProviderNamespace"], r.URL.Query().Get("api-version"))
 		return
 	}
 
 	b, err := f._getOpenShiftCluster(&request{
-		resourceID:        r.URL.Path,
+		resourceID:        resourceID,
 		resourceGroupName: vars["resourceGroupName"],
 		resourceName:      vars["resourceName"],
 		resourceType:      vars["resourceProviderNamespace"] + "/" + vars["resourceType"],
@@ -120,46 +128,4 @@ func (f *frontend) _getOpenShiftClusters(r *request) ([]byte, error) {
 	}
 
 	return json.MarshalIndent(rv, "", "  ")
-}
-
-func (f *frontend) getOpenShiftClusterCredentials(w http.ResponseWriter, r *http.Request) {
-	log := r.Context().Value(contextKeyLog).(*logrus.Entry)
-	vars := mux.Vars(r)
-
-	toExternal, found := api.APIs[api.APIVersionType{APIVersion: r.URL.Query().Get("api-version"), Type: "OpenShiftClusterCredentials"}]
-	if !found {
-		f.error(w, http.StatusNotFound, api.CloudErrorCodeInvalidResourceType, "The resource type '%s' could not be found in the namespace '%s' for api version '%s'.", vars["resourceType"], vars["resourceProviderNamespace"], r.URL.Query().Get("api-version"))
-		return
-	}
-
-	b, err := f._getOpenShiftClusterCredentials(&request{
-		resourceID:        filepath.Dir(r.URL.Path),
-		resourceGroupName: vars["resourceGroupName"],
-		toExternal:        toExternal,
-	})
-	if err != nil {
-		switch err := err.(type) {
-		case *api.CloudError:
-			f.cloudError(w, err)
-		default:
-			log.Error(err)
-			f.error(w, http.StatusInternalServerError, api.CloudErrorCodeInternalServerError, "", "Internal server error.")
-		}
-		return
-	}
-
-	w.Write(b)
-	w.Write([]byte{'\n'})
-}
-
-func (f *frontend) _getOpenShiftClusterCredentials(r *request) ([]byte, error) {
-	doc, err := f.db.Get(r.resourceID)
-	switch {
-	case cosmosdb.IsErrorStatusCode(err, http.StatusNotFound):
-		return nil, api.NewCloudError(http.StatusNotFound, api.CloudErrorCodeResourceNotFound, "", "The Resource '%s/%s' under resource group '%s' was not found.", r.resourceType, r.resourceName, r.resourceGroupName)
-	case err != nil:
-		return nil, err
-	}
-
-	return json.MarshalIndent(r.toExternal(doc.OpenShiftCluster), "", "  ")
 }
