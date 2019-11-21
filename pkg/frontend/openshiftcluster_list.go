6ef95475697ba3e92be4a2a965317939935a2092
Jim Minter <jminter@redhat.com>
2019-11-21 08:13:07 -0600

    frontend file reorg

---
+++ pkg/frontend/openshiftcluster_list.go
@@ -0,0 +1,72 @@
+package frontend
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+
+	"github.com/jim-minter/rp/pkg/api"
+)
+
+func (f *frontend) getOpenShiftClusters(w http.ResponseWriter, r *http.Request) {
+	log := r.Context().Value(contextKeyLog).(*logrus.Entry)
+	vars := mux.Vars(r)
+
+	toExternal, found := api.APIs[api.APIVersionType{APIVersion: r.URL.Query().Get("api-version"), Type: "OpenShiftCluster"}]
+	if !found {
+		f.error(w, http.StatusNotFound, api.CloudErrorCodeInvalidResourceType, "", "The resource type '%s' could not be found in the namespace '%s' for api version '%s'.", vars["resourceType"], vars["resourceProviderNamespace"], r.URL.Query().Get("api-version"))
+		return
+	}
+
+	b, err := f._getOpenShiftClusters(&request{
+		subscriptionID:    vars["subscriptionId"],
+		resourceGroupName: vars["resourceGroupName"],
+		resourceType:      vars["resourceProviderNamespace"] + "/" + vars["resourceType"],
+		toExternal:        toExternal,
+	})
+	if err != nil {
+		switch err := err.(type) {
+		case *api.CloudError:
+			f.cloudError(w, err)
+		default:
+			log.Error(err)
+			f.error(w, http.StatusInternalServerError, api.CloudErrorCodeInternalServerError, "", "Internal server error.")
+		}
+		return
+	}
+
+	w.Write(b)
+	w.Write([]byte{'\n'})
+}
+
+func (f *frontend) _getOpenShiftClusters(r *request) ([]byte, error) {
+	prefix := "/subscriptions/" + r.subscriptionID + "/"
+	if r.resourceGroupName != "" {
+		prefix += "resourceGroups/" + r.resourceGroupName + "/"
+	}
+	i := f.db.ListByPrefix(r.subscriptionID, prefix)
+
+	var rv struct {
+		Value []api.External `json:"value"`
+	}
+
+	for {
+		docs, err := i.Next()
+		if err != nil {
+			return nil, err
+		}
+		if docs == nil {
+			break
+		}
+
+		for _, doc := range docs.OpenShiftClusterDocuments {
+			doc.OpenShiftCluster.Type = r.resourceType
+			doc.OpenShiftCluster.Properties.ServicePrincipalProfile.ClientSecret = ""
+			rv.Value = append(rv.Value, r.toExternal(doc.OpenShiftCluster))
+		}
+	}
+
+	return json.MarshalIndent(rv, "", "  ")
+}
