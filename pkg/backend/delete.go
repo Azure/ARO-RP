5ea52aff6309845322660685cadb83b6a736ed97
Jim Minter <jminter@redhat.com>
2019-11-25 11:31:38 -0600

    tear down NSGs correctly at cluster delete

--- pkg/backend/delete.go
+++ pkg/backend/delete.go
@@ -10,6 +10,7 @@ import (
 	"github.com/sirupsen/logrus"
 
 	"github.com/jim-minter/rp/pkg/api"
+	"github.com/jim-minter/rp/pkg/util/subnet"
 )
 
 func (b *backend) delete(ctx context.Context, log *logrus.Entry, doc *api.OpenShiftClusterDocument) error {
@@ -23,6 +24,29 @@ func (b *backend) delete(ctx context.Context, log *logrus.Entry, doc *api.OpenSh
 	_, err := recordsets.Delete(ctx, os.Getenv("RESOURCEGROUP"), b.domain, "api."+doc.OpenShiftCluster.Name, dns.CNAME, "")
 	if err != nil {
 		return err
+	}
+
+	// TODO: ideally we would do this after all the VMs have been deleted
+	for _, subnetID := range []string{
+		doc.OpenShiftCluster.Properties.MasterProfile.SubnetID,
+		doc.OpenShiftCluster.Properties.WorkerProfiles[0].SubnetID,
+	} {
+		log.Printf("removing network security group from subnet %s", subnetID)
+
+		// TODO: there is probably an undesirable race condition here - check if etags can help.
+		s, err := subnet.Get(ctx, &doc.OpenShiftCluster.Properties.ServicePrincipalProfile, subnetID)
+		if err != nil {
+			return err
+		}
+
+		if s.SubnetPropertiesFormat != nil {
+			s.SubnetPropertiesFormat.NetworkSecurityGroup = nil
+
+			err = subnet.CreateOrUpdate(ctx, &doc.OpenShiftCluster.Properties.ServicePrincipalProfile, subnetID, s)
+			if err != nil {
+				return err
+			}
+		}
 	}
 
 	resp, err := groups.CheckExistence(ctx, doc.OpenShiftCluster.Properties.ResourceGroup)
