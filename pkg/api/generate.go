f1e787885acbf3a9ae70b7baf0ca471947cc275a
Angus Salkeld <asalkeld@redhat.com>
2019-11-28 12:31:26 +1000

    Use vendored generators

--- pkg/api/generate.go
+++ pkg/api/generate.go
@@ -1,3 +1,3 @@
-//go:generate go run github.com/alvaroloes/enumer -type InstallPhase -output zz_generated_installphase_enumer.go
+//go:generate go run ../../vendor/github.com/alvaroloes/enumer -type InstallPhase -output zz_generated_installphase_enumer.go
 
 package api
--- pkg/database/cosmosdb/generate.go
+++ pkg/database/cosmosdb/generate.go
@@ -1,3 +1,3 @@
-//go:generate go run github.com/jim-minter/go-cosmosdb/cmd/gencosmosdb github.com/jim-minter/rp/pkg/api,OpenShiftClusterDocument github.com/jim-minter/rp/pkg/api,SubscriptionDocument
+//go:generate go run ../../../vendor/github.com/jim-minter/go-cosmosdb/cmd/gencosmosdb github.com/jim-minter/rp/pkg/api,OpenShiftClusterDocument github.com/jim-minter/rp/pkg/api,SubscriptionDocument
 
 package cosmosdb
