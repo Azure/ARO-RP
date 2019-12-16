1529b8cfebf1646b3b105b88f5c97b43290c96d1
Jim Minter <jminter@redhat.com>
2019-12-16 10:51:36 -0600

    update supported python versions

--- python/az/aro/setup.py
+++ python/az/aro/setup.py
@@ -20,9 +20,10 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'License :: OSI Approved :: Apache Software License',
 ]
 
