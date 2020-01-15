927108f3b38667a21233150b827ebb2f9bccf5bb
Mikalai Radchuk <mradchuk@redhat.com>
2020-01-15 14:30:21 +0000

    Adds tests for async operations frontend

---
+++ pkg/frontend/asyncoperationresult_get_test.go
@@ -0,0 +1,256 @@
+package frontend
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/sirupsen/logrus"
+
+	"github.com/Azure/ARO-RP/pkg/api"
+	"github.com/Azure/ARO-RP/pkg/api/v20191231preview"
+	"github.com/Azure/ARO-RP/pkg/database"
+	"github.com/Azure/ARO-RP/pkg/database/cosmosdb"
+	"github.com/Azure/ARO-RP/pkg/env"
+	"github.com/Azure/ARO-RP/pkg/metrics/noop"
+	mock_database "github.com/Azure/ARO-RP/pkg/util/mocks/database"
+	utiltls "github.com/Azure/ARO-RP/pkg/util/tls"
+	"github.com/Azure/ARO-RP/test/util/listener"
+)
+
+func TestGetAsyncOperationResult(t *testing.T) {
+	ctx := context.Background()
+
+	clientkey, clientcerts, err := utiltls.GenerateKeyAndCertificate("client", nil, nil, false, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	serverkey, servercerts, err := utiltls.GenerateKeyAndCertificate("server", nil, nil, false, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pool := x509.NewCertPool()
+	pool.AddCert(servercerts[0])
+
+	cli := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs: pool,
+				Certificates: []tls.Certificate{
+					{
+						Certificate: [][]byte{clientcerts[0].Raw},
+						PrivateKey:  clientkey,
+					},
+				},
+			},
+		},
+	}
+
+	mockSubID := "00000000-0000-0000-0000-000000000000"
+	mockClusterDocKey := "22222222-2222-2222-2222-222222222222"
+	mockOpID := "11111111-1111-1111-1111-111111111111"
+
+	type test struct {
+		name           string
+		mocks          func(*mock_database.MockOpenShiftClusters, *mock_database.MockAsyncOperations)
+		wantStatusCode int
+		wantAsync      bool
+		wantResponse   func() *v20191231preview.OpenShiftCluster
+		wantError      string
+	}
+
+	for _, tt := range []*test{
+		{
+			name: "operation and cluster exist in db - final result is available with content",
+			mocks: func(openshiftClusters *mock_database.MockOpenShiftClusters, asyncOperations *mock_database.MockAsyncOperations) {
+				clusterDoc := &api.OpenShiftClusterDocument{
+					OpenShiftCluster: &api.OpenShiftCluster{
+						ID:   "fakeClusterID",
+						Name: "resourceName",
+						Type: "Microsoft.RedHatOpenShift/openshiftClusters",
+						Properties: api.Properties{
+							ServicePrincipalProfile: api.ServicePrincipalProfile{
+								ClientSecret: "clientSecret",
+							},
+						},
+					},
+				}
+
+				asyncOperations.EXPECT().
+					Get(gomock.Any(), mockOpID).
+					Return(&api.AsyncOperationDocument{
+						ID:                  mockOpID,
+						OpenShiftClusterKey: mockClusterDocKey,
+						OpenShiftCluster:    clusterDoc.OpenShiftCluster,
+					}, nil)
+
+				openshiftClusters.EXPECT().Get(gomock.Any(), mockClusterDocKey).
+					Return(clusterDoc, nil)
+			},
+			wantStatusCode: http.StatusOK,
+			wantResponse: func() *v20191231preview.OpenShiftCluster {
+				return &v20191231preview.OpenShiftCluster{
+					ID:   "fakeClusterID",
+					Name: "resourceName",
+					Type: "Microsoft.RedHatOpenShift/openshiftClusters",
+				}
+			},
+		},
+		{
+			name: "operation exists in db, but no cluster - final result is available with no content",
+			mocks: func(openshiftClusters *mock_database.MockOpenShiftClusters, asyncOperations *mock_database.MockAsyncOperations) {
+				asyncOperations.EXPECT().
+					Get(gomock.Any(), mockOpID).
+					Return(&api.AsyncOperationDocument{
+						ID:                  mockOpID,
+						OpenShiftClusterKey: mockClusterDocKey,
+					}, nil)
+
+				openshiftClusters.EXPECT().Get(gomock.Any(), mockClusterDocKey).
+					Return(nil, &cosmosdb.Error{StatusCode: http.StatusNotFound})
+			},
+			wantStatusCode: http.StatusNoContent,
+		},
+		{
+			name: "operation and cluster exist in db - final result is not yet available",
+			mocks: func(openshiftClusters *mock_database.MockOpenShiftClusters, asyncOperations *mock_database.MockAsyncOperations) {
+				asyncOperations.EXPECT().
+					Get(gomock.Any(), mockOpID).
+					Return(&api.AsyncOperationDocument{
+						ID:                  mockOpID,
+						OpenShiftClusterKey: mockClusterDocKey,
+					}, nil)
+
+				openshiftClusters.EXPECT().Get(gomock.Any(), mockClusterDocKey).
+					Return(&api.OpenShiftClusterDocument{
+						AsyncOperationID: mockOpID,
+					}, nil)
+			},
+			wantAsync:      true,
+			wantStatusCode: http.StatusAccepted,
+		},
+		{
+			name: "operation not found in db",
+			mocks: func(openshiftClusters *mock_database.MockOpenShiftClusters, asyncOperations *mock_database.MockAsyncOperations) {
+				asyncOperations.EXPECT().
+					Get(gomock.Any(), mockOpID).
+					Return(nil, &cosmosdb.Error{StatusCode: http.StatusNotFound})
+			},
+			wantStatusCode: http.StatusNotFound,
+			wantError:      `404: NotFound: : The entity was not found.`,
+		},
+		{
+			name: "internal error",
+			mocks: func(openshiftClusters *mock_database.MockOpenShiftClusters, asyncOperations *mock_database.MockAsyncOperations) {
+				asyncOperations.EXPECT().
+					Get(gomock.Any(), mockOpID).
+					Return(nil, errors.New("random error"))
+			},
+			wantStatusCode: http.StatusInternalServerError,
+			wantError:      `500: InternalServerError: : Internal server error.`,
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			defer cli.CloseIdleConnections()
+
+			l := listener.NewListener()
+			defer l.Close()
+
+			env := env.NewTest(l, clientcerts[0].Raw)
+			env.TLSKey, env.TLSCerts = serverkey, servercerts
+
+			cli.Transport.(*http.Transport).Dial = l.Dial
+
+			controller := gomock.NewController(t)
+			defer controller.Finish()
+
+			asyncOperations := mock_database.NewMockAsyncOperations(controller)
+			openshiftClusters := mock_database.NewMockOpenShiftClusters(controller)
+
+			tt.mocks(openshiftClusters, asyncOperations)
+
+			f, err := NewFrontend(ctx, logrus.NewEntry(logrus.StandardLogger()), env, &database.Database{
+				AsyncOperations:   asyncOperations,
+				OpenShiftClusters: openshiftClusters,
+			}, api.APIs, &noop.Noop{})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			go f.Run(ctx, nil, nil)
+
+			referer := fmt.Sprintf("/subscriptions/%s/providers/microsoft.redhatopenshift/locations/%s/operationresults/%s", mockSubID, env.Location(), mockOpID)
+
+			req, err := http.NewRequest(http.MethodGet, fmt.Sprintf(
+				"https://server/subscriptions/%s/providers/Microsoft.RedHatOpenShift/locations/%s/operationresults/%s?api-version=2019-12-31-preview",
+				mockSubID,
+				env.Location(),
+				mockOpID,
+			), nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+			req.Header = http.Header{
+				"Content-Type": []string{"application/json"},
+				"Referer":      []string{referer},
+			}
+			resp, err := cli.Do(req)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != tt.wantStatusCode {
+				t.Error(resp.StatusCode)
+			}
+
+			location := resp.Header.Get("Location")
+			if tt.wantAsync {
+				if location != referer {
+					t.Error(location)
+				}
+			} else {
+				if location != "" {
+					t.Error(location)
+				}
+			}
+
+			if tt.wantError == "" {
+				if tt.wantResponse != nil {
+					var oc *v20191231preview.OpenShiftCluster
+					err = json.NewDecoder(resp.Body).Decode(&oc)
+					if err != nil {
+						t.Fatal(err)
+					}
+
+					if !reflect.DeepEqual(oc, tt.wantResponse()) {
+						b, _ := json.Marshal(oc)
+						t.Error(string(b))
+					}
+				}
+			} else {
+				cloudErr := &api.CloudError{StatusCode: resp.StatusCode}
+				err = json.NewDecoder(resp.Body).Decode(&cloudErr)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if cloudErr.Error() != tt.wantError {
+					t.Error(cloudErr)
+				}
+			}
+		})
+	}
+}
---
+++ pkg/frontend/asyncoperationsstatus_get_test.go
@@ -0,0 +1,283 @@
+package frontend
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/sirupsen/logrus"
+
+	"github.com/Azure/ARO-RP/pkg/api"
+	"github.com/Azure/ARO-RP/pkg/database"
+	"github.com/Azure/ARO-RP/pkg/database/cosmosdb"
+	"github.com/Azure/ARO-RP/pkg/env"
+	"github.com/Azure/ARO-RP/pkg/metrics/noop"
+	mock_database "github.com/Azure/ARO-RP/pkg/util/mocks/database"
+	utiltls "github.com/Azure/ARO-RP/pkg/util/tls"
+	"github.com/Azure/ARO-RP/test/util/listener"
+)
+
+func TestGetAsyncOperationsStatus(t *testing.T) {
+	ctx := context.Background()
+
+	clientkey, clientcerts, err := utiltls.GenerateKeyAndCertificate("client", nil, nil, false, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	serverkey, servercerts, err := utiltls.GenerateKeyAndCertificate("server", nil, nil, false, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pool := x509.NewCertPool()
+	pool.AddCert(servercerts[0])
+
+	cli := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs: pool,
+				Certificates: []tls.Certificate{
+					{
+						Certificate: [][]byte{clientcerts[0].Raw},
+						PrivateKey:  clientkey,
+					},
+				},
+			},
+		},
+	}
+
+	mockSubID := "00000000-0000-0000-0000-000000000000"
+	mockClusterDocKey := "22222222-2222-2222-2222-222222222222"
+	mockOpID := "11111111-1111-1111-1111-111111111111"
+	mockOpStartTime := time.Now().Add(-time.Hour).UTC()
+	mockOpEndTime := time.Now().Add(-time.Minute).UTC()
+
+	type test struct {
+		name           string
+		mocks          func(*mock_database.MockOpenShiftClusters, *mock_database.MockAsyncOperations)
+		wantStatusCode int
+		wantResponse   func() *api.AsyncOperation
+		wantError      string
+	}
+
+	for _, tt := range []*test{
+		{
+			name: "operation and cluster exist in db - final result is available",
+			mocks: func(openshiftClusters *mock_database.MockOpenShiftClusters, asyncOperations *mock_database.MockAsyncOperations) {
+				asyncOperations.EXPECT().
+					Get(gomock.Any(), mockOpID).
+					Return(&api.AsyncOperationDocument{
+						ID:                  mockOpID,
+						OpenShiftClusterKey: mockClusterDocKey,
+						AsyncOperation: &api.AsyncOperation{
+							ID:                       "fakeOpPath",
+							Name:                     mockOpID,
+							InitialProvisioningState: api.ProvisioningStateUpdating,
+							ProvisioningState:        api.ProvisioningStateFailed,
+							StartTime:                mockOpStartTime,
+							EndTime:                  &mockOpEndTime,
+							Error: &api.CloudErrorBody{
+								Code:    api.CloudErrorCodeInternalServerError,
+								Message: "Some error.",
+							},
+						},
+					}, nil)
+
+				openshiftClusters.EXPECT().Get(gomock.Any(), mockClusterDocKey).
+					Return(&api.OpenShiftClusterDocument{}, nil)
+			},
+			wantStatusCode: http.StatusOK,
+			wantResponse: func() *api.AsyncOperation {
+				return &api.AsyncOperation{
+					ID:                "fakeOpPath",
+					Name:              mockOpID,
+					ProvisioningState: api.ProvisioningStateFailed,
+					StartTime:         mockOpStartTime,
+					EndTime:           &mockOpEndTime,
+					Error: &api.CloudErrorBody{
+						Code:    api.CloudErrorCodeInternalServerError,
+						Message: "Some error.",
+					},
+				}
+			},
+		},
+		{
+			name: "operation and cluster exist in db - final result is not yet available",
+			mocks: func(openshiftClusters *mock_database.MockOpenShiftClusters, asyncOperations *mock_database.MockAsyncOperations) {
+				asyncOperations.EXPECT().
+					Get(gomock.Any(), mockOpID).
+					Return(&api.AsyncOperationDocument{
+						ID:                  mockOpID,
+						OpenShiftClusterKey: mockClusterDocKey,
+						AsyncOperation: &api.AsyncOperation{
+							ID:                       "fakeOpPath",
+							Name:                     mockOpID,
+							InitialProvisioningState: api.ProvisioningStateUpdating,
+							ProvisioningState:        api.ProvisioningStateFailed,
+							StartTime:                mockOpStartTime,
+							EndTime:                  &mockOpEndTime,
+							Error: &api.CloudErrorBody{
+								Code:    api.CloudErrorCodeInternalServerError,
+								Message: "Some error.",
+							},
+						},
+					}, nil)
+
+				openshiftClusters.EXPECT().Get(gomock.Any(), mockClusterDocKey).
+					Return(&api.OpenShiftClusterDocument{
+						AsyncOperationID: mockOpID,
+					}, nil)
+			},
+			wantStatusCode: http.StatusOK,
+			wantResponse: func() *api.AsyncOperation {
+				return &api.AsyncOperation{
+					ID:                "fakeOpPath",
+					Name:              mockOpID,
+					ProvisioningState: api.ProvisioningStateUpdating,
+					StartTime:         mockOpStartTime,
+				}
+			},
+		},
+		{
+			name: "operation not found in db",
+			mocks: func(openshiftClusters *mock_database.MockOpenShiftClusters, asyncOperations *mock_database.MockAsyncOperations) {
+				asyncOperations.EXPECT().
+					Get(gomock.Any(), mockOpID).
+					Return(nil, &cosmosdb.Error{StatusCode: http.StatusNotFound})
+			},
+			wantStatusCode: http.StatusNotFound,
+			wantError:      `404: NotFound: : The entity was not found.`,
+		},
+		{
+			name: "operation exists in db, but no cluster",
+			mocks: func(openshiftClusters *mock_database.MockOpenShiftClusters, asyncOperations *mock_database.MockAsyncOperations) {
+				asyncOperations.EXPECT().
+					Get(gomock.Any(), mockOpID).
+					Return(&api.AsyncOperationDocument{
+						ID:                  mockOpID,
+						OpenShiftClusterKey: mockClusterDocKey,
+						AsyncOperation: &api.AsyncOperation{
+							ID:                       "fakeOpPath",
+							Name:                     mockOpID,
+							InitialProvisioningState: api.ProvisioningStateCreating,
+							ProvisioningState:        api.ProvisioningStateFailed,
+							StartTime:                mockOpStartTime,
+							EndTime:                  &mockOpEndTime,
+							Error: &api.CloudErrorBody{
+								Code:    api.CloudErrorCodeInternalServerError,
+								Message: "Some error.",
+							},
+						},
+					}, nil)
+
+				openshiftClusters.EXPECT().Get(gomock.Any(), mockClusterDocKey).
+					Return(nil, &cosmosdb.Error{StatusCode: http.StatusNotFound})
+			},
+			wantStatusCode: http.StatusOK,
+			wantResponse: func() *api.AsyncOperation {
+				return &api.AsyncOperation{
+					ID:                "fakeOpPath",
+					Name:              mockOpID,
+					ProvisioningState: api.ProvisioningStateFailed,
+					StartTime:         mockOpStartTime,
+					EndTime:           &mockOpEndTime,
+					Error: &api.CloudErrorBody{
+						Code:    api.CloudErrorCodeInternalServerError,
+						Message: "Some error.",
+					},
+				}
+			},
+		},
+		{
+			name: "internal error",
+			mocks: func(openshiftClusters *mock_database.MockOpenShiftClusters, asyncOperations *mock_database.MockAsyncOperations) {
+				asyncOperations.EXPECT().
+					Get(gomock.Any(), mockOpID).
+					Return(nil, errors.New("random error"))
+			},
+			wantStatusCode: http.StatusInternalServerError,
+			wantError:      `500: InternalServerError: : Internal server error.`,
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			defer cli.CloseIdleConnections()
+
+			l := listener.NewListener()
+			defer l.Close()
+
+			env := env.NewTest(l, clientcerts[0].Raw)
+			env.TLSKey, env.TLSCerts = serverkey, servercerts
+
+			cli.Transport.(*http.Transport).Dial = l.Dial
+
+			controller := gomock.NewController(t)
+			defer controller.Finish()
+
+			asyncOperations := mock_database.NewMockAsyncOperations(controller)
+			openshiftClusters := mock_database.NewMockOpenShiftClusters(controller)
+
+			tt.mocks(openshiftClusters, asyncOperations)
+
+			f, err := NewFrontend(ctx, logrus.NewEntry(logrus.StandardLogger()), env, &database.Database{
+				AsyncOperations:   asyncOperations,
+				OpenShiftClusters: openshiftClusters,
+			}, api.APIs, &noop.Noop{})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			go f.Run(ctx, nil, nil)
+
+			resp, err := cli.Get(fmt.Sprintf(
+				"https://server/subscriptions/%s/providers/Microsoft.RedHatOpenShift/locations/%s/operationsstatus/%s?api-version=2019-12-31-preview",
+				mockSubID,
+				env.Location(),
+				mockOpID,
+			))
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != tt.wantStatusCode {
+				t.Error(resp.StatusCode)
+			}
+
+			if tt.wantError == "" {
+				var op *api.AsyncOperation
+				err = json.NewDecoder(resp.Body).Decode(&op)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if !reflect.DeepEqual(op, tt.wantResponse()) {
+					b, _ := json.Marshal(op)
+					t.Error(string(b))
+				}
+
+			} else {
+				cloudErr := &api.CloudError{StatusCode: resp.StatusCode}
+				err = json.NewDecoder(resp.Body).Decode(&cloudErr)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if cloudErr.Error() != tt.wantError {
+					t.Error(cloudErr)
+				}
+			}
+		})
+	}
+}
