4e333310f7ad2127af29b81cfb6ea98a1bf2df15
Jim Minter <jminter@redhat.com>
2020-03-12 18:03:59 -0600

    bump cluster creation to 4.3.5

--- pkg/util/version/version.go
+++ pkg/util/version/version.go
@@ -4,6 +4,6 @@ package version
 // Licensed under the Apache License 2.0.
 
 const (
-	OpenShiftVersion  = "4.3.3"
-	OpenShiftPullSpec = "arosvc.azurecr.io/openshift-release-dev/ocp-release@sha256:9b8708b67dd9b7720cb7ab3ed6d12c394f689cc8927df0e727c76809ab383f44"
+	OpenShiftVersion  = "4.3.5"
+	OpenShiftPullSpec = "arosvc.azurecr.io/openshift-release-dev/ocp-release@sha256:64320fbf95d968fc6b9863581a92d373bc75f563a13ae1c727af37450579f61a"
 )
