5e229c422f4615f2be6042ec4d7f20b80aacd204
Sam Kingston <skingsto@redhat.com>
2020-03-20 13:22:45 +1000

    Emit the cluster version

--- pkg/monitor/cluster/cluster.go
+++ pkg/monitor/cluster/cluster.go
@@ -67,6 +67,10 @@ func NewMonitor(env env.Interface, log *logrus.Entry, oc *api.OpenShiftCluster, 
 func (mon *Monitor) Monitor(ctx context.Context) error {
 	mon.log.Debug("monitoring")
 
+	mon.emitGauge("cluster.version", 1, map[string]string{
+		"version": mon.oc.Properties.ClusterProfile.Version,
+	})
+
 	// If API is not returning 200, don't need to run the next checks
 	statusCode, err := mon.emitAPIServerHealthzCode(ctx)
 	if err != nil || statusCode != http.StatusOK {
