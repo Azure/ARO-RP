87a29e9e0042728d927a947625fc4e026857dd41
Jim Minter <jminter@redhat.com>
2020-04-01 16:35:21 -0600

    use apiVersion 2018-01-01-preview for roledefinition API calls

--- pkg/deploy/generator/resources.go
+++ pkg/deploy/generator/resources.go
@@ -23,14 +23,15 @@ import (
 )
 
 var apiVersions = map[string]string{
-	"authorization":     "2018-09-01-preview",
-	"compute":           "2019-03-01",
-	"containerregistry": "2019-05-01",
-	"dns":               "2018-05-01",
-	"documentdb":        "2019-08-01",
-	"keyvault":          "2016-10-01",
-	"msi":               "2018-11-30",
-	"network":           "2019-07-01",
+	"authorization":                "2018-09-01-preview",
+	"authorization-roledefinition": "2018-01-01-preview",
+	"compute":                      "2019-03-01",
+	"containerregistry":            "2019-05-01",
+	"dns":                          "2018-05-01",
+	"documentdb":                   "2019-08-01",
+	"keyvault":                     "2016-10-01",
+	"msi":                          "2018-11-30",
+	"network":                      "2019-07-01",
 }
 
 const (
@@ -1334,7 +1335,7 @@ func (g *generator) roleDefinitionTokenContributor() *arm.Resource {
 				},
 			},
 		},
-		APIVersion: apiVersions["authorization"],
+		APIVersion: apiVersions["authorization-roledefinition"],
 	}
 }
 
