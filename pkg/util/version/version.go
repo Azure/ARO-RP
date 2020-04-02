2c6bc24f5d7353063f2449095b4878eaaaa2822b
Jim Minter <jminter@redhat.com>
2020-04-02 08:40:27 -0600

    bump to 4.3.9

--- pkg/util/version/version.go
+++ pkg/util/version/version.go
@@ -8,8 +8,8 @@ import (
 )
 
 const (
-	OpenShiftVersion        = "4.3.8"
-	OpenShiftPullSpecFormat = "%s.azurecr.io/openshift-release-dev/ocp-release@sha256:a414f6308db72f88e9d2e95018f0cc4db71c6b12b2ec0f44587488f0a16efc42"
+	OpenShiftVersion        = "4.3.9"
+	OpenShiftPullSpecFormat = "%s.azurecr.io/openshift-release-dev/ocp-release@sha256:f0fada3c8216dc17affdd3375ff845b838ef9f3d67787d3d42a88dcd0f328eea"
 )
 
 func OpenShiftPullSpec(acrName string) string {
