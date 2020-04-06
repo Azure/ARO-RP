cdb4da4efb3b3d1c3463e37177947284fcaef79e
Jim Minter <jminter@redhat.com>
2020-04-06 17:45:44 -0500

    standardise cosmos-db API version

--- pkg/util/azureclient/mgmt/documentdb/databaseaccounts.go
+++ pkg/util/azureclient/mgmt/documentdb/databaseaccounts.go
@@ -6,7 +6,7 @@ package documentdb
 import (
 	"context"
 
-	mgmtdocumentdb "github.com/Azure/azure-sdk-for-go/services/cosmos-db/mgmt/2015-04-08/documentdb"
+	mgmtdocumentdb "github.com/Azure/azure-sdk-for-go/services/cosmos-db/mgmt/2019-08-01/documentdb"
 	"github.com/Azure/go-autorest/autorest"
 )
 
