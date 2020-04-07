8d9311a34406c0c74105e69b138c8fe83de07624
Jim Minter <jminter@redhat.com>
2020-04-06 21:38:16 -0500

    another attempt at rewording

--- python/az/aro/azext_aro/_validators.py
+++ python/az/aro/azext_aro/_validators.py
@@ -81,10 +81,11 @@ def _validate_int(key, i):
 
 def validate_pull_secret(namespace):
     if namespace.pull_secret is None:
-        logger.warning("No --pull-secret provided: cluster will not include " +
-                       "access to Red Hat container ecosystem, " +
-                       "certified samples, or operators.\n" +
-                       "For more information: https://catalog.redhat.com/\n")
+        # TODO: add aka.ms link here
+        warning = "No --pull-secret provided: cluster will not include samples or operators from " + \
+            "Red Hat or from certified partners."
+
+        logger.warning(warning)
 
     else:
         try:
