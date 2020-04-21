cce1dd4992d4af500761294eb1b0c9c778a6eeb6
Mangirdas Judeikis <Mangirdas@Judeikis.LT>
2020-04-21 13:28:52 +0100

    change metrics checking

--- pkg/monitor/cluster/clusteroperatorsversions.go
+++ pkg/monitor/cluster/clusteroperatorsversions.go
@@ -8,14 +8,22 @@ import (
 	"sort"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"github.com/Azure/ARO-RP/pkg/util/version"
 )
 
 func (mon *Monitor) emitClusterOperatorsVersions(ctx context.Context) error {
 	cos, err := mon.configcli.ConfigV1().ClusterOperators().List(metav1.ListOptions{})
 	if err != nil {
 		return err
+	}
+	cv, err := mon.configcli.ConfigV1().ClusterVersions().Get("version", metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	desiredVersion := cv.Status.Desired.Version
+	if cv.Spec.DesiredUpdate != nil &&
+		cv.Spec.DesiredUpdate.Version != "" {
+		desiredVersion = cv.Spec.DesiredUpdate.Version
 	}
 
 	sort.Slice(cos.Items, func(i, j int) bool { return cos.Items[i].Name < cos.Items[j].Name })
@@ -25,7 +33,7 @@ func (mon *Monitor) emitClusterOperatorsVersions(ctx context.Context) error {
 				continue
 			}
 
-			if v.Version == version.OpenShiftVersion {
+			if v.Version == desiredVersion {
 				continue
 			}
 
--- pkg/monitor/cluster/clusteroperatorversion_test.go
+++ pkg/monitor/cluster/clusteroperatorversion_test.go
@@ -13,29 +13,39 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	mock_metrics "github.com/Azure/ARO-RP/pkg/util/mocks/metrics"
-	"github.com/Azure/ARO-RP/pkg/util/version"
 )
 
 func TestEmitClusterOperatorsVersion(t *testing.T) {
 	ctx := context.Background()
 
-	configcli := fake.NewSimpleClientset(&configv1.ClusterOperator{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "console",
-		},
-		Status: configv1.ClusterOperatorStatus{
-			Versions: []configv1.OperandVersion{
-				{
-					Name:    "operator",
-					Version: "4.3.0",
-				},
-				{
-					Name:    "operator-good", // no metrics exected
-					Version: version.OpenShiftVersion,
+	configcli := fake.NewSimpleClientset(
+		&configv1.ClusterOperator{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "console",
+			},
+			Status: configv1.ClusterOperatorStatus{
+				Versions: []configv1.OperandVersion{
+					{
+						Name:    "operator",
+						Version: "4.3.0",
+					},
+					{
+						Name:    "operator-good", // no metrics exected
+						Version: "4.3.1",
+					},
 				},
 			},
 		},
-	})
+		&configv1.ClusterVersion{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "version",
+			},
+			Status: configv1.ClusterVersionStatus{
+				Desired: configv1.Update{
+					Version: "4.3.1",
+				},
+			},
+		})
 
 	controller := gomock.NewController(t)
 	defer controller.Finish()
