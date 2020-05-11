12a81f61cae04632cc3086f482c36e8433e42496
Jim Minter <jminter@redhat.com>
2020-05-11 08:50:44 -0500

    make random characters match k8s.io/apimachinery/pkg/util/rand.alphanums

--- python/az/aro/azext_aro/custom.py
+++ python/az/aro/azext_aro/custom.py
@@ -55,8 +55,8 @@ def aro_create(cmd,  # pylint: disable=too-many-locals
 
     subscription_id = get_subscription_id(cmd.cli_ctx)
 
-    random_id = ''.join(random.choice(
-        'bcdfghjklmnpqrstvwxyz0123456789') for _ in range(8))
+    random_id = ''.join(random.choice('bcdfghjklmnpqrstvwxz2456789')
+                        for _ in range(8))
 
     aad = AADManager(cmd.cli_ctx)
     if client_id is None:
