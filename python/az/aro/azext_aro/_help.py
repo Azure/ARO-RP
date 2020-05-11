f80774481d8569b3e0454ab3f3f7c24614ff6928
Troy Connor <troy0820@gmail.com>
2020-05-11 14:49:33 -0400

    [python]Add examples to help
    Add function to validate int for vm count and size
    
    Sychronize with upstream azure/azure-cli

--- python/az/aro/azext_aro/_help.py
+++ python/az/aro/azext_aro/_help.py
@@ -12,31 +12,53 @@ helps['aro'] = """
 helps['aro create'] = """
     type: command
     short-summary: Create a cluster.
+    examples:
+      - name: Create an OpenShift cluster.
+        text: az aro create --resource-group MyResourceGroup --name MyCluster  --vnet MyVnet --master-subnet MyMasterSubnet --worker-subnet MyWorkerSubnet
+      - name: Create an OpenShift cluster with 5 compute nodes and Red Hat pull secret.
+        text: az aro create --resource-group MyResourceGroup --name MyCluster --vnet MyVnet --master-subnet MyMasterSubnet --worker-subnet MyWorkerSubnet --worker-count 5 --pull-secret @pullsecret.txt
 """
 
 helps['aro list'] = """
     type: command
     short-summary: List clusters.
+    examples:
+      - name: List OpenShift clusters.
+        text: az aro list
+      - name: List OpenShift clusters with table view.
+        text: az aro list -o table
 """
 
 helps['aro delete'] = """
     type: command
     short-summary: Delete a cluster.
+    examples:
+      - name: Delete an OpenShift cluster.
+        text: az aro delete --name MyCluster --resource-group MyResourceGroup
 """
 
 helps['aro show'] = """
     type: command
     short-summary: Get the details of a cluster.
+    examples:
+      - name: Get the details of a Managed OpenShift cluster.
+        text: az aro show --name MyCluster --resource-group MyResourceGroup
 """
 
 helps['aro update'] = """
     type: command
     short-summary: Update a cluster.
+    examples:
+      - name: Update an existing cluster.
+        text: az aro update --name MyCluster --resource-group MyResourceGroup
 """
 
 helps['aro list-credentials'] = """
     type: command
     short-summary: List credentials of a cluster.
+    examples:
+      - name: List credentials of a cluster.
+        text: az aro list-credentials --name MyCluster --resource-group MyResourceGroup
 """
 
 helps['aro wait'] = """
