6628b91e4f61d76ab2f0bd7b9774aca8057e8a81
Angus Salkeld <asalkeld@redhat.com>
2020-05-19 23:31:10 +1000

    Fix internetChecker

--- pkg/controllers/internetchecker_controller.go
+++ pkg/controllers/internetchecker_controller.go
@@ -5,9 +5,11 @@ package controllers
 
 import (
 	"context"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/sirupsen/logrus"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -23,32 +25,46 @@ type InternetChecker struct {
 	AROCli        aroclient.AroV1alpha1Interface
 	Log           *logrus.Entry
 	Scheme        *runtime.Scheme
+	testurl       string
 }
 
 // +kubebuilder:rbac:groups=aro.openshift.io,resources=clusters,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=aro.openshift.io,resources=clusters/status,verbs=get;update;patch
 
+// TODO https://github.com/Azure/OpenShift/issues/185
 func (r *InternetChecker) Reconcile(request ctrl.Request) (ctrl.Result, error) {
 	if request.Name != aro.SingletonClusterName {
 		return reconcile.Result{}, nil
 	}
+	ctx := context.TODO()
+	if r.testurl == "" {
+		instance, err := r.AROCli.Clusters().Get(request.Name, v1.GetOptions{})
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+		if instance.Spec.ResourceID == "" {
+			return ReconcileResultRequeue, nil
+		}
+		r.testurl = "https://management.azure.com" + instance.Spec.ResourceID + "?api-version=2020-04-30"
+	}
 	r.Log.Info("Polling outgoing internet connection")
 
-	// TODO https://github.com/Azure/OpenShift/issues/185
-
-	req, err := http.NewRequest("GET", "https://management.azure.com", nil)
+	req, err := http.NewRequest("GET", r.testurl, nil)
 	if err != nil {
 		r.Log.Error(err, "failed building request")
 		return reconcile.Result{}, err
 	}
 	req.Header.Set("Content-Type", "application/json")
-
-	ctx := context.TODO()
-	sr := NewStatusReporter(r.Log, r.AROCli, request.Name)
 	client := &http.Client{}
 	resp, err := client.Do(req)
-	r.Log.Infof("response code %v, err %v", resp.Status, err)
-	if err != nil || resp.StatusCode != http.StatusOK {
+
+	sr := NewStatusReporter(r.Log, r.AROCli, request.Name)
+	// this is not ideal, but we can at least see that the site is working
+	// if it is returning Unauthorized.
+	if err != nil || resp.StatusCode != http.StatusUnauthorized {
+		defer resp.Body.Close()
+		b, err := ioutil.ReadAll(resp.Body)
+		r.Log.Warn(string(b))
 		err = sr.SetNoInternetConnection(ctx, err)
 	} else {
 		err = sr.SetInternetConnected(ctx)
