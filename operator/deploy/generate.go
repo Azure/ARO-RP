750ebadc465742d1ba5bbce42341f383bdcbb8e0
Angus Salkeld <asalkeld@redhat.com>
2020-05-25 13:25:18 +1000

    Move the top level generate to operator/

--- operator/deploy/generate.go
+++ operator/deploy/generate.go
@@ -1,5 +1,8 @@
 package deploy
 
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
 // build the operator's rbac based on in-code tags (search for "+kubebuilder:rbac")
 //go:generate go run ../../vendor/sigs.k8s.io/controller-tools/cmd/controller-gen "crd:trivialVersions=true" rbac:roleName=manager-role paths="../../pkg/controllers/..." paths="../apis/..."  output:dir=staticresources
 //go:generate go get github.com/go-bindata/go-bindata/go-bindata
