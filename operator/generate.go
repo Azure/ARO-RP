e57c51d1340d3bbc3d2df45179543494be4a83b5
Olga Mirensky <olga.mirensky@redhat.com>
2020-06-03 10:36:15 +1000

    Fix output location for clientset generated files

--- operator/generate.go
+++ operator/generate.go
@@ -3,8 +3,7 @@ package operator
 // Copyright (c) Microsoft Corporation.
 // Licensed under the Apache License 2.0.
 
-//go:generate go run ../vendor/k8s.io/code-generator/cmd/client-gen --clientset-name versioned --input-base github.com/Azure/ARO-RP/operator --input apis/aro.openshift.io/v1alpha1 --output-package github.com/Azure/ARO-RP/pkg/util/aro-operator-client/clientset --go-header-file ../hack/licenses/boilerplate.go.txt
-//go:generate go run ../vendor/k8s.io/code-generator/cmd/client-gen --clientset-name versioned --input-base github.com/Azure/ARO-RP/operator --input apis/aro.openshift.io/v1alpha1 --output-package github.com/Azure/ARO-RP/pkg/util/aro-operator-client/clientset --go-header-file ../hack/licenses/boilerplate.go.txt
+//go:generate go run ../vendor/k8s.io/code-generator/cmd/client-gen --clientset-name versioned --input-base github.com/Azure/ARO-RP --input operator/apis/aro.openshift.io/v1alpha1 --output-package github.com/Azure/ARO-RP/pkg/util/aro-operator-client/clientset --go-header-file ../hack/licenses/boilerplate.go.txt
 //go:generate gofmt -s -w ../pkg/util/aro-operator-client
 //go:generate go run ../vendor/golang.org/x/tools/cmd/goimports -w -local=github.com/Azure/ARO-RP ../pkg/util/aro-operator-client
 
