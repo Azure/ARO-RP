dfa1d4c75cae080a5b0d8320feaece220ea000fe
Petr Kotas <pkotas@redhat.com>
2020-06-04 13:21:57 +0000

    Add listforscope for roleassignment client
    Signed-off-by: Petr Kotas <pkotas@redhat.com>

--- pkg/util/azureclient/mgmt/authorization/roleassignments_addons.go
+++ pkg/util/azureclient/mgmt/authorization/roleassignments_addons.go
@@ -12,10 +12,29 @@ import (
 // RoleAssignmentsClientAddon is a minimal interface for azure RoleAssignmentsClientAddon
 type RoleAssignmentsClientAddon interface {
 	List(ctx context.Context, filter string) (result []authorization.RoleAssignment, err error)
+	ListForScope(ctx context.Context, scope string, filter string) (result []authorization.RoleAssignment, err error)
 }
 
 func (ra *roleAssignmentsClient) List(ctx context.Context, filter string) (result []authorization.RoleAssignment, err error) {
 	page, err := ra.RoleAssignmentsClient.List(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+
+	for page.NotDone() {
+		result = append(result, page.Values()...)
+
+		err = page.Next()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return result, nil
+}
+
+func (ra *roleAssignmentsClient) ListForScope(ctx context.Context, scope string, filter string) (result []authorization.RoleAssignment, err error) {
+	page, err := ra.RoleAssignmentsClient.ListForScope(ctx, scope, filter)
 	if err != nil {
 		return nil, err
 	}
