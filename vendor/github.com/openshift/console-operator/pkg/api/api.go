954c104ad8f54f16291f1c1839dad536e7168cbf
Mangirdas Judeikis <Mangirdas@Judeikis.LT>
2020-06-21 17:22:27 +0100

    vendor github.com/openshift/console-operator

--- vendor/github.com/openshift/console-operator/pkg/api/api.go
+++ vendor/github.com/openshift/console-operator/pkg/api/api.go
@@ -15,6 +15,7 @@ const (
 	OpenShiftConsoleConfigMapName       = "console-config"
 	OpenShiftConsolePublicConfigMapName = "console-public"
 	ServiceCAConfigMapName              = "service-ca"
+	DefaultIngressCertConfigMapName     = "default-ingress-cert"
 	OpenShiftConsoleDeploymentName      = OpenShiftConsoleName
 	OpenShiftConsoleServiceName         = OpenShiftConsoleName
 	OpenShiftConsoleRouteName           = OpenShiftConsoleName
@@ -22,6 +23,7 @@ const (
 	OAuthClientName                     = OpenShiftConsoleName
 	OpenShiftConfigManagedNamespace     = "openshift-config-managed"
 	OpenShiftConfigNamespace            = "openshift-config"
+	OpenShiftMonitoringConfigMapName    = "monitoring-shared-config"
 	OpenShiftCustomLogoConfigMapName    = "custom-logo"
 	TrustedCAConfigMapName              = "trusted-ca-bundle"
 	TrustedCABundleKey                  = "ca-bundle.crt"
