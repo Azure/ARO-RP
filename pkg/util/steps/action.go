adaa2ed06a97207d25a66a1f7b423f971707a9e9
Amber Brown <ambrown@redhat.com>
2020-07-14 14:06:25 +1000

    util/steps: a set of steps, and a runner to execute them in series.

---
+++ pkg/util/steps/action.go
@@ -0,0 +1,33 @@
+package steps
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+)
+
+// actionFunction is a function that takes a context and returns an error.
+//
+// Suitable for performing tasks.
+type actionFunction func(context.Context) error
+
+// Action returns a Step which will execute the action function `f`. Errors from
+// `f` are returned directly.
+func Action(f actionFunction) actionStep {
+	return actionStep{f}
+}
+
+type actionStep struct {
+	f actionFunction
+}
+
+func (s actionStep) run(ctx context.Context, log *logrus.Entry) error {
+	return s.f(ctx)
+}
+func (s actionStep) String() string {
+	return fmt.Sprintf("[Action %s]", friendlyName(s.f))
+}
