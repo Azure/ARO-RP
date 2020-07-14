dfce0726d94e4e7496b969c4227845346aa18ff5
Amber Brown <ambrown@redhat.com>
2020-07-14 14:08:49 +1000

    add a provided context to runsteps conditions in install

--- pkg/install/condition.go
+++ pkg/install/condition.go
@@ -4,6 +4,8 @@ package install
 // Licensed under the Apache License 2.0.
 
 import (
+	"context"
+
 	configv1 "github.com/openshift/api/config/v1"
 	consoleapi "github.com/openshift/console-operator/pkg/api"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -12,12 +14,12 @@ import (
 // condition functions should return an error only if it's not retryable
 // if a condition function encounters a retryable error it should return false, nil.
 
-func (i *Installer) bootstrapConfigMapReady() (bool, error) {
+func (i *Installer) bootstrapConfigMapReady(ctx context.Context) (bool, error) {
 	cm, err := i.kubernetescli.CoreV1().ConfigMaps("kube-system").Get("bootstrap", metav1.GetOptions{})
 	return err == nil && cm.Data["status"] == "complete", nil
 }
 
-func (i *Installer) apiServersReady() (bool, error) {
+func (i *Installer) apiServersReady(ctx context.Context) (bool, error) {
 	apiserver, err := i.configcli.ConfigV1().ClusterOperators().Get("kube-apiserver", metav1.GetOptions{})
 	if err != nil {
 		return false, nil
@@ -25,12 +27,12 @@ func (i *Installer) apiServersReady() (bool, error) {
 	return isOperatorAvailable(apiserver), nil
 }
 
-func (i *Installer) operatorConsoleExists() (bool, error) {
+func (i *Installer) operatorConsoleExists(ctx context.Context) (bool, error) {
 	_, err := i.operatorcli.OperatorV1().Consoles().Get(consoleapi.ConfigResourceName, metav1.GetOptions{})
 	return err == nil, nil
 }
 
-func (i *Installer) operatorConsoleReady() (bool, error) {
+func (i *Installer) operatorConsoleReady(ctx context.Context) (bool, error) {
 	consoleOperator, err := i.configcli.ConfigV1().ClusterOperators().Get("console", metav1.GetOptions{})
 	if err != nil {
 		return false, nil
@@ -38,7 +40,7 @@ func (i *Installer) operatorConsoleReady() (bool, error) {
 	return isOperatorAvailable(consoleOperator), nil
 }
 
-func (i *Installer) clusterVersionReady() (bool, error) {
+func (i *Installer) clusterVersionReady(ctx context.Context) (bool, error) {
 	cv, err := i.configcli.ConfigV1().ClusterVersions().Get("version", metav1.GetOptions{})
 	if err == nil {
 		for _, cond := range cv.Status.Conditions {
@@ -50,7 +52,7 @@ func (i *Installer) clusterVersionReady() (bool, error) {
 	return false, nil
 }
 
-func (i *Installer) ingressControllerReady() (bool, error) {
+func (i *Installer) ingressControllerReady(ctx context.Context) (bool, error) {
 	ingressOperator, err := i.configcli.ConfigV1().ClusterOperators().Get("ingress", metav1.GetOptions{})
 	if err != nil {
 		return false, nil
