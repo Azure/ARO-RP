31b6aae51f5553a238c8b4ffd003254ef28882ef
Nils Elde <nelde@redhat.com>
2020-07-21 15:51:36 -0400

    Add missing new parameter so that CI passes

--- pkg/frontend/openshiftcluster_putorpatch_test.go
+++ pkg/frontend/openshiftcluster_putorpatch_test.go
@@ -282,7 +282,7 @@ func TestPutOrPatchOpenShiftClusterAdminAPI(t *testing.T) {
 				AsyncOperations:   asyncOperations,
 				OpenShiftClusters: openShiftClusters,
 				Subscriptions:     subscriptions,
-			}, apis, &noop.Noop{}, nil, nil, nil, nil)
+			}, apis, &noop.Noop{}, nil, nil, nil, nil, nil)
 			if err != nil {
 				t.Fatal(err)
 			}
