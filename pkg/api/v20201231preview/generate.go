d7ea2fa7809393711a80609c3387d0ab6e66302c
Petr Kotas <pkotas@redhat.com>
2020-07-21 14:43:31 +0200

    Add new preview API

---
+++ pkg/api/v20201231preview/generate.go
@@ -0,0 +1,6 @@
+//go:generate go run ../../../hack/swagger ../../../swagger/redhatopenshift/resource-manager/Microsoft.RedHatOpenShift/stable/2020-12-31-preview
+
+package v20201231preview
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
