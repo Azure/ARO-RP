10a9a60015a8b98d040fabf20093a686ab8b7295
Amber Brown <ambrown@redhat.com>
2020-08-28 09:32:08 +1000

    initial fake updates
    Signed-off-by: Amber Brown (hawkowl) <ambrown@redhat.com>

--- pkg/util/gencosmosdb/cosmosdb/template_fake.go
+++ pkg/util/gencosmosdb/cosmosdb/template_fake.go
@@ -1,136 +1,244 @@
 package cosmosdb
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"net/http"
+	"strings"
+	"sync"
 
 	pkg "github.com/jim-minter/go-cosmosdb/pkg/gencosmosdb/cosmosdb/dummy"
+	"github.com/ugorji/go/codec"
 )
 
-// TODO: we should be doing lots of deep copying in this file - perhaps m should
-// be of type map[string][]byte and we should serialise?
-
 type fakeTemplateClient struct {
-	m map[string]*pkg.Template
+	docs       map[string]*string
+	jsonHandle *codec.JsonHandle
+	lock       sync.Locker
 }
 
-type fakeTemplateListIterator struct {
-	c       *fakeTemplateClient
-	options *Options
-}
-
-type fakeTemplateNotImplementedIterator struct {
-}
-
-// NewFakeTemplateClient returns a new fake template client
-func NewFakeTemplateClient() TemplateClient {
+func NewFakeTemplateClient(h *codec.JsonHandle) *fakeTemplateClient {
 	return &fakeTemplateClient{
-		m: map[string]*pkg.Template{},
+		docs:       make(map[string]*string),
+		jsonHandle: h,
+		lock:       &sync.Mutex{},
 	}
 }
 
-func (c *fakeTemplateClient) Create(ctx context.Context, partitionkey string, newtemplate *pkg.Template, options *Options) (*pkg.Template, error) {
+func (c *fakeTemplateClient) fromString(s *string) (*pkg.Template, error) {
+	res := &pkg.Template{}
+	d := codec.NewDecoder(bytes.NewBufferString(*s), c.jsonHandle)
+	err := d.Decode(&res)
+	return res, err
+}
+
+func (c *fakeTemplateClient) processPreTrigger(ctx context.Context, doc *pkg.Template, trigger string) error {
+	return ErrNotImplemented
+}
+
+func (c *fakeTemplateClient) Create(ctx context.Context, partitionkey string, doc *pkg.Template, options *Options) (*pkg.Template, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	_, ext := c.docs[doc.ID]
+	if ext {
+		return nil, &Error{
+			StatusCode: http.StatusPreconditionFailed,
+			Message:    "Entity with the specified id already exists in the system",
+		}
+	}
+
 	if options != nil {
-		return nil, ErrNotImplemented
+		for _, o := range options.PreTriggers {
+			err := c.processPreTrigger(ctx, doc, o)
+			if err != nil {
+				return nil, err
+			}
+		}
 	}
 
-	if _, ok := c.m[newtemplate.ID]; ok {
-		return nil, &Error{StatusCode: http.StatusConflict} // TODO: check this
+	buf := &bytes.Buffer{}
+	err := codec.NewEncoder(buf, c.jsonHandle).Encode(doc)
+	if err != nil {
+		return nil, err
 	}
 
-	c.m[newtemplate.ID] = newtemplate
-
-	return newtemplate, nil
+	out := buf.String()
+	c.docs[doc.ID] = &out
+	return c.fromString(&out)
 }
 
-func (c *fakeTemplateClient) List(options *Options) TemplateIterator {
-	return &fakeTemplateListIterator{c: c, options: options}
-}
+func (c *fakeTemplateClient) List(*Options) TemplateRawIterator {
+	c.lock.Lock()
+	defer c.lock.Unlock()
 
-func (c *fakeTemplateClient) ListAll(ctx context.Context, options *Options) (*pkg.Templates, error) {
-	if options != nil {
-		return nil, ErrNotImplemented
+	docs := make([]*string, 0, len(c.docs))
+	for _, d := range c.docs {
+		docs = append(docs, d)
 	}
 
+	return &fakeTemplateClientRawIterator{
+		docs:       docs,
+		jsonHandle: c.jsonHandle,
+	}
+}
+
+func (c *fakeTemplateClient) ListAll(context.Context, *Options) (*pkg.Templates, error) {
 	templates := &pkg.Templates{
-		Count:     len(c.m),
-		Templates: make([]*pkg.Template, 0, len(c.m)),
+		Count:     len(c.docs),
+		Templates: make([]*pkg.Template, 0, len(c.docs)),
 	}
 
-	for _, template := range c.m {
-		templates.Templates = append(templates.Templates, template)
+	for _, d := range c.docs {
+		dec, err := c.fromString(d)
+		if err != nil {
+			return nil, err
+		}
+		templates.Templates = append(templates.Templates, dec)
 	}
 
 	return templates, nil
 }
 
-func (c *fakeTemplateClient) Get(ctx context.Context, partitionkey, templateid string, options *Options) (*pkg.Template, error) {
-	if options != nil {
-		return nil, ErrNotImplemented
-	}
+func (c *fakeTemplateClient) Get(ctx context.Context, partitionkey string, documentId string, options *Options) (*pkg.Template, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
 
-	if _, ok := c.m[templateid]; !ok {
+	out, ext := c.docs[documentId]
+	if !ext {
 		return nil, &Error{StatusCode: http.StatusNotFound}
 	}
 
-	return c.m[templateid], nil
-}
-
-func (c *fakeTemplateClient) Replace(ctx context.Context, partitionkey string, newtemplate *pkg.Template, options *Options) (*pkg.Template, error) {
-	if options != nil {
-		return nil, ErrNotImplemented
+	dec, err := c.fromString(out)
+	if err != nil {
+		return nil, err
 	}
 
-	c.m[newtemplate.ID] = newtemplate
-
-	return newtemplate, nil
+	return dec, err
 }
+func (c *fakeTemplateClient) Replace(ctx context.Context, partitionkey string, doc *pkg.Template, options *Options) (*pkg.Template, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
 
-func (c *fakeTemplateClient) Delete(ctx context.Context, partitionkey string, template *pkg.Template, options *Options) error {
-	if options != nil {
-		return ErrNotImplemented
+	_, exists := c.docs[doc.ID]
+	if !exists {
+		return nil, &Error{StatusCode: http.StatusNotFound}
 	}
 
-	if _, ok := c.m[template.ID]; !ok {
+	if options != nil {
+		for _, o := range options.PreTriggers {
+			err := c.processPreTrigger(ctx, doc, o)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	buf := &bytes.Buffer{}
+	err := codec.NewEncoder(buf, c.jsonHandle).Encode(doc)
+	if err != nil {
+		return nil, err
+	}
+
+	out := buf.String()
+	c.docs[doc.ID] = &out
+	return c.fromString(&out)
+}
+
+func (c *fakeTemplateClient) Delete(ctx context.Context, partitionKey string, doc *pkg.Template, options *Options) error {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	_, ext := c.docs[doc.ID]
+	if !ext {
 		return &Error{StatusCode: http.StatusNotFound}
 	}
 
-	delete(c.m, template.ID)
-
+	delete(c.docs, doc.ID)
 	return nil
 }
 
-func (c *fakeTemplateClient) Query(partitionkey string, query *Query, options *Options) TemplateRawIterator {
+func (c *fakeTemplateClient) ChangeFeed(*Options) TemplateIterator {
 	return &fakeTemplateNotImplementedIterator{}
 }
 
-func (c *fakeTemplateClient) QueryAll(ctx context.Context, partitionkey string, query *Query, options *Options) (*pkg.Templates, error) {
-	return nil, ErrNotImplemented
+type fakeTemplateClientRawIterator struct {
+	docs         []*string
+	jsonHandle   *codec.JsonHandle
+	continuation int
 }
 
-func (c *fakeTemplateClient) ChangeFeed(options *Options) TemplateIterator {
-	return &fakeTemplateNotImplementedIterator{}
+func (i *fakeTemplateClientRawIterator) decode(inp []*string) ([]*pkg.Template, error) {
+	done := make([]*pkg.Template, 0)
+
+	for _, doc := range inp {
+		res := &pkg.Template{}
+		d := codec.NewDecoder(bytes.NewBufferString(*doc), i.jsonHandle)
+		err := d.Decode(&res)
+		if err != nil {
+			return nil, err
+		}
+		done = append(done, res)
+	}
+
+	return done, nil
 }
 
-func (i *fakeTemplateListIterator) Next(ctx context.Context, maxItemCount int) (*pkg.Templates, error) {
-	if i.options != nil {
-		return nil, ErrNotImplemented
-	}
-
-	templates := &pkg.Templates{
-		Count:     len(i.c.m),
-		Templates: make([]*pkg.Template, 0, len(i.c.m)),
-	}
-
-	for _, template := range i.c.m {
-		templates.Templates = append(templates.Templates, template)
-	}
-
-	return templates, nil
+func (i *fakeTemplateClientRawIterator) Next(ctx context.Context, maxItemCount int) (*pkg.Templates, error) {
+	docs := &pkg.Templates{}
+	err := i.NextRaw(ctx, maxItemCount, docs)
+	return docs, err
 }
 
-func (i *fakeTemplateListIterator) Continuation() string {
+func (i *fakeTemplateClientRawIterator) NextRaw(ctx context.Context, maxItemCount int, raw interface{}) error {
+	if i.continuation >= len(i.docs) {
+		return nil
+	}
+	var docs []*string
+
+	if maxItemCount == -1 {
+		docs = i.docs[i.continuation:]
+		i.continuation = len(i.docs)
+	} else {
+		docs = i.docs[i.continuation : i.continuation+maxItemCount]
+		i.continuation += maxItemCount
+	}
+
+	d, ok := raw.(*pkg.Templates)
+	if ok {
+		out, err := i.decode(docs)
+		if err != nil {
+			return err
+		}
+		d.Templates = out
+		d.Count = len(d.Templates)
+	} else {
+		var f strings.Builder
+
+		fmt.Fprintf(&f, `{"Count": %d, "Documents": [`, len(docs))
+		for n, doc := range docs {
+			fmt.Fprint(&f, *doc)
+			if n != len(docs) {
+				fmt.Fprint(&f, ",")
+			}
+		}
+		fmt.Fprint(&f, "]}")
+		d := codec.NewDecoder(bytes.NewBufferString(f.String()), i.jsonHandle)
+		err := d.Decode(&raw)
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (i *fakeTemplateClientRawIterator) Continuation() string {
 	return ""
+}
+
+type fakeTemplateNotImplementedIterator struct {
 }
 
 func (i *fakeTemplateNotImplementedIterator) Next(ctx context.Context, maxItemCount int) (*pkg.Templates, error) {
