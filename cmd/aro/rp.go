cccab8b7c6c665185f87507491249f5ad9479374
Jim Minter <jminter@redhat.com>
2020-09-03 13:08:10 -0500

    remove log and m from Database struct

--- cmd/aro/rp.go
+++ cmd/aro/rp.go
@@ -86,7 +86,7 @@ func rp(ctx context.Context, log *logrus.Entry) error {
 		return err
 	}
 
-	go db.EmitMetrics(ctx)
+	go db.EmitMetrics(ctx, log.WithField("component", "database"), m)
 
 	feCipher, err := encryption.NewXChaCha20Poly1305(ctx, _env, env.FrontendEncryptionSecretName)
 	if err != nil {
