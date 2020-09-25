0b50440469629c756e03fb623154b73688993c49
Jim Minter <jminter@redhat.com>
2020-09-25 15:34:39 -0500

    easy: remove billing E2E fields from pkg/env

--- pkg/env/prod.go
+++ pkg/env/prod.go
@@ -48,10 +48,6 @@ type prod struct {
 	clustersGenevaLoggingConfigVersion string
 	clustersGenevaLoggingEnvironment   string
 
-	e2eStorageAccountName   string
-	e2eStorageAccountRGName string
-	e2eStorageAccountSubID  string
-
 	log *logrus.Entry
 }
 
@@ -112,10 +108,6 @@ func newProd(ctx context.Context, log *logrus.Entry) (*prod, error) {
 
 	p.clustersGenevaLoggingPrivateKey = clustersGenevaLoggingPrivateKey
 	p.clustersGenevaLoggingCertificate = clustersGenevaLoggingCertificates[0]
-
-	p.e2eStorageAccountName = "arov4e2e"
-	p.e2eStorageAccountRGName = "global"
-	p.e2eStorageAccountSubID = "0923c7de-9fca-4d9e-baf3-131d0c5b2ea4"
 
 	if p.ACRResourceID() != "" { // TODO: ugh!
 		acrResource, err := azure.ParseResourceID(p.ACRResourceID())
@@ -258,16 +250,4 @@ func (p *prod) Zones(vmSize string) ([]string, error) {
 func (d *prod) CreateARMResourceGroupRoleAssignment(ctx context.Context, fpAuthorizer refreshable.Authorizer, resourceGroup string) error {
 	// ARM ResourceGroup role assignments are not required in production.
 	return nil
-}
-
-func (p *prod) E2EStorageAccountName() string {
-	return p.e2eStorageAccountName
-}
-
-func (p *prod) E2EStorageAccountRGName() string {
-	return p.e2eStorageAccountRGName
-}
-
-func (p *prod) E2EStorageAccountSubID() string {
-	return p.e2eStorageAccountSubID
 }
