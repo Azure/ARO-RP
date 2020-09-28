aa6531b8405b1122c0100b0efff8b98cdf1c4f58
Jim Minter <jminter@redhat.com>
2020-09-28 14:52:20 -0500

    give more time for internet e2e test

--- test/e2e/operator.go
+++ test/e2e/operator.go
@@ -101,13 +101,13 @@ var _ = Describe("ARO Operator - Internet checking", func() {
 		Expect(err).NotTo(HaveOccurred())
 
 		// confirm the conditions are correct
-		err = wait.PollImmediate(10*time.Second, time.Minute, func() (bool, error) {
+		err = wait.PollImmediate(10*time.Second, 2*time.Minute, func() (bool, error) {
 			co, err := clients.AROClusters.Clusters().Get("cluster", metav1.GetOptions{})
 			if err != nil {
 				return false, err
 			}
 
-			// log.Debugf("ClusterStatus.Conditions %s", co.Status.Conditions)
+			log.Debugf("ClusterStatus.Conditions %s", co.Status.Conditions)
 			return co.Status.Conditions.IsFalseFor(arov1alpha1.InternetReachableFromMaster) &&
 				co.Status.Conditions.IsFalseFor(arov1alpha1.InternetReachableFromWorker), nil
 		})
