56e74d694a758188d146eff0e7aeeb7692f8552c
Jim Minter <jminter@redhat.com>
2020-10-13 12:37:19 -0500

    remove stale mock files

--- pkg/env/generate.go
+++ pkg/env/generate.go
@@ -1,7 +1,8 @@
-//go:generate go run ../../vendor/github.com/golang/mock/mockgen -destination=../util/mocks/$GOPACKAGE/$GOPACKAGE.go github.com/Azure/ARO-RP/pkg/$GOPACKAGE Interface
-//go:generate go run ../../vendor/golang.org/x/tools/cmd/goimports -local=github.com/Azure/ARO-RP -e -w ../util/mocks/$GOPACKAGE/$GOPACKAGE.go
-
 package env
 
 // Copyright (c) Microsoft Corporation.
 // Licensed under the Apache License 2.0.
+
+//go:generate rm -rf ../util/mocks/$GOPACKAGE
+//go:generate go run ../../vendor/github.com/golang/mock/mockgen -destination=../util/mocks/$GOPACKAGE/$GOPACKAGE.go github.com/Azure/ARO-RP/pkg/$GOPACKAGE Interface
+//go:generate go run ../../vendor/golang.org/x/tools/cmd/goimports -local=github.com/Azure/ARO-RP -e -w ../util/mocks/$GOPACKAGE/$GOPACKAGE.go
