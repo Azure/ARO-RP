39f70444eb5036b301b4ff24abefacd9c4966192
Jim Minter <jminter@redhat.com>
2020-10-27 18:11:43 -0600

    workaround LB/PLS bug

--- pkg/frontend/openshiftcluster_putorpatch.go
+++ pkg/frontend/openshiftcluster_putorpatch.go
@@ -155,6 +155,10 @@ func (f *frontend) _putOrPatchOpenShiftCluster(ctx context.Context, r *http.Requ
 	doc.OpenShiftCluster.ID, doc.OpenShiftCluster.Name, doc.OpenShiftCluster.Type = oldID, oldName, oldType
 
 	if isCreate {
+		// on create, make the cluster resourcegroup ID lower case to work
+		// around LB/PLS bug
+		doc.OpenShiftCluster.Properties.ClusterProfile.ResourceGroupID = strings.ToLower(doc.OpenShiftCluster.Properties.ClusterProfile.ResourceGroupID)
+
 		doc.ClusterResourceGroupIDKey = strings.ToLower(doc.OpenShiftCluster.Properties.ClusterProfile.ResourceGroupID)
 		doc.ClientIDKey = strings.ToLower(doc.OpenShiftCluster.Properties.ServicePrincipalProfile.ClientID)
 		doc.OpenShiftCluster.Properties.ProvisioningState = api.ProvisioningStateCreating
