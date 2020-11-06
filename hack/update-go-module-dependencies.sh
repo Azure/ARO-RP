82c0f3590009cadc370b340557d9178973c89f57
Amber Brown <ambrown@redhat.com>
2020-11-06 15:51:11 +1100

    use curl + go proxy for update

--- hack/update-go-module-dependencies.sh
+++ hack/update-go-module-dependencies.sh
@@ -4,18 +4,16 @@ for x in vendor/github.com/openshift/*; do
 	if [[ "$x" = "vendor/github.com/openshift/installer" ]]; then
 		continue
 	fi
-	go mod edit -replace ${x##vendor/}=$(go list -mod=mod -m ${x##vendor/}@release-4.5 | sed -e 's/ /@/')
+	go mod edit -replace ${x##vendor/}=${x##vendor/}@$(curl https://proxy.golang.org/${x##vendor/}/@v/release-4.5.info | jq -r ."Version")
 done
 
-for x in aws azure gcp openstack; do
-	go mod edit -replace sigs.k8s.io/cluster-api-provider-$x=$(go list -mod=mod -m github.com/openshift/cluster-api-provider-$x@release-4.5 | sed -e 's/ /@/')
+for x in aws azure; do
+	go mod edit -replace sigs.k8s.io/cluster-api-provider-$x=github.com/openshift/cluster-api-provider-$x@$(curl https://proxy.golang.org/github.com/openshift/cluster-api-provider-$x/@v/release-4.5.info | jq -r ."Version")
 done
 
-go mod edit -replace github.com/metal3-io/baremetal-operator=$(go list -mod=mod -m github.com/openshift/baremetal-operator@release-4.5 | sed -e 's/ /@/')
+go mod edit -replace github.com/openshift/installer=github.com/jim-minter/installer@$(curl https://proxy.golang.org/github.com/jim-minter/installer/@v/release-4.5-azure-slim.info | jq -r ."Version")
 
-go mod edit -replace github.com/openshift/installer=$(go list -mod=mod -m github.com/jim-minter/installer@release-4.5-azure | sed -e 's/ /@/')
-
-go mod edit -replace k8s.io/kube-openapi=$(go list -mod=mod -m k8s.io/kube-openapi@release-1.18 | sed -e 's/ /@/')
+go mod edit -replace k8s.io/kube-openapi=k8s.io/kube-openapi@$(curl https://proxy.golang.org/k8s.io/kube-openapi/@v/release-1.18.info | jq -r ."Version")
 
 go get -u ./...
 
