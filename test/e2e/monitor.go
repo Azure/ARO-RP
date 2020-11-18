4c6a3ecac7fba2c7085bb69dc413a12ae0a640ff
Jim Minter <jminter@redhat.com>
2020-11-18 10:24:14 -0600

    add monitor e2e test

---
+++ test/e2e/monitor.go
@@ -0,0 +1,29 @@
+package e2e
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/Azure/ARO-RP/pkg/api"
+	"github.com/Azure/ARO-RP/pkg/metrics/noop"
+	"github.com/Azure/ARO-RP/pkg/monitor/cluster"
+)
+
+var _ = Describe("Monitor", func() {
+	Specify("a monitor run should not return any errors", func() {
+		ctx := context.Background()
+
+		mon, err := cluster.NewMonitor(ctx, log, clients.RestConfig, &api.OpenShiftCluster{
+			ID: resourceIDFromEnv(),
+		}, &noop.Noop{}, true)
+		Expect(err).NotTo(HaveOccurred())
+
+		errs := mon.Monitor(ctx)
+		Expect(errs).To(HaveLen(0))
+	})
+})
