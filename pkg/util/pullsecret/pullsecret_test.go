67b78ddd74145193a6ee9483c7708f4c5a03a843
Petr Kotas <pkotas@redhat.com>
2020-12-02 14:06:26 +0100

    Add ps merge test
    Signed-off-by: Petr Kotas <pkotas@redhat.com>

--- pkg/util/pullsecret/pullsecret_test.go
+++ pkg/util/pullsecret/pullsecret_test.go
@@ -98,6 +98,21 @@ func TestSetRegistryProfiles(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "delete",
+			ps:   original,
+			rps:  []*api.RegistryProfile{},
+			want: &pullSecret{
+				Auths: map[string]map[string]interface{}{
+					"arosvc.azurecr.io": {
+						"auth": "x",
+					},
+					"registry.redhat.io": {
+						"auth": "y",
+					},
+				},
+			},
+		},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
 			ps, changed, err := SetRegistryProfiles(tt.ps, tt.rps...)
@@ -117,6 +132,186 @@ func TestSetRegistryProfiles(t *testing.T) {
 
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Error(ps)
+			}
+		})
+	}
+}
+
+func TestMerge(t *testing.T) {
+	originalPS := `{"auths":{"arosvc.azurecr.io":{"auth":"x"},"registry.redhat.io":{"auth":"y"}}}`
+
+	for _, tt := range []struct {
+		name        string
+		base        string
+		ps          string
+		wantPS      pullSecret
+		wantChanged bool
+		wantError   error
+	}{
+		{
+			name: "both the same",
+			ps:   originalPS,
+			base: originalPS,
+			wantPS: pullSecret{
+				Auths: map[string]map[string]interface{}{
+					"arosvc.azurecr.io": {
+						"auth": "x",
+					},
+					"registry.redhat.io": {
+						"auth": "y",
+					},
+				},
+			},
+			wantChanged: false,
+			wantError:   nil,
+		},
+		{
+			name: "ps empty",
+			ps:   ``,
+			base: originalPS,
+			wantPS: pullSecret{
+				Auths: map[string]map[string]interface{}{
+					"arosvc.azurecr.io": {
+						"auth": "x",
+					},
+					"registry.redhat.io": {
+						"auth": "y",
+					},
+				},
+			},
+			wantChanged: false,
+			wantError:   nil,
+		},
+		{
+			name: "base empty",
+			ps:   originalPS,
+			base: ``,
+			wantPS: pullSecret{
+				Auths: map[string]map[string]interface{}{
+					"arosvc.azurecr.io": {
+						"auth": "x",
+					},
+					"registry.redhat.io": {
+						"auth": "y",
+					},
+				},
+			},
+			wantChanged: true,
+			wantError:   nil,
+		},
+		{
+			name: "ps add new",
+			ps:   `{"auths":{"arosvc.azurecr.io":{"auth":"x"},"registry.redhat.io":{"auth":"y"},"aroacr.azure.io":{"auth":"z"}}}`,
+			base: originalPS,
+			wantPS: pullSecret{
+				Auths: map[string]map[string]interface{}{
+					"arosvc.azurecr.io": {
+						"auth": "x",
+					},
+					"registry.redhat.io": {
+						"auth": "y",
+					},
+					"aroacr.azure.io": {
+						"auth": "z",
+					},
+				},
+			},
+			wantChanged: true,
+			wantError:   nil,
+		},
+		{
+			name: "ps remove one",
+			base: `{"auths":{"arosvc.azurecr.io":{"auth":"x"},"registry.redhat.io":{"auth":"y"},"aroacr.azure.io":{"auth":"z"}}}`,
+			ps:   originalPS,
+			wantPS: pullSecret{
+				Auths: map[string]map[string]interface{}{
+					"arosvc.azurecr.io": {
+						"auth": "x",
+					},
+					"registry.redhat.io": {
+						"auth": "y",
+					},
+					"aroacr.azure.io": {
+						"auth": "z",
+					},
+				},
+			},
+			wantChanged: false,
+			wantError:   nil,
+		},
+		{
+			name: "ps change key one",
+			ps:   `{"auths":{"arosvc.azurecr.io":{"auth":"a"},"registry.redhat.io":{"auth":"y"}}}`,
+			base: originalPS,
+			wantPS: pullSecret{
+				Auths: map[string]map[string]interface{}{
+					"arosvc.azurecr.io": {
+						"auth": "a",
+					},
+					"registry.redhat.io": {
+						"auth": "y",
+					},
+				},
+			},
+			wantChanged: true,
+			wantError:   nil,
+		},
+		{
+			name: "base add new",
+			base: `{"auths":{"arosvc.azurecr.io":{"auth":"x"},"registry.redhat.io":{"auth":"y"},"aroacr.azure.io":{"auth":"z"}}}`,
+			ps:   originalPS,
+			wantPS: pullSecret{
+				Auths: map[string]map[string]interface{}{
+					"arosvc.azurecr.io": {
+						"auth": "x",
+					},
+					"registry.redhat.io": {
+						"auth": "y",
+					},
+					"aroacr.azure.io": {
+						"auth": "z",
+					},
+				},
+			},
+			wantChanged: false,
+			wantError:   nil,
+		},
+		{
+			name: "base delete one",
+			base: `{"auths":{"arosvc.azurecr.io":{"auth":"x"}}}`,
+			ps:   originalPS,
+			wantPS: pullSecret{
+				Auths: map[string]map[string]interface{}{
+					"arosvc.azurecr.io": {
+						"auth": "x",
+					},
+					"registry.redhat.io": {
+						"auth": "y",
+					},
+				},
+			},
+			wantChanged: true,
+			wantError:   nil,
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			ps, changed, err := Merge(tt.base, tt.ps)
+			if err != tt.wantError {
+				t.Fatalf("Want err: %s, Got: %s", tt.wantError.Error(), err.Error())
+			}
+
+			if changed != tt.wantChanged {
+				t.Fatalf("Want changed: %t, got: %t", tt.wantChanged, changed)
+			}
+
+			var got pullSecret
+			err = json.Unmarshal([]byte(ps), &got)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if !reflect.DeepEqual(got, tt.wantPS) {
+				t.Errorf("wrong ps: %s", ps)
 			}
 		})
 	}
