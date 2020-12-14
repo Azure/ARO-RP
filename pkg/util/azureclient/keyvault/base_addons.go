f5ec223238f91709ac2ea51ea71d9911cf34d8ef
Jim Minter <jminter@redhat.com>
2020-12-14 11:48:38 -0600

    add GetSecretVersions

--- pkg/util/azureclient/keyvault/base_addons.go
+++ pkg/util/azureclient/keyvault/base_addons.go
@@ -12,10 +12,29 @@ import (
 // BaseClientAddons contains addons for BaseClient
 type BaseClientAddons interface {
 	GetSecrets(ctx context.Context, vaultBaseURL string, maxresults *int32) (secrets []azkeyvault.SecretItem, err error)
+	GetSecretVersions(ctx context.Context, vaultBaseURL string, secretName string, maxresults *int32) (result []azkeyvault.SecretItem, err error)
 }
 
 func (c *baseClient) GetSecrets(ctx context.Context, vaultBaseURL string, maxresults *int32) (secrets []azkeyvault.SecretItem, err error) {
 	page, err := c.BaseClient.GetSecrets(ctx, vaultBaseURL, maxresults)
+	if err != nil {
+		return nil, err
+	}
+
+	for page.NotDone() {
+		secrets = append(secrets, page.Values()...)
+
+		err = page.NextWithContext(ctx)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return secrets, nil
+}
+
+func (c *baseClient) GetSecretVersions(ctx context.Context, vaultBaseURL string, secretName string, maxresults *int32) (secrets []azkeyvault.SecretItem, err error) {
+	page, err := c.BaseClient.GetSecretVersions(ctx, vaultBaseURL, secretName, maxresults)
 	if err != nil {
 		return nil, err
 	}
