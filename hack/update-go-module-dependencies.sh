6ec5e0fc56ba0d7fbc19ba60b421e05b817b6276
Amber Brown <ambrown@redhat.com>
2021-01-07 14:21:47 +1100

    use non-slim

--- hack/update-go-module-dependencies.sh
+++ hack/update-go-module-dependencies.sh
@@ -11,7 +11,7 @@ for x in aws azure; do
 	go mod edit -replace sigs.k8s.io/cluster-api-provider-$x=github.com/openshift/cluster-api-provider-$x@$(curl https://proxy.golang.org/github.com/openshift/cluster-api-provider-$x/@v/release-4.6.info | jq -r ."Version")
 done
 
-go mod edit -replace github.com/openshift/installer=github.com/jim-minter/installer@$(curl https://proxy.golang.org/github.com/jim-minter/installer/@v/release-4.6-azure-slim.info | jq -r ."Version")
+go mod edit -replace github.com/openshift/installer=github.com/jim-minter/installer@$(curl https://proxy.golang.org/github.com/jim-minter/installer/@v/release-4.6-azure.info | jq -r ."Version")
 
 go mod edit -replace k8s.io/kube-openapi=k8s.io/kube-openapi@$(curl https://proxy.golang.org/k8s.io/kube-openapi/@v/release-1.19.info | jq -r ."Version")
 
