9bf69688d73324f8c103a65bccc3f121dfe6581c
Mangirdas Judeikis <Mangirdas@Judeikis.LT>
2021-01-12 12:56:49 +0000

    update version

--- pkg/util/version/const.go
+++ pkg/util/version/const.go
@@ -13,8 +13,8 @@ var GitCommit = "unknown"
 
 // InstallStream describes stream we are defaulting to for all new clusters
 var InstallStream = &Stream{
-	Version:  NewVersion(4, 5, 22),
-	PullSpec: "quay.io/openshift-release-dev/ocp-release@sha256:adb5ef06c54ff75ca9033f222ac5e57f2fd82e49bdd84f737d460ae542c8af60",
+	Version:  NewVersion(4, 5, 24),
+	PullSpec: "quay.io/openshift-release-dev/ocp-release@sha256:f3ce0aeebb116bbc7d8982cc347ffc68151c92598dfb0cc45aaf3ce03bb09d11",
 }
 
 // Streams describes list of streams we support for upgrades
@@ -23,11 +23,11 @@ var (
 		InstallStream,
 		{
 			Version:  NewVersion(4, 4, 31),
-			PullSpec: "quay.io/openshift-release-dev/ocp-release@sha256:679db43a28a42fc41784ea3d4976d9d60cd194757cfdbea6137d6d0093db8c8d",
+			PullSpec: "quay.io/openshift-release-dev/ocp-release@sha256:52ca6e018793f068f994ff1e85d86283fd4a9875390dffde55c97fd59d03a009",
 		},
 		{
 			Version:  NewVersion(4, 3, 40),
-			PullSpec: "quay.io/openshift-release-dev/ocp-release@sha256:59cc585be7b4ad069a18f6f1a3309391e172192744ee65fa6e499c8b337edda4",
+			PullSpec: "quay.io/openshift-release-dev/ocp-release@sha256:9ff90174a170379e90a9ead6e0d8cf6f439004191f80762764a5ca3dbaab01dc",
 		},
 	}
 )
