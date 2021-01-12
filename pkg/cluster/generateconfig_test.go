1ac74a66ead161c18af9f77cbad2d6f37318acbd
Amber Brown <ambrown@redhat.com>
2021-01-12 11:50:53 +1100

    add test for getRHCOSImage

---
+++ pkg/cluster/generateconfig_test.go
@@ -0,0 +1,22 @@
+package cluster
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+import (
+	"context"
+	"testing"
+)
+
+func TestRHCOSImage(t *testing.T) {
+	ctx := context.Background()
+
+	err, i := getRHCOSImage(ctx)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if i == nil {
+		t.Error(i)
+	}
+}
