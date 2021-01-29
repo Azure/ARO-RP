307be22dd75e6bc83de57578aea44b3d904f66ad
Jim Minter <jminter@redhat.com>
2021-01-29 12:58:40 -0600

    fix broken local create regression

--- test/e2e/setup.go
+++ test/e2e/setup.go
@@ -156,7 +156,10 @@ func setup(ctx context.Context) error {
 		return err
 	}
 
-	vnetResourceGroup = os.Getenv("CLUSTER")
+	vnetResourceGroup = os.Getenv("RESOURCEGROUP") // TODO: remove this when we deploy and peer a vnet per cluster create
+	if os.Getenv("CI") != "" {
+		vnetResourceGroup = os.Getenv("CLUSTER")
+	}
 	clusterName = os.Getenv("CLUSTER")
 
 	if os.Getenv("CI") != "" { // always create cluster in CI
