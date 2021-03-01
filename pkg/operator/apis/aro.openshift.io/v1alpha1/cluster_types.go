eacd7e7ece28444bc8967db7c1465849e65af1d2
Petr Kotas <pkotas@redhat.com>
2021-03-01 17:43:27 +0100

    Introduce samples operator control
    Add option to control samples operator managed state.
    When `registry.redhat.io` key is not present in pull-seceret
    and feature gate ManageSamplesOperator in cluster object
    is enabled the managed state is flipped to unmanaged.
    
    Signed-off-by: Petr Kotas <pkotas@redhat.com>

--- pkg/operator/apis/aro.openshift.io/v1alpha1/cluster_types.go
+++ pkg/operator/apis/aro.openshift.io/v1alpha1/cluster_types.go
@@ -14,6 +14,7 @@ const (
 	InternetReachableFromWorker status.ConditionType = "InternetReachableFromWorker"
 	MachineValid                status.ConditionType = "MachineValid"
 	RedHatKeyPresent            status.ConditionType = "RedHatKeyPresent"
+	SamplesOperatorEnabled      status.ConditionType = "SamplesOperatorEnabled"
 )
 
 // AllConditionTypes is a operator conditions currently in use, any condition not in this list is not
@@ -57,7 +58,8 @@ type ClusterSpec struct {
 
 // FeaturesSpec defines ARO operator feature gates
 type FeaturesSpec struct {
-	PersistentPrometheus bool `json:"persistentPrometheus,omitempty"`
+	PersistentPrometheus  bool `json:"persistentPrometheus,omitempty"`
+	ManageSamplesOperator bool `json:"manageSamplesOperator,omitempty"`
 }
 
 // ClusterStatus defines the observed state of Cluster
