0a56374984cf11107a4ae7e344c52da8d9618cdf
Jim Minter <jminter@redhat.com>
2021-03-19 18:13:21 -0600

    move from network 2019-07-01 to 2020-08-01

--- pkg/operator/controllers/azurensg/subnetnsg_test.go
+++ pkg/operator/controllers/azurensg/subnetnsg_test.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"testing"
 
-	mgmtnetwork "github.com/Azure/azure-sdk-for-go/services/network/mgmt/2019-07-01/network"
+	mgmtnetwork "github.com/Azure/azure-sdk-for-go/services/network/mgmt/2020-08-01/network"
 	"github.com/Azure/go-autorest/autorest/azure"
 	"github.com/Azure/go-autorest/autorest/to"
 	"github.com/golang/mock/gomock"
