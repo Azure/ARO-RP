33deb905ca4f9e13d2a66de4936b0c8acfdf46ea
Michael Andescavage <michaelandescavage@cloudfitsoftware.com>
2021-04-12 23:00:41 -0700

    mirroring pipelines

---
+++ .pipelines/onebranch/scripts/mirrorpipeline.sh
@@ -0,0 +1,13 @@
+SERVICE_GROUP_ROOT=$BUILD_SOURCESDIRECTORY/ev2/Mirroring/ServiceGroupRoot
+EV2_BIN=$SERVICE_GROUP_ROOT/bin
+ARO_DIR=$BUILD_SOURCESDIRECTORY/deployer/drop
+
+mkdir $OB_OUTPUTDIRECTORY
+
+cd $ARO_DIR
+tar -cvf $EV2_BIN/aro.tar aro
+cd $SERVICE_GROUP_ROOT/bin
+tar -rf $EV2_BIN/aro.tar mirror.sh
+rm mirror.sh
+
+cp -r $SERVICE_GROUP_ROOT $OB_OUTPUTDIRECTORY/ServiceGroupRoot/
