c3ec20c4867daa14068e4200d7cf166f619d98c1
Michael Andescavage <michaelandescavage@cloudfitsoftware.com>
2021-05-10 11:28:31 -0700

    updated script to have .tar extension

--- .pipelines/onebranch/scripts/bootstrapper.sh
+++ .pipelines/onebranch/scripts/bootstrapper.sh
@@ -4,7 +4,7 @@ EV2_BIN=$SERVICE_GROUP_ROOT/bin
 mkdir $OB_OUTPUTDIRECTORY
 
 cd $EV2_BIN
-tar -rf $EV2_BIN/bootstrap-resources bootstrapper.sh
+tar -rvf $EV2_BIN/bootstrap-resources.tar bootstrapper.sh
 rm bootstrapper.sh
 
 cp -r $SERVICE_GROUP_ROOT $OB_OUTPUTDIRECTORY/ServiceGroupRoot/
