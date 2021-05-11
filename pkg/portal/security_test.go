c0ea3615bdd5c8eadd342fd203b1249d5054ce41
Amber Brown <ambrown@redhat.com>
2021-05-11 14:13:17 +1000

    Perform some polling to sidestep a race condition that I'm not sure we can wait on deterministically. It usually takes a few ms, but wait up to a second just in case of system pauses

--- pkg/portal/security_test.go
+++ pkg/portal/security_test.go
@@ -28,6 +28,7 @@ import (
 	testdatabase "github.com/Azure/ARO-RP/test/database"
 	"github.com/Azure/ARO-RP/test/util/listener"
 	testlog "github.com/Azure/ARO-RP/test/util/log"
+	"github.com/Azure/ARO-RP/test/util/testpoller"
 )
 
 var (
@@ -326,7 +327,7 @@ func TestSecurity(t *testing.T) {
 					return
 				}
 
-				// skipping https://server because the http.ServeContent() calls in the
+				// perform some polling on static files because the http.ServeContent() calls in the
 				// portal's serve() and index() handlers[1] issued a call to io.Copy()[2]
 				// causes a race condition with the audit hook. The response was returned
 				// to the client and the testlog.AssertAuditPayloads() was called immediately,
@@ -338,7 +339,12 @@ func TestSecurity(t *testing.T) {
 				// [1] https://github.com/Azure/ARO-RP/blob/master/pkg/portal/portal.go#L222-L247
 				// [2] https://go.googlesource.com/go/+/go1.16.2/src/net/http/fs.go#337
 				if tt.name == "/" || tt.name == "/main.js" {
-					return
+					err = testpoller.Poll(1*time.Second, 5*time.Millisecond, func() (bool, error) {
+						return len(auditHook.AllEntries()) == 1, nil
+					})
+					if err != nil {
+						t.Error(err)
+					}
 				}
 
 				payload := auditPayloadFixture()
