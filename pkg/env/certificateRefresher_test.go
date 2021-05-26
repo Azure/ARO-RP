760e15f2c5a0141815e56a3dfb50711ea66c2c6f
Petr Kotas <pkotas@redhat.com>
2021-05-26 18:29:33 +0200

    Add CertificateRefresher
    Wraps around KeyvaultManager.GetCertificateSecret and
    acts and simple poller. Refreshes certificates in regular
    intervals, when error happens, it is logged and old
    certificate is preserved.
    
    If certificate is already rotated azure-wise the RP will
    fail to authenticate, and there will be mention in the logs
    about the error.
    
    Signed-off-by: Petr Kotas <pkotas@redhat.com>

---
+++ pkg/env/certificateRefresher_test.go
@@ -0,0 +1,281 @@
+package env
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+import (
+	"context"
+	"crypto/rsa"
+	"crypto/x509"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/sirupsen/logrus"
+
+	"github.com/Azure/ARO-RP/pkg/util/keyvault"
+	mock_keyvault "github.com/Azure/ARO-RP/pkg/util/mocks/keyvault"
+	utilpem "github.com/Azure/ARO-RP/pkg/util/pem"
+)
+
+const testCertBundle1 = `-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDwFPCd+9yh5Qyx
+ctlSvtG6D/5Y1ClB33NSaJ2x7ZwW+xJH2NkWaR4eAP0vzrlV9zdfc5PxF0+skOEd
+DuyhFQwgzGXWfoIFwaxtkvibWo8+qz1PDzxdWnlK2Qk9BbF44I3J0cCYE0+6NXLR
+/8gmUVzt2Rk1xFez8dVvFEEJDK6OiVEeduMAeTfjmUcHBUqsHzPtDbxMsG4fhhuE
+yWH58f9QKIk9Q9SPZ07jAnSKcVuWY+0Tox4797hPNTrn2/7c6BqsitOijUD/55RB
+Jo3wpiLcyyIWxLgkMgIlg01nDAY0qhOjFPoPlUCKXdRr7BhazanGCArK0z2FzIWA
+5S4CEDflAgMBAAECggEABX+lRykGk5qoYMQNoCyIpydInwY077JLdN66heG4Snpz
+n7uitTWxH+TL57VnX0WrOf9uqv3qsDwdO8okt0fBIFsuFeyN083swhG0qfI4B6pq
+XA4wRr8UuhcgdApWVztlY/Lu40zF7bDdsVuXXPFOHJB1WFrn21I1njariqaEtPT6
+z5bWELFP5Syq9WfXY5ug4MhNXLMuoMQTLXtspQ0M0gCldJEb7dQzczgiODI/q+JU
+uMROZ5xssAr8C880fapvaxo3dcBqt4W22ya7zhahduLgfG4FsMUjYOBvWhe+gBA0
+YPGz9Qaej/pLjYQgiKUOSkR0d8tkT50AfZXD1d4kHQKBgQDx3rarPnAk+1B2HruI
+Duxy8DG7hGCzd5PqlaSpQpDKqJJY6DaoZ+tuV0CkqqODNe+L+i69wk1JwFR8HOyr
+ReILU0AvN1DxexRvIc4KTCRjC4xOTbrMOOtSMRchVIaGOnCN5ElfJxKEuoBYCLPi
+Xw7QVkrI7lV1Jhc8QW6X+7EW2wKBgQD+G3u25ngsk+9scGxq34pVz3YohVyine8h
+fUgsyCVdERRR5RKLrKng/GSdgTzl0+smwO7N+/9Q+QSL1SANr2zjmHVa6n02Hehm
+S+4YvHsA4WPGFrlmaVgO80KwVNmm5Xey6p0PnNt9zKYi4SRrEHPRMd8+EQuqL2Nn
+KkVlI4pIPwKBgQC06Kti3HnO/3bIUuZbtyXeNpBMPJCDy+4EKVeXDmX0Xy/Pdijj
+v47V4kdEoylYS/BXl5J8dqeOgV/v0UaoOMYBSIyahFpztGatVPCivR7+QjX4n6UX
+eX9x46v0Tx+rqGxlhRnoJPZx9nlm32OE7yrKY7DeJ34d+JaqiBprbWOgvwKBgHEV
+7hLRsn20QIMz7SwK29eggmc6IqXEP53Z0XsMf4RRi4d+uKgsaVXVPTnTQDTQAQC4
+MA6/rTpt+BX6/U7Z2U3YlbGmVZ715G1SMV4U03Dq3apUhqILE8NjgzRSLqLV0FVx
+kABYwF3V68HuDHURV1msJjvK/jP47vYEm+mMzYelAoGAK2zS87DVUMbwiQOHveGQ
+lhvC6HZsja/CEk2C3RTo+0IsEzc/O8/qyDZd6+rqCmJ9ViKHccvtYwRTzRrrDtP1
+XQ9FR4VO2eH2YLAREzCgfykEzwBPFinM1kBhtpbZID4n8YizSODJTI9pasC1z/zL
+x5MTZmC2cAtxmyx3pY/KFa8=
+-----END PRIVATE KEY-----
+-----BEGIN CERTIFICATE-----
+MIIDLjCCAhagAwIBAgIQHkfoyE/KTKuEhOTumdk4+zANBgkqhkiG9w0BAQsFADAU
+MRIwEAYDVQQDEwlwZXRyLnRlc3QwHhcNMjEwNTI2MDkzNjU5WhcNMjEwNjI2MDk0
+NjU5WjAUMRIwEAYDVQQDEwlwZXRyLnRlc3QwggEiMA0GCSqGSIb3DQEBAQUAA4IB
+DwAwggEKAoIBAQDwFPCd+9yh5QyxctlSvtG6D/5Y1ClB33NSaJ2x7ZwW+xJH2NkW
+aR4eAP0vzrlV9zdfc5PxF0+skOEdDuyhFQwgzGXWfoIFwaxtkvibWo8+qz1PDzxd
+WnlK2Qk9BbF44I3J0cCYE0+6NXLR/8gmUVzt2Rk1xFez8dVvFEEJDK6OiVEeduMA
+eTfjmUcHBUqsHzPtDbxMsG4fhhuEyWH58f9QKIk9Q9SPZ07jAnSKcVuWY+0Tox47
+97hPNTrn2/7c6BqsitOijUD/55RBJo3wpiLcyyIWxLgkMgIlg01nDAY0qhOjFPoP
+lUCKXdRr7BhazanGCArK0z2FzIWA5S4CEDflAgMBAAGjfDB6MA4GA1UdDwEB/wQE
+AwIFoDAJBgNVHRMEAjAAMB0GA1UdJQQWMBQGCCsGAQUFBwMBBggrBgEFBQcDAjAf
+BgNVHSMEGDAWgBSiCXS05A51/N/eKXEBLiNi/W5ZwDAdBgNVHQ4EFgQUogl0tOQO
+dfzf3ilxAS4jYv1uWcAwDQYJKoZIhvcNAQELBQADggEBADPwcFnUq8NYMlyZriF1
+Yk3tTiLwlMrwHQViz143t/C+lMlQfVME515xxn1SUdEG0JAseCOGiIsqLVpwc042
+cFkBgbCAkIMg3BIJoKMIFMEXdlcbQ9TlGY0QQPxvfT+L1giGaK6mcmBuIBi9iM9k
+8ClJkAbkMgX3A68eWbI8PEaV/KyyHD/zHX/UmnyquXYxUZ9Cdazt3rG7vmt+NTw1
+LlsPZY/5jnNhkjNt+qgruByc5/XNcVJE4VZcEUiDaAwhi5XDigIPx7eM9tF+yuIf
+C+cV51VbGUPQBvJTr0LjGaUDbryrVOAwma5qNi4ekdcOvSrBBs4hflrn8up+HUoX
+zB0=
+-----END CERTIFICATE-----`
+
+const testCertBundle2 = `-----BEGIN PRIVATE KEY-----
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDGwJS2bDR2ugfs
+ODr1/VkhX9A2b6/w66r4habosbIeX9meMsKZbtsMOrWH2uten9H7L0o2rYbU5c3L
+AtEaeEplGG4AamtF4s35axIF8/jgMvkswHjodb2f79iImOx96tE/Odhu3T/uoR1V
+543Q1f80/HZzdpqnIpNZd/NrDMv2Hjm1LcLFMpcZFBpdF4CmAm6J59LjfZWm5qq5
+aJf3MFEdxsX0VwEURmP0lrsvZTJhsu+httv+soFareiOk2W5nU0UhFSpKfOrFfld
+cHxNPX6T2wr7JTYLDfgeRG6VoboPwcdVCCllrKWUlP9zdq+A6zbByHqC+QG6IP2c
+zzXUAMR9AgMBAAECggEAOHAjSpH7a+NzsH5fL88bakC87VhVy8IAMMYzGUysWbe6
+IhQj8lHqXdSmC8f8okgb5ooNNu2bpgUDpSxNmIikB4UiZ3fQsm2vM65V1d7rwy52
+V2DodIpKqIoBIPjb3v25IY0ZipHFP8v8epJgUPcTm0Y9lJgPXnqRAQzw0Gs319FI
+wxVy/RkwJ/p6BYufJs/T7HjCjCSsBA0JPx46qiCGA90u4gmYOwQFmZoEjlcMzPaL
+Oinbfs2WN5mSazev/4zY/bEnFuO1vbpvx1qAEZpoDvN2CeYjAHvhmMe5zQwEFgsY
+Bjhfd8pHuujbkIvHNo8mcRY8D7tFggw0QC8YZBl9+QKBgQDNcJQPQPaLUy8ZUxfL
+8HMfpnVPAt9g0mctMo52CiF95WqMjXhJ5tbn7J2R6JFawCoIunNAWDwCkOg9dzgE
+SwkGAQjSvHNlLOAIx1ej+vjeLvjyCgXqiv66bG16JK4ZA3K/Dewe/o27Tbpny0/7
+AkfjrknhyRjK4pnnZ/kWj6wJtwKBgQD3qqrXHBJD78lo0HnZmNLeI8noM5ruJD23
+9dfTEkr99/cpe6mdD2OJKNsBfUB4y/Ces7EHPa6UEPLl36vIHhg+5chrqrNbRfIv
+LeCeRPkjVTxEuPCtUx7Bt1LqlM1tPfOp67V6160aroLnpIdUsWSOQFuAvGqHKLhG
+Nau31SDzawKBgQC9PIYlxuFTVTx9R10ULljdPqewMCUzOpxvtbIkaRCQt1J+RZIY
+ANrUp9A9Js09muUdRSIEk0Iz2ucSN08SJUwai7lk5NIm0D9N1tGT6wpzHzGRQkpQ
+0dfyQQ5XBJKZ1+NKubhWlIRZlC+wjEcQH/m4cEL+CA8eU70Qu2VmstD14QKBgEmo
+PWT6WUhRMUJ19jdL5zLfy/W+G07GAoEKoaSJpToBHEX/HEO0xvKM7w1zVdBXPvnE
+EVtI8fnhTIwnSGyc3rMeHcw/mVYE6HE1oL8RXlMuz1zU7+dseBI+1m8j0DC0Ixqf
+GnstV7M+wXnpCcKbe39/DnesEbae2qcu4SIsRb9/AoGBAJw6iTZm2kjk/AK3i/ZN
+RpuP6/8jbHZNTvLz8Aw9J4wxoM8wp3kpJe0kMOgzWZPiya4QX7kCma+uoibiR3v/
+ohPVpYqZtLFdfEd9pDXMZCg/q1sOcL94gNiv9w0QCbuvBhAno7OHk9JZMIr3pJ0h
+tT1hverLghBfQ8KhO9Gk+FSM
+-----END PRIVATE KEY-----
+-----BEGIN CERTIFICATE-----
+MIIDLjCCAhagAwIBAgIQD/cl3WNvT3ynxkUONemaPjANBgkqhkiG9w0BAQsFADAU
+MRIwEAYDVQQDEwlwZXRyLnRlc3QwHhcNMjEwNTI2MDkzNDQzWhcNMjEwNjI2MDk0
+NDQzWjAUMRIwEAYDVQQDEwlwZXRyLnRlc3QwggEiMA0GCSqGSIb3DQEBAQUAA4IB
+DwAwggEKAoIBAQDGwJS2bDR2ugfsODr1/VkhX9A2b6/w66r4habosbIeX9meMsKZ
+btsMOrWH2uten9H7L0o2rYbU5c3LAtEaeEplGG4AamtF4s35axIF8/jgMvkswHjo
+db2f79iImOx96tE/Odhu3T/uoR1V543Q1f80/HZzdpqnIpNZd/NrDMv2Hjm1LcLF
+MpcZFBpdF4CmAm6J59LjfZWm5qq5aJf3MFEdxsX0VwEURmP0lrsvZTJhsu+httv+
+soFareiOk2W5nU0UhFSpKfOrFfldcHxNPX6T2wr7JTYLDfgeRG6VoboPwcdVCCll
+rKWUlP9zdq+A6zbByHqC+QG6IP2czzXUAMR9AgMBAAGjfDB6MA4GA1UdDwEB/wQE
+AwIFoDAJBgNVHRMEAjAAMB0GA1UdJQQWMBQGCCsGAQUFBwMBBggrBgEFBQcDAjAf
+BgNVHSMEGDAWgBTVoGOzKcn+Y+vypnh2e5BzIcQ6NzAdBgNVHQ4EFgQU1aBjsynJ
+/mPr8qZ4dnuQcyHEOjcwDQYJKoZIhvcNAQELBQADggEBAGREAWRpsAUlhLtnqgyh
+qhRDd0xuzlBte5uMWYJxojwK7jmJlb4YbIgm6OfSMW3cd1xN497egji5AiiW37wN
+UDONqYUL4qF3nnPJl9yjvptJCdpZQC1Ma5b2BLF8X8kurh8rYtyx2NNncj6/p/IA
+D/R4qcuA0UaXLdARsppjLTCzk6T/Z8v4qaaXQJBfdHLY2fZo4vSI1yLyTcGia703
+Zn30eMz0PLFCkMEYhZADUia0peReH08WO7RTtE5Nf8r5ZiFSfuJJlBmURMU3MAxJ
+PeihXgcD511I3eMq7A8r7+xlNFEFYaleYwmoYSOt0cBGtoZttXDYacR/xvEkVaxO
+1LQ=
+-----END CERTIFICATE-----`
+
+// equalPrivKey is added manually, it has been added to go1.15, whilst ARO uses go1.14
+// TODO: Remove once ARO is on go1.15
+func equalPrivKey(one, other *rsa.PrivateKey) bool {
+	if one == nil && other == nil {
+		return true
+	}
+
+	pubEqual := one.PublicKey.N.Cmp(other.PublicKey.N) == 0 && one.PublicKey.E == other.PublicKey.E
+	expEqual := one.D.Cmp(other.D) == 0
+	if !pubEqual || !expEqual {
+		return false
+	}
+
+	primesEqual := len(one.Primes) == len(other.Primes)
+	if !primesEqual {
+		return false
+	}
+
+	for i := range one.Primes {
+		if one.Primes[i].Cmp(other.Primes[i]) != 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestRefreshingCertificate(t *testing.T) {
+	mockController := gomock.NewController(t)
+
+	key1, certs1, err := utilpem.Parse([]byte(testCertBundle1))
+	if err != nil {
+		return
+	}
+
+	key2, certs2, err := utilpem.Parse([]byte(testCertBundle2))
+	if err != nil {
+		return
+	}
+
+	testCertName := "rotation-test-0"
+
+	cannotPull := errors.New("Cannot pull certificates from keyvault")
+
+	tt := []struct {
+		name           string
+		interval       time.Duration
+		sleep          time.Duration
+		managerFactory func(*gomock.Controller) keyvault.Manager
+		wantKey        *rsa.PrivateKey
+		wantCert       *x509.Certificate
+		wantErr        error
+		wantUpdate     bool
+	}{
+		{
+			name:     "test initial certificate, pull exactly once, ticker is still waiting",
+			interval: 10 * time.Minute,
+			sleep:    10 * time.Millisecond,
+			managerFactory: func(controller *gomock.Controller) keyvault.Manager {
+				manager := mock_keyvault.NewMockManager(controller)
+				manager.EXPECT().GetCertificateSecret(gomock.Any(), testCertName).Return(key1, certs1, nil)
+				return manager
+			},
+			wantKey:  key1,
+			wantCert: certs1[0],
+			wantErr:  nil,
+		},
+		{
+			name:     "test refresh certificate, pull exactly twice, first on start, second on refresh",
+			interval: 50 * time.Millisecond,
+			sleep:    60 * time.Millisecond,
+			managerFactory: func(controller *gomock.Controller) keyvault.Manager {
+				manager := mock_keyvault.NewMockManager(mockController)
+				gomock.InOrder(
+					manager.EXPECT().GetCertificateSecret(gomock.Any(), testCertName).Return(key1, certs1, nil),
+					manager.EXPECT().GetCertificateSecret(gomock.Any(), testCertName).Return(key2, certs2, nil),
+				)
+				return manager
+			},
+			wantKey:  key2,
+			wantCert: certs2[0],
+			wantErr:  nil,
+		},
+		{
+			name:     "test initial error, pull exactly once with an error",
+			interval: 1 * time.Minute,
+			sleep:    20 * time.Millisecond,
+			managerFactory: func(controller *gomock.Controller) keyvault.Manager {
+				manager := mock_keyvault.NewMockManager(mockController)
+				manager.EXPECT().GetCertificateSecret(gomock.Any(), testCertName).Return(nil, nil, cannotPull)
+				return manager
+			},
+			wantKey:  nil,
+			wantCert: nil,
+			wantErr:  cannotPull,
+		},
+		{
+			name:     "test refresh error, pull exactly twice, first on start, second time with an error",
+			interval: 50 * time.Millisecond,
+			sleep:    60 * time.Millisecond,
+			managerFactory: func(controller *gomock.Controller) keyvault.Manager {
+				manager := mock_keyvault.NewMockManager(controller)
+				gomock.InOrder(
+					manager.EXPECT().GetCertificateSecret(gomock.Any(), testCertName).Return(key1, certs1, nil),
+					manager.EXPECT().GetCertificateSecret(gomock.Any(), testCertName).Return(nil, nil, cannotPull),
+				)
+				return manager
+			},
+			wantKey:  key1,
+			wantCert: certs1[0],
+			wantErr:  nil,
+		},
+		{
+			name:     "test refresh, pull exactly 10 times",
+			interval: 10 * time.Millisecond,
+			sleep:    100 * time.Millisecond,
+			managerFactory: func(controller *gomock.Controller) keyvault.Manager {
+				manager := mock_keyvault.NewMockManager(controller)
+				manager.EXPECT().GetCertificateSecret(gomock.Any(), testCertName).Return(key1, certs1, nil).Times(10)
+				return manager
+			},
+			wantKey:  key1,
+			wantCert: certs1[0],
+			wantErr:  nil,
+		},
+	}
+
+	for _, test := range tt {
+		t.Run(test.name, func(t *testing.T) {
+			stop := make(chan struct{})
+			defer close(stop)
+
+			refreshing := newCertificateRefresher(
+				logrus.NewEntry(logrus.StandardLogger()),
+				test.interval,
+				test.managerFactory(mockController),
+				testCertName,
+				stop,
+			)
+
+			err := refreshing.Start(context.Background())
+			if err != test.wantErr {
+				t.Fatal(err)
+			}
+
+			if err != nil {
+				return
+			}
+
+			time.Sleep(test.sleep)
+
+			testkey, testCerts := refreshing.GetCertificates()
+			if !equalPrivKey(testkey, test.wantKey) {
+				t.Error("returned private key does not match")
+			}
+
+			if !testCerts[0].Equal(test.wantCert) {
+				t.Error("returned certificate does not match")
+			}
+		})
+	}
+}
