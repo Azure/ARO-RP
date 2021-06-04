d9f13c3fdd9de1fc32e0fd431c2e042b78949b92
Drew Anderson <dranders@redhat.com>
2021-06-04 12:33:46 +0200

    activate workaround

--- pkg/util/azureclient/mgmt/insights/activitylogs_addons.go
+++ pkg/util/azureclient/mgmt/insights/activitylogs_addons.go
@@ -6,15 +6,16 @@ package insights
 import (
 	"context"
 
-	mgmtinsights "github.com/Azure/azure-sdk-for-go/services/preview/monitor/mgmt/2018-03-01/insights"
+	//"github.com/Azure/azure-sdk-for-go/services/preview/monitor/mgmt/2018-03-01/insights"
+	"github.com/Azure/ARO-RP/pkg/util/azureclient/mgmt/insights/insights"
 )
 
 // ActivityLogsClientAddons contains addons for ActivityLogsClient
 type ActivityLogsClientAddons interface {
-	List(ctx context.Context, filter string, selectParameter string) (result []mgmtinsights.EventData, err error)
+	List(ctx context.Context, filter string, selectParameter string) (result []insights.EventData, err error)
 }
 
-func (c *activityLogsClient) List(ctx context.Context, filter string, selectParameter string) (result []mgmtinsights.EventData, err error) {
+func (c *activityLogsClient) List(ctx context.Context, filter string, selectParameter string) (result []insights.EventData, err error) {
 	page, err := c.ActivityLogsClient.List(ctx, filter, selectParameter)
 	if err != nil {
 		return nil, err
