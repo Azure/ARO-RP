65d2c9abe7e3e963b7f107f73ac42209967769eb
Zach Jones <zachjones@cloudfitsoftware.com>
2021-06-17 16:34:17 -0600

    feat: added onebranch pipeline for geneva logs account configuration
    feat: added geneva.sh script that gets ran during onebranch pipeline
    
    fix: corrected service group root path in geneva.sh in one branch pipeline
    
    feat: added onebranch pipeline files for geneva bootstrapper
    
    fix: updated names of geneva tar files
    
    fix: removed unnecessary commands from geneva and genev bootstrapper onebranch pipeline scripts
    
    fix: removed kusto-bootstrapper references as it no longer exists
    
    fix: updated linux container images in geneva pipelines

---
+++ .pipelines/onebranch/scripts/geneva-bootstrapper.sh
@@ -0,0 +1,5 @@
+SERVICE_GROUP_ROOT=$BUILD_SOURCESDIRECTORY/ARO.Pipelines/ev2/Logging/Geneva-Bootstrapper/ServiceGroupRoot
+
+mkdir $OB_OUTPUTDIRECTORY
+
+cp -r $SERVICE_GROUP_ROOT $OB_OUTPUTDIRECTORY/ServiceGroupRoot/
