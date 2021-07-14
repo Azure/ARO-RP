74c83b322e01383e7993a362bdc5e1d535cae46d
Amber Brown <ambrown@redhat.com>
2021-07-14 15:59:10 +1000

    add a script to encode SecureStrings for dev

---
+++ hack/aead/aead.go
@@ -0,0 +1,93 @@
+package main
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+import (
+	"bufio"
+	"context"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/Azure/ARO-RP/pkg/api"
+	"github.com/Azure/ARO-RP/pkg/env"
+	"github.com/Azure/ARO-RP/pkg/util/encryption"
+	"github.com/Azure/ARO-RP/pkg/util/keyvault"
+	utillog "github.com/Azure/ARO-RP/pkg/util/log"
+)
+
+var (
+	fileName = flag.String("file", "-", "File to read. '-' for stdin.")
+)
+
+func run(ctx context.Context, log *logrus.Entry) error {
+	flag.Parse()
+
+	var (
+		file io.Reader
+		err  error
+		v    string
+	)
+
+	if *fileName == "-" {
+		file = os.Stdin
+	} else {
+		file, err = os.Open(*fileName)
+		if err != nil {
+			return err
+		}
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		v = v + scanner.Text() + "\n"
+	}
+
+	_env, err := env.NewCore(ctx, log)
+	if err != nil {
+		return err
+	}
+
+	msiKVAuthorizer, err := _env.NewMSIAuthorizer(env.MSIContextRP, _env.Environment().ResourceIdentifiers.KeyVault)
+	if err != nil {
+		return err
+	}
+
+	serviceKeyvaultURI, err := keyvault.URI(_env, env.ServiceKeyvaultSuffix)
+	if err != nil {
+		return err
+	}
+
+	serviceKeyvault := keyvault.NewManager(msiKVAuthorizer, serviceKeyvaultURI)
+
+	key, err := serviceKeyvault.GetBase64Secret(ctx, env.EncryptionSecretName)
+	if err != nil {
+		return err
+	}
+
+	aead, err := encryption.NewXChaCha20Poly1305(ctx, key)
+	if err != nil {
+		return err
+	}
+
+	b, err := aead.Seal([]byte(api.SecureString(v)))
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(base64.StdEncoding.EncodeToString(b))
+	return nil
+}
+
+func main() {
+	log := utillog.GetLogger()
+
+	if err := run(context.Background(), log); err != nil {
+		log.Fatal(err)
+	}
+}
