65a2a9d836152cf13e159c3233960004d3b700ba
Drew Anderson <dranders@redhat.com>
2021-07-19 10:26:37 +0200

    default to OpenShiftSDN provider in production

--- pkg/cluster/generateconfig.go
+++ pkg/cluster/generateconfig.go
@@ -100,7 +100,7 @@ func (m *manager) generateInstallConfig(ctx context.Context) (*installconfig.Ins
 		workerZones = []string{""}
 	}
 
-	SDNProvider := string(api.SDNProviderOVNKubernetes)
+	SDNProvider := string(api.SDNProviderOpenShiftSDN)
 	if m.doc.OpenShiftCluster.Properties.NetworkProfile.SDNProvider != "" {
 		SDNProvider = string(m.doc.OpenShiftCluster.Properties.NetworkProfile.SDNProvider)
 	}
