a066161a6e14a0a2925f4a2cea3df0f0c6ac15b0
Nils Elde <nelde@redhat.com>
2021-08-27 16:48:52 -0400

    standardize placement of generate command

--- pkg/api/v20200430/generate.go
+++ pkg/api/v20200430/generate.go
@@ -1,6 +1,6 @@
-//go:generate go run ../../../hack/swagger github.com/Azure/ARO-RP/pkg/api/v20200430 ../../../swagger/redhatopenshift/resource-manager/Microsoft.RedHatOpenShift/stable/2020-04-30
-
 package v20200430
 
 // Copyright (c) Microsoft Corporation.
 // Licensed under the Apache License 2.0.
+
+//go:generate go run ../../../hack/swagger github.com/Azure/ARO-RP/pkg/api/v20200430 ../../../swagger/redhatopenshift/resource-manager/Microsoft.RedHatOpenShift/stable/2020-04-30
