30a724a80ea0d42e374ddf9f47d4f0a4eb2d7158
Ulrich Schlueter <uschlute@redhat.com>
2021-11-04 14:57:27 +0100

    feat: Add Pod Restart Metric
    send metric data to geneva for pods/containers with more than 10 restarts.
    
    ticket: https://msazure.visualstudio.com/AzureRedHatOpenShift/_workitems/edit/9534801

--- pkg/util/namespace/namespace.go
+++ pkg/util/namespace/namespace.go
@@ -7,10 +7,18 @@ import (
 	"strings"
 )
 
-// IsOpenShift returns true if ns is an openshift managed namespace.
+// IsOpenShift returns true if ns is an openshift managed namespace, including the default namespace
 func IsOpenShift(ns string) bool {
 	return ns == "" ||
 		ns == "default" ||
+		ns == "openshift" ||
+		strings.HasPrefix(ns, "kube-") ||
+		strings.HasPrefix(ns, "openshift-")
+}
+
+// IsOpenShiftSystemNamespace returns true if ns is an openshift managed namespace, without the default namespace
+func IsOpenShiftSystemNamespace(ns string) bool {
+	return ns == "" ||
 		ns == "openshift" ||
 		strings.HasPrefix(ns, "kube-") ||
 		strings.HasPrefix(ns, "openshift-")
