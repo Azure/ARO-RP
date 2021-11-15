6c49abc69eba665ea1791d447e79035066fb6ee4
Ross Bryan <robryan@redhat.com>
2021-11-15 09:53:48 -0500

    update fluentbit

--- pkg/util/version/const.go
+++ pkg/util/version/const.go
@@ -56,7 +56,7 @@ var (
 
 // FluentbitImage contains the location of the Fluentbit container image
 func FluentbitImage(acrDomain string) string {
-	return acrDomain + "/fluentbit:1.7.8-1"
+	return acrDomain + "/fluentbit:1.8.9-1"
 }
 
 // MdmImage contains the location of the MDM container image
