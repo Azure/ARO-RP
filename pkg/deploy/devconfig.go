a16f66b94d2c0a19ef648e2caac7e1f2bc3ff6a2
bennerv <10840174+bennerv@users.noreply.github.com>
2021-11-16 13:46:04 -0500

    Update docs and required dev-config params

--- pkg/deploy/devconfig.go
+++ pkg/deploy/devconfig.go
@@ -136,7 +136,7 @@ func DevConfig(_env env.Core) (*Config, error) {
 				adminKeyvaultAccessPolicy(_env),
 				deployKeyvaultAccessPolicy(_env),
 			},
-			FluentbitImage:       to.StringPtr(os.Getenv("FLUENTBIT_IMAGE")),
+			FluentbitImage:       to.StringPtr(version.FluentbitImage(os.Getenv("USER") + _env.Environment().ContainerRegistryDNSSuffix)),
 			FPClientID:           to.StringPtr(os.Getenv("AZURE_FP_CLIENT_ID")),
 			FPServicePrincipalID: to.StringPtr(os.Getenv("AZURE_FP_SERVICE_PRINCIPAL_ID")),
 			GatewayDomains: []string{
@@ -191,6 +191,7 @@ func DevConfig(_env env.Core) (*Config, error) {
 			SSHPublicKey:                      to.StringPtr(string(sshPublicKey)),
 			SubscriptionResourceGroupLocation: to.StringPtr(_env.Location()),
 			SubscriptionResourceGroupName:     to.StringPtr(os.Getenv("USER") + "-subscription"),
+			VMSSCleanupEnabled:                to.BoolPtr(true),
 			VMSize:                            to.StringPtr("Standard_D2s_v3"),
 		},
 	}, nil
