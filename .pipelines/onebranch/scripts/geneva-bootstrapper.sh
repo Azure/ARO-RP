9d070e11d1b56b9a456f71a7fa2938177108b614
zacharyljones <69012315+zacharyljones@users.noreply.github.com>
2022-02-11 07:44:27 -0700

    chore: removed logging onebranch pipelines files from aro-rp repo (#1942)

--- .pipelines/onebranch/scripts/geneva-bootstrapper.sh
+++
@@ -1,5 +0,0 @@
-SERVICE_GROUP_ROOT=$BUILD_SOURCESDIRECTORY/ARO.Pipelines/ev2/Logging/Geneva-Bootstrapper/ServiceGroupRoot
-
-mkdir $OB_OUTPUTDIRECTORY
-
-cp -r $SERVICE_GROUP_ROOT $OB_OUTPUTDIRECTORY/ServiceGroupRoot/
