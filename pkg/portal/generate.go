ba01319b6cdefb2eefa3c522d170d2868b7b79fa
Ellis Johnson <elljohns@redhat.com>
2022-03-02 16:41:32 +1000

    Modified portal backend to allow v1 and v2 portals to run at the same time

--- pkg/portal/generate.go
+++ pkg/portal/generate.go
@@ -3,5 +3,5 @@ package portal
 // Copyright (c) Microsoft Corporation.
 // Licensed under the Apache License 2.0.
 
-//go:generate go run ../../vendor/github.com/go-bindata/go-bindata/go-bindata -nometadata -pkg $GOPACKAGE -prefix ../../portal/build/ ../../portal/build/...
+//go:generate go run ../../vendor/github.com/go-bindata/go-bindata/go-bindata -nometadata -pkg $GOPACKAGE -prefix ../../portal/ ../../portal/v1/dist/... ../../portal/v2/build/...
 //go:generate gofmt -s -l -w bindata.go
