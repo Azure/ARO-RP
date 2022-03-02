13802daf949103fea54f6b51caa76da59f6907b3
Caden Marchese <56140267+cadenmarchese@users.noreply.github.com>
2022-03-02 15:20:00 -0500

    L series support - RP changes (#1751)
    * add L-series SKUs to internal, admin, validate api
    
    * make client

--- pkg/api/validate/vm.go
+++ pkg/api/validate/vm.go
@@ -54,7 +54,16 @@ func VMSizeIsValid(vmSize api.VMSize, requiredD2sV3Workers, isMaster bool) bool 
 				api.VMSizeStandardF72sV2,
 				api.VMSizeStandardG5,
 				api.VMSizeStandardGS5,
-				api.VMSizeStandardM128ms:
+				api.VMSizeStandardM128ms,
+				api.VMSizeStandardL4s,
+				api.VMSizeStandardL8s,
+				api.VMSizeStandardL16s,
+				api.VMSizeStandardL32s,
+				api.VMSizeStandardL8sV2,
+				api.VMSizeStandardL16sV2,
+				api.VMSizeStandardL32sV2,
+				api.VMSizeStandardL48sV2,
+				api.VMSizeStandardL64sV2:
 				return true
 			}
 		}
