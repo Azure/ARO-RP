edc03001c94b8e7cbafe1f5bb5219645ee46b7e2
Ari Lima <arielcdlima@gmail.com>
2022-04-07 14:31:57 -0700

    added unit tests for two new functions

--- pkg/cluster/networkprofile_test.go
+++ pkg/cluster/networkprofile_test.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"github.com/Azure/ARO-RP/pkg/api"
 	testdatabase "github.com/Azure/ARO-RP/test/database"
+	mcv1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
+	mcofake "github.com/openshift/machine-config-operator/pkg/generated/clientset/versioned/fake"
 	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"strings"
 	"testing"
 )
 
-// Test our populate MTUSize function
+// TestPopulateMTUSize will test our populate MTUSize function
 func TestPopulateMTUSize(t *testing.T) {
 	ctx := context.Background()
 
@@ -120,13 +123,17 @@ func TestPopulateMTUSize(t *testing.T) {
 			// Run populateMTUSize and assert the correct results
 			err = tt.m.populateMTUSize(ctx)
 			assert.Equal(t, tt.expectedErr, err, "Unexpected error exception")
-			assert.Equal(t, tt.m.doc.OpenShiftCluster.Properties.NetworkProfile.MTUSize, tt.expectedMTU, "MTUSize was not updated as expected exception")
+			assert.Equal(t, tt.expectedMTU, tt.m.doc.OpenShiftCluster.Properties.NetworkProfile.MTUSize, "MTUSize was not updated as expected exception")
 		})
 	}
 }
 
+// TestEnsureMTUSize will test our ensureMTUSize function
 func TestEnsureMTUSize(t *testing.T) {
 	ctx := context.Background()
+
+	// Define the DB instance we will use to run the PatchWithLease function
+	key := "/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/resourceGroup/providers/Microsoft.RedHatOpenShift/openShiftClusters/resourceName"
 
 	// Run tests
 	for _, tt := range []struct {
@@ -135,11 +142,153 @@ func TestEnsureMTUSize(t *testing.T) {
 		expectedMTU api.MTUSize
 		expectedErr error
 	}{
-		{},
+		{
+			name: "MTUSize set to 1500",
+			m: manager{
+				doc: &api.OpenShiftClusterDocument{
+					Key: strings.ToLower(key),
+					OpenShiftCluster: &api.OpenShiftCluster{
+						ID: key,
+						Properties: api.OpenShiftClusterProperties{
+							ProvisioningState: api.ProvisioningStateSucceeded,
+							NetworkProfile: api.NetworkProfile{
+								MTUSize: api.MTU1500,
+							},
+						},
+					},
+				},
+				mcocli: mcofake.NewSimpleClientset(
+					&mcv1.MachineConfig{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "master",
+						},
+					}),
+				subscriptionDoc: &api.SubscriptionDocument{
+					Subscription: &api.Subscription{
+						Properties: &api.SubscriptionProperties{
+							RegisteredFeatures: []api.RegisteredFeatureProfile{
+								{},
+							},
+						},
+					},
+				},
+			},
+			expectedMTU: api.MTU1500,
+			expectedErr: nil,
+		},
+		{
+			name: "MTUSize set to 3900",
+			m: manager{
+				doc: &api.OpenShiftClusterDocument{
+					Key: strings.ToLower(key),
+					OpenShiftCluster: &api.OpenShiftCluster{
+						ID: key,
+						Properties: api.OpenShiftClusterProperties{
+							ProvisioningState: api.ProvisioningStateSucceeded,
+							NetworkProfile: api.NetworkProfile{
+								MTUSize: api.MTU3900,
+							},
+						},
+					},
+				},
+				mcocli: mcofake.NewSimpleClientset(
+					&mcv1.MachineConfig{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "master",
+						},
+					}),
+				subscriptionDoc: &api.SubscriptionDocument{
+					Subscription: &api.Subscription{
+						Properties: &api.SubscriptionProperties{
+							RegisteredFeatures: []api.RegisteredFeatureProfile{
+								{},
+							},
+						},
+					},
+				},
+			},
+			expectedMTU: api.MTU3900,
+			expectedErr: nil,
+		},
+		{
+			name: "No MTUSize & MachineConfig found",
+			m: manager{
+				doc: &api.OpenShiftClusterDocument{
+					Key: strings.ToLower(key),
+					OpenShiftCluster: &api.OpenShiftCluster{
+						ID: key,
+						Properties: api.OpenShiftClusterProperties{
+							ProvisioningState: api.ProvisioningStateSucceeded,
+						},
+					},
+				},
+				mcocli: mcofake.NewSimpleClientset(
+					&mcv1.MachineConfig{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "99-master-mtu",
+						},
+					}),
+				subscriptionDoc: &api.SubscriptionDocument{
+					Subscription: &api.Subscription{
+						Properties: &api.SubscriptionProperties{
+							RegisteredFeatures: []api.RegisteredFeatureProfile{
+								{},
+							},
+						},
+					},
+				},
+			},
+			expectedMTU: api.MTU3900,
+			expectedErr: nil,
+		},
+		{
+			name: "No MTUSize & MachineConfig not found",
+			m: manager{
+				doc: &api.OpenShiftClusterDocument{
+					Key: strings.ToLower(key),
+					OpenShiftCluster: &api.OpenShiftCluster{
+						ID: key,
+						Properties: api.OpenShiftClusterProperties{
+							ProvisioningState: api.ProvisioningStateSucceeded,
+						},
+					},
+				},
+				mcocli: mcofake.NewSimpleClientset(
+					&mcv1.MachineConfig{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "master",
+						},
+					}),
+				subscriptionDoc: &api.SubscriptionDocument{
+					Subscription: &api.Subscription{
+						Properties: &api.SubscriptionProperties{
+							RegisteredFeatures: []api.RegisteredFeatureProfile{
+								{},
+							},
+						},
+					},
+				},
+			},
+			expectedMTU: api.MTU1500,
+			expectedErr: nil,
+		},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
+			// Create the DB to test the cluster
+			openShiftClustersDatabase, _ := testdatabase.NewFakeOpenShiftClusters()
+			fixture := testdatabase.NewFixture().WithOpenShiftClusters(openShiftClustersDatabase)
+			fixture.AddOpenShiftClusterDocuments(tt.m.doc)
+			err := fixture.Create()
+			if err != nil {
+				t.Fatal(err)
+			}
+			tt.m.db = openShiftClustersDatabase
 
-			tt.m.ensureMTUSize(ctx)
+			err = tt.m.ensureMTUSize(ctx)
+
+			assert.Equal(t, tt.expectedErr, err, "Unexpected error exception")
+			assert.Equal(t, tt.expectedMTU, tt.m.doc.OpenShiftCluster.Properties.NetworkProfile.MTUSize, "MTUSize was not updated as expected exception")
+
 		})
 	}
 }
