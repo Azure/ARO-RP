309531ffeb93b1365ab9104ed6b1f7d4a7d8dbea
Jeremy Facchetti <jfacchet@redhat.com>
2022-04-08 14:57:59 +0200

    moved init in tests instead of before tests

--- pkg/gateway/linkid_test.go
+++ pkg/gateway/linkid_test.go
@@ -16,26 +16,6 @@ import (
 )
 
 func TestGatewayVerification(t *testing.T) {
-	mockController := gomock.NewController(t)
-	defer mockController.Finish()
-	gatewayMap := map[string]*api.Gateway{
-		"1":        {ID: "1", StorageSuffix: "suffix-1", ImageRegistryStorageAccountName: "account1"},
-		"2":        {ID: "2", StorageSuffix: "suffix-2", ImageRegistryStorageAccountName: "account2"},
-		"deleting": {ID: "deleting", StorageSuffix: "suffix-5", ImageRegistryStorageAccountName: "account5", Deleting: true},
-	}
-
-	mockCore := mock_env.NewMockCore(mockController)
-	mockCore.
-		EXPECT().
-		Environment().
-		Return(&azureclient.AROEnvironment{Environment: azure.Environment{StorageEndpointSuffix: "storageEndpointSuffix"}}).
-		AnyTimes()
-
-	gateway := gateway{
-		m:        mock_metrics.NewMockInterface(mockController),
-		gateways: gatewayMap,
-		env:      mockCore,
-	}
 
 	for _, tt := range []struct {
 		name          string
@@ -91,6 +71,27 @@ func TestGatewayVerification(t *testing.T) {
 			deleting: true,
 		}} {
 		t.Run(tt.name, func(t *testing.T) {
+			mockController := gomock.NewController(t)
+			defer mockController.Finish()
+
+			gatewayMap := map[string]*api.Gateway{
+				"1":        {ID: "1", StorageSuffix: "suffix-1", ImageRegistryStorageAccountName: "account1"},
+				"2":        {ID: "2", StorageSuffix: "suffix-2", ImageRegistryStorageAccountName: "account2"},
+				"deleting": {ID: "deleting", StorageSuffix: "suffix-5", ImageRegistryStorageAccountName: "account5", Deleting: true},
+			}
+
+			mockCore := mock_env.NewMockCore(mockController)
+			mockCore.
+				EXPECT().
+				Environment().
+				Return(&azureclient.AROEnvironment{Environment: azure.Environment{StorageEndpointSuffix: "storageEndpointSuffix"}}).
+				AnyTimes()
+
+			gateway := gateway{
+				m:        mock_metrics.NewMockInterface(mockController),
+				gateways: gatewayMap,
+				env:      mockCore,
+			}
 
 			gatewayID, isAllowed, err := gateway.gatewayVerification(tt.host, tt.idParam)
 
