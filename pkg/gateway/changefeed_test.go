e009f842c829700cde46a5793509a81128e034de
Jeremy Facchetti <jfacchet@redhat.com>
2022-04-08 14:50:15 +0200

    refactored tests to use DeepEqual instead of reimplementing it

--- pkg/gateway/changefeed_test.go
+++ pkg/gateway/changefeed_test.go
@@ -5,101 +5,88 @@ package gateway
 
 import (
 	"context"
+	"reflect"
 	"testing"
 	"time"
 
 	"github.com/Azure/ARO-RP/pkg/api"
 	"github.com/Azure/ARO-RP/pkg/database/cosmosdb"
+	"github.com/Azure/ARO-RP/pkg/util/cmp"
 )
 
 func TestUpdateFromIterator(t *testing.T) {
-
 	for _, tt := range []struct {
-		name                   string
-		gatewaysAlreadyPresent []*api.Gateway
-		docsInIterator         []*api.GatewayDocument
-		wantAfterUpdate        map[string]*api.Gateway
+		name           string
+		docsInIterator []*api.GatewayDocument
+		gateways       map[string]*api.Gateway
+		wantGateways   map[string]*api.Gateway
 	}{
 		{
-			name:            "add to empty",
-			wantAfterUpdate: map[string]*api.Gateway{"notDeleting": {ID: "notDeleting"}},
+			name: "add to empty",
 			docsInIterator: []*api.GatewayDocument{
 				{ID: "notDeleting", Gateway: &api.Gateway{ID: "notDeleting"}},
+			},
+			gateways: map[string]*api.Gateway{},
+			wantGateways: map[string]*api.Gateway{
+				"notDeleting": {ID: "notDeleting"},
 			},
 		},
 		{
 			name: "do nothing",
-			wantAfterUpdate: map[string]*api.Gateway{
-				"alreadyPresent": {ID: "alreadyPresent"},
-			},
 			docsInIterator: []*api.GatewayDocument{
 				{ID: "alreadyPresent", Gateway: &api.Gateway{ID: "alreadyPresent"}},
 			},
-			gatewaysAlreadyPresent: []*api.Gateway{
-				{ID: "alreadyPresent"},
+			gateways: map[string]*api.Gateway{
+				"alreadyPresent": {ID: "alreadyPresent"},
+			},
+			wantGateways: map[string]*api.Gateway{
+				"alreadyPresent": {ID: "alreadyPresent"},
 			},
 		},
 		{
 			name: "add to not empty",
-			wantAfterUpdate: map[string]*api.Gateway{
-				"notDeleting":    {ID: "notDeleting"},
-				"alreadyPresent": {ID: "alreadyPresent"},
-			},
 			docsInIterator: []*api.GatewayDocument{
 				{ID: "notDeleting", Gateway: &api.Gateway{ID: "notDeleting"}},
 			},
-			gatewaysAlreadyPresent: []*api.Gateway{
-				{ID: "alreadyPresent"},
+			gateways: map[string]*api.Gateway{
+				"alreadyPresent": {ID: "alreadyPresent"},
+			},
+			wantGateways: map[string]*api.Gateway{
+				"notDeleting":    {ID: "notDeleting"},
+				"alreadyPresent": {ID: "alreadyPresent"},
 			},
 		},
 		{
 			name: "remove existing",
-			wantAfterUpdate: map[string]*api.Gateway{
-				"alreadyPresent": {ID: "alreadyPresent"},
-			},
 			docsInIterator: []*api.GatewayDocument{
 				{ID: "alreadyPresent", Gateway: &api.Gateway{ID: "alreadyPresent"}},
 				{ID: "to_delete", Gateway: &api.Gateway{ID: "to_delete", Deleting: true}},
 			},
-			gatewaysAlreadyPresent: []*api.Gateway{
-				{ID: "alreadyPresent"},
-				{ID: "to_delete"},
+			gateways: map[string]*api.Gateway{
+				"alreadyPresent": {ID: "alreadyPresent"},
+				"to_delete":      {ID: "to_delete"},
+			},
+			wantGateways: map[string]*api.Gateway{
+				"alreadyPresent": {ID: "alreadyPresent"},
 			},
 		},
 	} {
-		gateway := gateway{
-			gateways: make(map[string]*api.Gateway),
-		}
-
-		fakeIterator := cosmosdb.NewFakeGatewayDocumentIterator(tt.docsInIterator, 0)
-
 		t.Run(tt.name, func(t *testing.T) {
 			ticker := time.NewTicker(1)
 			ctx, cancel := context.WithCancel(context.TODO())
 
-			for _, v := range tt.gatewaysAlreadyPresent {
-				gateway.gateways[v.ID] = v
+			gateway := gateway{
+				gateways: tt.gateways,
 			}
+
+			fakeIterator := cosmosdb.NewFakeGatewayDocumentIterator(tt.docsInIterator, 0)
 
 			go gateway.updateFromIterator(ctx, ticker, fakeIterator)
 			time.Sleep(time.Second)
 			cancel()
 
-			//need to check both ways, to ensure that all that are in one
-			//are in the other and vice versa
-			for k, v := range gateway.gateways {
-				if tt.wantAfterUpdate[k] == nil {
-					t.Error(k)
-				} else if v.ID != tt.wantAfterUpdate[k].ID {
-					t.Error(k)
-				}
-			}
-			for k, v := range tt.wantAfterUpdate {
-				if gateway.gateways[k] == nil {
-					t.Error(k)
-				} else if v.ID != gateway.gateways[k].ID {
-					t.Error(k)
-				}
+			if !reflect.DeepEqual(gateway.gateways, tt.wantGateways) {
+				t.Error(cmp.Diff(gateway.gateways, tt.wantGateways))
 			}
 		})
 
