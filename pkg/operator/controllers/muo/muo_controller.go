1d56eaa793f4fdc413064d158ef158ef1acd4709
Jeremy Facchetti <jfacchet@redhat.com>
2022-04-22 17:05:42 +0200

    fixed some style
    cleanup
    
    fixed styling
    
    fixed failing tests because of case on errs

--- pkg/operator/controllers/muo/muo_controller.go
+++ pkg/operator/controllers/muo/muo_controller.go
@@ -141,7 +141,7 @@ func (r *Reconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.
 			return r.deployer.IsReady(ctx)
 		}, timeoutCtx.Done())
 		if err != nil {
-			return reconcile.Result{}, fmt.Errorf("Managed Upgrade Operator deployment timed out on Ready: %w", err)
+			return reconcile.Result{}, fmt.Errorf("managed Upgrade Operator deployment timed out on Ready: %w", err)
 		}
 	} else if strings.EqualFold(managed, "false") {
 		err := r.deployer.Remove(ctx)
