31fe083ff656773c21962b78e79f17f756da41f3
Spencer Amann <samann@redhat.com>
2022-04-25 13:17:53 -0400

    refactor large test into multiple test cases

--- pkg/gateway/gateway_test.go
+++ pkg/gateway/gateway_test.go
@@ -21,7 +21,7 @@ import (
 	utilnet "github.com/Azure/ARO-RP/pkg/util/net"
 )
 
-func TestNewGatway(t *testing.T) {
+func TestNewGateway(t *testing.T) {
 	emptyEnv := &azureclient.AROEnvironment{
 		Environment: azure.Environment{
 			ActiveDirectoryEndpoint:    "",
@@ -112,14 +112,14 @@ func TestNewGatway(t *testing.T) {
 
 			if tt.wantErr != "" {
 				if err == nil {
-					t.Errorf("Expected error: '%s' but did not receive any errors", tt.wantErr)
+					t.Errorf("Expected error: %q but did not receive any errors", tt.wantErr)
 				}
 				if err.Error() != tt.wantErr {
-					t.Errorf("The expected error: '%s' did not match the actual error: '%s'", tt.wantErr, err.Error())
+					t.Errorf("The expected error: %q did not match the actual error: %q", tt.wantErr, err.Error())
 				}
 				return // no need to continue processing the tests
 			} else if err != nil {
-				t.Errorf("Got an unexpected error: %s", err.Error())
+				t.Errorf("Got an unexpected error: %q", err.Error())
 				return
 			}
 
@@ -128,45 +128,109 @@ func TestNewGatway(t *testing.T) {
 			if len(gtwyValue.allowList) != len(tt.expectedAllowList) {
 				t.Errorf("expected %d but got %d domains in the allow list", len(tt.expectedAllowList), len(gtwyValue.allowList))
 			}
+
 			for _, expectedDomain := range tt.expectedAllowList {
 				_, ok := gtwyValue.allowList[expectedDomain]
 				if !ok {
-					t.Errorf("wanted domain: %s but it was missing", expectedDomain)
+					t.Errorf("wanted domain: %q but it was missing", expectedDomain)
 				}
 			}
+		})
+	}
+}
 
-			if gtwyValue.httpl.(*proxyproto.Listener).Listener != httpl {
-				t.Error("httpl was not set properly")
-			}
-			if gtwyValue.httpsl.(*proxyproto.Listener).Listener != httpsl {
-				t.Error("httpsl was not set properly")
-			}
-			if gtwyValue.s.ReadTimeout != 10*time.Second {
-				t.Errorf("gateway http server timeout set to %v when it should be 10s", gtwyValue.s.ReadTimeout)
-			}
-			if gtwyValue.s.IdleTimeout != 2*time.Minute {
-				t.Errorf("gateway http server timeout set to %v when it should be 2m0s", gtwyValue.s.IdleTimeout)
-			}
-			if gtwyValue.s.ErrorLog.Prefix() != "" {
-				t.Errorf("gateway http server ErrorLog should have empty prefix but was found with: %s", gtwyValue.s.ErrorLog.Prefix())
-			}
-			if gtwyValue.s.ErrorLog.Flags() != 0 {
-				t.Errorf("gateway http server ErrorLog should have flags set as 0 but was found with: %d", gtwyValue.s.ErrorLog.Flags())
-			}
-			if gtwyValue.s.BaseContext(httpl) != ctx {
-				t.Error("gateway http server BaseContext should return the ctx of the gateway")
-			}
-			if gtwyValue.s.Handler == nil {
-				t.Error("gateway http server should have its handler set")
-			}
-			if len(gtwyValue.gateways) != 0 {
-				t.Error("gateways should initially be empty")
-			}
-			if gtwyValue.m != metrics {
-				t.Error("metrics was not set properly")
-			}
-			if gtwyValue.ready.Load() != true {
-				t.Error("a new gateway should start as ready")
+func TestNewGatewayDefaultConditions(t *testing.T) {
+	populatedEnv := &azureclient.AROEnvironment{
+		Environment: azure.Environment{
+			ActiveDirectoryEndpoint:    "https://activedirectory/redhattest",
+			ResourceManagerEndpoint:    "https://resourcemanager/redhattest",
+			ContainerRegistryDNSSuffix: "dnsSuffix",
+		},
+	}
+
+	httpl, _ := utilnet.Listen("tcp", ":8080", SocketSize)
+	httpsl, _ := utilnet.Listen("tcp", ":8443", SocketSize)
+	acrResourceID := "/subscriptions/93aeba23-2f76-4307-be82-02921df010cf/resourceGroups/global/providers/Microsoft.ContainerRegistry/registries/resourceName"
+	gatewayDomains := "doMain1,Domain2"
+	ctx := context.Background()
+	baseLog := logrus.NewEntry(logrus.StandardLogger())
+
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+	db := mock_database.NewMockGateway(controller)
+	metrics := mock_metrics.NewMockEmitter(controller)
+	env := mock_env.NewMockCore(controller)
+	env.EXPECT().Environment().AnyTimes().Return(populatedEnv)
+	env.EXPECT().Location().AnyTimes().Return("location")
+
+	gtwy, _ := NewGateway(ctx, env, baseLog, baseLog, db, httpsl, httpl, acrResourceID, gatewayDomains, metrics)
+
+	gateway, _ := gtwy.(*gateway)
+
+	for _, tt := range []struct {
+		name                           string
+		failedGatewayCreationCondition bool
+		failedGatewayCreationReason    string
+	}{
+		{
+			name:                           "httpl is set",
+			failedGatewayCreationCondition: gateway.httpl.(*proxyproto.Listener).Listener != httpl,
+			failedGatewayCreationReason:    "httpl was not set properly",
+		},
+		{
+			name:                           "httpsl is set",
+			failedGatewayCreationCondition: gateway.httpsl.(*proxyproto.Listener).Listener != httpsl,
+			failedGatewayCreationReason:    "httpsl was not set properly",
+		},
+		{
+			name:                           "Read timeout is set",
+			failedGatewayCreationCondition: gateway.s.ReadTimeout != 10*time.Second,
+			failedGatewayCreationReason:    "gateway http server timeout should be set to 10s",
+		},
+		{
+			name:                           "Idle timeout is set",
+			failedGatewayCreationCondition: gateway.s.IdleTimeout != 2*time.Minute,
+			failedGatewayCreationReason:    "gateway http server timeout should be set to 2m0s",
+		},
+		{
+			name:                           "error log prefix is set",
+			failedGatewayCreationCondition: gateway.s.ErrorLog.Prefix() != "",
+			failedGatewayCreationReason:    "gateway error log prefix should be blank",
+		},
+		{
+			name:                           "error log flags are set to 0",
+			failedGatewayCreationCondition: gateway.s.ErrorLog.Flags() != 0,
+			failedGatewayCreationReason:    "gateway error log flags should be set to 0",
+		},
+		{
+			name:                           "baseContext is the context of the gateway",
+			failedGatewayCreationCondition: gateway.s.BaseContext(httpl) != ctx,
+			failedGatewayCreationReason:    "gateway http server BaseContext should return the ctx of the gateway",
+		},
+		{
+			name:                           "gateway http server handler is set",
+			failedGatewayCreationCondition: gateway.s.Handler == nil,
+			failedGatewayCreationReason:    "gateway http server should have its handler set",
+		},
+		{
+			name:                           "a new gateway has an empty list of gateways",
+			failedGatewayCreationCondition: len(gateway.gateways) != 0,
+			failedGatewayCreationReason:    "gateways should be empty",
+		},
+		{
+			name:                           "gateways metrics is set",
+			failedGatewayCreationCondition: gateway.m != metrics,
+			failedGatewayCreationReason:    "gateway metrics are not set properly",
+		},
+		{
+			name:                           "gateway is ready on creation",
+			failedGatewayCreationCondition: gateway.ready.Load() != true,
+			failedGatewayCreationReason:    "gateway is not ready when it should be",
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.failedGatewayCreationCondition {
+				t.Error(tt.failedGatewayCreationReason)
 			}
 		})
 	}
