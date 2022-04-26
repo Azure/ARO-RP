564c9ff0b4991bb4612e6bd52248bc39eea1e76f
Jeremy Facchetti <jfacchet@redhat.com>
2022-04-26 11:56:58 +0200

    changed according to mikalai's remarks

--- pkg/api/validate/dynamic/quota.go
+++ pkg/api/validate/dynamic/quota.go
@@ -11,57 +11,54 @@ import (
 	"github.com/Azure/ARO-RP/pkg/api"
 )
 
-type azureVM struct {
-	Size      api.VMSize
-	CoreCount int
-	Family    string
-}
+func addRequiredResources(requiredResources map[string]int, vmSize api.VMSize, count int) error {
+	vmTypesMap := map[api.VMSize]struct {
+		CoreCount int
+		Family    string
+	}{
+		api.VMSizeStandardD2sV3: {CoreCount: 2, Family: "standardDSv3Family"},
 
-func addRequiredResources(requiredResources map[string]int, vmSize api.VMSize, role string, count int) error {
-	var vmTypesMap map[api.VMSize]azureVM = map[api.VMSize]azureVM{
-		api.VMSizeStandardD2sV3: {Size: api.VMSizeStandardD2sV3, CoreCount: 2, Family: "standardDSv3Family"},
+		api.VMSizeStandardD4asV4:  {CoreCount: 4, Family: "standardDASv4Family"},
+		api.VMSizeStandardD8asV4:  {CoreCount: 8, Family: "standardDASv4Family"},
+		api.VMSizeStandardD16asV4: {CoreCount: 16, Family: "standardDASv4Family"},
+		api.VMSizeStandardD32asV4: {CoreCount: 32, Family: "standardDASv4Family"},
 
-		api.VMSizeStandardD4asV4:  {Size: api.VMSizeStandardD4asV4, CoreCount: 4, Family: "standardDASv4Family"},
-		api.VMSizeStandardD8asV4:  {Size: api.VMSizeStandardD8asV4, CoreCount: 8, Family: "standardDASv4Family"},
-		api.VMSizeStandardD16asV4: {Size: api.VMSizeStandardD16asV4, CoreCount: 16, Family: "standardDASv4Family"},
-		api.VMSizeStandardD32asV4: {Size: api.VMSizeStandardD32asV4, CoreCount: 32, Family: "standardDASv4Family"},
+		api.VMSizeStandardD4sV3:  {CoreCount: 4, Family: "standardDSv3Family"},
+		api.VMSizeStandardD8sV3:  {CoreCount: 8, Family: "standardDSv3Family"},
+		api.VMSizeStandardD16sV3: {CoreCount: 16, Family: "standardDSv3Family"},
+		api.VMSizeStandardD32sV3: {CoreCount: 32, Family: "standardDSv3Family"},
 
-		api.VMSizeStandardD4sV3:  {Size: api.VMSizeStandardD4sV3, CoreCount: 4, Family: "standardDSv3Family"},
-		api.VMSizeStandardD8sV3:  {Size: api.VMSizeStandardD8sV3, CoreCount: 8, Family: "standardDSv3Family"},
-		api.VMSizeStandardD16sV3: {Size: api.VMSizeStandardD16sV3, CoreCount: 16, Family: "standardDSv3Family"},
-		api.VMSizeStandardD32sV3: {Size: api.VMSizeStandardD32sV3, CoreCount: 32, Family: "standardDSv3Family"},
+		api.VMSizeStandardE4sV3:   {CoreCount: 4, Family: "standardESv3Family"},
+		api.VMSizeStandardE8sV3:   {CoreCount: 8, Family: "standardESv3Family"},
+		api.VMSizeStandardE16sV3:  {CoreCount: 16, Family: "standardESv3Family"},
+		api.VMSizeStandardE32sV3:  {CoreCount: 32, Family: "standardESv3Family"},
+		api.VMSizeStandardE64isV3: {CoreCount: 64, Family: "standardESv3Family"},
+		api.VMSizeStandardE64iV3:  {CoreCount: 64, Family: "standardESv3Family"},
 
-		api.VMSizeStandardE4sV3:   {Size: api.VMSizeStandardE4sV3, CoreCount: 4, Family: "standardESv3Family"},
-		api.VMSizeStandardE8sV3:   {Size: api.VMSizeStandardE8sV3, CoreCount: 8, Family: "standardESv3Family"},
-		api.VMSizeStandardE16sV3:  {Size: api.VMSizeStandardE16sV3, CoreCount: 16, Family: "standardESv3Family"},
-		api.VMSizeStandardE32sV3:  {Size: api.VMSizeStandardE32sV3, CoreCount: 32, Family: "standardESv3Family"},
-		api.VMSizeStandardE64isV3: {Size: api.VMSizeStandardE64isV3, CoreCount: 64, Family: "standardESv3Family"},
-		api.VMSizeStandardE64iV3:  {Size: api.VMSizeStandardE64iV3, CoreCount: 64, Family: "standardESv3Family"},
+		api.VMSizeStandardF4sV2:  {CoreCount: 4, Family: "standardFSv2Family"},
+		api.VMSizeStandardF8sV2:  {CoreCount: 8, Family: "standardFSv2Family"},
+		api.VMSizeStandardF16sV2: {CoreCount: 16, Family: "standardFSv2Family"},
+		api.VMSizeStandardF32sV2: {CoreCount: 32, Family: "standardFSv2Family"},
+		api.VMSizeStandardF72sV2: {CoreCount: 72, Family: "standardFSv2Family"},
 
-		api.VMSizeStandardF4sV2:  {Size: api.VMSizeStandardF4sV2, CoreCount: 4, Family: "standardFSv2Family"},
-		api.VMSizeStandardF8sV2:  {Size: api.VMSizeStandardF8sV2, CoreCount: 8, Family: "standardFSv2Family"},
-		api.VMSizeStandardF16sV2: {Size: api.VMSizeStandardF16sV2, CoreCount: 16, Family: "standardFSv2Family"},
-		api.VMSizeStandardF32sV2: {Size: api.VMSizeStandardF32sV2, CoreCount: 32, Family: "standardFSv2Family"},
-		api.VMSizeStandardF72sV2: {Size: api.VMSizeStandardF72sV2, CoreCount: 72, Family: "standardFSv2Family"},
+		api.VMSizeStandardM128ms: {CoreCount: 128, Family: "standardMSFamily"},
+		api.VMSizeStandardG5:     {CoreCount: 32, Family: "standardGFamily"},
+		api.VMSizeStandardGS5:    {CoreCount: 32, Family: "standardGFamily"},
 
-		api.VMSizeStandardM128ms: {Size: api.VMSizeStandardM128ms, CoreCount: 128, Family: "standardMSFamily"},
-		api.VMSizeStandardG5:     {Size: api.VMSizeStandardG5, CoreCount: 32, Family: "standardGFamily"},
-		api.VMSizeStandardGS5:    {Size: api.VMSizeStandardGS5, CoreCount: 32, Family: "standardGFamily"},
-
-		api.VMSizeStandardL4s:    {Size: api.VMSizeStandardL4s, CoreCount: 4, Family: "standardLsv2Family"},
-		api.VMSizeStandardL8s:    {Size: api.VMSizeStandardL8s, CoreCount: 8, Family: "standardLsv2Family"},
-		api.VMSizeStandardL16s:   {Size: api.VMSizeStandardL16s, CoreCount: 16, Family: "standardLsv2Family"},
-		api.VMSizeStandardL32s:   {Size: api.VMSizeStandardL32s, CoreCount: 32, Family: "standardLsv2Family"},
-		api.VMSizeStandardL8sV2:  {Size: api.VMSizeStandardL8sV2, CoreCount: 8, Family: "standardLsv2Family"},
-		api.VMSizeStandardL16sV2: {Size: api.VMSizeStandardL16sV2, CoreCount: 16, Family: "standardLsv2Family"},
-		api.VMSizeStandardL32sV2: {Size: api.VMSizeStandardL32sV2, CoreCount: 32, Family: "standardLsv2Family"},
-		api.VMSizeStandardL48sV2: {Size: api.VMSizeStandardL48sV2, CoreCount: 48, Family: "standardLsv2Family"},
-		api.VMSizeStandardL64sV2: {Size: api.VMSizeStandardL64sV2, CoreCount: 64, Family: "standardLsv2Family"},
+		api.VMSizeStandardL4s:    {CoreCount: 4, Family: "standardLsv2Family"},
+		api.VMSizeStandardL8s:    {CoreCount: 8, Family: "standardLsv2Family"},
+		api.VMSizeStandardL16s:   {CoreCount: 16, Family: "standardLsv2Family"},
+		api.VMSizeStandardL32s:   {CoreCount: 32, Family: "standardLsv2Family"},
+		api.VMSizeStandardL8sV2:  {CoreCount: 8, Family: "standardLsv2Family"},
+		api.VMSizeStandardL16sV2: {CoreCount: 16, Family: "standardLsv2Family"},
+		api.VMSizeStandardL32sV2: {CoreCount: 32, Family: "standardLsv2Family"},
+		api.VMSizeStandardL48sV2: {CoreCount: 48, Family: "standardLsv2Family"},
+		api.VMSizeStandardL64sV2: {CoreCount: 64, Family: "standardLsv2Family"},
 	}
 
 	vm, ok := vmTypesMap[vmSize]
 	if !ok {
-		return fmt.Errorf("unsupported %s VMSize %s", role, vmSize)
+		return fmt.Errorf("unsupported VMSize %s", vmSize)
 	}
 
 	requiredResources["virtualMachines"] += count
@@ -83,14 +80,14 @@ func (dv *dynamic) ValidateQuota(ctx context.Context, oc *api.OpenShiftCluster) 
 	}
 
 	requiredResources := map[string]int{}
-	err := addRequiredResources(requiredResources, oc.Properties.MasterProfile.VMSize, "MasterProfile", 3)
+	err := addRequiredResources(requiredResources, oc.Properties.MasterProfile.VMSize, 3)
 	if err != nil {
 		return err
 	}
 
 	//worker node resource calculation
 	for _, w := range oc.Properties.WorkerProfiles {
-		err = addRequiredResources(requiredResources, w.VMSize, "WorkerProfile", w.Count)
+		err = addRequiredResources(requiredResources, w.VMSize, w.Count)
 		if err != nil {
 			return err
 		}
