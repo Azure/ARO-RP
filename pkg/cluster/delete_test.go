01da0793cad92e80b17beb47dc67f32b18f62b42
bennerv <10840174+bennerv@users.noreply.github.com>
2022-05-04 14:27:24 -0400

    Fix deleteNic when the nic is in failed provisioning state

--- pkg/cluster/delete_test.go
+++ pkg/cluster/delete_test.go
@@ -6,10 +6,11 @@ package cluster
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"testing"
 
-	mgmtfeatures "github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2019-07-01/features"
-	"github.com/Azure/go-autorest/autorest/to"
+	mgmtnetwork "github.com/Azure/azure-sdk-for-go/services/network/mgmt/2020-08-01/network"
+	"github.com/Azure/go-autorest/autorest"
 	"github.com/golang/mock/gomock"
 	"github.com/sirupsen/logrus"
 
@@ -24,50 +25,63 @@ func TestDeleteNic(t *testing.T) {
 	clusterRG := "cluster-rg"
 	nicName := "nic-name"
 	location := "eastus"
+	resourceId := fmt.Sprintf("/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Network/networkInterfaces/%s", subscription, clusterRG, nicName)
+
+	nic := mgmtnetwork.Interface{
+		Name:                      &nicName,
+		Location:                  &location,
+		ID:                        &resourceId,
+		InterfacePropertiesFormat: &mgmtnetwork.InterfacePropertiesFormat{},
+	}
 
 	tests := []struct {
-		name              string
-		mocks             func(*mock_network.MockInterfacesClient)
-		provisioningState *string
-		wantErr           string
+		name    string
+		mocks   func(*mock_network.MockInterfacesClient)
+		wantErr string
 	}{
 		{
 			name: "nic is in succeeded provisioning state",
 			mocks: func(networkInterfaces *mock_network.MockInterfacesClient) {
+				nic.InterfacePropertiesFormat.ProvisioningState = mgmtnetwork.Succeeded
+				networkInterfaces.EXPECT().Get(gomock.Any(), clusterRG, nicName, "").Return(nic, nil)
 				networkInterfaces.EXPECT().DeleteAndWait(gomock.Any(), clusterRG, nicName).Return(nil)
 			},
-			provisioningState: to.StringPtr("SUCCEEDED"),
 		},
 		{
 			name: "nic is in failed provisioning state",
 			mocks: func(networkInterfaces *mock_network.MockInterfacesClient) {
+				nic.InterfacePropertiesFormat.ProvisioningState = mgmtnetwork.Failed
+				networkInterfaces.EXPECT().Get(gomock.Any(), clusterRG, nicName, "").Return(nic, nil)
 				networkInterfaces.EXPECT().CreateOrUpdateAndWait(gomock.Any(), clusterRG, nicName, gomock.Any()).Return(nil)
 				networkInterfaces.EXPECT().DeleteAndWait(gomock.Any(), clusterRG, nicName).Return(nil)
 			},
-			provisioningState: to.StringPtr("FAILED"),
 		},
 		{
 			name: "provisioning state is failed and CreateOrUpdateAndWait returns error",
 			mocks: func(networkInterfaces *mock_network.MockInterfacesClient) {
+				nic.InterfacePropertiesFormat.ProvisioningState = mgmtnetwork.Failed
+				networkInterfaces.EXPECT().Get(gomock.Any(), clusterRG, nicName, "").Return(nic, nil)
 				networkInterfaces.EXPECT().CreateOrUpdateAndWait(gomock.Any(), clusterRG, nicName, gomock.Any()).Return(fmt.Errorf("Failed to update"))
 			},
-			provisioningState: to.StringPtr("FAILED"),
-			wantErr:           "Failed to update",
+			wantErr: "Failed to update",
+		},
+		{
+			name: "nic no longer exists - do nothing",
+			mocks: func(networkInterfaces *mock_network.MockInterfacesClient) {
+				notFound := autorest.DetailedError{
+					StatusCode: http.StatusNotFound,
+				}
+				networkInterfaces.EXPECT().Get(gomock.Any(), clusterRG, nicName, "").Return(nic, notFound)
+			},
 		},
 		{
 			name: "DeleteAndWait returns error",
 			mocks: func(networkInterfaces *mock_network.MockInterfacesClient) {
+				nic.InterfacePropertiesFormat.ProvisioningState = mgmtnetwork.Succeeded
+				networkInterfaces.EXPECT().Get(gomock.Any(), clusterRG, nicName, "").Return(nic, nil)
 				networkInterfaces.EXPECT().DeleteAndWait(gomock.Any(), clusterRG, nicName).Return(fmt.Errorf("Failed to delete"))
 			},
-			provisioningState: to.StringPtr("SUCCEEDED"),
-			wantErr:           "Failed to delete",
-		},
-		{
-			name: "provisioningState is nil",
-			mocks: func(networkInterfaces *mock_network.MockInterfacesClient) {
-				networkInterfaces.EXPECT().DeleteAndWait(gomock.Any(), clusterRG, nicName).Return(nil)
-			},
-			provisioningState: nil,
+			wantErr: "Failed to delete",
 		},
 	}
 
@@ -97,13 +111,7 @@ func TestDeleteNic(t *testing.T) {
 				interfaces: networkInterfaces,
 			}
 
-			resource := mgmtfeatures.GenericResourceExpanded{
-				Name:              to.StringPtr(nicName),
-				ID:                to.StringPtr(fmt.Sprintf("/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Network/networkInterfaces/%s", subscription, clusterRG, nicName)),
-				ProvisioningState: tt.provisioningState,
-			}
-
-			err := m.deleteNic(ctx, resource)
+			err := m.deleteNic(ctx, nicName)
 			if err != nil && err.Error() != tt.wantErr {
 				t.Errorf("got error: '%s'", err.Error())
 			}
