d4cc19514438d88e8450a57151af7c23b91c3413
Aldo Fuster Turpin <afustert@redhat.com>
2022-05-06 15:13:12 +0200

    refactor: apply Guard clauses in _validators.py to simplify code

--- python/az/aro/azext_aro/_validators.py
+++ python/az/aro/azext_aro/_validators.py
@@ -24,70 +24,76 @@ logger = get_logger(__name__)
 def validate_cidr(key):
     def _validate_cidr(namespace):
         cidr = getattr(namespace, key)
-        if cidr is not None:
-            try:
-                ipaddress.IPv4Network(cidr)
-            except ValueError as e:
-                raise InvalidArgumentValueError(f"Invalid --{key.replace('_', '-')} '{cidr}'.") from e
+        if cidr is None:
+            return
+        try:
+            ipaddress.IPv4Network(cidr)
+        except ValueError as e:
+            raise InvalidArgumentValueError(f"Invalid --{key.replace('_', '-')} '{cidr}'.") from e
 
     return _validate_cidr
 
 
 def validate_client_id(namespace):
-    if namespace.client_id is not None:
-        try:
-            uuid.UUID(namespace.client_id)
-        except ValueError as e:
-            raise InvalidArgumentValueError(f"Invalid --client-id '{namespace.client_id}'.") from e
+    if namespace.client_id is None:
+        return
+    try:
+        uuid.UUID(namespace.client_id)
+    except ValueError as e:
+        raise InvalidArgumentValueError(f"Invalid --client-id '{namespace.client_id}'.") from e
 
-        if namespace.client_secret is None or not str(namespace.client_secret):
-            raise RequiredArgumentMissingError('Must specify --client-secret with --client-id.')
+    if namespace.client_secret is None or not str(namespace.client_secret):
+        raise RequiredArgumentMissingError('Must specify --client-secret with --client-id.')
 
 
 def validate_client_secret(isCreate):
     def _validate_client_secret(namespace):
-        if isCreate and namespace.client_secret is not None:
-            if namespace.client_id is None or not str(namespace.client_id):
-                raise RequiredArgumentMissingError('Must specify --client-id with --client-secret.')
+        if not isCreate or namespace.client_secret is None:
+            return
+        if namespace.client_id is None or not str(namespace.client_id):
+            raise RequiredArgumentMissingError('Must specify --client-id with --client-secret.')
 
     return _validate_client_secret
 
 
 def validate_cluster_resource_group(cmd, namespace):
-    if namespace.cluster_resource_group is not None:
-        client = get_mgmt_service_client(
-            cmd.cli_ctx, ResourceType.MGMT_RESOURCE_RESOURCES)
+    if namespace.cluster_resource_group is None:
+        return
+    client = get_mgmt_service_client(
+        cmd.cli_ctx, ResourceType.MGMT_RESOURCE_RESOURCES)
 
-        if client.resource_groups.check_existence(namespace.cluster_resource_group):
-            raise InvalidArgumentValueError(
-                f"Invalid --cluster-resource-group '{namespace.cluster_resource_group}':"
-                " resource group must not exist.")
+    if client.resource_groups.check_existence(namespace.cluster_resource_group):
+        raise InvalidArgumentValueError(
+            f"Invalid --cluster-resource-group '{namespace.cluster_resource_group}':"
+            " resource group must not exist.")
 
 
 def validate_disk_encryption_set(cmd, namespace):
-    if namespace.disk_encryption_set is not None:
-        if not is_valid_resource_id(namespace.disk_encryption_set):
-            raise InvalidArgumentValueError(
-                f"Invalid --disk-encryption-set '{namespace.disk_encryption_set}', has to be a resource ID.")
+    if namespace.disk_encryption_set is None:
+        return
+    if not is_valid_resource_id(namespace.disk_encryption_set):
+        raise InvalidArgumentValueError(
+            f"Invalid --disk-encryption-set '{namespace.disk_encryption_set}', has to be a resource ID.")
 
-        desid = parse_resource_id(namespace.disk_encryption_set)
-        compute_client = get_mgmt_service_client(cmd.cli_ctx, ResourceType.MGMT_COMPUTE)
-        try:
-            compute_client.disk_encryption_sets.get(resource_group_name=desid['resource_group'],
-                                                    disk_encryption_set_name=desid['name'])
-        except CloudError as err:
-            raise InvalidArgumentValueError(
-                f"Invald --disk-encryption-set, error when getting '{namespace.disk_encryption_set}':"
-                f" {str(err)}") from err
+    desid = parse_resource_id(namespace.disk_encryption_set)
+    compute_client = get_mgmt_service_client(cmd.cli_ctx, ResourceType.MGMT_COMPUTE)
+    try:
+        compute_client.disk_encryption_sets.get(resource_group_name=desid['resource_group'],
+                                                disk_encryption_set_name=desid['name'])
+    except CloudError as err:
+        raise InvalidArgumentValueError(
+            f"Invald --disk-encryption-set, error when getting '{namespace.disk_encryption_set}':"
+            f" {str(err)}") from err
 
 
 def validate_domain(namespace):
-    if namespace.domain is not None:
-        if not re.match(r'^' +
-                        r'([a-z0-9]|[a-z0-9][-a-z0-9]{0,61}[a-z0-9])' +
-                        r'(\.([a-z0-9]|[a-z0-9][-a-z0-9]{0,61}[a-z0-9]))*' +
-                        r'$', namespace.domain):
-            raise InvalidArgumentValueError(f"Invalid --domain '{namespace.domain}'.")
+    if namespace.domain is None:
+        return
+    if not re.match(r'^' +
+                    r'([a-z0-9]|[a-z0-9][-a-z0-9]{0,61}[a-z0-9])' +
+                    r'(\.([a-z0-9]|[a-z0-9][-a-z0-9]{0,61}[a-z0-9]))*' +
+                    r'$', namespace.domain):
+        raise InvalidArgumentValueError(f"Invalid --domain '{namespace.domain}'.")
 
 
 def validate_pull_secret(namespace):
@@ -97,20 +103,21 @@ def validate_pull_secret(namespace):
             "Red Hat or from certified partners."
 
         logger.warning(warning)
+        return
 
-    else:
-        try:
-            if not isinstance(json.loads(namespace.pull_secret), dict):
-                raise Exception()
-        except Exception as e:
-            raise InvalidArgumentValueError("Invalid --pull-secret.") from e
+    try:
+        if not isinstance(json.loads(namespace.pull_secret), dict):
+            raise Exception()
+    except Exception as e:
+        raise InvalidArgumentValueError("Invalid --pull-secret.") from e
 
 
 def validate_sdn(namespace):
-    if namespace.software_defined_network is not None:
-        if namespace.software_defined_network not in ['OVNKubernetes', 'OpenshiftSDN']:
-            raise InvalidArgumentValueError(
-                f"Invalid --software-defined-network '{namespace.software_defined_network}'.")
+    if namespace.software_defined_network is None:
+        return
+    if namespace.software_defined_network not in ['OVNKubernetes', 'OpenshiftSDN']:
+        raise InvalidArgumentValueError(
+            f"Invalid --software-defined-network '{namespace.software_defined_network}'.")
 
 
 def validate_subnet(key):
@@ -185,10 +192,11 @@ def validate_subnets(master_subnet, worker_subnet):
 def validate_visibility(key):
     def _validate_visibility(namespace):
         visibility = getattr(namespace, key)
-        if visibility is not None:
-            visibility = visibility.capitalize()
-            if visibility not in ['Private', 'Public']:
-                raise InvalidArgumentValueError(f"Invalid --{key.replace('_', '-')} '{visibility}'.")
+        if visibility is None:
+            return
+        visibility = visibility.capitalize()
+        if visibility not in ['Private', 'Public']:
+            raise InvalidArgumentValueError(f"Invalid --{key.replace('_', '-')} '{visibility}'.")
 
     return _validate_visibility
 
@@ -196,15 +204,16 @@ def validate_visibility(key):
 def validate_vnet(cmd, namespace):
     validate_vnet_resource_group_name(namespace)
 
-    if namespace.vnet:
-        if not is_valid_resource_id(namespace.vnet):
-            namespace.vnet = resource_id(
-                subscription=get_subscription_id(cmd.cli_ctx),
-                resource_group=namespace.vnet_resource_group_name,
-                namespace='Microsoft.Network',
-                type='virtualNetworks',
-                name=namespace.vnet,
-            )
+    if not namespace.vnet:
+        return
+    if not is_valid_resource_id(namespace.vnet):
+        namespace.vnet = resource_id(
+            subscription=get_subscription_id(cmd.cli_ctx),
+            resource_group=namespace.vnet_resource_group_name,
+            namespace='Microsoft.Network',
+            type='virtualNetworks',
+            name=namespace.vnet,
+        )
 
 
 def validate_vnet_resource_group_name(namespace):
@@ -213,18 +222,21 @@ def validate_vnet_resource_group_name(namespace):
 
 
 def validate_worker_count(namespace):
-    if namespace.worker_count:
-        if namespace.worker_count < 3:
-            raise InvalidArgumentValueError('--worker-count must be greater than or equal to 3.')
+    if not namespace.worker_count:
+        return 
+    if namespace.worker_count < 3:
+        raise InvalidArgumentValueError('--worker-count must be greater than or equal to 3.')
 
 
 def validate_worker_vm_disk_size_gb(namespace):
-    if namespace.worker_vm_disk_size_gb:
-        if namespace.worker_vm_disk_size_gb < 128:
-            raise InvalidArgumentValueError('--worker-vm-disk-size-gb must be greater than or equal to 128.')
+    if not namespace.worker_vm_disk_size_gb:
+        return
+    if namespace.worker_vm_disk_size_gb < 128:
+        raise InvalidArgumentValueError('--worker-vm-disk-size-gb must be greater than or equal to 128.')
 
 
 def validate_refresh_cluster_credentials(namespace):
-    if namespace.refresh_cluster_credentials:
-        if namespace.client_secret is not None or namespace.client_id is not None:
-            raise RequiredArgumentMissingError('--client-id and --client-secret must be not set with --refresh-credentials.')  # pylint: disable=line-too-long
+    if not namespace.refresh_cluster_credentials:
+        return
+    if namespace.client_secret is not None or namespace.client_id is not None:
+        raise RequiredArgumentMissingError('--client-id and --client-secret must be not set with --refresh-credentials.')  # pylint: disable=line-too-long
