4b29316dc9d3833c5ccfdacbc8dd94793e288a5c
Aldo Fuster Turpin <afustert@redhat.com>
2022-05-16 09:29:55 +0200

    Update hack/lint-go.sh to exit when golangci-lint is not found
    Co-authored-by: Jeremy Facchetti <facchettos@gmail.com>

--- hack/lint-go.sh
+++ hack/lint-go.sh
@@ -1,6 +1,7 @@
 if ! command -v golangci-lint &> /dev/null
 then
     echo "WARNING golangci-lint could not be found, golang lint skipped.To install it: https://golangci-lint.run/usage/install/#local-installation"
+    exit 1
 else
     golangci-lint run
 fi
