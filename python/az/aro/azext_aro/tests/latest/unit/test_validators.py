0287ac7798f35920340ee0d915aeb3729a5ba7ca
Aldo Fuster Turpin <afustert@redhat.com>
2022-05-18 14:28:28 +0200

    refactor test_validator.py to use classes instead of namedtuples. Use mocks instead of specific defined classes

--- python/az/aro/azext_aro/tests/latest/unit/test_validators.py
+++ python/az/aro/azext_aro/tests/latest/unit/test_validators.py
@@ -1,61 +1,51 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the Apache License 2.0.
 
-from collections import namedtuple
-from typing import List
+from typing import Dict, List
 import unittest
 from unittest.mock import Mock, patch
 from azext_aro._validators import validate_cidr, validate_client_id, validate_client_secret, validate_cluster_resource_group, validate_disk_encryption_set
 from azure.cli.core.azclierror import InvalidArgumentValueError, InvalidArgumentValueError, RequiredArgumentMissingError
 
 
-class Namespace:
-    def __init__(self, client_id=None, client_secret=None, cluster_resource_group=None, disk_encryption_set=None):
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.cluster_resource_group = cluster_resource_group
-        self.disk_encryption_set = disk_encryption_set
-
-
 class TestValidators(unittest.TestCase):
 
     def test_validate_cidr(self):
-        class Dummyclass:
-            def __init__(self, key=None):
-                self.key = key
+        class TestData():
+            def __init__(self, test_description: str = None, dummyclass: Mock = None, attribute_to_get_from_object: str = None, expected_exception: Exception = None) -> None:
+                self.test_description = test_description
+                self.dummyclass = dummyclass
+                self.attribute_to_get_from_object = attribute_to_get_from_object
+                self.expected_exception = expected_exception
 
-        namedtuple_name = 'Testdata'
-        namedtuple_attributes = ["test_description", 'dummyclass', 'attribute_to_get_from_object', "expected_exception"]
-        TestData = namedtuple(namedtuple_name, namedtuple_attributes)
-
-        testcases: List[namedtuple] = [
+        testcases: List[TestData] = [
             TestData(
                 test_description="should not raise exception when valid IPv4 address",
-                dummyclass=Dummyclass('192.168.0.0/28'),
+                dummyclass=Mock(key='192.168.0.0/28'),
                 attribute_to_get_from_object='key',
                 expected_exception=None
             ),
             TestData(
                 test_description="should raise InvalidArgumentValueError when non valid IPv4 address due to beeing a simple string",
-                dummyclass=Dummyclass('this is an invalid network'),
+                dummyclass=Mock(key='this is an invalid network'),
                 attribute_to_get_from_object='key',
                 expected_exception=InvalidArgumentValueError
             ),
             TestData(
                 test_description="should raise InvalidArgumentValueError when non valid IPv4 address due to invalid network ID",
-                dummyclass=Dummyclass('192.168.0.0.0.0/28'),
+                dummyclass=Mock(key='192.168.0.0.0.0/28'),
                 attribute_to_get_from_object='key',
                 expected_exception=InvalidArgumentValueError
             ),
             TestData(
                 test_description="should raise InvalidArgumentValueError when non valid IPv4 address due to invalid hostID",
-                dummyclass=Dummyclass('192.168.0.0.0.0/2888'),
+                dummyclass=Mock(key='192.168.0.0.0.0/2888'),
                 attribute_to_get_from_object='key',
                 expected_exception=InvalidArgumentValueError
             ),
             TestData(
                 test_description="should not raise exception when IPv4 address is None ",
-                dummyclass=Dummyclass(None),
+                dummyclass=Mock(key=None),
                 attribute_to_get_from_object='key',
                 expected_exception=None
             )
@@ -70,35 +60,37 @@ class TestValidators(unittest.TestCase):
                     validate_cidr_fn(tc.dummyclass)
 
     def test_validate_client_id(self):
-        namedtuple_name = 'Testdata'
-        namedtuple_attributes = ["test_description", 'namespace', "expected_exception"]
-        TestData = namedtuple(namedtuple_name, namedtuple_attributes)
+        class TestData():
+            def __init__(self, test_description: str = None, namespace: Mock = None, expected_exception: Exception = None) -> None:
+                self.test_description = test_description
+                self.namespace = namespace
+                self.expected_exception = expected_exception
 
-        testcases: List[namedtuple] = [
+        testcases: List[TestData] = [
             TestData(
-                "should return None when namespace.client_id is None",
-                Namespace(client_id=None),
-                None
+                test_description="should return None when namespace.client_id is None",
+                namespace=Mock(client_id=None),
+                expected_exception=None
             ),
             TestData(
-                "should raise InvalidArgumentValueError when it can not create a UUID from namespace.client_id",
-                Namespace(client_id="invalid_client_id"),
-                InvalidArgumentValueError
+                test_description="should raise InvalidArgumentValueError when it can not create a UUID from namespace.client_id",
+                namespace=Mock(client_id="invalid_client_id"),
+                expected_exception=InvalidArgumentValueError
             ),
             TestData(
-                "should raise RequiredArgumentMissingError when can not crate a string representation from namespace.client_secret because is None",
-                Namespace(client_id="12345678123456781234567812345678", client_secret=None),
-                RequiredArgumentMissingError
+                test_description="should raise RequiredArgumentMissingError when can not crate a string representation from namespace.client_secret because is None",
+                namespace=Mock(client_id="12345678123456781234567812345678", client_secret=None),
+                expected_exception=RequiredArgumentMissingError
             ),
             TestData(
-                "should raise RequiredArgumentMissingError when can not crate a string representation from namespace.client_secret because it is an empty string",
-                Namespace(client_id="12345678123456781234567812345678", client_secret=""),
-                RequiredArgumentMissingError
+                test_description="should raise RequiredArgumentMissingError when can not crate a string representation from namespace.client_secret because it is an empty string",
+                namespace=Mock(client_id="12345678123456781234567812345678", client_secret=""),
+                expected_exception=RequiredArgumentMissingError
             ),
             TestData(
-                "should not raise exception when namespace.client_id is a valid input for creating a UUID and namespace.client_secret has a valid str representation",
-                Namespace(client_id="12345678123456781234567812345678", client_secret="12345"),
-                None
+                test_description="should not raise exception when namespace.client_id is a valid input for creating a UUID and namespace.client_secret has a valid str representation",
+                namespace=Mock(client_id="12345678123456781234567812345678", client_secret="12345"),
+                expected_exception=None
             )
         ]
 
@@ -110,39 +102,42 @@ class TestValidators(unittest.TestCase):
                     validate_client_id(tc.namespace)
 
     def test_validate_client_secret(self):
-        namedtuple_name = 'Testdata'
-        namedtuple_attributes = ["test_description", "isCreate", 'namespace', "expected_exception"]
-        TestData = namedtuple(namedtuple_name, namedtuple_attributes)
+        class TestData():
+            def __init__(self, test_description: str = None, isCreate: bool = None, namespace: Mock = None, expected_exception: Exception = None) -> None:
+                self.test_description = test_description
+                self.isCreate = isCreate
+                self.namespace = namespace
+                self.expected_exception = expected_exception
 
-        testcases: List[namedtuple] = [
+        testcases: List[TestData] = [
             TestData(
                 test_description="should not raise exception when isCreate is false",
                 isCreate=False,
-                namespace=Namespace(client_id=None),
+                namespace=Mock(client_id=None),
                 expected_exception=None
             ),
             TestData(
                 test_description="should not raise exception when namespace.client_secret is None",
                 isCreate=True,
-                namespace=Namespace(client_secret=None),
+                namespace=Mock(client_secret=None),
                 expected_exception=None
             ),
             TestData(
                 test_description="should raise RequiredArgumentMissingError exception when namespace.client_id is None and client_secret is not None",
                 isCreate=True,
-                namespace=Namespace(client_id=None, client_secret="123"),
+                namespace=Mock(client_id=None, client_secret="123"),
                 expected_exception=RequiredArgumentMissingError
             ),
             TestData(
                 test_description="should raise RequiredArgumentMissingError exception when can not crate a string representation from namespace.client_id because it is empty",
                 isCreate=True,
-                namespace=Namespace(client_id="", client_secret="123"),
+                namespace=Mock(client_id="", client_secret="123"),
                 expected_exception=RequiredArgumentMissingError
             ),
             TestData(
                 test_description="should raise RequiredArgumentMissingError exception when can not crate a string representation from namespace.client_id because it is None",
                 isCreate=True,
-                namespace=Namespace(client_id=None, client_secret="123"),
+                namespace=Mock(client_id=None, client_secret="123"),
                 expected_exception=RequiredArgumentMissingError
             )
         ]
@@ -157,30 +152,34 @@ class TestValidators(unittest.TestCase):
 
     @patch('azext_aro._validators.get_mgmt_service_client')
     def test_validate_cluster_resource_group(self, get_mgmt_service_client_mock):
-        namedtuple_name = 'Testdata'
-        namedtuple_attributes = ["test_description", "client_mock", "cmd_mock", 'namespace', "expected_exception"]
-        TestData = namedtuple(namedtuple_name, namedtuple_attributes)
+        class TestData():
+            def __init__(self, test_description: str = None, client_mock: Mock = None, cmd_mock: Mock = None, namespace: Mock = None, expected_exception: Exception = None) -> None:
+                self.test_description = test_description
+                self.client_mock = client_mock
+                self.cmd_mock = cmd_mock
+                self.namespace = namespace
+                self.expected_exception = expected_exception
 
-        testcases: List[namedtuple] = [
+        testcases: List[TestData] = [
             TestData(
                 test_description="should not raise any exception when namespace.cluster_resource_group is None",
                 client_mock=None,
                 cmd_mock=None,
-                namespace=Namespace(cluster_resource_group=None),
+                namespace=Mock(cluster_resource_group=None),
                 expected_exception=None
             ),
             TestData(
                 test_description="should raise InvalidArgumentValueError exception when namespace.cluster_resource_group is not None and resource group exists in the client returned by get_mgmt_service_client",
                 client_mock=Mock(**{"resource_groups.check_existence.return_value": True}),
                 cmd_mock=Mock(cli_ctx=None),
-                namespace=Namespace(cluster_resource_group="some_resource_group"),
+                namespace=Mock(cluster_resource_group="some_resource_group"),
                 expected_exception=InvalidArgumentValueError
             ),
             TestData(
                 test_description="should not raise any exception when namespace.cluster_resource_group is not None and resource group does not exists in the client returned by get_mgmt_service_client",
                 client_mock=Mock(**{"resource_groups.check_existence.return_value": False}),
                 cmd_mock=Mock(cli_ctx=None),
-                namespace=Namespace(cluster_resource_group="some_resource_group"),
+                namespace=Mock(cluster_resource_group="some_resource_group"),
                 expected_exception=None
             ),
         ]
@@ -198,15 +197,21 @@ class TestValidators(unittest.TestCase):
     @patch('azext_aro._validators.parse_resource_id')
     @patch('azext_aro._validators.is_valid_resource_id')
     def test_validate_disk_encryption_set(self, is_valid_resource_id_mock, parse_resource_id_mock, get_mgmt_service_client_mock):
-        namedtuple_name = 'Testdata'
-        namedtuple_attributes = ["test_description", "cmd_mock", "namespace", "is_valid_resource_id_return_value", "compute_client_mock", "expected_exception", "parse_resource_id_mock_return_value"]
-        TestData = namedtuple(namedtuple_name, namedtuple_attributes)
+        class TestData():
+            def __init__(self, test_description: str = None, cmd_mock: Mock = None, namespace: Mock = None, is_valid_resource_id_return_value: bool = None, compute_client_mock: Mock = None, expected_exception: Exception = None, parse_resource_id_mock_return_value: Dict = None) -> None:
+                self.test_description = test_description
+                self.cmd_mock = cmd_mock
+                self.namespace = namespace
+                self.is_valid_resource_id_return_value = is_valid_resource_id_return_value
+                self.compute_client_mock = compute_client_mock
+                self.expected_exception = expected_exception
+                self.parse_resource_id_mock_return_value = parse_resource_id_mock_return_value
 
-        testcases: List[namedtuple] = [
+        testcases: List[TestData] = [
             TestData(
                 test_description="should not raise any exception when namespace.disk_encryption_set is None",
                 cmd_mock=None,
-                namespace=Namespace(disk_encryption_set=None),
+                namespace=Mock(disk_encryption_set=None),
                 is_valid_resource_id_return_value=None,
                 compute_client_mock=None,
                 expected_exception=None,
@@ -215,7 +220,7 @@ class TestValidators(unittest.TestCase):
             TestData(
                 test_description="should raise InvalidArgumentValueError exception when namespace.disk_encryption_set is not None and is_valid_resource_id(namespace.disk_encryption_set) returns False",
                 cmd_mock=None,
-                namespace=Namespace(disk_encryption_set="something different than None"),
+                namespace=Mock(disk_encryption_set="something different than None"),
                 is_valid_resource_id_return_value=False,
                 compute_client_mock=None,
                 expected_exception=InvalidArgumentValueError,
@@ -224,7 +229,7 @@ class TestValidators(unittest.TestCase):
             TestData(
                 test_description="should not raise any exception when compute_client.disk_encryption_sets.get() not raises CludError exception",
                 cmd_mock=Mock(cli_ctx=None),
-                namespace=Namespace(disk_encryption_set="something different than None"),
+                namespace=Mock(disk_encryption_set="something different than None"),
                 is_valid_resource_id_return_value=True,
                 compute_client_mock=Mock(),
                 expected_exception=None,
