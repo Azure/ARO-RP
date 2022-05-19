63bd1bb21f82a0c206a9bc7338615e8f6ed1a680
Aldo Fuster Turpin <afustert@redhat.com>
2022-05-19 10:55:50 +0200

    refactor (simplify code): remove explicit assignemnt to None when it is the default value

--- python/az/aro/azext_aro/tests/latest/unit/test_validators.py
+++ python/az/aro/azext_aro/tests/latest/unit/test_validators.py
@@ -23,7 +23,6 @@ class TestValidators(unittest.TestCase):
                 test_description="should not raise exception when valid IPv4 address",
                 dummyclass=Mock(key='192.168.0.0/28'),
                 attribute_to_get_from_object='key',
-                expected_exception=None
             ),
             TestData(
                 test_description="should raise InvalidArgumentValueError when non valid IPv4 address due to beeing a simple string",
@@ -46,8 +45,7 @@ class TestValidators(unittest.TestCase):
             TestData(
                 test_description="should not raise exception when IPv4 address is None ",
                 dummyclass=Mock(key=None),
-                attribute_to_get_from_object='key',
-                expected_exception=None
+                attribute_to_get_from_object='key'
             )
         ]
 
@@ -69,8 +67,7 @@ class TestValidators(unittest.TestCase):
         testcases: List[TestData] = [
             TestData(
                 test_description="should return None when namespace.client_id is None",
-                namespace=Mock(client_id=None),
-                expected_exception=None
+                namespace=Mock(client_id=None)
             ),
             TestData(
                 test_description="should raise InvalidArgumentValueError when it can not create a UUID from namespace.client_id",
@@ -89,8 +86,7 @@ class TestValidators(unittest.TestCase):
             ),
             TestData(
                 test_description="should not raise exception when namespace.client_id is a valid input for creating a UUID and namespace.client_secret has a valid str representation",
-                namespace=Mock(client_id="12345678123456781234567812345678", client_secret="12345"),
-                expected_exception=None
+                namespace=Mock(client_id="12345678123456781234567812345678", client_secret="12345")
             )
         ]
 
@@ -113,14 +109,12 @@ class TestValidators(unittest.TestCase):
             TestData(
                 test_description="should not raise exception when isCreate is false",
                 isCreate=False,
-                namespace=Mock(client_id=None),
-                expected_exception=None
+                namespace=Mock(client_id=None)
             ),
             TestData(
                 test_description="should not raise exception when namespace.client_secret is None",
                 isCreate=True,
-                namespace=Mock(client_secret=None),
-                expected_exception=None
+                namespace=Mock(client_secret=None)
             ),
             TestData(
                 test_description="should raise RequiredArgumentMissingError exception when namespace.client_id is None and client_secret is not None",
@@ -163,10 +157,7 @@ class TestValidators(unittest.TestCase):
         testcases: List[TestData] = [
             TestData(
                 test_description="should not raise any exception when namespace.cluster_resource_group is None",
-                client_mock=None,
-                cmd_mock=None,
-                namespace=Mock(cluster_resource_group=None),
-                expected_exception=None
+                namespace=Mock(cluster_resource_group=None)
             ),
             TestData(
                 test_description="should raise InvalidArgumentValueError exception when namespace.cluster_resource_group is not None and resource group exists in the client returned by get_mgmt_service_client",
@@ -179,8 +170,7 @@ class TestValidators(unittest.TestCase):
                 test_description="should not raise any exception when namespace.cluster_resource_group is not None and resource group does not exists in the client returned by get_mgmt_service_client",
                 client_mock=Mock(**{"resource_groups.check_existence.return_value": False}),
                 cmd_mock=Mock(cli_ctx=None),
-                namespace=Mock(cluster_resource_group="some_resource_group"),
-                expected_exception=None
+                namespace=Mock(cluster_resource_group="some_resource_group")
             ),
         ]
 
@@ -210,21 +200,13 @@ class TestValidators(unittest.TestCase):
         testcases: List[TestData] = [
             TestData(
                 test_description="should not raise any exception when namespace.disk_encryption_set is None",
-                cmd_mock=None,
-                namespace=Mock(disk_encryption_set=None),
-                is_valid_resource_id_return_value=None,
-                compute_client_mock=None,
-                expected_exception=None,
-                parse_resource_id_mock_return_value=None
+                namespace=Mock(disk_encryption_set=None)
             ),
             TestData(
                 test_description="should raise InvalidArgumentValueError exception when namespace.disk_encryption_set is not None and is_valid_resource_id(namespace.disk_encryption_set) returns False",
-                cmd_mock=None,
                 namespace=Mock(disk_encryption_set="something different than None"),
                 is_valid_resource_id_return_value=False,
-                compute_client_mock=None,
                 expected_exception=InvalidArgumentValueError,
-                parse_resource_id_mock_return_value=None
             ),
             TestData(
                 test_description="should not raise any exception when compute_client.disk_encryption_sets.get() not raises CludError exception",
@@ -232,7 +214,6 @@ class TestValidators(unittest.TestCase):
                 namespace=Mock(disk_encryption_set="something different than None"),
                 is_valid_resource_id_return_value=True,
                 compute_client_mock=Mock(),
-                expected_exception=None,
                 parse_resource_id_mock_return_value={"resource_group": None, "name": None}
             )
         ]
@@ -242,6 +223,7 @@ class TestValidators(unittest.TestCase):
 
             if tc.compute_client_mock is not None:
                 tc.compute_client_mock.get.return_value = None
+                get_mgmt_service_client_mock.return_value = tc.compute_client_mock
 
             if tc.expected_exception is None:
                 validate_disk_encryption_set(tc.cmd_mock, tc.namespace)
