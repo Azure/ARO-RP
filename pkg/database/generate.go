96a1386aaf6d0a7505ffb1b2be94e48a60dee19e
Amber Brown <ambrown@redhat.com>
2022-06-21 17:53:00 +1000

    fix the portal API tests to use the UUID generation where needed, and don't use the mock db for absolutely nothing

--- pkg/database/generate.go
+++
@@ -1,7 +0,0 @@
-package database
-
-// Copyright (c) Microsoft Corporation.
-// Licensed under the Apache License 2.0.
-
-//go:generate go run ../../vendor/github.com/golang/mock/mockgen -destination=../util/mocks/$GOPACKAGE/$GOPACKAGE.go github.com/Azure/ARO-RP/pkg/$GOPACKAGE Gateway
-//go:generate go run ../../vendor/golang.org/x/tools/cmd/goimports -local=github.com/Azure/ARO-RP -e -w ../util/mocks/$GOPACKAGE/$GOPACKAGE.go
