627431f721acdbda2d3b8c5b19cfc50eef31e99b
Jory Horeman <jhoreman@redhat.com>
2022-06-22 15:35:11 -0700

    setting password auth to true through generator resources

--- pkg/deploy/generator/resources_gateway.go
+++ pkg/deploy/generator/resources_gateway.go
@@ -644,6 +644,9 @@ for scan in baseline clamav software; do
   /usr/local/bin/azsecd config -s $scan -d P1D
 done
 
+# We need to manually set PasswordAuthentication to true in order for the VMSS Access JIT to work
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
 restorecon -RF /var/log/*
 
 (sleep 30; reboot) &
