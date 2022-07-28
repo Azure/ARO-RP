4b33d0c1778382fba03826ec91cd1a5293ea771b
Anshul Verma <anshulvermapatel@gmail.com>
2022-07-28 21:58:53 +0530

    Removed the TODO comment

--- pkg/api/v20220904/openshiftinstallversions_convert.go
+++ pkg/api/v20220904/openshiftinstallversions_convert.go
@@ -7,12 +7,6 @@ import "github.com/Azure/ARO-RP/pkg/api"
 
 type installOpenShiftVersionsConverter struct{}
 
-// TODO: Change the comment
-// OpenShiftClusterCredentialsToExternal returns a new external representation
-// of the internal object, reading from the subset of the internal object's
-// fields that appear in the external representation.  ToExternal does not
-// modify its argument; there is no pointer aliasing between the passed and
-// returned objects.
 func (*installOpenShiftVersionsConverter) ToExternal(iov *api.InstallOpenShiftVersions) interface{} {
 	return iov
 }
