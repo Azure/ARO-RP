572e9fa209f89d3222828ef21178a5032953d81f
Petr Kotas <pkotas@redhat.com>
2022-08-02 17:58:34 +0200

    Fix the aks name in get kubeconfig (#2296)
    Signed-off-by: Petr Kotas <pkotas@redhat.com>

--- hack/get-admin-aks-kubeconfig.sh
+++ hack/get-admin-aks-kubeconfig.sh
@@ -7,7 +7,7 @@ fi
 
 [ -f aks.kubeconfig ] && rm -f aks.kubeconfig
 
-if az aks get-credentials --admin -g "$RESOURCEGROUP" -n aro-aks-cluster --public-fqdn -f aks.kubeconfig 2>/dev/null; then
+if az aks get-credentials --admin -g "$RESOURCEGROUP" -n aro-aks-cluster-001 --public-fqdn -f aks.kubeconfig 2>/dev/null; then
     chmod 600 aks.kubeconfig
 else
     echo "Error generating AKS kubeconfig"
