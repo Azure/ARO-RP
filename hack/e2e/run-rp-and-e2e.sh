75f9d2cf6d5f092444debcb3582f72efe9f1b983
Petr Kotas <pkotas@redhat.com>
2022-08-19 16:57:36 +0200

    Add vpn code run before e2e to connect to the aks
    Signed-off-by: Petr Kotas <pkotas@redhat.com>

--- hack/e2e/run-rp-and-e2e.sh
+++ hack/e2e/run-rp-and-e2e.sh
@@ -37,6 +37,15 @@ kill_rp(){
     wait $rppid
 }
 
+run_vpn() {
+    sudo openvpn --config secrets/$VPN --daemon --writepid vpnpid
+    sleep 10
+}
+
+kill_vpn() {
+    while read pid; do sudo kill $pid; done < vpnpid
+}
+
 deploy_e2e_db() {
     echo "########## 📦 Creating new DB $DATABASE_NAME in $DATABASE_ACCOUNT_NAME ##########"
 
