01bca9c96b9eb34250d38df1082b48f74bafc157
Arris Li <huili@redhat.com>
2022-08-26 15:23:39 +1200

    remove commented code

--- pkg/operator/controllers/checker/ingressreplica_test.go
+++ pkg/operator/controllers/checker/ingressreplica_test.go
@@ -11,55 +11,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// func TestIngressReplicaChecker(t *testing.T) {
-// 	ctx := context.Background()
-
-// 	var zeroReplica int32 = 0
-
-// 	aroCluster := &arov1alpha1.Cluster{
-// 		ObjectMeta: metav1.ObjectMeta{
-// 			Name: arov1alpha1.SingletonClusterName,
-// 		},
-// 	}
-
-// 	ingressController := &operatorv1.IngressController{
-// 		ObjectMeta: metav1.ObjectMeta{
-// 			Name:      "default",
-// 			Namespace: "openshift-ingress-operator",
-// 		},
-// 		Spec: operatorv1.IngressControllerSpec{
-// 			Replicas: &(zeroReplica),
-// 		},
-// 	}
-
-// 	arocli := arofake.NewSimpleClientset(aroCluster)
-
-// 	operatorcli := operatorfake.NewSimpleClientset(ingressController)
-
-// 	checker := NewIngressReplicaChecker(arocli, operatorcli, "")
-
-// 	err := checker.Check(ctx)
-
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-
-// 	cluster, err := arocli.AroV1alpha1().Clusters().Get(ctx, arov1alpha1.SingletonClusterName, metav1.GetOptions{})
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-// 	t.Error(cluster)
-
-// 	ingressController, err = operatorcli.OperatorV1().IngressControllers("openshift-ingress-operator").Get(ctx, "default", metav1.GetOptions{})
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-
-// 	if *ingressController.Spec.Replicas != 1 {
-// 		t.Errorf("invalid replica count: %d", *ingressController.Spec.Replicas)
-// 	}
-// }
-
 func TestIngressReplicaChecker(t *testing.T) {
 	ctx := context.Background()
 
