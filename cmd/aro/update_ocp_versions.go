c2f8cb26412560b9ecc6f7abf6286ca30d3cd12c
darthhexx <darthhexx@gmail.com>
2022-08-26 12:01:09 +1000

    Ensure the DST_ACR_NAME env variable is set

--- cmd/aro/update_ocp_versions.go
+++ cmd/aro/update_ocp_versions.go
@@ -46,6 +46,14 @@ func getVersionsDatabase(ctx context.Context, log *logrus.Entry) (database.OpenS
 		return nil, err
 	}
 
+	for _, key := range []string{
+		"DST_ACR_NAME",
+	} {
+		if _, found := os.LookupEnv(key); !found {
+			return nil, fmt.Errorf("environment variable %q unset", key)
+		}
+	}
+
 	if !_env.IsLocalDevelopmentMode() {
 		for _, key := range []string{
 			"MDM_ACCOUNT",
