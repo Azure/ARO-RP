2bf08f348e429cc2b680486d8e90b83d47c7f6ad
Andrew Denton <adenton@redhat.com>
2022-08-30 13:36:26 -0700

    Temporarily force the rp nsg to redeploy

--- pkg/deploy/predeploy.go
+++ pkg/deploy/predeploy.go
@@ -126,7 +126,11 @@ func (d *deployer) PreDeploy(ctx context.Context) error {
 	// key the decision to deploy NSGs on the existence of the gateway
 	// predeploy.  We do this in order to refresh the RP NSGs when the gateway
 	// is deployed for the first time.
-	var isCreate bool
+
+	// TODO: *** REVERT AFTER RELEASE ***
+	// *** Forcing this to true here to ensure the rp_in_geneva NSG gets updated with GenevaActions service tag.
+	// *** Once the NSGs get updated, revert this change (set p.DefaultValue = false) so that the NSGs aren't getting redeployed every release.
+	var isCreate bool = true
 	_, err = d.deployments.Get(ctx, d.config.GatewayResourceGroupName, strings.TrimSuffix(generator.FileGatewayProductionPredeploy, ".json"))
 	if isDeploymentNotFoundError(err) {
 		isCreate = true
