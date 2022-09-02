608bc0bbb084490583404b999ce5ee4487adbc0a
tschneid <tschneid@redhat.com>
2022-09-02 13:19:06 -0500

    bump ado agent to 2.206.1

--- pkg/deploy/generator/resources_dev.go
+++ pkg/deploy/generator/resources_dev.go
@@ -344,7 +344,7 @@ yum -y install azure-cli podman podman-docker jq gcc gpgme-devel libassuan-devel
 mkdir -p /etc/containers/
 touch /etc/containers/nodocker
 
-VSTS_AGENT_VERSION=2.193.1
+VSTS_AGENT_VERSION=2.206.1
 mkdir /home/cloud-user/agent
 pushd /home/cloud-user/agent
 curl -s https://vstsagentpackage.azureedge.net/agent/${VSTS_AGENT_VERSION}/vsts-agent-linux-x64-${VSTS_AGENT_VERSION}.tar.gz | tar -xz
