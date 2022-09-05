d3f09db5c8e61ccde0fb08445c7173b51e1b901d
Amber Brown <ambrown@redhat.com>
2022-09-05 13:24:51 +1000

    remove checks for hive interface non-nil-ness

--- pkg/cluster/hive.go
+++ pkg/cluster/hive.go
@@ -11,13 +11,6 @@ import (
 
 func (m *manager) hiveCreateNamespace(ctx context.Context) error {
 	m.log.Info("creating a namespace in the hive cluster")
-	if !m.installViaHive {
-		if m.hiveClusterManager == nil {
-			// TODO(hive): remove this once we have Hive everywhere
-			m.log.Info("skipping: no hive cluster manager")
-			return nil
-		}
-	}
 
 	if m.doc.OpenShiftCluster.Properties.HiveProfile.Namespace != "" {
 		m.log.Info("skipping: namespace already exists")
@@ -38,25 +31,11 @@ func (m *manager) hiveCreateNamespace(ctx context.Context) error {
 
 func (m *manager) hiveEnsureResources(ctx context.Context) error {
 	m.log.Info("registering with hive")
-	if m.hiveClusterManager == nil {
-		// TODO(hive): remove this once we have Hive everywhere
-		m.log.Info("skipping: no hive cluster manager")
-		return nil
-	}
-
 	return m.hiveClusterManager.CreateOrUpdate(ctx, m.subscriptionDoc, m.doc)
 }
 
 func (m *manager) hiveClusterDeploymentReady(ctx context.Context) (bool, error) {
 	m.log.Info("waiting for cluster deployment to become ready")
-	if !m.installViaHive {
-		if m.hiveClusterManager == nil {
-			// TODO(hive): remove this if once we have Hive everywhere
-			m.log.Info("skipping: no hive cluster manager")
-			return true, nil
-		}
-	}
-
 	return m.hiveClusterManager.IsClusterDeploymentReady(ctx, m.doc)
 }
 
@@ -67,25 +46,11 @@ func (m *manager) hiveClusterInstallationComplete(ctx context.Context) (bool, er
 
 func (m *manager) hiveResetCorrelationData(ctx context.Context) error {
 	m.log.Info("resetting correlation data for hive")
-	if !m.installViaHive {
-		if m.hiveClusterManager == nil {
-			// TODO(hive): remove this if once we have Hive everywhere
-			m.log.Info("skipping: no hive cluster manager")
-			return nil
-		}
-	}
-
 	return m.hiveClusterManager.ResetCorrelationData(ctx, m.doc)
 }
 
 func (m *manager) hiveDeleteResources(ctx context.Context) error {
 	m.log.Info("deregistering cluster with hive")
-	if m.hiveClusterManager == nil {
-		// TODO(hive): remove this once we have Hive everywhere
-		m.log.Info("skipping: no hive cluster manager")
-		return nil
-	}
-
 	namespace := m.doc.OpenShiftCluster.Properties.HiveProfile.Namespace
 	if namespace == "" {
 		m.log.Info("skipping: no hive namespace in cluster document")
