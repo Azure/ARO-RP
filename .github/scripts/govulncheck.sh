42910ef735fc6f98c875a3d6be8754ed919020ce
Carlo Wisse <contact@carlowisse.com>
2022-09-20 15:31:51 +1000

    Moved script to multiline run command workflow

--- .github/scripts/govulncheck.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if GOMEMLIMIT=2GiB govulncheck ./... | grep -q 'No vulnerabilities found'; then
-  echo "No vulnerabilities found."
-  exit 0
-else
-  echo "Vulnerabilities have been found, please run `govulncheck ./...` locally and fix before merging."
-  exit 1
-fi
