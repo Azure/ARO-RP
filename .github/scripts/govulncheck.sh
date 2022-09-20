8f81383ed6b6588280a68d73886c275c19f26985
Carlo Wisse <contact@carlowisse.com>
2022-09-20 15:27:10 +1000

    Add execution perms to govulncheck.sh

--- .github/scripts/govulncheck.sh
+++ .github/scripts/govulncheck.sh
@@ -4,6 +4,6 @@ if GOMEMLIMIT=2GiB govulncheck ./... | grep -q 'No vulnerabilities found'; then
   echo "No vulnerabilities found."
   exit 0
 else
-  echo "Vulnerabilities found, please run `govulncheck ./...` locally and fix before merging."
+  echo "Vulnerabilities have been found, please run `govulncheck ./...` locally and fix before merging."
   exit 1
 fi
