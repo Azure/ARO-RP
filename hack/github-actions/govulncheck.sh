3c32b64ef211645573261e3184e19124fe9554df
Carlo Wisse <contact@carlowisse.com>
2022-09-21 10:01:01 +1000

    test full output

--- hack/github-actions/govulncheck.sh
+++ hack/github-actions/govulncheck.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if GOMEMLIMIT=10GiB govulncheck ./... | grep -q 'No vulnerabilities found'; then
-  echo "No vulnerabilities found."
-  exit 0
-else
-  echo "Vulnerabilities found, please run $(govulncheck ./...) locally and fix before merging."
-  exit 1
-fi
+GOMEMLIMIT=10GiB govulncheck ./...
+
+# if GOMEMLIMIT=10GiB govulncheck ./... | grep -q 'No vulnerabilities found'; then
+#   echo "No vulnerabilities found."
+#   exit 0
+# else
+#   echo "Vulnerabilities found, please run $(govulncheck ./...) locally and fix before merging."
+#   exit 1
+# fi
