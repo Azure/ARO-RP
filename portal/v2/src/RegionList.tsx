77023681806e9d66353c2b9a7119d53e364a3294
Ellis Johnson <elljohns@redhat.com>
2022-10-06 18:05:32 +1100

    Rebuilt bindata

--- portal/v2/src/RegionList.tsx
+++ portal/v2/src/RegionList.tsx
@@ -1,22 +1,11 @@
 import { Component } from "react"
-import { Nav, INavLinkGroup, INavStyles, IRenderGroupHeaderProps, IRenderFunction, IFontStyles, IStyle } from "@fluentui/react"
+import { Nav, INavLinkGroup, INavStyles, IRenderGroupHeaderProps, IRenderFunction } from "@fluentui/react"
 
 const navStyles: Partial<INavStyles> = {
   chevronIcon: {
     visibility: 'hidden'
   }
 };
-
-const headerStyle: Partial<IStyle> = {
-  h3: {
-    "font-family": '"Segoe UI","Segoe UI Web (West European)", "Segoe UI", -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif;',
-    "font-weight": "600",
-    "font-size": "24px",
-    "line-height": "32px",
-  }
-}
-
-
 
 interface RegionComponentProps {
   item: IRegionList
@@ -36,7 +25,7 @@ export interface IRegion {
 }
 
 const _onRenderGroupHeader: IRenderFunction<IRenderGroupHeaderProps> = (props): JSX.Element => {
-  return (<h3 className="">{props?.name}</h3>);
+  return (<h3>{props?.name}</h3>);
 };
 
 export class RegionComponent extends Component<
