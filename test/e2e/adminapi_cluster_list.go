61669a737c93a1069af49aba1fa74bef66969512
Mikalai Radchuk <mradchuk@redhat.com>
2022-10-10 17:04:18 +0100

    Admin API E2E fixups

--- test/e2e/adminapi_cluster_list.go
+++ test/e2e/adminapi_cluster_list.go
@@ -6,8 +6,6 @@ package e2e
 import (
 	"context"
 	"fmt"
-	"net/http"
-	"net/url"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -18,24 +16,14 @@ import (
 var _ = Describe("[Admin API] List clusters action", func() {
 	BeforeEach(skipIfNotInDevelopmentEnv)
 
-	It("should be able to return list of all clusters with admin fields", func() {
+	It("must return list of clusters with admin fields", func() {
 		ctx := context.Background()
 		resourceID := resourceIDFromEnv()
 
-		By("requesting the cluster document via RP admin API")
-		ocs := adminClustersList(ctx, "/admin/providers/Microsoft.RedHatOpenShift/openShiftClusters")
-
-		By("checking that we received the expected cluster")
-		var oc *admin.OpenShiftCluster
-		for i := range ocs {
-			if ocs[i].ID == resourceID {
-				oc = ocs[i]
-			}
-		}
-		Expect(oc).ToNot(BeNil())
+		testAdminClustersList(ctx, "/admin/providers/Microsoft.RedHatOpenShift/openShiftClusters", resourceID)
 	})
 
-	It("should be able to return list clusters with admin fields by subscription", func() {
+	It("must return list of clusters with admin fields by subscription", func() {
 		ctx := context.Background()
 		resourceID := resourceIDFromEnv()
 
@@ -43,7 +31,7 @@ var _ = Describe("[Admin API] List clusters action", func() {
 		testAdminClustersList(ctx, path, resourceID)
 	})
 
-	It("should be able to return list clusters with admin fields by resource group", func() {
+	It("must return list of clusters with admin fields by resource group", func() {
 		ctx := context.Background()
 		resourceID := resourceIDFromEnv()
 
@@ -52,32 +40,11 @@ var _ = Describe("[Admin API] List clusters action", func() {
 	})
 })
 
-func adminClustersList(ctx context.Context, path string) []*admin.OpenShiftCluster {
-	By("requesting the cluster document via RP admin API")
-	ocs := make([]*admin.OpenShiftCluster, 0)
-	params := url.Values{}
-	for {
-		var list admin.OpenShiftClusterList
-		resp, err := adminRequest(ctx, http.MethodGet, path, params, nil, &list)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(resp.StatusCode).To(Equal(http.StatusOK))
-
-		ocs = append(ocs, list.OpenShiftClusters...)
-
-		if list.NextLink == "" {
-			break
-		}
-
-		params = nextParams(list.NextLink)
-	}
-	return ocs
-}
-
 func testAdminClustersList(ctx context.Context, path, wantResourceID string) {
-	By("requesting the cluster document via RP admin API")
-	ocs := adminClustersList(ctx, path)
+	By("listing the cluster documents via RP admin API")
+	ocs := adminListClusters(ctx, path)
 
-	By("checking that we received the expected cluster")
+	By("verifying that we received the expected cluster")
 	var oc *admin.OpenShiftCluster
 	for i := range ocs {
 		if ocs[i].ID == wantResourceID {
@@ -85,16 +52,11 @@ func testAdminClustersList(ctx context.Context, path, wantResourceID string) {
 		}
 	}
 	Expect(oc).ToNot(BeNil())
+	Expect(oc.ID).To(Equal(wantResourceID))
 
 	By("checking that fields available only in Admin API have values")
 	// Note: some fields will have empty values
 	// on successfully provisioned cluster (oc.Properties.Install, for example)
 	Expect(oc.Properties.StorageSuffix).ToNot(BeEmpty())
-}
-
-func nextParams(nextLink string) url.Values {
-	url, err := url.Parse(nextLink)
-	Expect(err).NotTo(HaveOccurred())
-
-	return url.Query()
+	Expect(oc.Properties.InfraID).ToNot(BeEmpty())
 }
