6a8971d3d1fe7d264e9a113b7a231c5cbe1e6f6f
darthhexx <darthhexx@gmail.com>
2022-10-18 20:30:31 +1000

    Add mocks and update tests to include paged ManagedCluster results

--- pkg/util/liveconfig/hive_test.go
+++ pkg/util/liveconfig/hive_test.go
@@ -6,7 +6,6 @@ package liveconfig
 import (
 	"context"
 	"embed"
-	"encoding/base64"
 	"testing"
 
 	mgmtcontainerservice "github.com/Azure/azure-sdk-for-go/services/containerservice/mgmt/2021-10-01/containerservice"
@@ -27,17 +26,67 @@ func TestProdHive(t *testing.T) {
 
 	mcc := mock_containerservice.NewMockManagedClustersClient(controller)
 
+	managedClustersList := mgmtcontainerservice.ManagedClusterListResult{
+		Value: &[]mgmtcontainerservice.ManagedCluster{
+			{
+				Name:     to.StringPtr("aro-aks-cluster-001"),
+				Location: to.StringPtr("eastus"),
+				ManagedClusterProperties: &mgmtcontainerservice.ManagedClusterProperties{
+					NodeResourceGroup: to.StringPtr("rp-eastus-aks1"),
+				},
+			},
+			{
+				Name:     to.StringPtr("aro-aks-cluster-002"),
+				Location: to.StringPtr("eastus"),
+				ManagedClusterProperties: &mgmtcontainerservice.ManagedClusterProperties{
+					NodeResourceGroup: to.StringPtr("rp-eastus-aks2"),
+				},
+			},
+			{
+				Name:     to.StringPtr("aro-aks-cluster-003"),
+				Location: to.StringPtr("eastus"),
+				ManagedClusterProperties: &mgmtcontainerservice.ManagedClusterProperties{
+					NodeResourceGroup: to.StringPtr("rp-eastus-aks3"),
+				},
+			},
+			{
+				Name:     to.StringPtr("aro-aks-cluster-001"),
+				Location: to.StringPtr("westus"),
+				ManagedClusterProperties: &mgmtcontainerservice.ManagedClusterProperties{
+					NodeResourceGroup: to.StringPtr("rp-eastus-aks1"),
+				},
+			},
+			{
+				Name:     to.StringPtr("aro-aks-cluster-001"),
+				Location: to.StringPtr("australiaeast"),
+				ManagedClusterProperties: &mgmtcontainerservice.ManagedClusterProperties{
+					NodeResourceGroup: to.StringPtr("rp-eastus-aks2"),
+				},
+			},
+			{
+				Name:     to.StringPtr("aro-aks-cluster-001"),
+				Location: to.StringPtr("brazilsouth"),
+				ManagedClusterProperties: &mgmtcontainerservice.ManagedClusterProperties{
+					NodeResourceGroup: to.StringPtr("rp-eastus-aks1"),
+				},
+			},
+		},
+	}
+
+	resultPage := mgmtcontainerservice.NewManagedClusterListResultPage(managedClustersList, func(ctx context.Context, mclr mgmtcontainerservice.ManagedClusterListResult) (mgmtcontainerservice.ManagedClusterListResult, error) {
+		return mgmtcontainerservice.ManagedClusterListResult{}, nil
+	})
+	mcc.EXPECT().List(gomock.Any()).Return(resultPage, nil)
+
 	kc, err := hiveEmbeddedFiles.ReadFile("testdata/kubeconfig")
 	if err != nil {
 		t.Fatal(err)
 	}
-	enc := make([]byte, base64.StdEncoding.EncodedLen(len(kc)))
-	base64.StdEncoding.Encode(enc, kc)
 
 	kcresp := &[]mgmtcontainerservice.CredentialResult{
 		{
 			Name:  to.StringPtr("example"),
-			Value: to.ByteSlicePtr(enc),
+			Value: to.ByteSlicePtr(kc),
 		},
 	}
 
@@ -45,7 +94,7 @@ func TestProdHive(t *testing.T) {
 		Kubeconfigs: kcresp,
 	}
 
-	mcc.EXPECT().ListClusterUserCredentials(gomock.Any(), "rp-eastus", "aro-aks-cluster-001", "").Return(resp, nil)
+	mcc.EXPECT().ListClusterAdminCredentials(gomock.Any(), "rp-eastus", "aro-aks-cluster-001", "public").Return(resp, nil)
 
 	lc := NewProd("eastus", mcc)
 
