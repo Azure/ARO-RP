e91337e0c8e1f7742a0ccbdd6775883865ec075c
Matthew Barnes <mbarnes@fedoraproject.org>
2022-11-01 12:49:37 -0400

    Remove pkg/util/refreshable
    No longer used.

--- pkg/util/refreshable/generate.go
+++
@@ -1,8 +0,0 @@
-package refreshable
-
-// Copyright (c) Microsoft Corporation.
-// Licensed under the Apache License 2.0.
-
-//go:generate rm -rf ../mocks/$GOPACKAGE
-//go:generate go run ../../../vendor/github.com/golang/mock/mockgen -destination=../mocks/$GOPACKAGE/$GOPACKAGE.go github.com/Azure/ARO-RP/pkg/util/$GOPACKAGE Authorizer
-//go:generate go run ../../../vendor/golang.org/x/tools/cmd/goimports -local=github.com/Azure/ARO-RP -e -w ../mocks/$GOPACKAGE/$GOPACKAGE.go
