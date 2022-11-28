61cf9f6a0131c3e104282d880c1b3fac8f3334b3
Jeremy Facchetti <jfacchet@redhat.com>
2022-11-28 09:50:48 +0100

    now returns the struct type instead of an interface

--- pkg/frontend/adminactions/azureactions.go
+++ pkg/frontend/adminactions/azureactions.go
@@ -54,7 +54,7 @@ type azureActions struct {
 
 // NewAzureActions returns an azureActions
 func NewAzureActions(log *logrus.Entry, env env.Interface, oc *api.OpenShiftCluster,
-	subscriptionDoc *api.SubscriptionDocument) (AzureActions, error) {
+	subscriptionDoc *api.SubscriptionDocument) (*azureActions, error) {
 	fpAuth, err := env.FPAuthorizer(subscriptionDoc.Subscription.Properties.TenantID,
 		env.Environment().ResourceManagerEndpoint)
 	if err != nil {
