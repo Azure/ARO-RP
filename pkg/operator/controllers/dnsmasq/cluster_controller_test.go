988a8a121aa4d9781f238d6e2c061b9023f05d6c
Tanmay Satam <tsatam@redhat.com>
2022-11-28 12:01:40 -0500

    Refactor dnsmasq unit tests to reduce overcomplicated list pattern

--- pkg/operator/controllers/dnsmasq/cluster_controller_test.go
+++ pkg/operator/controllers/dnsmasq/cluster_controller_test.go
@@ -5,12 +5,14 @@ package dnsmasq
 
 import (
 	"context"
+	"strconv"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	mcv1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
 	mcofake "github.com/openshift/machine-config-operator/pkg/generated/clientset/versioned/fake"
 	"github.com/sirupsen/logrus"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kruntime "k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -30,110 +32,111 @@ func TestClusterReconciler(t *testing.T) {
 	fakeDh := func(controller *gomock.Controller) *mock_dynamichelper.MockInterface {
 		return mock_dynamichelper.NewMockInterface(controller)
 	}
-
-	tests := []struct {
-		name    string
-		arocli  *arofake.Clientset
-		mcocli  *mcofake.Clientset
-		mocks   func(mdh *mock_dynamichelper.MockInterface)
-		request ctrl.Request
-		wantErr bool
-	}{
-		{
-			name:    "no cluster",
-			arocli:  fakeAro(),
-			mcocli:  fakeMco(),
-			mocks:   func(mdh *mock_dynamichelper.MockInterface) {},
-			request: ctrl.Request{},
-			wantErr: true,
-		},
-		{
-			name: "controller disabled",
-			arocli: fakeAro(
-				&arov1alpha1.Cluster{
-					ObjectMeta: metav1.ObjectMeta{Name: "cluster"},
-					Status:     arov1alpha1.ClusterStatus{},
-					Spec: arov1alpha1.ClusterSpec{
-						OperatorFlags: arov1alpha1.OperatorFlags{
-							controllerEnabled: "false",
-						},
-					},
+	cluster := func(enabled bool) *arov1alpha1.Cluster {
+		return &arov1alpha1.Cluster{
+			ObjectMeta: metav1.ObjectMeta{Name: "cluster"},
+			Status:     arov1alpha1.ClusterStatus{},
+			Spec: arov1alpha1.ClusterSpec{
+				OperatorFlags: arov1alpha1.OperatorFlags{
+					controllerEnabled: strconv.FormatBool(enabled),
 				},
-			),
-			mcocli:  fakeMco(),
-			mocks:   func(mdh *mock_dynamichelper.MockInterface) {},
-			request: ctrl.Request{},
-			wantErr: false,
-		},
-		{
-			name: "no MachineConfigPools does nothing",
-			arocli: fakeAro(
-				&arov1alpha1.Cluster{
-					ObjectMeta: metav1.ObjectMeta{Name: "cluster"},
-					Status:     arov1alpha1.ClusterStatus{},
-					Spec: arov1alpha1.ClusterSpec{
-						OperatorFlags: arov1alpha1.OperatorFlags{
-							controllerEnabled: "true",
-						},
-					},
-				},
-			),
-			mcocli: fakeMco(),
-			mocks: func(mdh *mock_dynamichelper.MockInterface) {
-				mdh.EXPECT().Ensure(gomock.Any()).Times(1)
 			},
-			request: ctrl.Request{},
-			wantErr: false,
-		},
-		{
-			name: "valid MachineConfigPool creates ARO DNS MachineConfig",
-			arocli: fakeAro(
-				&arov1alpha1.Cluster{
-					ObjectMeta: metav1.ObjectMeta{Name: "cluster"},
-					Status:     arov1alpha1.ClusterStatus{},
-					Spec: arov1alpha1.ClusterSpec{
-						OperatorFlags: arov1alpha1.OperatorFlags{
-							controllerEnabled: "true",
-						},
-					},
-				},
-			),
-			mcocli: fakeMco(
-				&mcv1.MachineConfigPool{
-					ObjectMeta: metav1.ObjectMeta{Name: "master"},
-					Status:     mcv1.MachineConfigPoolStatus{},
-					Spec:       mcv1.MachineConfigPoolSpec{},
-				},
-			),
-			mocks: func(mdh *mock_dynamichelper.MockInterface) {
-				mdh.EXPECT().Ensure(gomock.Any(), gomock.AssignableToTypeOf(&mcv1.MachineConfig{})).Times(1)
-			},
-			request: ctrl.Request{},
-			wantErr: false,
-		},
+		}
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			controller := gomock.NewController(t)
-			defer controller.Finish()
+	t.Run("when no cluster resource is present, returns error", func(t *testing.T) {
+		controller := gomock.NewController(t)
+		defer controller.Finish()
 
-			mdh := fakeDh(controller)
-			tt.mocks(mdh)
+		arocli := fakeAro()
+		mcocli := fakeMco()
+		dh := fakeDh(controller)
 
-			r := &ClusterReconciler{
-				log:    logrus.NewEntry(logrus.StandardLogger()),
-				arocli: tt.arocli,
-				mcocli: tt.mcocli,
-				dh:     mdh,
-			}
+		r := &ClusterReconciler{
+			log:    logrus.NewEntry(logrus.StandardLogger()),
+			arocli: arocli,
+			mcocli: mcocli,
+			dh:     dh,
+		}
 
-			_, err := r.Reconcile(context.Background(), tt.request)
+		_, err := r.Reconcile(context.Background(), ctrl.Request{})
 
-			if (err != nil) != tt.wantErr {
-				t.Errorf("ClusterReconciler.Reconcile() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-		})
-	}
+		if !kerrors.IsNotFound(err) {
+			t.Errorf("wanted error: cluster not found, got error: %v", err)
+		}
+	})
+
+	t.Run("when controller is disabled, returns with no error", func(t *testing.T) {
+		controller := gomock.NewController(t)
+		defer controller.Finish()
+
+		arocli := fakeAro(cluster(false))
+		mcocli := fakeMco()
+		dh := fakeDh(controller)
+
+		r := &ClusterReconciler{
+			log:    logrus.NewEntry(logrus.StandardLogger()),
+			arocli: arocli,
+			mcocli: mcocli,
+			dh:     dh,
+		}
+
+		_, err := r.Reconcile(context.Background(), ctrl.Request{})
+
+		if err != nil {
+			t.Errorf("wanted no error, got error: %v", err)
+		}
+	})
+
+	t.Run("when no MachineConfigPools are present, does nothing", func(t *testing.T) {
+		controller := gomock.NewController(t)
+		defer controller.Finish()
+
+		arocli := fakeAro(cluster(true))
+		mcocli := fakeMco()
+		dh := fakeDh(controller)
+		dh.EXPECT().Ensure(gomock.Any()).Times(1)
+
+		r := &ClusterReconciler{
+			log:    logrus.NewEntry(logrus.StandardLogger()),
+			arocli: arocli,
+			mcocli: mcocli,
+			dh:     dh,
+		}
+
+		_, err := r.Reconcile(context.Background(), ctrl.Request{})
+
+		if err != nil {
+			t.Errorf("wanted no error, got error: %v", err)
+		}
+	})
+
+	t.Run("when valid MachineConfigPool is present, creates ARO DNS MachineConfig", func(t *testing.T) {
+		controller := gomock.NewController(t)
+		defer controller.Finish()
+
+		arocli := fakeAro(cluster(true))
+		mcocli := fakeMco(
+			&mcv1.MachineConfigPool{
+				ObjectMeta: metav1.ObjectMeta{Name: "master"},
+				Status:     mcv1.MachineConfigPoolStatus{},
+				Spec:       mcv1.MachineConfigPoolSpec{},
+			},
+		)
+		dh := fakeDh(controller)
+		dh.EXPECT().Ensure(gomock.Any(), gomock.AssignableToTypeOf(&mcv1.MachineConfig{})).Times(1)
+
+		r := &ClusterReconciler{
+			log:    logrus.NewEntry(logrus.StandardLogger()),
+			arocli: arocli,
+			mcocli: mcocli,
+			dh:     dh,
+		}
+
+		_, err := r.Reconcile(context.Background(), ctrl.Request{})
+
+		if err != nil {
+			t.Errorf("wanted no error, got error: %v", err)
+		}
+	})
 }
--- pkg/operator/controllers/dnsmasq/machineconfig_controller_test.go
+++ pkg/operator/controllers/dnsmasq/machineconfig_controller_test.go
@@ -5,12 +5,14 @@ package dnsmasq
 
 import (
 	"context"
+	"strconv"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	mcv1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
 	mcofake "github.com/openshift/machine-config-operator/pkg/generated/clientset/versioned/fake"
 	"github.com/sirupsen/logrus"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kruntime "k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -31,118 +33,126 @@ func TestMachineConfigReconciler(t *testing.T) {
 	fakeDh := func(controller *gomock.Controller) *mock_dynamichelper.MockInterface {
 		return mock_dynamichelper.NewMockInterface(controller)
 	}
-
-	tests := []struct {
-		name    string
-		arocli  *arofake.Clientset
-		mcocli  *mcofake.Clientset
-		mocks   func(mdh *mock_dynamichelper.MockInterface)
-		request ctrl.Request
-		wantErr bool
-	}{
-		{
-			name:    "no cluster",
-			arocli:  fakeAro(),
-			mcocli:  fakeMco(),
-			mocks:   func(mdh *mock_dynamichelper.MockInterface) {},
-			request: ctrl.Request{},
-			wantErr: true,
-		},
-		{
-			name: "controller disabled",
-			arocli: fakeAro(
-				&arov1alpha1.Cluster{
-					ObjectMeta: metav1.ObjectMeta{Name: "cluster"},
-					Status:     arov1alpha1.ClusterStatus{},
-					Spec: arov1alpha1.ClusterSpec{
-						OperatorFlags: arov1alpha1.OperatorFlags{
-							controllerEnabled: "false",
-						},
-					},
-				},
-			),
-			mcocli:  fakeMco(),
-			mocks:   func(mdh *mock_dynamichelper.MockInterface) {},
-			request: ctrl.Request{},
-			wantErr: false,
-		},
-		{
-			name: "no MachineConfigPool for MachineConfig does nothing",
-			arocli: fakeAro(
-				&arov1alpha1.Cluster{
-					ObjectMeta: metav1.ObjectMeta{Name: "cluster"},
-					Status:     arov1alpha1.ClusterStatus{},
-					Spec: arov1alpha1.ClusterSpec{
-						OperatorFlags: arov1alpha1.OperatorFlags{
-							controllerEnabled: "true",
-						},
-					},
-				},
-			),
-			mcocli: fakeMco(),
-			mocks:  func(mdh *mock_dynamichelper.MockInterface) {},
-			request: ctrl.Request{
-				NamespacedName: types.NamespacedName{
-					Namespace: "",
-					Name:      "99-custom-aro-dns",
+	cluster := func(enabled bool) *arov1alpha1.Cluster {
+		return &arov1alpha1.Cluster{
+			ObjectMeta: metav1.ObjectMeta{Name: "cluster"},
+			Status:     arov1alpha1.ClusterStatus{},
+			Spec: arov1alpha1.ClusterSpec{
+				OperatorFlags: arov1alpha1.OperatorFlags{
+					controllerEnabled: strconv.FormatBool(enabled),
 				},
 			},
-			wantErr: false,
-		},
-		{
-			name: "valid MachineConfigPool for MachineConfig reconciles MachineConfig",
-			arocli: fakeAro(
-				&arov1alpha1.Cluster{
-					ObjectMeta: metav1.ObjectMeta{Name: "cluster"},
-					Status:     arov1alpha1.ClusterStatus{},
-					Spec: arov1alpha1.ClusterSpec{
-						OperatorFlags: arov1alpha1.OperatorFlags{
-							controllerEnabled: "true",
-						},
-					},
-				},
-			),
-			mcocli: fakeMco(
-				&mcv1.MachineConfigPool{
-					ObjectMeta: metav1.ObjectMeta{Name: "custom"},
-					Status:     mcv1.MachineConfigPoolStatus{},
-					Spec:       mcv1.MachineConfigPoolSpec{},
-				},
-			),
-			mocks: func(mdh *mock_dynamichelper.MockInterface) {
-				mdh.EXPECT().Ensure(gomock.Any(), gomock.Any()).Times(1)
-			},
-			request: ctrl.Request{
-				NamespacedName: types.NamespacedName{
-					Namespace: "",
-					Name:      "99-custom-aro-dns",
-				},
-			},
-			wantErr: false,
-		},
+		}
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			controller := gomock.NewController(t)
-			defer controller.Finish()
+	t.Run("when no cluster resource is present, returns error", func(t *testing.T) {
+		controller := gomock.NewController(t)
+		defer controller.Finish()
 
-			mdh := fakeDh(controller)
-			tt.mocks(mdh)
+		arocli := fakeAro()
+		mcocli := fakeMco()
+		dh := fakeDh(controller)
 
-			r := &MachineConfigReconciler{
-				log:    logrus.NewEntry(logrus.StandardLogger()),
-				arocli: tt.arocli,
-				mcocli: tt.mcocli,
-				dh:     mdh,
-			}
+		r := &MachineConfigReconciler{
+			log:    logrus.NewEntry(logrus.StandardLogger()),
+			arocli: arocli,
+			mcocli: mcocli,
+			dh:     dh,
+		}
 
-			_, err := r.Reconcile(context.Background(), tt.request)
+		request := ctrl.Request{}
 
-			if (err != nil) != tt.wantErr {
-				t.Errorf("MachineConfigReconciler.Reconcile() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-		})
-	}
+		_, err := r.Reconcile(context.Background(), request)
+
+		if !kerrors.IsNotFound(err) {
+			t.Errorf("wanted error: cluster not found, got error: %v", err)
+		}
+	})
+
+	t.Run("when controller is disabled, returns with no error", func(t *testing.T) {
+		controller := gomock.NewController(t)
+		defer controller.Finish()
+
+		arocli := fakeAro(cluster(false))
+		mcocli := fakeMco()
+		dh := fakeDh(controller)
+
+		r := &MachineConfigReconciler{
+			log:    logrus.NewEntry(logrus.StandardLogger()),
+			arocli: arocli,
+			mcocli: mcocli,
+			dh:     dh,
+		}
+
+		_, err := r.Reconcile(context.Background(), ctrl.Request{})
+
+		if err != nil {
+			t.Errorf("wanted no error, got error: %v", err)
+		}
+	})
+
+	t.Run("when no corresponding MachineConfigPool for the requested MachineConfig is present, does nothing", func(t *testing.T) {
+		controller := gomock.NewController(t)
+		defer controller.Finish()
+
+		arocli := fakeAro(cluster(true))
+		mcocli := fakeMco()
+		dh := fakeDh(controller)
+
+		request := ctrl.Request{
+			NamespacedName: types.NamespacedName{
+				Namespace: "",
+				Name:      "99-custom-aro-dns",
+			},
+		}
+
+		r := &MachineConfigReconciler{
+			log:    logrus.NewEntry(logrus.StandardLogger()),
+			arocli: arocli,
+			mcocli: mcocli,
+			dh:     dh,
+		}
+
+		_, err := r.Reconcile(context.Background(), request)
+
+		if err != nil {
+			t.Errorf("wanted no error, got error: %v", err)
+		}
+	})
+
+	t.Run("when a valid MachineConfigPool exists for the requested MachineConfig, reconciles MachineConifg", func(t *testing.T) {
+		controller := gomock.NewController(t)
+		defer controller.Finish()
+
+		arocli := fakeAro(cluster(true))
+		mcocli := fakeMco(
+			&mcv1.MachineConfigPool{
+				ObjectMeta: metav1.ObjectMeta{Name: "custom"},
+				Status:     mcv1.MachineConfigPoolStatus{},
+				Spec:       mcv1.MachineConfigPoolSpec{},
+			},
+		)
+		dh := fakeDh(controller)
+		dh.EXPECT().Ensure(gomock.Any(), gomock.Any()).Times(1)
+
+		request := ctrl.Request{
+			NamespacedName: types.NamespacedName{
+				Namespace: "",
+				Name:      "99-custom-aro-dns",
+			},
+		}
+
+		r := &MachineConfigReconciler{
+			log:    logrus.NewEntry(logrus.StandardLogger()),
+			arocli: arocli,
+			mcocli: mcocli,
+			dh:     dh,
+		}
+
+		_, err := r.Reconcile(context.Background(), request)
+
+		if err != nil {
+			t.Errorf("wanted no error, got error: %v", err)
+		}
+	})
 }
