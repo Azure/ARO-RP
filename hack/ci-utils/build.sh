afbcebea4e9fe3fe2d1f1a128e73d5f14d7077e7
Srinivas Atmakuri <sreenivas.atmakuri@live.com>
2022-11-29 16:32:20 +0530

    testing-7

--- hack/ci-utils/build.sh
+++ hack/ci-utils/build.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
 set -xe
-OPTION=$2
+if [[ -z "$1" ]]; then
+    go run hack/ci-utils/differenceChecker/main.go make generate
+    make build-all
+    make unit-test-go
+    exit 0
+fi
 
+OPTION=$1
 case "$OPTION" in
     "generate")
         echo "Running make generate."
         make generate
+        exit 0
     ;;
     "build-all")
         echo "Running make build-all."
         make build-all
+        exit 0
     ;;
     "unit-test-go")
         echo "Running make unit-test-go."
         make unit-test-go
+        exit 0
     ;;
     "validate-fips")
         echo "Running make validate-fips."
         make validate-fips
+        exit 0
     ;;
-    *) go run hack/ci-utils/differenceChecker/main.go make generate
-        make build-all
-        make unit-test-go
-    ;;
+    *)
+        echo "No valid input is provided"
+        exit 1
 esac
