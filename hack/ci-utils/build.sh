647327c6027abffabab97bddf4c57c238d7b2fa0
Srinivas Atmakuri <sreenivas.atmakuri@live.com>
2022-11-29 16:11:36 +0530

    testing-5

--- hack/ci-utils/build.sh
+++ hack/ci-utils/build.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-set -xe 
+set -xe
+OPTION=$2
 
-go run hack/ci-utils/differenceChecker/main.go make generate
-make build-all
-make unit-test-go
+case "$OPTION" in
+    "generate")
+        echo "Running make generate."
+        make generate
+    ;;
+    "build-all")
+        echo "Running make build-all."
+        make build-all
+    ;;
+    "unit-test-go")
+        echo "Running make unit-test-go."
+        make unit-test-go
+    ;;
+    "validate-fips")
+        echo "Running make validate-fips."
+        make validate-fips
+    ;;
+    *) go run hack/ci-utils/differenceChecker/main.go make generate
+        make build-all
+        make unit-test-go
+    ;;
+esac
