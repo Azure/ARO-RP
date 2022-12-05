cfcf0fb679eba73e41c21c176857f99381ff81d4
Mikalai Radchuk <mradchuk@redhat.com>
2022-12-05 14:49:22 +0000

    Split internet checker into a separate controller

--- pkg/operator/controllers/checkers/internetchecker/checker_test.go
+++ pkg/operator/controllers/checkers/internetchecker/checker_test.go
@@ -1,4 +1,4 @@
-package checker
+package internetchecker
 
 // Copyright (c) Microsoft Corporation.
 // Licensed under the Apache License 2.0.
@@ -15,12 +15,6 @@ import (
 	"time"
 
 	operatorv1 "github.com/openshift/api/operator/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"github.com/Azure/ARO-RP/pkg/operator"
-	arov1alpha1 "github.com/Azure/ARO-RP/pkg/operator/apis/aro.openshift.io/v1alpha1"
-	arofake "github.com/Azure/ARO-RP/pkg/operator/clientset/versioned/fake"
-	utillog "github.com/Azure/ARO-RP/pkg/util/log"
 )
 
 type fakeResponse struct {
@@ -69,8 +63,6 @@ var (
 )
 
 func TestInternetCheckerCheck(t *testing.T) {
-	ctx := context.Background()
-
 	var testCases = []struct {
 		name          string
 		responses     []*fakeResponse
@@ -100,63 +92,21 @@ func TestInternetCheckerCheck(t *testing.T) {
 		{
 			name:          "timedout request",
 			responses:     []*fakeResponse{networkUnreach, timedoutReq, timedoutReq, timedoutReq, timedoutReq, timedoutReq},
-			wantErr:       "requeue",
+			wantErr:       "https://not-used-in-test.io: context deadline exceeded",
 			wantCondition: operatorv1.ConditionFalse,
 		},
 	}
 
-	roleToConditionTypeMap := map[string]string{
-		operator.RoleMaster: arov1alpha1.InternetReachableFromMaster,
-		operator.RoleWorker: arov1alpha1.InternetReachableFromWorker,
-	}
-
-	for _, testRole := range []string{operator.RoleMaster, operator.RoleWorker} {
-		t.Run(testRole, func(t *testing.T) {
-			for _, test := range testCases {
-				t.Run(test.name, func(t *testing.T) {
-					arocli := arofake.NewSimpleClientset(
-						&arov1alpha1.Cluster{
-							ObjectMeta: metav1.ObjectMeta{
-								Name: arov1alpha1.SingletonClusterName,
-							},
-							Spec: arov1alpha1.ClusterSpec{
-								InternetChecker: arov1alpha1.InternetCheckerSpec{
-									URLs: []string{urltocheck},
-								},
-							},
-						},
-					)
-
-					r := &InternetChecker{
-						log:          utillog.GetLogger(),
-						role:         testRole,
-						checkTimeout: 100 * time.Millisecond,
-
-						arocli:     arocli,
-						httpClient: &testClient{responses: test.responses},
-					}
-					err := r.Check(ctx)
-					if err != nil && err.Error() != test.wantErr ||
-						err == nil && test.wantErr != "" {
-						t.Error(err)
-					}
-
-					instance, err := arocli.AroV1alpha1().Clusters().Get(ctx, arov1alpha1.SingletonClusterName, metav1.GetOptions{})
-					if err != nil {
-						t.Error(err)
-					}
-
-					var condition operatorv1.OperatorCondition
-					for _, condition = range instance.Status.Conditions {
-						if condition.Type == roleToConditionTypeMap[testRole] {
-							break
-						}
-					}
-
-					if condition.Status != test.wantCondition {
-						t.Errorf(string(condition.Status))
-					}
-				})
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			r := &checker{
+				checkTimeout: 100 * time.Millisecond,
+				httpClient:   &testClient{responses: test.responses},
+			}
+			err := r.Check([]string{urltocheck})
+			if err != nil && err.Error() != test.wantErr ||
+				err == nil && test.wantErr != "" {
+				t.Error(err)
 			}
 		})
 	}
