ed94cfe395edfad7dcdb8aab44fb57a38504a104
Srinivas Atmakuri <sreenivas.atmakuri@live.com>
2022-12-07 14:40:05 +0530

    testing-15

--- hack/ci-utils/build.sh
+++ hack/ci-utils/build.sh
@@ -13,22 +13,22 @@ case "$OPTION" in
     "generate")
         echo "Running make generate."
         go run hack/ci-utils/differenceChecker/main.go make generate
-        exit 0
+        if [[ -z "$(/usr/bin/git status -s)" ]]; then exit 0; else exit 1; fi
     ;;
     "build-all")
         echo "Running make build-all."
         make build-all
-        exit 0
+        if [[ -z "$(/usr/bin/git status -s)" ]]; then exit 0; else exit 1; fi
     ;;
     "unit-test-go")
         echo "Running make unit-test-go."
         make unit-test-go
-        exit 0
+        if [[ -z "$(/usr/bin/git status -s)" ]]; then exit 0; else exit 1; fi
     ;;
     "validate-fips")
         echo "Running make validate-fips."
         make validate-fips
-        exit 0
+        if [[ -z "$(/usr/bin/git status -s)" ]]; then exit 0; else exit 1; fi
     ;;
     *)
         echo "No valid input is provided"
