d2270049d252de23f9d7500c1d1def1d1971c37f
Srinivas Atmakuri <sreenivas.atmakuri@live.com>
2022-12-16 09:47:26 +0530

    fixes git status to skip few files

--- hack/ci-utils/build.sh
+++ hack/ci-utils/build.sh
@@ -13,22 +13,22 @@ case "$OPTION" in
     "generate")
         echo "Running make generate."
         go run hack/ci-utils/differenceChecker/main.go make generate
-        if [[ -z "$(/usr/bin/git status -s)" ]]; then exit 0; else exit 1; fi
+        if [[ -z "$(/usr/bin/git status -s -- ':!.dockerignore' ':!env.example' ':!env-int.example')" ]]; then exit 0; else exit 1; fi
     ;;
     "build-all")
         echo "Running make build-all."
         make build-all
-        if [[ -z "$(/usr/bin/git status -s)" ]]; then exit 0; else exit 1; fi
+        if [[ -z "$(/usr/bin/git status -s -- ':!.dockerignore' ':!env.example' ':!env-int.example')" ]]; then exit 0; else exit 1; fi
     ;;
     "unit-test-go")
         echo "Running make unit-test-go."
         make unit-test-go
-        if [[ -z "$(/usr/bin/git status -s)" ]]; then exit 0; else exit 1; fi
+        if [[ -z "$(/usr/bin/git status -s -- ':!.dockerignore' ':!env.example' ':!env-int.example')" ]]; then exit 0; else exit 1; fi
     ;;
     "validate-fips")
         echo "Running make validate-fips."
         make validate-fips
-        if [[ -z "$(/usr/bin/git status -s)" ]]; then exit 0; else exit 1; fi
+        if [[ -z "$(/usr/bin/git status -s -- ':!.dockerignore' ':!env.example' ':!env-int.example')" ]]; then exit 0; else exit 1; fi
     ;;
     *)
         echo "No valid input is provided"
