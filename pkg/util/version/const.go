6e9f430c10f6de8db1ff93cf06b9b3f1c719a209
Jeremy Facchetti <jfacchet@redhat.com>
2022-12-16 11:19:18 +0100

    version bump

--- pkg/util/version/const.go
+++ pkg/util/version/const.go
@@ -27,8 +27,8 @@ var GitCommit = "unknown"
 
 // InstallStream describes stream we are defaulting to for all new clusters
 var InstallStream = &Stream{
-	Version:  NewVersion(4, 10, 42),
-	PullSpec: "quay.io/openshift-release-dev/ocp-release@sha256:b89ada9261a1b257012469e90d7d4839d0d2f99654f5ce76394fa3f06522b600",
+	Version:  NewVersion(4, 10, 40),
+	PullSpec: "quay.io/openshift-release-dev/ocp-release@sha256:b9fad814fb4442e7e852b0614d9bb4e2ebc5e1a2fa51623aa838b4ee0e4a5369",
 }
 
 // UpgradeStreams describes list of streams we support for upgrades
