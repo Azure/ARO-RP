c5c863ee82c74e2dd09e1cb18c9a2f6a5fa9831a
Jeremy Facchetti <jfacchet@redhat.com>
2023-01-05 09:41:04 +0100

    Revert "quota validation to frontend" commits
    This reverts commit b3823876f5cef081645e3d158e85af4c677fcf00.
    This reverts commit e9dec20e21ef4cb6668b9eea12f2357ff3406826.
    This reverts commit 6181939f8023e5a26acfd6a36a3c4e889c6f7360.
    This reverts commit 5c03b199ef115546d399a904004cc1fd2f5836f0.
    This reverts commit d7d95511034f690f93e2b191fbb09dfb57bbf8c5.
    This reverts commit 4bb8f0f5bbd8fe626e79c078154a8b836a71a584.
    This reverts commit a13d3340d02032162b0e23a7cc528f83f0daf8c7.
    This reverts commit 88537d3a43b9a4c6867d502881ce4cbe1d4ba3cf.
    This reverts commit b4a4d975ee3036ed5d63f262e5d9355194f32f1a.
    This reverts commit 410df02cc7234cf784269f733e533a597a211d64.
    This reverts commit 74f76fdcaea32407bfb9b21071e950346cb35a1e.
    This reverts commit 16fcfd184c11dfba7aa0f2e4071e458d5056144a.
    This reverts commit 61cf9f6a0131c3e104282d880c1b3fac8f3334b3.

--- pkg/frontend/frontend.go
+++ pkg/frontend/frontend.go
@@ -71,8 +71,6 @@ type frontend struct {
 	azureActionsFactory azureActionsFactory
 	ocEnricherFactory   ocEnricherFactory
 
-	quotaValidator QuotaValidator
-
 	l net.Listener
 	s *http.Server
 
@@ -111,7 +109,7 @@ func NewFrontend(ctx context.Context,
 	aead encryption.AEAD,
 	kubeActionsFactory kubeActionsFactory,
 	azureActionsFactory azureActionsFactory,
-	ocEnricherFactory ocEnricherFactory) (*frontend, error) {
+	ocEnricherFactory ocEnricherFactory) (Runnable, error) {
 	f := &frontend{
 		auditLog:                      auditLog,
 		baseLog:                       baseLog,
@@ -127,7 +125,6 @@ func NewFrontend(ctx context.Context,
 		kubeActionsFactory:            kubeActionsFactory,
 		azureActionsFactory:           azureActionsFactory,
 		ocEnricherFactory:             ocEnricherFactory,
-		quotaValidator:                quotaValidator{},
 
 		// add default installation version so it's always supported
 		enabledOcpVersions: map[string]*api.OpenShiftVersion{
