07e7c85131f1978a4eff133bbc60367fcc3e0ec1
Srinivas Atmakuri <sreenivas.atmakuri@live.com>
2023-02-08 10:16:44 +0530

    minor change

--- pkg/cluster/install.go
+++ pkg/cluster/install.go
@@ -173,9 +173,9 @@ func (m *manager) Update(ctx context.Context) error {
 		steps.Condition(m.apiServersReady, 30*time.Minute, true),
 		steps.Action(m.configureAPIServerCertificate),
 		steps.Action(m.configureIngressCertificate),
+		steps.Action(m.renewMDSDCertificate),
 		steps.Action(m.updateOpenShiftSecret),
 		steps.Action(m.updateAROSecret),
-		steps.Action(m.renewMDSDCertificate),
 	}
 
 	if m.adoptViaHive {
