6ad1d5dfe556f284939e8282c8288978a7de6b04
Ankur Singh <49074231+sankur-codes@users.noreply.github.com>
2023-02-15 21:58:24 +0530

    Added clusterNetworkEntry

--- pkg/portal/cluster/network.go
+++ pkg/portal/cluster/network.go
@@ -5,20 +5,27 @@ package cluster
 
 import (
 	"context"
+	"fmt"
 	"strconv"
 
 	networkv1 "github.com/openshift/api/network/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+type ClusterNetworkEntry struct {
+	CIDR             string `json:"cidr"`
+	HostSubnetLength string `json:"hostsubnetlength"`
+}
+
 type ClusterNetwork struct {
-	Name               string `json:"name"`
-	PluginName         string `json:"pluginname"`
-	NetworkCIDR        string `json:"networkcidr"`
-	ServiceNetworkCIDR string `json:"servicenetworkcidr"`
-	HostSubnetLength   string `json:"hostsubnetlength"`
-	MTU                string `json:"mtu"`
-	VXLANPort          string `json:"vxlanport"`
+	Name                  string                `json:"name"`
+	PluginName            string                `json:"pluginname"`
+	NetworkCIDR           string                `json:"networkcidr"`
+	ServiceNetworkCIDR    string                `json:"servicenetworkcidr"`
+	HostSubnetLength      string                `json:"hostsubnetlength"`
+	MTU                   string                `json:"mtu"`
+	VXLANPort             string                `json:"vxlanport"`
+	ClusterNetworkEntries []ClusterNetworkEntry `json:"clusternetworkentry"`
 }
 
 type ClusterNetworkList struct {
@@ -33,6 +40,9 @@ func NetworkData(clusterNetworks *networkv1.ClusterNetworkList) *NetworkInformat
 	final := &NetworkInformation{
 		ClusterNetworkList: getClusterNetworkList(clusterNetworks),
 	}
+	fmt.Println()
+	fmt.Println(final)
+	fmt.Println()
 	return final
 }
 
@@ -44,16 +54,31 @@ func getClusterNetworkList(clusterNetworks *networkv1.ClusterNetworkList) Cluste
 
 	for i, clusNet := range clusterNetworks.Items {
 		clusterNetwork := ClusterNetwork{
-			Name:               clusNet.Name,
-			PluginName:         clusNet.PluginName,
-			NetworkCIDR:        clusNet.Network,
-			ServiceNetworkCIDR: clusNet.ServiceNetwork,
-			HostSubnetLength:   strconv.FormatUint(uint64(clusNet.HostSubnetLength), 10),
-			MTU:                getMTU(clusNet),
-			VXLANPort:          getVXLANPort(clusNet),
+			Name:                  clusNet.Name,
+			PluginName:            clusNet.PluginName,
+			NetworkCIDR:           clusNet.Network,
+			ServiceNetworkCIDR:    clusNet.ServiceNetwork,
+			HostSubnetLength:      strconv.FormatUint(uint64(clusNet.HostSubnetLength), 10),
+			MTU:                   getMTU(clusNet),
+			VXLANPort:             getVXLANPort(clusNet),
+			ClusterNetworkEntries: getClusterNetworkEntries(clusNet),
 		}
 		final.ClusterNetwork[i] = clusterNetwork
 	}
+	return final
+}
+
+func getClusterNetworkEntries(clusterNetwork networkv1.ClusterNetwork) []ClusterNetworkEntry {
+	final := make([]ClusterNetworkEntry, len(clusterNetwork.ClusterNetworks))
+
+	for i, clusNetEnt := range clusterNetwork.ClusterNetworks {
+		clusterNetworkEntry := ClusterNetworkEntry{
+			CIDR:             clusNetEnt.CIDR,
+			HostSubnetLength: strconv.FormatUint(uint64(clusNetEnt.HostSubnetLength), 10),
+		}
+		final[i] = clusterNetworkEntry
+	}
+
 	return final
 }
 
