dd4f4a6f18fec4d337657feddfc3083485e9a765
Matthew Barnes <mbarnes@fedoraproject.org>
2023-02-17 12:08:56 -0500

    Remove pkg/util/azureclient/graphrbac
    No longer used.

--- pkg/util/azureclient/graphrbac/generate.go
+++
@@ -1,8 +0,0 @@
-package graphrbac
-
-// Copyright (c) Microsoft Corporation.
-// Licensed under the Apache License 2.0.
-
-//go:generate rm -rf ../../../util/mocks/$GOPACKAGE
-//go:generate go run ../../../../vendor/github.com/golang/mock/mockgen -destination=../../../util/mocks/azureclient/$GOPACKAGE/$GOPACKAGE.go github.com/Azure/ARO-RP/pkg/util/azureclient/$GOPACKAGE ApplicationsClient,ServicePrincipalClient
-//go:generate go run ../../../../vendor/golang.org/x/tools/cmd/goimports -local=github.com/Azure/ARO-RP -e -w ../../../util/mocks/azureclient/$GOPACKAGE/$GOPACKAGE.go
