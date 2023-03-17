e007f1e0ff00cfa518201e66e78c78545d4795a8
Nick Hundstad <v-nhundstad@microsoft.com>
2023-03-17 18:30:04 -0400

    Add discription for why the retry delay was changed to 3 seconds

--- pkg/util/azureclient/applens/applens_client_options.go
+++ pkg/util/azureclient/applens/applens_client_options.go
@@ -26,6 +26,12 @@ func NewClientOptions() *ClientOptions {
 		azcore.ClientOptions{
 			Retry: policy.RetryOptions{
 				MaxRetries: 3,
+				// ARO-2567
+				// If the retry logic takes longer than 60 seconds,
+				// the correct error message will not be captured.
+				// With a setting of 3 seconds it was erroring out
+				// in ~30 seconds (3 seconds + 12 seconds + round
+				// trip / response time from all 3 calls).
 				RetryDelay: time.Second * 3,
 			},
 			Telemetry: policy.TelemetryOptions{
