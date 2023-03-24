594d67419f76e0c0a6bc5ce704d5fea7972ed3c8
tschneid <tschneid@redhat.com>
2023-03-23 19:45:24 -0500

    default to empty outboundtype, let RP decide

--- python/az/aro/azext_aro/custom.py
+++ python/az/aro/azext_aro/custom.py
@@ -140,7 +140,7 @@ def aro_create(cmd,  # pylint: disable=too-many-locals
         network_profile=openshiftcluster.NetworkProfile(
             pod_cidr=pod_cidr or '10.128.0.0/14',
             service_cidr=service_cidr or '172.30.0.0/16',
-            outbound_type=outbound_type or 'Loadbalancer',
+            outbound_type=outbound_type or '',
         ),
         master_profile=openshiftcluster.MasterProfile(
             vm_size=master_vm_size or 'Standard_D8s_v3',
