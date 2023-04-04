5af68ffc68bfce1cb026d73606d6b3aae78a2af8
Matthew Barnes <mbarnes@fedoraproject.org>
2023-04-04 10:09:50 -0400

    azureclient: Remove ActiveDirectoryGraphScope
    No longer used.

--- pkg/util/azureclient/environments.go
+++ pkg/util/azureclient/environments.go
@@ -30,10 +30,9 @@ type AROEnvironment struct {
 	Cloud cloud.Configuration
 	// Microsoft identity platform scopes used by ARO
 	// See https://learn.microsoft.com/EN-US/azure/active-directory/develop/scopes-oidc#the-default-scope
-	ResourceManagerScope      string
-	KeyVaultScope             string
-	ActiveDirectoryGraphScope string
-	MicrosoftGraphScope       string
+	ResourceManagerScope string
+	KeyVaultScope        string
+	MicrosoftGraphScope  string
 }
 
 // AzureRbacPDPEnvironment contains cloud specific instance of Authz RBAC PDP Remote Server
@@ -59,10 +58,9 @@ var (
 			Endpoint:   "https://%s.authorization.azure.net/providers/Microsoft.Authorization/checkAccess?api-version=2021-06-01-preview",
 			OAuthScope: "https://authorization.azure.net/.default",
 		},
-		ResourceManagerScope:      azure.PublicCloud.ResourceManagerEndpoint + "/.default",
-		KeyVaultScope:             azure.PublicCloud.ResourceIdentifiers.KeyVault + "/.default",
-		ActiveDirectoryGraphScope: azure.PublicCloud.GraphEndpoint + "/.default",
-		MicrosoftGraphScope:       azure.PublicCloud.MicrosoftGraphEndpoint + "/.default",
+		ResourceManagerScope: azure.PublicCloud.ResourceManagerEndpoint + "/.default",
+		KeyVaultScope:        azure.PublicCloud.ResourceIdentifiers.KeyVault + "/.default",
+		MicrosoftGraphScope:  azure.PublicCloud.MicrosoftGraphEndpoint + "/.default",
 	}
 
 	// USGovernmentCloud contains additional ARO information for the US Gov cloud environment.
@@ -82,10 +80,9 @@ var (
 			Endpoint:   "https://%s.authorization.azure.us/providers/Microsoft.Authorization/checkAccess?api-version=2021-06-01-preview",
 			OAuthScope: "https://authorization.azure.us/.default",
 		},
-		ResourceManagerScope:      azure.USGovernmentCloud.ResourceManagerEndpoint + "/.default",
-		KeyVaultScope:             azure.USGovernmentCloud.ResourceIdentifiers.KeyVault + "/.default",
-		ActiveDirectoryGraphScope: azure.USGovernmentCloud.GraphEndpoint + "/.default",
-		MicrosoftGraphScope:       azure.USGovernmentCloud.MicrosoftGraphEndpoint + "/.default",
+		ResourceManagerScope: azure.USGovernmentCloud.ResourceManagerEndpoint + "/.default",
+		KeyVaultScope:        azure.USGovernmentCloud.ResourceIdentifiers.KeyVault + "/.default",
+		MicrosoftGraphScope:  azure.USGovernmentCloud.MicrosoftGraphEndpoint + "/.default",
 	}
 )
 
