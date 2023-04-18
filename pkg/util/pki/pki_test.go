2f3d44ee181fb98dda63936215b207e8f0ee3e58
Nick Hundstad <v-nhundstad@microsoft.com>
2023-04-18 12:15:11 -0400

    Add test for us endpoints

--- pkg/util/pki/pki_test.go
+++ pkg/util/pki/pki_test.go
@@ -55,3 +55,48 @@ func TestGetTlsConfig(t *testing.T) {
 
 	defer resp.Body.Close()
 }
+
+func TestFairfaxGetTlsConfig(t *testing.T) {
+	kpiUrl := "https://issuer.pki.azure.us/dsms/issuercertificates?getissuersv3&caName=%s"
+	testUrl := "https://diag-runtimehost-prod-bn1-001.azurewebsites.us"
+
+	caName := "ssladmin"
+	caCertPool, err := GetTlsCertPool(kpiUrl, caName)
+
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	if caCertPool == nil {
+		t.Error("Expected non-nil CertPool")
+	}
+
+	url := fmt.Sprintf(kpiUrl, caName)
+	if _, ok := caMap[url]; !ok {
+		t.Errorf("Expected caMap to contain entry for %s", caName)
+	}
+
+	// Create a new tls.Config with the custom CA certificate
+	tlsConfig := &tls.Config{
+		RootCAs:            caCertPool,
+		InsecureSkipVerify: false,
+	}
+
+	// Use the tls.Config with your client
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: tlsConfig,
+		},
+	}
+
+	resp, err := client.Get(testUrl)
+
+	if err != nil {
+		if _, ok := err.(x509.UnknownAuthorityError); ok {
+			t.Errorf("Invalid SSL/TLS certificate")
+		}
+		t.Errorf("Error calling %s", testUrl)
+	}
+
+	defer resp.Body.Close()
+}
