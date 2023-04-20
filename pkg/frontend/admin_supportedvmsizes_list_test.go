83184a2881a08d4abfda2352d27e03e05f000e02
Srinivas Atmakuri <sreenivas.atmakuri@live.com>
2023-04-20 22:30:05 +0530

    Test cases for GA Get SupportedVMSizes

---
+++ pkg/frontend/admin_supportedvmsizes_list_test.go
@@ -0,0 +1,72 @@
+package frontend
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/go-test/deep"
+
+	"github.com/Azure/ARO-RP/pkg/api"
+	"github.com/Azure/ARO-RP/pkg/api/validate"
+)
+
+func TestSupportedvmsizes(t *testing.T) {
+	mastervmsizes := validate.SupportedVMSizesByRole(validate.VMRoleMaster)
+	workervmsizes := validate.SupportedVMSizesByRole(validate.VMRoleWorker)
+
+	type test struct {
+		name         string
+		vmRole       string
+		wantResponse map[api.VMSize]api.VMSizeStruct
+		wantError    string
+	}
+
+	for _, tt := range []*test{
+		{
+			name:         "vmRole is invalid",
+			vmRole:       "invalidVMRole",
+			wantError:    `400: InvalidParameter: : The provided vmRole 'invalidVMRole' is invalid. vmRole can only be master or worker`,
+			wantResponse: nil,
+		},
+		{
+			name:         "vmRole is empty",
+			vmRole:       "",
+			wantError:    `400: InvalidParameter: : The provided vmRole '' is invalid. vmRole can only be master or worker`,
+			wantResponse: nil,
+		},
+		{
+			name:         "master as vmRole",
+			vmRole:       "master",
+			wantError:    "",
+			wantResponse: mastervmsizes,
+		},
+		{
+			name:         "worker as vmRole",
+			vmRole:       "worker",
+			wantError:    "",
+			wantResponse: workervmsizes,
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &frontend{}
+			gotResponse, err := f.supportedVMSizesForRole(tt.vmRole)
+			if err != nil && err.Error() != tt.wantError || err == nil && tt.wantError != "" {
+				t.Error(err)
+			}
+			// if gotResponse and wantResponse are nil, skip
+			if gotResponse != nil || tt.wantResponse != nil {
+				v := map[api.VMSize]api.VMSizeStruct{}
+				err = json.Unmarshal(gotResponse, &v)
+				if err != nil {
+					t.Error(err)
+				}
+				if diff := deep.Equal(v, tt.wantResponse); diff != nil {
+					t.Errorf("unexpected response %s, wanted to match %#v (%s)", string(gotResponse), tt.wantResponse, diff)
+				}
+			}
+		})
+	}
+}
