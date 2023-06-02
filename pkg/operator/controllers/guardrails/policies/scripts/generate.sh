d14ac0e8003d444f76844a70ff48b0dd709079c1
Arris Li <huili@redhat.com>
2023-06-02 12:40:23 +1200

    update generate.sh to support single dir gen

--- pkg/operator/controllers/guardrails/policies/scripts/generate.sh
+++ pkg/operator/controllers/guardrails/policies/scripts/generate.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 
-set -eu
+set -euo pipefail
+IFS=$'\n\t'
 
 template_src_path="gktemplates-src"
 template_path="gktemplates"
 
+generate_template() {
+  local tmpl=$1
+  local filename="$(basename -- ${tmpl} .tmpl).yaml"
+  echo "Generating ${template_path}/${filename} from ${tmpl}"
+  gomplate -f "${tmpl}" > "${template_path}/${filename}"
+}
+
+check_required_files() {
+  local src_dir=$1
+  for req in src.rego src_test.rego; do
+    if [[ ! -f "${src_dir}/${req}" ]]; then
+      echo "${src_dir}/${req} is missing"
+      return 1
+    fi
+  done
+}
+
 main() {
+  local sub_dir="${1:-}"
+  local search_path="${template_src_path}"
+
+  if [[ ! -z "${sub_dir}" ]]; then
+    search_path="${search_path}/${sub_dir}"
+  fi
 
   if [[ ! -d ${template_path} ]]; then
     mkdir -p "${template_path}"
   fi
 
-  for tmpl in $(find ${template_src_path} -name '*.tmpl'); do
-    filename="$(basename -- ${tmpl} .tmpl).yaml"
-    echo "Generating ${template_path}/${filename} from ${tmpl}"
-    gomplate -f "${tmpl}" > "${template_path}/${filename}"
-
-    src_dir="$(dirname "${tmpl}")"
-    for req in src.rego src_test.rego; do
-      if [[ ! -f "${src_dir}/${req}" ]]; then
-        echo "${src_dir}/${req} is missing"
-        exit 1
-      fi
-    done
+  find "${search_path}" -name '*.tmpl' | while read -r tmpl; do
+    generate_template "${tmpl}" || exit 1
+    check_required_files "$(dirname "${tmpl}")" || exit 1
   done
 }
 
-main
+main "$@"
