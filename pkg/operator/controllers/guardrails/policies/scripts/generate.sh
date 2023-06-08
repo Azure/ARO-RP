6a8e8d2d503653fb1f03a136c364a34259600579
Arris Li <huili@redhat.com>
2023-06-08 23:56:29 +1200

    update scripts to support params

--- pkg/operator/controllers/guardrails/policies/scripts/generate.sh
+++ pkg/operator/controllers/guardrails/policies/scripts/generate.sh
@@ -1,43 +1,39 @@
 #!/bin/bash
 
-set -euo pipefail
+set -e
 IFS=$'\n\t'
 
 template_src_path="gktemplates-src"
 template_path="gktemplates"
 
-generate_template() {
-  local tmpl=$1
-  local filename="$(basename -- ${tmpl} .tmpl).yaml"
-  echo "Generating ${template_path}/${filename} from ${tmpl}"
-  gomplate -f "${tmpl}" > "${template_path}/${filename}"
-}
-
-check_required_files() {
-  local src_dir=$1
-  for req in src.rego src_test.rego; do
-    if [[ ! -f "${src_dir}/${req}" ]]; then
-      echo "${src_dir}/${req} is missing"
-      return 1
-    fi
-  done
-}
-
 main() {
-  local sub_dir="${1:-}"
-  local search_path="${template_src_path}"
-
-  if [[ ! -z "${sub_dir}" ]]; then
-    search_path="${search_path}/${sub_dir}"
-  fi
-
   if [[ ! -d ${template_path} ]]; then
     mkdir -p "${template_path}"
   fi
 
-  find "${search_path}" -name '*.tmpl' | while read -r tmpl; do
-    generate_template "${tmpl}" || exit 1
-    check_required_files "$(dirname "${tmpl}")" || exit 1
+  # Optional policy folder param
+  policy_folder="$1"
+  if [[ -n $policy_folder ]]; then
+    # If policy_folder was provided, prepend it with path and trailing slash
+    policy_folder="${template_src_path}/${policy_folder}/"
+  else
+    # Otherwise, just operate on the whole template source path
+    policy_folder="${template_src_path}/"
+  fi
+
+  # Go through all the .tmpl and .rego files and generate constraint templates 
+  for tmpl in $(find ${policy_folder} -name '*.tmpl'); do
+    filename="$(basename -- ${tmpl} .tmpl).yaml"
+    echo "Generating ${template_path}/${filename} from ${tmpl}"
+    gomplate -f "${tmpl}" > "${template_path}/${filename}"
+
+    src_dir="$(dirname "${tmpl}")"
+    for req in src.rego src_test.rego; do
+      if [[ ! -f "${src_dir}/${req}" ]]; then
+        echo "${src_dir}/${req} is missing"
+        exit 1
+      fi
+    done
   done
 }
 
