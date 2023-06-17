cc0180b6b94eda5e015cad59c95648939ffc705f
nont <nontkrub@gmail.com>
2023-06-16 17:23:24 -0700

    Add the preconfiguredNSG validation to dynamic

--- pkg/api/validate/openshiftcluster_validatedynamic.go
+++ pkg/api/validate/openshiftcluster_validatedynamic.go
@@ -199,6 +199,11 @@ func (dv *openShiftClusterDynamicValidator) Dynamic(ctx context.Context) error {
 		return err
 	}
 
+	err = fpDynamic.ValidatePreConfiguredNSGs(ctx, dv.oc, subnets)
+	if err != nil {
+		return err
+	}
+
 	tenantID := dv.subscriptionDoc.Subscription.Properties.TenantID
 	options := dv.env.Environment().ClientSecretCredentialOptions()
 	tokenCredential, err := azidentity.NewClientSecretCredential(
@@ -254,6 +259,11 @@ func (dv *openShiftClusterDynamicValidator) Dynamic(ctx context.Context) error {
 	}
 
 	err = spDynamic.ValidateEncryptionAtHost(ctx, dv.oc)
+	if err != nil {
+		return err
+	}
+
+	err = spDynamic.ValidatePreConfiguredNSGs(ctx, dv.oc, subnets)
 	if err != nil {
 		return err
 	}
