e6816b8fa67faa9748fc6ebc9b21d8f687235194
tschneid <tschneid@redhat.com>
2023-08-17 18:49:54 -0500

    add managed ips to arm templates

--- pkg/cluster/deploybaseresources.go
+++ pkg/cluster/deploybaseresources.go
@@ -153,10 +153,26 @@ func (m *manager) deployBaseResourceTemplate(ctx context.Context) error {
 
 	// Create a public load balancer routing if needed
 	if m.doc.OpenShiftCluster.Properties.NetworkProfile.OutboundType == api.OutboundTypeLoadbalancer {
+		var outboundIPs []api.ResourceReference
+		if m.doc.OpenShiftCluster.Properties.APIServerProfile.Visibility == api.VisibilityPublic {
+			resources = append(resources,
+				m.networkPublicIPAddress(azureRegion, infraID+"-pip-v4"),
+			)
+			if m.doc.OpenShiftCluster.Properties.NetworkProfile.LoadBalancerProfile.ManagedOutboundIPs != nil {
+				outboundIPs = append(outboundIPs, api.ResourceReference{ID: m.doc.OpenShiftCluster.Properties.ClusterProfile.ResourceGroupID + "/providers/Microsoft.Network/publicIPAddresses/" + infraID + "-pip-v4"})
+			}
+		}
+		if m.doc.OpenShiftCluster.Properties.NetworkProfile.LoadBalancerProfile.ManagedOutboundIPs != nil {
+			for i := len(outboundIPs); i < m.doc.OpenShiftCluster.Properties.NetworkProfile.LoadBalancerProfile.ManagedOutboundIPs.Count; i++ {
+				ipName := genManagedOutboundIPName()
+				resources = append(resources, m.networkPublicIPAddress(azureRegion, ipName))
+				outboundIPs = append(outboundIPs, api.ResourceReference{ID: m.doc.OpenShiftCluster.Properties.ClusterProfile.ResourceGroupID + "/providers/Microsoft.Network/publicIPAddresses/" + ipName})
+			}
+		}
+		m.patchEffectiveOutboundIPs(ctx, outboundIPs)
 		// Normal private clusters still need a public load balancer
 		resources = append(resources,
-			m.networkPublicIPAddress(azureRegion, infraID+"-pip-v4"),
-			m.networkPublicLoadBalancer(azureRegion),
+			m.networkPublicLoadBalancer(azureRegion, outboundIPs),
 		)
 		// If the cluster is public we still want the default public IP address
 		if m.doc.OpenShiftCluster.Properties.IngressProfiles[0].Visibility == api.VisibilityPublic {
