a4d5791da19c2a77ec4ea4d1f4ff10e1b354d6d3
jonachang <jonachang@microsoft.com>
2023-10-16 16:37:32 -0700

    add testing words

--- pkg/poc/frontend.go
+++ pkg/poc/frontend.go
@@ -55,7 +55,7 @@ func (f *frontend) getRouter() chi.Router {
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		f.logger.Infof("Received request: %s", time.Now().String())
-		w.Write([]byte("****** ARO-RP on AKS PoC ******"))
+		w.Write([]byte("****** ARO-RP on AKS PoC MISE******"))
 	})
 	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
