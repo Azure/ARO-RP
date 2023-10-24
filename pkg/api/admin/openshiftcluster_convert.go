8a90e89e2878449ca488be08dc76671f52f72120
Nicolas Ontiveros <nicolas.ontiveros@microsoft.com>
2023-10-24 07:01:03 -0700

    Fix openshiftcluster_convert.go

--- pkg/api/admin/openshiftcluster_convert.go
+++ pkg/api/admin/openshiftcluster_convert.go
@@ -31,7 +31,7 @@ func (c openShiftClusterConverter) ToExternal(oc *api.OpenShiftCluster) interfac
 			CreatedAt:               oc.Properties.CreatedAt,
 			CreatedBy:               oc.Properties.CreatedBy,
 			ProvisionedBy:           oc.Properties.ProvisionedBy,
-			MaintenanceState:        oc.Properties.MaintenanceState,
+			MaintenanceState:        MaintenanceState(oc.Properties.MaintenanceState),
 			ClusterProfile: ClusterProfile{
 				Domain:               oc.Properties.ClusterProfile.Domain,
 				Version:              oc.Properties.ClusterProfile.Version,
@@ -234,7 +234,7 @@ func (c openShiftClusterConverter) ToInternal(_oc interface{}, out *api.OpenShif
 	out.Properties.OperatorVersion = oc.Properties.OperatorVersion
 	out.Properties.CreatedBy = oc.Properties.CreatedBy
 	out.Properties.ProvisionedBy = oc.Properties.ProvisionedBy
-	out.Properties.MaintenanceState = oc.Properties.MaintenanceState
+	out.Properties.MaintenanceState = api.MaintenanceState(oc.Properties.MaintenanceState)
 	out.Properties.ClusterProfile.Domain = oc.Properties.ClusterProfile.Domain
 	out.Properties.ClusterProfile.FipsValidatedModules = api.FipsValidatedModules(oc.Properties.ClusterProfile.FipsValidatedModules)
 	out.Properties.ClusterProfile.Version = oc.Properties.ClusterProfile.Version
