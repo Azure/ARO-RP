ecb0cf15659766d3046d9637a6fccdde3aaa6fd7
Ana Clara Zoppi Serpa <azoppiserpa@microsoft.com>
2023-12-15 12:11:16 -0800

    removing license

--- python/az/aro/azext_aro/aaz/latest/network/vnet/_show.py
+++ python/az/aro/azext_aro/aaz/latest/network/vnet/_show.py
@@ -1,6 +1,3 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the Apache License 2.0.
-
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
