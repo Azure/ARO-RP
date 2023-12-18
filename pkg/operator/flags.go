819734834732f4eeda601d2553f348b311fa9c22
Ana Clara Zoppi Serpa <azoppiserpa@microsoft.com>
2023-12-18 14:54:50 -0800

    running validate-go

--- pkg/operator/flags.go
+++ pkg/operator/flags.go
@@ -1,5 +1,8 @@
 package operator
 
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
 const (
 	AlertWebhookEnabled                = "aro.alertwebhook.enabled"
 	AzureSubnetsEnabled                = "aro.azuresubnets.enabled"
