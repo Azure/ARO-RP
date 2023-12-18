f0baa52efbc946e635f2b155bf16e52307a375ba
Ana Clara Zoppi Serpa <azoppiserpa@microsoft.com>
2023-12-18 14:49:44 -0800

    removing controllerEnabled

--- pkg/operator/controllers/workaround/workaround_controller.go
+++ pkg/operator/controllers/workaround/workaround_controller.go
@@ -21,8 +21,6 @@ import (
 
 const (
 	ControllerName = "Workaround"
-
-	controllerEnabled = operator.WorkaroundEnabled
 )
 
 // Reconciler the point of the workaround controller is to apply
@@ -51,7 +49,7 @@ func (r *Reconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.
 		return reconcile.Result{}, err
 	}
 
-	if !instance.Spec.OperatorFlags.GetSimpleBoolean(controllerEnabled) {
+	if !instance.Spec.OperatorFlags.GetSimpleBoolean(operator.WorkaroundEnabled) {
 		r.log.Debug("controller is disabled")
 		return reconcile.Result{}, nil
 	}
