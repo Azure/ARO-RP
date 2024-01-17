6d3042d4f13d1a30ed868ce0524c5edb35bd3ff8
kimorris27 <kimorris@redhat.com>
2024-01-17 15:18:30 -0600

    Simplify logic and properly handle the case where the Service is not found during the delete call

--- test/e2e/adminapi_delete_managedresource.go
+++ test/e2e/adminapi_delete_managedresource.go
@@ -54,10 +54,7 @@ var _ = Describe("[Admin API] Delete managed resource action", func() {
 			// wait for deletion to prevent flakes on retries
 			Eventually(func(g Gomega, ctx context.Context) {
 				err := clients.Kubernetes.CoreV1().Services("default").Delete(ctx, "test", metav1.DeleteOptions{})
-				Expect(err).NotTo(HaveOccurred())
-
-				_, err = clients.Kubernetes.CoreV1().Services("default").Get(ctx, "test", metav1.GetOptions{})
-				g.Expect(kerrors.IsNotFound(err)).To(BeTrue(), "expect Service to be deleted")
+				g.Expect((err == nil || kerrors.IsNotFound(err))).To(BeTrue(), "expect Service to be deleted")
 			}).WithContext(ctx).WithTimeout(DefaultEventuallyTimeout).Should(Succeed())
 		}()
 
