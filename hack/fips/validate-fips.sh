f66a58c73de522fea41705bb8d917d12149d02b1
Amber Brown <ambrown@redhat.com>
2024-01-23 09:18:48 +1100

    fix

--- hack/fips/validate-fips.sh
+++ hack/fips/validate-fips.sh
@@ -5,8 +5,8 @@ set -xe
 # check if we can build and have built a valid FIPS-compatible binary
 res=$(go run github.com/acardace/fips-detect@v0.0.0-20230309083406-7157dae5bafd ${1} -j)
 
-binary=$(echo $res | jq -r '.goBinaryFips.value')
-lib=$(echo $res | jq -r '.cryptoLibFips.value')
+binary=$(echo $res | go run ./hack/jq -r '.goBinaryFips.value')
+lib=$(echo $res | go run ./hack/jq -r '.cryptoLibFips.value')
 
 if [[ $binary == "false" ]]; then
 	echo "binary is not FIPS compatible"
