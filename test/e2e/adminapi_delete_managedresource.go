727c6cddf7232fd8f2e2cbed45a288d3ac607657
Maitiú Ó Ciaráin <mociarain@redhat.com>
2024-01-30 13:29:07 +0100

    Update adminapi_delete_managedresources.go

--- test/e2e/adminapi_delete_managedresource.go
+++ test/e2e/adminapi_delete_managedresource.go
@@ -46,8 +46,8 @@ var _ = Describe("[Admin API] Delete managed resource action", func() {
 		var pipAddressID string
 
 		By("creating a test service of type loadbalancer")
-		_, err := clients.Kubernetes.CoreV1().Services("default").Create(ctx, &loadBalancerService, metav1.CreateOptions{})
-		Expect(err).NotTo(HaveOccurred())
+		creationCall := clients.Kubernetes.CoreV1().Services("default").Create
+		CreateK8sObjectWithRetry(ctx, creationCall, &loadBalancerService, metav1.CreateOptions{})
 
 		defer func() {
 			By("cleaning up the k8s loadbalancer service")
@@ -66,8 +66,8 @@ var _ = Describe("[Admin API] Delete managed resource action", func() {
 
 		// wait for ingress IP to be assigned as this indicate the service is ready
 		Eventually(func(g Gomega, ctx context.Context) {
-			service, err = clients.Kubernetes.CoreV1().Services("default").Get(ctx, "test", metav1.GetOptions{})
-			g.Expect(err).NotTo(HaveOccurred())
+			getCall := clients.Kubernetes.CoreV1().Services("default").Get
+			service = GetK8sObjectWithRetry(ctx, getCall, "test", metav1.GetOptions{})
 			g.Expect(service.Status.LoadBalancer.Ingress).To(HaveLen(1))
 		}).WithContext(ctx).WithTimeout(DefaultEventuallyTimeout).Should(Succeed())
 
