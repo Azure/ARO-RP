2f6263e1ea9e7a5aec23ad8daa8ebdd086fcf090
Amber Brown <ambrown@redhat.com>
2024-02-06 10:46:37 +1100

    Go 1.20 changes

--- pkg/containerinstall/install_test.go
+++ pkg/containerinstall/install_test.go
@@ -27,7 +27,7 @@ import (
 	testlog "github.com/Azure/ARO-RP/test/util/log"
 )
 
-const TEST_PULLSPEC = "registry.access.redhat.com/ubi8/go-toolset:1.18.10"
+const TEST_PULLSPEC = "registry.access.redhat.com/ubi8/go-toolset:1.20.10"
 
 var _ = Describe("Podman", Ordered, func() {
 	var err error
