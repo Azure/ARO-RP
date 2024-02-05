bee510d7e9d53844334d570c0f6cd2797398a3b8
Amber Brown <ambrown@redhat.com>
2024-02-05 11:43:26 +1100

    update

--- pkg/cluster/gatherlogs.go
+++ pkg/cluster/gatherlogs.go
@@ -4,27 +4,24 @@ package cluster
 // Licensed under the Apache License 2.0.
 
 import (
-	"bufio"
 	"context"
-	"encoding/base64"
 	"encoding/json"
-	"strings"
 
-	mgmtcompute "github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2020-06-01/compute"
-	mgmtstorage "github.com/Azure/azure-sdk-for-go/services/storage/mgmt/2019-06-01/storage"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/Azure/ARO-RP/pkg/cluster/failurediagnostics"
 	"github.com/Azure/ARO-RP/pkg/util/steps"
-	"github.com/Azure/ARO-RP/pkg/util/stringutils"
 )
 
 func (m *manager) gatherFailureLogs(ctx context.Context) {
+	d := failurediagnostics.NewFailureDiagnostics(m.log, m.env, m.doc, m.virtualMachines, m.storage)
+
 	for _, f := range []func(context.Context) (interface{}, error){
 		m.logClusterVersion,
 		m.logNodes,
 		m.logClusterOperators,
 		m.logIngressControllers,
-		m.logAzureInformation,
+		d.LogAzureInformation,
 	} {
 		o, err := f(ctx)
 		if err != nil {
@@ -106,69 +103,4 @@ func (m *manager) logIngressControllers(ctx context.Context) (interface{}, error
 	}
 
 	return ics.Items, nil
-}
-
-type vminfo struct {
-	VMID     string
-	Name     string
-	SKU      string
-	State    string
-	Statuses []mgmtcompute.InstanceViewStatus
-}
-
-func (m *manager) logAzureInformation(ctx context.Context) (interface{}, error) {
-	if m.virtualMachines == nil {
-		return nil, nil
-	}
-
-	items := make([]interface{}, 0)
-	resourceGroupName := stringutils.LastTokenByte(m.doc.OpenShiftCluster.Properties.ClusterProfile.ResourceGroupID, '/')
-	vms, err := m.virtualMachines.List(ctx, resourceGroupName)
-	if err != nil {
-		items = append(items, err)
-		return items, nil
-	}
-
-	consoleURIs := make([][]string, 0)
-
-	for _, v := range vms {
-		items = append(items, v)
-		if v.InstanceView != nil && v.InstanceView.BootDiagnostics != nil && v.InstanceView.BootDiagnostics.SerialConsoleLogBlobURI != nil {
-			consoleURIs = append(consoleURIs, []string{*v.Name, *v.InstanceView.BootDiagnostics.SerialConsoleLogBlobURI})
-		}
-	}
-
-	blob, err := m.storage.BlobService(ctx, resourceGroupName, "cluster"+m.doc.OpenShiftCluster.Properties.StorageSuffix, mgmtstorage.R, mgmtstorage.SignedResourceTypesO)
-	if err != nil {
-		items = append(items, err)
-		return items, nil
-	}
-
-	for _, i := range consoleURIs {
-		parts := strings.Split(i[1], "/")
-
-		c := blob.GetContainerReference(parts[1])
-		b := c.GetBlobReference(parts[2])
-
-		rc, err := b.Get(nil)
-		if err != nil {
-			items = append(items, err)
-			continue
-		}
-		defer rc.Close()
-
-		logForVM := m.log.WithField("failedRoleInstance", i[0])
-
-		b64Reader := base64.NewDecoder(base64.StdEncoding, rc)
-
-		scanner := bufio.NewScanner(b64Reader)
-		for scanner.Scan() {
-			logForVM.Info(scanner.Text())
-		}
-		if err := scanner.Err(); err != nil {
-			items = append(items, err)
-		}
-	}
-
-	return items, nil
 }
