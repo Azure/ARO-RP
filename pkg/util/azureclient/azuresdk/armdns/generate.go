4c510a35053e3b956649b844c8679144f9ca8399
jonachang <jonachang@microsoft.com>
2024-02-13 08:58:10 -0800

    fix file path

--- pkg/util/azureclient/azuresdk/armdns/generate.go
+++ pkg/util/azureclient/azuresdk/armdns/generate.go
@@ -3,6 +3,6 @@ package armdns
 // Copyright (c) Microsoft Corporation.
 // Licensed under the Apache License 2.0.
 
-//go:generate rm -rf ../../../../../mocks/azureclient/azuresdk/$GOPACKAGE
-//go:generate go run ../../../../../vendor/github.com/golang/mock/mockgen -destination=../../../mocks/azureclient/azuresdk/$GOPACKAGE/armdns.go -source=armdns.go
-//go:generate go run ../../../../../vendor/golang.org/x/tools/cmd/goimports -local=github.com/Azure/ARO-RP -e -w ../../../mocks/azureclient/azuresdk/$GOPACKAGE/armdns.go
+//go:generate rm -rf ../../../../../pkg/util/mocks/azureclient/azuresdk/$GOPACKAGE
+//go:generate go run ../../../../../vendor/github.com/golang/mock/mockgen -destination=../../../../../pkg/util/mocks/azureclient/azuresdk/$GOPACKAGE/armdns.go -source=armdns.go
+//go:generate go run ../../../../../vendor/golang.org/x/tools/cmd/goimports -local=github.com/Azure/ARO-RP -e -w ../../../../../../pkg/util/mocks/azureclient/azuresdk/$GOPACKAGE/armdns.go
