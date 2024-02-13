fe6e04a7a9061845683071be03bd2092346472dd
jonachang <jonachang@microsoft.com>
2024-02-13 09:23:27 -0800

    fix generate.go

--- pkg/util/azureclient/azuresdk/armdns/generate.go
+++ pkg/util/azureclient/azuresdk/armdns/generate.go
@@ -4,5 +4,5 @@ package armdns
 // Licensed under the Apache License 2.0.
 
 //go:generate rm -rf ../../../../../pkg/util/mocks/azureclient/azuresdk/$GOPACKAGE
-//go:generate go run ../../../../../vendor/github.com/golang/mock/mockgen -destination=../../../../../pkg/util/mocks/azureclient/azuresdk/$GOPACKAGE/armdns.go -source=armdns.go
-//go:generate go run ../../../../../vendor/golang.org/x/tools/cmd/goimports -local=github.com/Azure/ARO-RP -e -w ../../../../../../pkg/util/mocks/azureclient/azuresdk/$GOPACKAGE/armdns.go
+//go:generate go run ../../../../../vendor/github.com/golang/mock/mockgen -destination=../../../mocks/azureclient/azuresdk/$GOPACKAGE/armdns.go -source=armdns.go
+//go:generate go run ../../../../../vendor/golang.org/x/tools/cmd/goimports -local=github.com/Azure/ARO-RP -e -w ../../../mocks/azureclient/azuresdk/$GOPACKAGE/armdns.go
