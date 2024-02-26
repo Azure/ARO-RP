0f018405a14009074b1daffe667270efdd156074
Maitiú Ó Ciaráin <mociarain@redhat.com>
2024-02-26 10:56:37 +0000

    Fix shared_cluster.sh (#3414)
    * Use SHARED_CLUSTER_SHARED_CLUSTER_LOCATION
    
    * Remove unneeded quotes
    
    * Simplify the names

--- hack/shared-cluster.sh
+++ hack/shared-cluster.sh
@@ -15,8 +15,8 @@ check_env_set(){
   fi
 }
 
-check_env_set SHARED_CLUSTER_SHARED_CLUSTER_LOCATION
-check_env_set SHARED_CLUSTER_CLUSTER_NAME
+check_env_set SHARED_CLUSTER_LOCATION
+check_env_set SHARED_CLUSTER_NAME
 check_env_set SHARED_CLUSTER_RESOURCE_GROUP_NAME
 check_env_set SHARED_CLUSTER_CLUSTER_RESOURCE_GROUP_NAME
 
@@ -61,25 +61,28 @@ elif [[ $1 == create ]]; then
     az aro create \
       --resource-group $SHARED_CLUSTER_RESOURCE_GROUP_NAME \
       --cluster-resource-group $SHARED_CLUSTER_CLUSTER_RESOURCE_GROUP_NAME \
-      --name $SHARED_CLUSTER_CLUSTER_NAME \
+      --name $SHARED_CLUSTER_NAME \
       --vnet aro-vnet \
       --master-subnet master-subnet \
       --worker-subnet worker-subnet
 
     CLUSTER_RESOURCE_GROUP_ID=az aro show \
-      --name $SHARED_CLUSTER_CLUSTER_NAME \
+      --name $SHARED_CLUSTER_NAME \
       --resource-group $SHARED_CLUSTER_RESOURCE_GROUP_NAME \
       | jq .clusterProfile.resourceGroupId
 
+    echo "Adding tag to cluster..."
     # This tag stops the RG being cleaned up
     az tag create \
-      --resource-id "$CLUSTER_RESOURCE_GROUP_ID" \
+      --resource-id $CLUSTER_RESOURCE_GROUP_ID \
       --tags persist=true
 
 elif [[ $1 == "delete" ]]; then
   echo "Deleting cluster..."
-  az aro delete --name $SHARED_CLUSTER_CLUSTER_NAME -g $SHARED_CLUSTER_RESOURCE_GROUP_NAME --yes
+  az aro delete --name $SHARED_CLUSTER_NAME -g $SHARED_CLUSTER_RESOURCE_GROUP_NAME --yes
+  echo "Deleting Vnet..."
   az network vnet delete --name aro-vnet -g $SHARED_CLUSTER_RESOURCE_GROUP_NAME
+  echo "Deleting group..."
   az group delete --resource-group $SHARED_CLUSTER_RESOURCE_GROUP_NAME --yes
 
 else
