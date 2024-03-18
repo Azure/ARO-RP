ca57ccf8412c9a4d5ca4fae560674011e9e9dff3
Tony Schneider <tschneid@redhat.com>
2024-03-18 12:27:33 -0500

    fix fake uuid race condition

--- pkg/util/uuid/fake/fake_uuid.go
+++ pkg/util/uuid/fake/fake_uuid.go
@@ -4,17 +4,21 @@ package fake
 // Licensed under the Apache License 2.0.
 
 import (
+	"sync"
+
 	"github.com/Azure/ARO-RP/pkg/util/uuid"
 )
 
 type fakeGenerator struct {
 	words      []string
 	currentPos int
+	mu         *sync.Mutex
 }
 
 func NewGenerator(predefinedWords []string) uuid.Generator {
 	return &fakeGenerator{
 		words: predefinedWords,
+		mu:    &sync.Mutex{},
 	}
 }
 
@@ -23,6 +27,9 @@ func (f *fakeGenerator) movePos() {
 }
 
 func (f *fakeGenerator) Generate() string {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
 	defer f.movePos()
 	if len(f.words) < f.currentPos {
 		return ""
