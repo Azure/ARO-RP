42c9d103749b89c05e5bf7b87b81f3f153079eba
Tanmay Satam <tsatam@redhat.com>
2024-03-20 16:46:47 -0400

    Implement MissingFields on PartitionKeyRanges
    Fixes the previously-identified issue
    
    Signed-off-by: Amber Brown (hawkowl) <ambrown@redhat.com>

--- pkg/util/gencosmosdb/cosmosdb/collection.go
+++ pkg/util/gencosmosdb/cosmosdb/collection.go
@@ -168,6 +168,8 @@ type PartitionKeyRanges struct {
 
 // PartitionKeyRange represents a partition key range
 type PartitionKeyRange struct {
+	MissingFields
+
 	ID                 string                  `json:"id,omitempty"`
 	ResourceID         string                  `json:"_rid,omitempty"`
 	Timestamp          int                     `json:"_ts,omitempty"`
@@ -189,6 +191,27 @@ type PartitionKeyRangeStatus string
 const (
 	PartitionKeyRangeStatusOnline PartitionKeyRangeStatus = "online"
 )
+
+// MissingFields retains values that do not map to struct fields during JSON
+// marshalling/unmarshalling.  MissingFields implements
+// github.com/ugorji/go/codec.MissingFielder.
+type MissingFields struct {
+	m map[string]interface{}
+}
+
+// CodecMissingField is called to set a missing field and value pair
+func (mf *MissingFields) CodecMissingField(field []byte, value interface{}) bool {
+	if mf.m == nil {
+		mf.m = map[string]interface{}{}
+	}
+	(mf.m)[string(field)] = value
+	return true
+}
+
+// CodecMissingFields returns the set of fields which are not struct fields
+func (mf *MissingFields) CodecMissingFields() map[string]interface{} {
+	return mf.m
+}
 
 type collectionClient struct {
 	*databaseClient
