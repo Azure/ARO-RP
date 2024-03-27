b9c8f31e056ea88fa189ff4d7fe309f5551433f5
Amber Brown <ambrown@redhat.com>
2024-03-27 23:32:06 +1100

    try and fix the e2e test

--- test/e2e/operator.go
+++ test/e2e/operator.go
@@ -614,28 +614,26 @@ var _ = Describe("ARO Operator - dnsmasq", func() {
 		Expect(err).NotTo(HaveOccurred())
 	})
 
-	Describe("must handle the lifetime of the `99-${MCP}-custom-dns MachineConfig for every MachineConfigPool ${MCP}", Ordered, func() {
-		It("should create an ARO DNS MachineConfig when creating a custom MachineConfigPool", func(ctx context.Context) {
-			Eventually(func(g Gomega, ctx context.Context) []string {
-				return getMachineConfigNames(g, ctx)
-			}).WithContext(ctx).WithTimeout(timeout).WithPolling(polling).
-				Should(ContainElement(mcName))
-		})
+	It("must handle the lifetime of the `99-${MCP}-custom-dns MachineConfig for every MachineConfigPool ${MCP}", func(ctx context.Context) {
+		By("should create an ARO DNS MachineConfig when creating a custom MachineConfigPool")
+		Eventually(func(g Gomega, _ctx context.Context) []string {
+			return getMachineConfigNames(g, _ctx)
+		}).WithContext(ctx).WithTimeout(timeout).WithPolling(polling).
+			Should(ContainElement(mcName))
 
-		It("should have the MachineConfig be owned by the Operator", func(ctx context.Context) {
-			customMachineConfig, err := clients.MachineConfig.MachineconfigurationV1().MachineConfigs().Get(ctx, mcName, metav1.GetOptions{})
-			Expect(err).NotTo(HaveOccurred())
-			Expect(customMachineConfig.ObjectMeta.OwnerReferences[0].Name).To(Equal(mcpName))
-		})
+		By("should have the MachineConfig be owned by the Operator")
+		customMachineConfig, err := clients.MachineConfig.MachineconfigurationV1().MachineConfigs().Get(ctx, mcName, metav1.GetOptions{})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(customMachineConfig.ObjectMeta.OwnerReferences[0].Name).To(Equal(mcpName))
 
-		It("should delete the MachineConfig when deleting the custom MachineConfigPool", func(ctx context.Context) {
-			err := clients.MachineConfig.MachineconfigurationV1().MachineConfigPools().Delete(ctx, mcpName, metav1.DeleteOptions{})
-			Expect(err).NotTo(HaveOccurred())
-			Eventually(func(g Gomega) {
-				machineConfigs := getMachineConfigNames(g, ctx)
-				g.Expect(machineConfigs).NotTo(ContainElement(mcName))
-			}).WithContext(ctx).WithTimeout(timeout).WithPolling(polling).Should(Succeed())
-		})
+		By("should delete the MachineConfig when deleting the custom MachineConfigPool")
+		err = clients.MachineConfig.MachineconfigurationV1().MachineConfigPools().Delete(ctx, mcpName, metav1.DeleteOptions{})
+		Expect(err).NotTo(HaveOccurred())
+		Eventually(func(g Gomega, _ctx context.Context) {
+			machineConfigs := getMachineConfigNames(g, _ctx)
+			g.Expect(machineConfigs).NotTo(ContainElement(mcName))
+		}).WithContext(ctx).WithTimeout(timeout).WithPolling(polling).Should(Succeed())
+
 	})
 })
 
