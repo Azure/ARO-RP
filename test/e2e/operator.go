8b1f04434e91e9149620cdc6d248e787cad4cf80
Amber Brown <ambrown@redhat.com>
2024-03-28 19:17:59 +1100

    try and fix the e2e test

--- test/e2e/operator.go
+++ test/e2e/operator.go
@@ -683,7 +683,7 @@ var _ = Describe("ARO Operator - dnsmasq", func() {
 
 			return config.Labels
 		}).WithContext(ctx).WithPolling(polling).WithTimeout(timeout).MustPassRepeatedly(3).Should(Equal(map[string]string{
-			"machineconfiguration.openshift.io/role": "custom",
+			"machineconfiguration.openshift.io/role": mcpName,
 			"testlabel":                              "testvalue",
 		}))
 
@@ -706,7 +706,7 @@ var _ = Describe("ARO Operator - dnsmasq", func() {
 
 			return config.Labels
 		}).WithContext(ctx).WithPolling(polling).WithTimeout(timeout).Should(Equal(map[string]string{
-			"machineconfiguration.openshift.io/role": "custom",
+			"machineconfiguration.openshift.io/role": mcpName,
 		}))
 	})
 })
