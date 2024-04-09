3432d3a84a0e3a078704846de628d24d4d865a7d
b-jhoreman <b-jhoreman@microsoft.com>
2024-04-09 11:05:07 -0700

    adding retry logic on yum commands to avoid resource locking

--- pkg/deploy/generator/scripts/rpVMSS.sh
+++ pkg/deploy/generator/scripts/rpVMSS.sh
@@ -8,13 +8,13 @@ systemctl reload sshd.service
 #Adding retry logic to yum commands in order to avoid stalling out on resource locks
 echo "running RHUI fix"
 for attempt in {1..5}; do
-  yum update -y --disablerepo='*' --enablerepo='rhui-microsoft-azure*'
+  yum update -y --disablerepo='*' --enablerepo='rhui-microsoft-azure*' && break
   if [[ ${attempt} -lt 5 ]]; then sleep 10; else exit 1; fi
 done
 
 echo "running yum update"
 for attempt in {1..5}; do
-  yum -y -x WALinuxAgent -x WALinuxAgent-udev update --allowerasing
+  yum -y -x WALinuxAgent -x WALinuxAgent-udev update --allowerasing && break
   if [[ ${attempt} -lt 5 ]]; then sleep 10; else exit 1; fi
 done
 
