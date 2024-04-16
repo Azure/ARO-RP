8105d49155601f0f8aa9335f3e8c9c21adfb2fec
Amber Brown <ambrown@redhat.com>
2024-04-16 14:43:53 +1000

    add a cli app to generate these SPs

---
+++ hack/e2e/cmd/root.go
@@ -0,0 +1,29 @@
+package cmd
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "e2e",
+	Short: "Tooling for e2e",
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ hack/e2e/main.go
@@ -0,0 +1,10 @@
+package main
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+import "github.com/Azure/ARO-RP/hack/e2e/cmd"
+
+func main() {
+	cmd.Execute()
+}
