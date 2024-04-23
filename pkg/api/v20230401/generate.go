b59a6b3d22914b95f9e27d9a0c7725ef0c1ab98d
Petr <pkotas@redhat.com>
2024-04-23 15:31:49 +0200

    Move ARM swagger to subfolder
    To add new HCP RP the ARO RP is moved
    into the subfolder openshiftclusters.
    
    There are no additional changes, no
    impact on the SDK and clients.

--- pkg/api/v20230401/generate.go
+++ pkg/api/v20230401/generate.go
@@ -3,4 +3,4 @@ package v20230401
 // Copyright (c) Microsoft Corporation.
 // Licensed under the Apache License 2.0.
 
-//go:generate go run ../../../hack/swagger github.com/Azure/ARO-RP/pkg/api/v20230401 ../../../swagger/redhatopenshift/resource-manager/Microsoft.RedHatOpenShift/stable/2023-04-01
+//go:generate go run ../../../hack/swagger github.com/Azure/ARO-RP/pkg/api/v20230401 ../../../swagger/redhatopenshift/resource-manager/Microsoft.RedHatOpenShift/openshiftclusters/stable/2023-04-01
