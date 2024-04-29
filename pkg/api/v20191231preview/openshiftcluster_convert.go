46601fb45ab6e463ce5b131c387c7c043f7af55b
cadenmarchese <caden.marchese@gmail.com>
2024-04-29 13:02:11 -0400

    fix lint

--- pkg/api/v20191231preview/openshiftcluster_convert.go
+++ pkg/api/v20191231preview/openshiftcluster_convert.go
@@ -46,7 +46,7 @@ func (c openShiftClusterConverter) ToExternal(oc *api.OpenShiftCluster) interfac
 		},
 	}
 
-	if oc.Properties.ServicePrincipalProfile != nil {		
+	if oc.Properties.ServicePrincipalProfile != nil {
 		out.Properties.ServicePrincipalProfile = &ServicePrincipalProfile{
 			ClientID:     oc.Properties.ServicePrincipalProfile.ClientID,
 			ClientSecret: string(oc.Properties.ServicePrincipalProfile.ClientSecret),
