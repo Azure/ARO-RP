1a55353ab5de1ec5824262852f6af43c6b144007
Ayato Tokubi <atokubi@redhat.com>
2024-05-28 09:47:56 +0100

    subnets

--- pkg/cluster/cluster.go
+++ pkg/cluster/cluster.go
@@ -96,10 +96,11 @@ type manager struct {
 	armFPPrivateEndpoints    armnetwork.PrivateEndpointsClient
 	rpPrivateLinkServices    network.PrivateLinkServicesClient // TODO: use armRPPrivateLinkServices instead.
 	armRPPrivateLinkServices armnetwork.PrivateLinkServicesClient
+	armSubnets               armnetwork.SubnetsClient
 
 	dns     dns.Manager
 	storage storage.Manager
-	subnet  subnet.Manager
+	subnet  subnet.Manager // TODO: use armSubnet instead.
 	graph   graph.Manager
 	rpBlob  azblob.Manager
 
@@ -227,6 +228,11 @@ func New(ctx context.Context, log *logrus.Entry, _env env.Interface, db database
 		return nil, err
 	}
 
+	armSubnetsClient, err := armnetwork.NewSubnetsClient(r.SubscriptionID, fpCredClusterTenant, &clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
 	return &manager{
 		log:                      log,
 		env:                      _env,
@@ -263,6 +269,7 @@ func New(ctx context.Context, log *logrus.Entry, _env env.Interface, db database
 		armFPPrivateEndpoints:    armFPPrivateEndpoints,
 		rpPrivateLinkServices:    network.NewPrivateLinkServicesClient(_env.Environment(), _env.SubscriptionID(), msiAuthorizer),
 		armRPPrivateLinkServices: armRPPrivateLinkServices,
+		armSubnets:               armSubnetsClient,
 
 		dns:     dns.NewManager(_env, localFPAuthorizer),
 		storage: storage,
