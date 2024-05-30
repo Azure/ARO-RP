14a2a986e9fda04fe4cbbd6d9ab57f734c2bd560
Ayato Tokubi <atokubi@redhat.com>
2024-05-31 00:19:16 +0100

    use track2 sdk in createOrUpdateRouterIPEarly

--- pkg/util/subnet/subnet.go
+++ pkg/util/subnet/subnet.go
@@ -5,12 +5,10 @@ package subnet
 
 import (
 	"context"
-	"net"
 
 	mgmtnetwork "github.com/Azure/azure-sdk-for-go/services/network/mgmt/2020-08-01/network"
 	"github.com/Azure/go-autorest/autorest"
 	"github.com/Azure/go-autorest/autorest/azure"
-	"github.com/apparentlymart/go-cidr/cidr"
 
 	apisubnet "github.com/Azure/ARO-RP/pkg/api/util/subnet"
 	"github.com/Azure/ARO-RP/pkg/util/azureclient"
@@ -25,7 +23,6 @@ type Subnet struct {
 type Manager interface {
 	Get(ctx context.Context, subnetID string) (*mgmtnetwork.Subnet, error)
 	GetAll(ctx context.Context, subnetIds []string) ([]*mgmtnetwork.Subnet, error)
-	GetHighestFreeIP(ctx context.Context, subnetID string) (string, error)
 	CreateOrUpdate(ctx context.Context, subnetID string, subnet *mgmtnetwork.Subnet) error
 }
 
@@ -63,54 +60,6 @@ func (m *manager) get(ctx context.Context, subnetID, expand string) (*mgmtnetwor
 	}
 
 	return &subnet, nil
-}
-
-func (m *manager) GetHighestFreeIP(ctx context.Context, subnetID string) (string, error) {
-	// Probably anyone who calls this function has a race condition.
-
-	subnet, err := m.get(ctx, subnetID, "ipConfigurations")
-	if err != nil {
-		return "", err
-	}
-
-	// grab the first addresPrefix in the subnet
-	var subnetCIDR *net.IPNet
-	if subnet.AddressPrefix == nil {
-		_, subnetCIDR, err = net.ParseCIDR((*subnet.AddressPrefixes)[0])
-	} else {
-		_, subnetCIDR, err = net.ParseCIDR(*subnet.AddressPrefix)
-	}
-
-	if err != nil {
-		return "", err
-	}
-
-	bottom, top := cidr.AddressRange(subnetCIDR)
-
-	allocated := map[string]struct{}{}
-
-	// first four addresses and the broadcast address are reserved:
-	// https://docs.microsoft.com/en-us/azure/virtual-network/private-ip-addresses#allocation-method
-	for i, ip := 0, bottom; i < 4 && !ip.Equal(top); i, ip = i+1, cidr.Inc(ip) {
-		allocated[ip.String()] = struct{}{}
-	}
-	allocated[top.String()] = struct{}{}
-
-	if subnet.IPConfigurations != nil {
-		for _, ipconfig := range *subnet.IPConfigurations {
-			if ipconfig.PrivateIPAddress != nil {
-				allocated[*ipconfig.PrivateIPAddress] = struct{}{}
-			}
-		}
-	}
-
-	for ip := top; !ip.Equal(cidr.Dec(bottom)); ip = cidr.Dec(ip) {
-		if _, ok := allocated[ip.String()]; !ok {
-			return ip.String(), nil
-		}
-	}
-
-	return "", nil
 }
 
 // CreateOrUpdate updates the linked subnet
