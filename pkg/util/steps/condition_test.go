a33264f0050c70ebed5eace5697f2fd4e2465034
Amber Brown <ambrown@redhat.com>
2024-06-12 17:29:37 +1000

    use an instantiated struct for this test, like the real steps

--- pkg/util/steps/condition_test.go
+++ pkg/util/steps/condition_test.go
@@ -9,21 +9,29 @@ import (
 	"testing"
 )
 
+type teststruct struct{}
+
 // functionnames that will be used in the conditionFunction below
 // All the keys of map timeoutConditionErrors
-func attachNSGs(context.Context) (bool, error)                             { return false, nil }
-func apiServersReady(context.Context) (bool, error)                        { return false, nil }
-func minimumWorkerNodesReady(context.Context) (bool, error)                { return false, nil }
-func operatorConsoleExists(context.Context) (bool, error)                  { return false, nil }
-func operatorConsoleReady(context.Context) (bool, error)                   { return false, nil }
-func clusterVersionReady(context.Context) (bool, error)                    { return false, nil }
-func ingressControllerReady(context.Context) (bool, error)                 { return false, nil }
-func aroDeploymentReady(context.Context) (bool, error)                     { return false, nil }
-func ensureAROOperatorRunningDesiredVersion(context.Context) (bool, error) { return false, nil }
-func hiveClusterDeploymentReady(context.Context) (bool, error)             { return false, nil }
-func hiveClusterInstallationComplete(context.Context) (bool, error)        { return false, nil }
+func (n *teststruct) attachNSGs(context.Context) (bool, error)              { return false, nil }
+func (n *teststruct) apiServersReady(context.Context) (bool, error)         { return false, nil }
+func (n *teststruct) minimumWorkerNodesReady(context.Context) (bool, error) { return false, nil }
+func (n *teststruct) operatorConsoleExists(context.Context) (bool, error)   { return false, nil }
+func (n *teststruct) operatorConsoleReady(context.Context) (bool, error)    { return false, nil }
+func (n *teststruct) clusterVersionReady(context.Context) (bool, error)     { return false, nil }
+func (n *teststruct) ingressControllerReady(context.Context) (bool, error)  { return false, nil }
+func (n *teststruct) aroDeploymentReady(context.Context) (bool, error)      { return false, nil }
+func (n *teststruct) ensureAROOperatorRunningDesiredVersion(context.Context) (bool, error) {
+	return false, nil
+}
+func (n *teststruct) hiveClusterDeploymentReady(context.Context) (bool, error) { return false, nil }
+func (n *teststruct) hiveClusterInstallationComplete(context.Context) (bool, error) {
+	return false, nil
+}
 
 func TestEnrichConditionTimeoutError(t *testing.T) {
+	s := &teststruct{}
+
 	for _, tt := range []struct {
 		desc        string
 		function    conditionFunction
@@ -41,57 +49,57 @@ func TestEnrichConditionTimeoutError(t *testing.T) {
 		},
 		{
 			desc:     "test conditionfail for func - attachNSGs",
-			function: attachNSGs,
+			function: s.attachNSGs,
 			wantErr:  "500: DeploymentFailed: : Failed to attach the ARO NSG to the cluster subnets. Please retry, and if the issue persists, raise an Azure support ticket",
 		},
 		{
 			desc:     "test conditionfail for func - apiServersReady",
-			function: apiServersReady,
+			function: s.apiServersReady,
 			wantErr:  "500: DeploymentFailed: : Kube API has not initialised successfully and is unavailable. Please retry, and if the issue persists, raise an Azure support ticket",
 		},
 		{
 			desc:     "test conditionfail for func - minimumWorkerNodesReady",
-			function: minimumWorkerNodesReady,
+			function: s.minimumWorkerNodesReady,
 			wantErr:  "500: DeploymentFailed: : Minimum number of worker nodes have not been successfully created. Please retry, and if the issue persists, raise an Azure support ticket",
 		},
 		{
 			desc:     "test conditionfail for func - operatorConsoleExists",
-			function: operatorConsoleExists,
+			function: s.operatorConsoleExists,
 			wantErr:  "500: DeploymentFailed: : Console Cluster Operator has failed to initialize successfully. Please retry, and if the issue persists, raise an Azure support ticket",
 		},
 		{
 			desc:     "test conditionfail for func - operatorConsoleReady",
-			function: operatorConsoleReady,
+			function: s.operatorConsoleReady,
 			wantErr:  "500: DeploymentFailed: : Console Cluster Operator has not started successfully. Please retry, and if the issue persists, raise an Azure support ticket",
 		},
 		{
 			desc:     "test conditionfail for func - clusterVersionReady",
-			function: clusterVersionReady,
+			function: s.clusterVersionReady,
 			wantErr:  "500: DeploymentFailed: : Cluster Verion is not reporting status as ready. Please retry, and if the issue persists, raise an Azure support ticket",
 		},
 		{
 			desc:     "test conditionfail for func - clusterVersionReady",
-			function: ingressControllerReady,
+			function: s.ingressControllerReady,
 			wantErr:  "500: DeploymentFailed: : Ingress Cluster Operator has not started successfully. Please retry, and if the issue persists, raise an Azure support ticket",
 		},
 		{
 			desc:     "test conditionfail for func - aroDeploymentReady",
-			function: aroDeploymentReady,
+			function: s.aroDeploymentReady,
 			wantErr:  "500: DeploymentFailed: : ARO Cluster Operator has failed to initialize successfully. Please retry, and if the issue persists, raise an Azure support ticket",
 		},
 		{
 			desc:     "test conditionfail for func - ensureAROOperatorRunningDesiredVersion",
-			function: ensureAROOperatorRunningDesiredVersion,
+			function: s.ensureAROOperatorRunningDesiredVersion,
 			wantErr:  "500: DeploymentFailed: : ARO Cluster Operator is not running desired version. Please retry, and if the issue persists, raise an Azure support ticket",
 		},
 		{
 			desc:     "test conditionfail for func - hiveClusterDeploymentReady",
-			function: hiveClusterDeploymentReady,
+			function: s.hiveClusterDeploymentReady,
 			wantErr:  "500: DeploymentFailed: : Timed out waiting for the condition to be ready. Please retry, and if the issue persists, raise an Azure support ticket",
 		},
 		{
 			desc:     "test conditionfail for func - hiveClusterInstallationComplete",
-			function: hiveClusterInstallationComplete,
+			function: s.hiveClusterInstallationComplete,
 			wantErr:  "500: DeploymentFailed: : Timed out waiting for the condition to complete. Please retry, and if the issue persists, raise an Azure support ticket",
 		},
 	} {
