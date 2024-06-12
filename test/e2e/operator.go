5974f9fec0259eae17907b031eea7aace3ca9cf5
Tony Schneider <tschneid@redhat.com>
2024-06-12 15:42:38 -0500

    Exclude "Geneva Logging" test

--- test/e2e/operator.go
+++ test/e2e/operator.go
@@ -122,7 +122,7 @@ var _ = Describe("ARO Operator - Internet checking", func() {
 	})
 })
 
-var _ = Describe("ARO Operator - Geneva Logging", func() {
+var _ = XDescribe("ARO Operator - Geneva Logging", func() {
 	It("must be repaired if DaemonSet deleted", func(ctx context.Context) {
 		mdsdIsReady := func(g Gomega, ctx context.Context) {
 			done, err := ready.CheckDaemonSetIsReady(ctx, clients.Kubernetes.AppsV1().DaemonSets("openshift-azure-logging"), "mdsd")()
