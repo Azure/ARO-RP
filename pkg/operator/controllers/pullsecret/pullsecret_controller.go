835352dd2c3eee4414abd62bd86cde132da8ced9
Sanjana Lawande <slawande@redhat.com>
2024-06-24 06:06:33 -0700

    Adding logging statements for pullsecret controller (#3613)
    * Adding logging for pullsecret controller
    
    * Fixing unit tests
    
    Co-authored-by: Sanjana Lawande <slawande@slawande-thinkpadt14sgen2i.remote.csb>

--- pkg/operator/controllers/pullsecret/pullsecret_controller.go
+++ pkg/operator/controllers/pullsecret/pullsecret_controller.go
@@ -184,15 +184,22 @@ func (r *Reconciler) ensureGlobalPullSecret(ctx context.Context, operatorSecret,
 		// allows for simpler logic flow, when delete and create are not handled separately
 		// this call happens only when there is a need to change, it has no significant impact on performance
 		err := r.client.Delete(ctx, secret)
+		r.log.Info("Global Pull secret Not Found, Creating Again")
 		if err != nil && !kerrors.IsNotFound(err) {
 			return nil, err
 		}
 
 		err = r.client.Create(ctx, secret)
+		if err == nil {
+			r.log.Info("Global Pull secret Created")
+		}
 		return secret, err
 	}
 
 	err = r.client.Update(ctx, secret)
+	if err == nil {
+		r.log.Info("Updated Existing Global Pull secret")
+	}
 	return secret, err
 }
 
