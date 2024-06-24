d9c71918c6e9658d08a82879186adaed1dfbb0b6
Amber Brown <ambrown@redhat.com>
2024-06-24 14:49:40 +1000

    remove gofmt on removed mock

--- pkg/api/generate.go
+++ pkg/api/generate.go
@@ -4,4 +4,3 @@ package api
 // Licensed under the Apache License 2.0.
 
 //go:generate go run ../../vendor/github.com/alvaroloes/enumer -type InstallPhase -output zz_generated_installphase_enumer.go
-//go:generate go run ../../vendor/golang.org/x/tools/cmd/goimports -local=github.com/Azure/ARO-RP -e -w ../util/mocks/api/api.go
