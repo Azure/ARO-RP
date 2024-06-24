3b71da544f5955e368b19e79b1f8e0edd36b8c50
Amber Brown <ambrown@redhat.com>
2024-06-24 14:44:11 +1000

    remove converter mocks

--- pkg/api/generate.go
+++ pkg/api/generate.go
@@ -4,5 +4,4 @@ package api
 // Licensed under the Apache License 2.0.
 
 //go:generate go run ../../vendor/github.com/alvaroloes/enumer -type InstallPhase -output zz_generated_installphase_enumer.go
-//go:generate go run ../../vendor/github.com/golang/mock/mockgen -destination=../util/mocks/api/api.go github.com/Azure/ARO-RP/pkg/api SyncSetConverter,MachinePoolConverter,SyncIdentityProviderConverter,SecretConverter
 //go:generate go run ../../vendor/golang.org/x/tools/cmd/goimports -local=github.com/Azure/ARO-RP -e -w ../util/mocks/api/api.go
