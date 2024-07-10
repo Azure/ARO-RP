2a6c48c2a4ceeab4a3b54b2dfc4a0c8920962874
oraz <oraz@redhat.com>
2024-07-10 14:05:01 +0300

    Get default image, tag and import it to ACR
    Add new functions and scripts to get the images tag and set it nicely for importing and copying images by digest (e.g., for the fluenbit image).

--- hack/get-image-tag.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-extract_image_tag() {       
-    # Extract the line containing the return statement
-    local return_line=$(grep -A 1 "func $1" "$2" | grep 'return')
-    echo $return_line | sed 's/.*"\(.*\)@sha256.*/\1/'
-}
