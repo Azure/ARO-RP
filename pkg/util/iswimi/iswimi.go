03f22f01f9c0139ba7e7f59c163e3b3c718fafd2
Sanjana Lawande <slawande@slawande-thinkpadt14sgen2i.remote.csb>
2024-07-11 14:23:17 -0700

    adding license statement

--- pkg/util/iswimi/iswimi.go
+++ pkg/util/iswimi/iswimi.go
@@ -1,5 +1,8 @@
 package iswimi
 
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
 import "github.com/Azure/ARO-RP/pkg/api"
 
 // IsWimi checks whether a cluster is Workload Identity or classic
