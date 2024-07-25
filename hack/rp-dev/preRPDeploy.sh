954842e77f37d57fa6f7340d997ceedf1dcbe89b
oraz <oraz@redhat.com>
2024-07-25 17:09:23 +0300

    Add two bash scripts for automating the resources creation prior to RP deployment
    setupRPConfig is used to create the dev-config.yaml for predeploy RP deployments (e.g.,4 resource groups, 4 key vaults, NSGs, and etc.). preRPDeploy is used to install Hive with AKS, mirror images to ACR, and more a tasks. rp-dev-helper is fixed with input validation and a new cleanup function

---
+++ hack/rp-dev/preRPDeploy.sh
@@ -0,0 +1,113 @@
+#!/bin/bash -e
+
+source ./hack/devtools/deploy-shared-env.sh
+deploy_vpn_for_dedicated_rp
+echo "Success step 4a 🚀 - VPN has been deployed"
+
+deploy_aks_dev
+echo "Success step 4b 🚀 - AKS has been deployed"
+echo "Success step 4 ✅ - VPN & AKS have been deployed"
+
+vpn_configuration
+screen -dmS connect_dev_vpn bash -c "sudo openvpn secrets/vpn-$LOCATION.ovpn; sleep 10; exec bash" # open new socket to run concurrently
+make aks.kubeconfig
+HOME=/usr ./hack/hive-generate-config.sh # HOME is default to /usr which isn't in PATH
+KUBECONFIG=$(pwd)/aks.kubeconfig ./hack/hive-dev-install.sh
+echo "Success step 5 ✅ - Hive has been installed"
+
+export DST_ACR_NAME=${AZURE_PREFIX}aro
+export SRC_AUTH_QUAY=$(echo $USER_PULL_SECRET | jq -r '.auths."quay.io".auth')
+export SRC_AUTH_REDHAT=$(echo $USER_PULL_SECRET | jq -r '.auths."registry.redhat.io".auth')
+export DST_AUTH=$(echo -n '00000000-0000-0000-0000-000000000000:'$(az acr login -n $DST_ACR_NAME --expose-token | jq -r .accessToken) | base64 -w0)
+docker login -u 00000000-0000-0000-0000-000000000000 -p "$(echo $DST_AUTH | base64 -d | cut -d':' -f2)" "$DST_ACR_NAME.azurecr.io"
+echo "Success step 6a ✈️ 🏷️ - Login to ACR"
+
+go run -tags containers_image_openpgp,exclude_graphdriver_btrfs ./cmd/aro mirror latest
+echo "Success step 6b ✈️ 📦 - Mirror OCP images"
+
+source ./hack/devtools/rp-dev-helper.sh
+mdm_image_tag=$(get_digest_tag "MdmImage")
+mdsd_image_tag=$(get_digest_tag "MdsdImage")
+az acr import --name $DST_ACR_NAME.azurecr.io${mdm_image_tag} --source linuxgeneva-microsoft.azurecr.io${mdm_image_tag}
+az acr import --name $DST_ACR_NAME.azurecr.io${mdsd_image_tag} --source linuxgeneva-microsoft.azurecr.io${mdsd_image_tag}
+echo "Success step 6c ✈️ 📦 - Import MDM and MDSD to ACR" # can run only once
+
+make publish-image-aro-multistage
+echo "Success step 6d ✈️ 📦 - Build, push and import to ARO image to ACR"
+
+fluentbit_image_tag=$(get_digest_tag "FluentbitImage")
+copy_digest_tag $PULL_SECRET "arointsvc" $DST_ACR_NAME $fluentbit_image_tag
+echo "Success step 6e ✈️ 📦 - Copy Fluenbit image to ACR"
+echo "Success step 6 ✅ - Mirror repos to ACR"
+
+export PARENT_DOMAIN_NAME=osadev.cloud
+export PARENT_DOMAIN_RESOURCEGROUP=dns
+export GLOBAL_RESOURCEGROUP=${AZURE_PREFIX}-global
+
+for DOMAIN_NAME in ${AZURE_PREFIX}-clusters.$PARENT_DOMAIN_NAME ${AZURE_PREFIX}-rp.$PARENT_DOMAIN_NAME; do
+  CHILD_DOMAIN_PREFIX="$(cut -d. -f1 <<<$DOMAIN_NAME)"
+  echo "########## Creating NS record to DNS Zone $CHILD_DOMAIN_PREFIX ##########"
+  az network dns record-set ns create \
+    --resource-group "$PARENT_DOMAIN_RESOURCEGROUP" \
+    --zone "$PARENT_DOMAIN_NAME" \
+    --name "$CHILD_DOMAIN_PREFIX" >/dev/null
+  for ns in $(az network dns zone show \
+    --resource-group "$GLOBAL_RESOURCEGROUP" \
+    --name "$DOMAIN_NAME" \
+    --query nameServers -o tsv); do
+    az network dns record-set ns add-record \
+    --resource-group "$PARENT_DOMAIN_RESOURCEGROUP" \
+    --zone "$PARENT_DOMAIN_NAME" \
+    --record-set-name "$CHILD_DOMAIN_PREFIX" \
+    --nsdname "$ns" >/dev/null
+  done
+done
+echo "Success step 7a ✅ - Update DNS Child Domains"
+
+az keyvault certificate import \
+    --vault-name "$KEYVAULT_PREFIX-svc" \
+    --name rp-mdm \
+    --file secrets/rp-metrics-int.pem >/dev/null
+az keyvault certificate import \
+    --vault-name "$KEYVAULT_PREFIX-gwy" \
+    --name gwy-mdm \
+    --file secrets/rp-metrics-int.pem >/dev/null
+az keyvault certificate import \
+    --vault-name "$KEYVAULT_PREFIX-svc" \
+    --name rp-mdsd \
+    --file secrets/rp-logging-int.pem >/dev/null
+az keyvault certificate import \
+    --vault-name "$KEYVAULT_PREFIX-gwy" \
+    --name gwy-mdsd \
+    --file secrets/rp-logging-int.pem >/dev/null
+az keyvault certificate import \
+    --vault-name "$KEYVAULT_PREFIX-svc" \
+    --name cluster-mdsd \
+    --file secrets/cluster-logging-int.pem >/dev/null
+az keyvault certificate import \
+    --vault-name "$KEYVAULT_PREFIX-svc" \
+    --name dev-arm \
+    --file secrets/arm.pem >/dev/null
+az keyvault certificate import \
+    --vault-name "$KEYVAULT_PREFIX-svc" \
+    --name rp-firstparty \
+    --file secrets/firstparty.pem >/dev/null
+az keyvault certificate import \
+    --vault-name "$KEYVAULT_PREFIX-svc" \
+    --name rp-server \
+    --file secrets/localhost.pem >/dev/null
+az keyvault certificate import \
+    --vault-name "$KEYVAULT_PREFIX-por" \
+    --name portal-server \
+    --file secrets/localhost.pem >/dev/null
+az keyvault certificate import \
+    --vault-name "$KEYVAULT_PREFIX-por" \
+    --name portal-client \
+    --file secrets/portal-client.pem >/dev/null
+echo "Success step 7b ✅ - Update certificates in keyvault"
+
+gitCommit=$(git rev-parse --short=7 HEAD)
+az vmss delete --resource-group ${AZURE_PREFIX}-aro-$LOCATION --name rp-vmss-$gitCommit --force-deletion
+az vmss delete --resource-group ${AZURE_PREFIX}-gwy-$LOCATION --name gateway-vmss-$gitCommit --force-deletion
+echo "Success step 7c  ✅ - Remove RP VMSS and GWY VMSS"
+echo "Success step 7 ✅ - Final preperation before deploying RP"
---
+++ hack/rp-dev/setupRPConfig.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -e
+# TODO - check if needed
+export USER=dummy
+secretSA=$(grep -A 2 secretSA .pipelines/templates/rp-dev/rp-dev-params.yml | grep 'default:' | awk '{print $2}')
+# Export the SECRET_SA_ACCOUNT_NAME environment variable and run make secrets
+az account set -s fe16a035-e540-4ab7-80d9-373fa9a3d6ae
+export SECRET_SA_ACCOUNT_NAME=$secretSA && make secrets
+
+# Define the expected directory and file names
+expected_dir="secrets"
+files=("env" "dev-ca.crt" "dev-client.crt")
+
+# Validate that the secrets directory and required files exist
+[ -d "$expected_dir" ] || { echo "Directory '$expected_dir' has not been created."; exit 1; }
+for file in "${files[@]}"; do
+  [ -f "$expected_dir/$file" ] || { echo "File '$file' does not exist inside the directory '$expected_dir'."; exit 1; }
+done
+export LOCATION=$(grep -A 2 location $paramsFile| grep 'default:' | awk '{print $2}' | head -n 1)
+# Source environment variables from the secrets file
+source secrets/env
+echo "Success step 1 ✅ - Directory '$expected_dir' has been created with files - ${files[@]}"
+
+export AZURE_PREFIX=$1 NO_CACHE=true ARO_INSTALL_VIA_HIVE=true ARO_ADOPT_BY_HIVE=true DATABASE_NAME=ARO
+
+paramsFile=".pipelines/templates/rp-dev/rp-dev-params.yml"
+varFile=".pipelines/templates/rp-dev/rp-dev-vars.yml"
+# Export environment variables from parameters
+azure_resource_name=${AZURE_PREFIX}-aro-$LOCATION
+# TODO truncate to 20 characters
+export RESOURCEGROUP=$azure_resource_name DATABASE_ACCOUNT_NAME=$azure_resource_name KEYVAULT_PREFIX=$azure_resource_name
+gitCommit=$(git rev-parse --short=7 HEAD)
+export ARO_IMAGE=${AZURE_PREFIX}aro.azurecr.io/aro:$gitCommit
+# TODO - check if needed
+# Generate SSH key
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+
+# Run the make command to generate dev-config.yaml
+make dev-config.yaml
+
+# Check if the dev-config.yaml file exists
+[ -f "dev-config.yaml" ] || { echo "File dev-config.yaml does not exist."; exit 1; }
+echo "Success step 2 ✅ - Config file dev-config.yaml has been created"
