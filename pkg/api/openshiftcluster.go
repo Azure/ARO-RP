3124291c8765801702ffe85430b10e3fb39b406e
cadenmarchese <caden.marchese@gmail.com>
2024-07-26 13:42:14 -0400

    rename IdentityType to ResourceIdentityType to match v3/types.json

--- pkg/api/openshiftcluster.go
+++ pkg/api/openshiftcluster.go
@@ -809,19 +809,20 @@ type ClusterUserAssignedIdentity struct {
 	PrincipalID string `json:"principalId,omitempty"`
 }
 
-type IdentityType string
+// The identity type.
+type ResourceIdentityType string
 
 // IdentityType constants
 const (
-	IdentityTypeSystemAssigned IdentityType = "SystemAssigned"
-	IdentityTypeUserAssigned   IdentityType = "UserAssigned"
+	IdentityTypeSystemAssigned ResourceIdentityType = "SystemAssigned"
+	IdentityTypeUserAssigned   ResourceIdentityType = "UserAssigned"
 )
 
 // Identity stores information about the cluster MSI(s) in a workload identity cluster.
 type Identity struct {
 	MissingFields
 
-	Type                   IdentityType                           `json:"type,omitempty"`
+	Type                   ResourceIdentityType                   `json:"type,omitempty"`
 	PrincipalID            string                                 `json:"principalId,omitempty" swagger:"readOnly"`
 	UserAssignedIdentities map[string]ClusterUserAssignedIdentity `json:"userAssignedIdentities,omitempty"`
 	IdentityURL            string                                 `json:"identityURL,omitempty" mutable:"true"`
