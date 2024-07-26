48649846b0368567604ab43b2b951c6056089f46
Amber Brown <ambrown@redhat.com>
2024-07-26 13:55:55 +1000

    mimo error code

--- pkg/util/clusteroperators/isavailable.go
+++ pkg/util/clusteroperators/isavailable.go
@@ -1,6 +1,8 @@
 package clusteroperators
 
 import (
+	"fmt"
+
 	configv1 "github.com/openshift/api/config/v1"
 )
 
@@ -10,4 +12,14 @@ func IsOperatorAvailable(operator *configv1.ClusterOperator) bool {
 		m[cond.Type] = cond.Status
 	}
 	return m[configv1.OperatorAvailable] == configv1.ConditionTrue && m[configv1.OperatorProgressing] == configv1.ConditionFalse
+}
+
+func OperatorStatusText(operator *configv1.ClusterOperator) string {
+	m := make(map[configv1.ClusterStatusConditionType]configv1.ConditionStatus, len(operator.Status.Conditions))
+	for _, cond := range operator.Status.Conditions {
+		m[cond.Type] = cond.Status
+	}
+	return fmt.Sprintf("%s %s=%s, %s=%s", operator.Name,
+		configv1.OperatorAvailable, m[configv1.OperatorAvailable], configv1.OperatorProgressing, m[configv1.OperatorProgressing],
+	)
 }
