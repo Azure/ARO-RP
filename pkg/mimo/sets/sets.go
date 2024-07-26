8c29c8bea32be1c4f968e36eb6eaa1952383f1bf
Amber Brown <ambrown@redhat.com>
2024-07-26 13:56:01 +1000

    more work on sets

--- pkg/mimo/sets/sets.go
+++ pkg/mimo/sets/sets.go
@@ -1,8 +1,28 @@
 package sets
 
-import "time"
+import (
+	"time"
+
+	"github.com/Azure/ARO-RP/pkg/api"
+	"github.com/Azure/ARO-RP/pkg/util/mimo"
+	"github.com/Azure/ARO-RP/pkg/util/steps"
+)
 
 const DEFAULT_POLL_TIME = time.Second * 10
 const DEFAULT_TIMEOUT_DURATION = time.Minute * 20
 
-var DEFAULT_MAINTENANCE_SETS = map[string]MaintenanceSet{}
+var DEFAULT_MAINTENANCE_SETS = map[string]MaintenanceSet{
+	"9b741734-6505-447f-8510-85eb0ae561a2": TLSCertRotation,
+}
+
+func run(t mimo.TaskContext, s []steps.Step) (api.MaintenanceManifestState, string) {
+	_, err := steps.Run(t, t.Log(), DEFAULT_POLL_TIME, s, t.Now)
+
+	if err != nil {
+		if mimo.IsRetryableError(err) {
+			return api.MaintenanceManifestStatePending, err.Error()
+		}
+		return api.MaintenanceManifestStateFailed, err.Error()
+	}
+	return api.MaintenanceManifestStateCompleted, t.GetResultMessage()
+}
