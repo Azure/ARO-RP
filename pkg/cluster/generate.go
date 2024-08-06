0f6b4ad4aa76ac487784130c636ee14e80decd4a
Nont <nontkrub@gmail.com>
2024-08-06 15:52:15 -0500

    [Rebase out] verbose goimports in cluster/generate

--- pkg/cluster/generate.go
+++ pkg/cluster/generate.go
@@ -6,4 +6,4 @@ package cluster
 //go:generate go run ../../vendor/github.com/golang/mock/mockgen -destination=../util/mocks/$GOPACKAGE/$GOPACKAGE.go github.com/Azure/ARO-RP/pkg/$GOPACKAGE Interface
 //go:generate go run ../../vendor/github.com/golang/mock/mockgen -destination=../util/mocks/samplesclient/versioned.go github.com/openshift/client-go/samples/clientset/versioned Interface
 //go:generate go run ../../vendor/github.com/golang/mock/mockgen -destination=../util/mocks/samples/samples.go github.com/openshift/client-go/samples/clientset/versioned/typed/samples/v1 SamplesV1Interface,ConfigInterface
-//go:generate go run ../../vendor/golang.org/x/tools/cmd/goimports -local=github.com/Azure/ARO-RP -e -w ../util/mocks/$GOPACKAGE/$GOPACKAGE.go
+//go:generate go run ../../vendor/golang.org/x/tools/cmd/goimports -local=github.com/Azure/ARO-RP -e -w -v ../util/mocks/$GOPACKAGE/$GOPACKAGE.go
