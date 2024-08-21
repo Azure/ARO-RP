9201c9a658eb7b513a7ad422bdc6437d2bd64e38
Edison Cardenas <ecardena@redhat.com>
2024-08-21 21:00:38 +1200

    ARO-9263: Add unit test for checker

--- pkg/mimo/tasks/cluster/acrtoken_checker.go
+++ pkg/mimo/tasks/cluster/acrtoken_checker.go
@@ -6,16 +6,24 @@ package cluster
 import (
 	"context"
 	"errors"
+	"fmt"
 	"time"
 
+	arov1alpha1 "github.com/Azure/ARO-RP/pkg/operator/apis/aro.openshift.io/v1alpha1"
 	"github.com/Azure/ARO-RP/pkg/util/acrtoken"
 	"github.com/Azure/ARO-RP/pkg/util/mimo"
+	"k8s.io/apimachinery/pkg/types"
 )
 
-func EnsureACRToken(ctx context.Context) error {
+func EnsureACRTokenIsValid(ctx context.Context) error {
 	th, err := mimo.GetTaskContext(ctx)
 	if err != nil {
 		return mimo.TerminalError(err)
+	}
+
+	ch, err := th.ClientHelper()
+	if err != nil {
+		return err
 	}
 
 	env := th.Environment()
@@ -29,13 +37,20 @@ func EnsureACRToken(ctx context.Context) error {
 		return err
 	}
 
+	cluster := &arov1alpha1.Cluster{}
+	err = ch.GetOne(ctx, types.NamespacedName{Name: arov1alpha1.SingletonClusterName}, cluster)
+	if err != nil {
+		return mimo.NewMIMOError(err, mimo.MIMOErrorTypeTerminalError)
+	}
+
 	rp := token.GetRegistryProfile(th.GetOpenshiftClusterDocument().OpenShiftCluster)
 	var now = time.Now().UTC()
 	expiry := rp.Expiry.Time
 
 	if expiry.After(now) {
-		return mimo.TerminalError(errors.New("ACR Token has expired"))
+		return mimo.TerminalError(errors.New("ACR token has expired"))
 	}
 
+	th.SetResultMessage(fmt.Sprintf("ACR token is valid"))
 	return nil
 }
