faa2408ac8fe499fc99eab4f4ab8cff667ea9075
Edison Cardenas <ecardena@redhat.com>
2024-09-17 15:59:38 +1200

    ARO-9263: Revise logic to check issue date instead of expiry

--- pkg/api/admin/openshiftcluster.go
+++ pkg/api/admin/openshiftcluster.go
@@ -468,8 +468,8 @@ const (
 
 // RegistryProfile represents a registry profile
 type RegistryProfile struct {
-	Name     string     `json:"name,omitempty"`
-	Username string     `json:"username,omitempty"`
+	Name      string     `json:"name,omitempty"`
+	Username  string     `json:"username,omitempty"`
 	IssueDate *date.Time `json:"issueDate,omitempty"`
 }
 
