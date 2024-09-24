1696176a9777f4902f0b45c3f4897a147ae2e507
kimorris27 <kimorris@redhat.com>
2024-09-24 14:15:28 -0500

    Add explanatory comments for use of mockgen's source mode

--- pkg/util/azureclient/azuresdk/armmsi/generate.go
+++ pkg/util/azureclient/azuresdk/armmsi/generate.go
@@ -3,6 +3,7 @@ package armmsi
 // Copyright (c) Microsoft Corporation.
 // Licensed under the Apache License 2.0.
 
+// Use source mode to prevent some issues related to generics being present in the interface.
 //go:generate rm -rf ../../../../../pkg/util/mocks/azureclient/azuresdk/$GOPACKAGE
 //go:generate mockgen -source ./federated_identity_credentials.go -destination=../../../mocks/azureclient/azuresdk/$GOPACKAGE/$GOPACKAGE.go github.com/Azure/ARO-RP/pkg/util/azureclient/azuresdk/$GOPACKAGE FederatedIdentityCredentialsClient
 //go:generate goimports -local=github.com/Azure/ARO-RP -e -w ../../../mocks/azureclient/azuresdk/$GOPACKAGE/$GOPACKAGE.go
