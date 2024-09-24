80e45d93e4881962dc1d639eb776a9ccf750cf1e
kimorris27 <kimorris@redhat.com>
2024-09-24 10:16:34 -0500

    Import a missing package from the Uber mock fork and vendor it in

---
+++ pkg/util/mocks/hack.go
@@ -0,0 +1,8 @@
+package mocks
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+// See https://github.com/golang/mock/issues/494#issuecomment-715602984.
+// It seems that this issue still hasn't been fixed in the Uber fork.
+import _ "go.uber.org/mock/mockgen/model"
