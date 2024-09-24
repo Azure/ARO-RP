a27171b13c9f819a0e3ca3f7a1a191b0c1ec436b
kimorris27 <kimorris@redhat.com>
2024-09-24 17:25:50 -0500

    Generate mocks for Azure clients added in cluster MSI PR

---
+++ pkg/util/azureclient/azuresdk/azsecrets/generate.go
@@ -0,0 +1,9 @@
+package azsecrets
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+// Use source mode to prevent some issues related to generics being present in the interface.
+//go:generate rm -rf ../../../../../pkg/util/mocks/azureclient/azuresdk/$GOPACKAGE
+//go:generate mockgen -source ./client.go -destination=../../../mocks/azureclient/azuresdk/$GOPACKAGE/$GOPACKAGE.go github.com/Azure/ARO-RP/pkg/util/azureclient/azuresdk/$GOPACKAGE Client
+//go:generate goimports -local=github.com/Azure/ARO-RP -e -w ../../../mocks/azureclient/azuresdk/$GOPACKAGE/$GOPACKAGE.go
