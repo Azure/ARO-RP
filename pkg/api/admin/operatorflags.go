94a485c9cd062fce4a7da18f71013ea14b3b81d2
Amber Brown <ambrown@redhat.com>
2024-10-02 14:21:31 +1000

    operator flags patches + tests

--- pkg/api/admin/operatorflags.go
+++ pkg/api/admin/operatorflags.go
@@ -11,15 +11,9 @@ import (
 	"github.com/Azure/ARO-RP/pkg/operator"
 )
 
-type operatorFlagsMergeStrategyStruct struct {
-	OperatorFlagsMergeStrategy string
-	Cluster                    *api.OpenShiftCluster
-}
-
 const (
-	operatorFlagsMergeStrategyDefault string = "merge"
-	operatorFlagsMergeStrategyMerge   string = "merge"
-	operatorFlagsMergeStrategyReset   string = "reset"
+	OperatorFlagsMergeStrategyMerge string = "merge"
+	OperatorFlagsMergeStrategyReset string = "reset"
 )
 
 // When a cluster is edited via the PATCH Cluster Geneva Action (aka an Admin Update)
@@ -30,38 +24,31 @@ const (
 // reset: The provided cluster flags are laid on top of the default cluster flags,
 // essentially ‘resetting’ the flags if no new flags are provided.
 func OperatorFlagsMergeStrategy(oc *api.OpenShiftCluster, body []byte) error {
-	payload := operatorFlagsMergeStrategyStruct{
-		OperatorFlagsMergeStrategy: operatorFlagsMergeStrategyDefault,
-		Cluster:                    &api.OpenShiftCluster{},
-	}
+	payload := &OpenShiftCluster{}
 
 	err := json.Unmarshal(body, &payload)
 	if err != nil {
 		return api.NewCloudError(http.StatusBadRequest, api.CloudErrorCodeInvalidRequestContent, "", "The request content was invalid and could not be deserialized: %q.", err)
 	}
 
+	// if it's empty, use the default of merge, which is performed by
+	// deserialising the body JSON later
+	if payload.OperatorFlagsMergeStrategy == "" {
+		return nil
+	}
+
 	// return error if OperatorFlagsMergeStrategy is not merge or reset, default is merge
-	if payload.OperatorFlagsMergeStrategy != operatorFlagsMergeStrategyMerge &&
-		payload.OperatorFlagsMergeStrategy != operatorFlagsMergeStrategyReset {
+	if payload.OperatorFlagsMergeStrategy != OperatorFlagsMergeStrategyMerge &&
+		payload.OperatorFlagsMergeStrategy != OperatorFlagsMergeStrategyReset {
 		return api.NewCloudError(http.StatusBadRequest, api.CloudErrorCodeInvalidParameter, "", "invalid operatorFlagsMergeStrategy '%s', can only be 'merge' or 'reset'", payload.OperatorFlagsMergeStrategy)
 	}
-	// return if payload is empty
-	if payload.Cluster == nil {
-		return nil
-	}
-	properties := &payload.Cluster.Properties
-	if properties == nil || properties.OperatorFlags == nil {
-		return nil
-	}
+
 	// return nil, if OperatorFlagsMergeStrategy is merge and payload has not operatorFlags
 	// return operatorFlags of payload, if OperatorFlagsMergeStrategy is merge and payload has operatorFlags
 	// return defaultOperatorFlags, if OperatorFlagsMergeStrategy is reset and payload has not operatorFlags
 	// return defaultOperatorFlags + operatorFlags of payload, if OperatorFlagsMergeStrategy is reset and payload has operatorFlags
-	if payload.OperatorFlagsMergeStrategy == operatorFlagsMergeStrategyReset {
+	if payload.OperatorFlagsMergeStrategy == OperatorFlagsMergeStrategyReset {
 		oc.Properties.OperatorFlags = operator.DefaultOperatorFlags()
-		for operatorflag, value := range payload.Cluster.Properties.OperatorFlags {
-			oc.Properties.OperatorFlags[operatorflag] = value
-		}
 	}
 
 	return nil
