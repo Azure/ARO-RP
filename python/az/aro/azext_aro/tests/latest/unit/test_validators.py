d64782c410f1155a79047849b5d1fb8900242c28
Sanjana Lawande <slawande@redhat.com>
2024-10-09 08:29:42 -0700

    disallow refresh_cluster_credentials with platowrm_workload_identities (#3870)
    * disallow refresh_cluster_credentials with platowrm_workload_identities
    
    * add pylint comment
    
    * fix python lint issue
    
    * fix pylint issue
    
    * Revert "disallow refresh_cluster_credentials with platform_workload_identities"
    
    This reverts commit 329322a0fecb15127b316c726f305d374092b113.
    
    * disallow refresh_cluster_credentials with platform_workload_identities fix

--- python/az/aro/azext_aro/tests/latest/unit/test_validators.py
+++ python/az/aro/azext_aro/tests/latest/unit/test_validators.py
@@ -816,8 +816,13 @@ test_validate_refresh_cluster_credentials_data = [
         RequiredArgumentMissingError
     ),
     (
+        "should raise MutuallyExclusiveArgumentError Exception because namespace.platform_workload_identities is present",
+        Mock(platform_workload_identities=[Mock(resource_id='Foo')], client_id=None, client_secret=None),
+        MutuallyExclusiveArgumentError
+    ),
+    (
         "should not raise any Exception because namespace.client_secret is None and namespace.client_id is None",
-        Mock(upgradeable_to=None, client_secret=None, client_id=None),
+        Mock(upgradeable_to=None, client_secret=None, client_id=None, platform_workload_identities=None),
         None
     ),
     (
