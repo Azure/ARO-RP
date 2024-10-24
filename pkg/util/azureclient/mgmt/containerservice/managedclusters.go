e231e6985c5821ebce944a5a652847671edbcfc3
Daniel J. Holmes (jaitaiwan) <daholmes@redhat.com>
2024-10-25 09:53:24 +1000

    feat: switch to containerservice v6

--- pkg/util/azureclient/mgmt/containerservice/managedclusters.go
+++ pkg/util/azureclient/mgmt/containerservice/managedclusters.go
@@ -4,8 +4,10 @@ package containerservice
 // Licensed under the Apache License 2.0.
 
 import (
-	mgmtcontainerservice "github.com/Azure/azure-sdk-for-go/services/containerservice/mgmt/2021-10-01/containerservice"
-	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	armcontainerservice "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v6"
 
 	"github.com/Azure/ARO-RP/pkg/util/azureclient"
 )
@@ -16,18 +18,45 @@ type ManagedClustersClient interface {
 }
 
 type managedClustersClient struct {
-	mgmtcontainerservice.ManagedClustersClient
+	*armcontainerservice.ManagedClustersClient
 }
 
 var _ ManagedClustersClient = &managedClustersClient{}
 
+// NewDefaultManagedClustersClient creates a new ManagedClustersClient with default options
+func NewDefaultManagedClustersClient(environment *azureclient.AROEnvironment, subscriptionId string, tokenizer azcore.TokenCredential) (ManagedClustersClient, error) {
+	options := arm.ClientOptions{
+		ClientOptions: azcore.ClientOptions{
+			Cloud: environment.Cloud,
+		},
+	}
+
+	return NewManagedClustersClient(subscriptionId, tokenizer, options)
+}
+
 // NewManagedClustersClient creates a new ManagedClustersClient
-func NewManagedClustersClient(environment *azureclient.AROEnvironment, subscriptionID string, authorizer autorest.Authorizer) ManagedClustersClient {
-	client := mgmtcontainerservice.NewManagedClustersClientWithBaseURI(environment.ResourceManagerEndpoint, subscriptionID)
-	client.Authorizer = authorizer
-	client.Sender = azureclient.DecorateSenderWithLogging(client.Sender)
+func NewManagedClustersClient(subscriptionId string, tokenizer azcore.TokenCredential, options arm.ClientOptions) (ManagedClustersClient, error) {
+	clientFactory, err := armcontainerservice.NewClientFactory(subscriptionId, tokenizer, &options)
+	if err != nil {
+		return nil, err
+	}
+
+	client := clientFactory.NewManagedClustersClient()
 
 	return &managedClustersClient{
 		ManagedClustersClient: client,
+	}, nil
+}
+
+// Creates a new ManagedClustersClient with specified transport, useful for testing with fakes
+// https://github.com/Azure/azure-sdk-for-go/blob/main/sdk/resourcemanager/containerservice/armcontainerservice/fake_example_test.go
+func NewManagedClustersClientWithTransport(environment *azureclient.AROEnvironment, subscriptionId string, tokenizer azcore.TokenCredential, transporter policy.Transporter) (ManagedClustersClient, error) {
+	options := arm.ClientOptions{
+		ClientOptions: azcore.ClientOptions{
+			Cloud:     environment.Cloud,
+			Transport: transporter,
+		},
 	}
+
+	return NewManagedClustersClient(subscriptionId, tokenizer, options)
 }
