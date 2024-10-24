cd2de78674b8a4941dfd0cf4322a15806988d523
Ayato Tokubi <atokubi@redhat.com>
2024-10-24 14:35:09 +0100

    change role definition names

--- hack/role/role.go
+++ hack/role/role.go
@@ -85,37 +85,37 @@ var (
 	ocBinary        = flag.String("oc-bin", "oc", "path to oc binary")
 	permissionSets  = []permissionSet{
 		{
-			"CloudControllerManager",
+			"cloud-controller-manager",
 			"0000_26_cloud-controller-manager-operator_14_credentialsrequest-azure.yaml",
 			"a1f96423-95ce-4224-ab27-4e3dc72facd4",
 		},
 		{
-			"MachineApiOperator",
+			"machine-api",
 			"0000_30_machine-api-operator_00_credentials-request.yaml",
 			"0358943c-7e01-48ba-8889-02cc51d78637",
 		},
 		{
-			"ImageRegistryOperator",
+			"image-registry",
 			"0000_50_cluster-image-registry-operator_01-registry-credentials-request-azure.yaml",
 			"8b32b316-c2f5-4ddf-b05b-83dacd2d08b5",
 		},
 		{
-			"ClusterIngressOperator",
+			"ingress",
 			"0000_50_cluster-ingress-operator_00-ingress-credentials-request.yaml",
 			"0336e1d3-7a87-462b-b6db-342b63f7802c",
 		},
 		{
-			"NetworkOperator",
+			"cloud-network-config",
 			"0000_50_cluster-network-operator_02-cncc-credentials.yaml",
 			"be7a6435-15ae-4171-8f30-4a343eff9e8f",
 		},
 		{
-			"StorageOperator",
+			"disk-csi-driver",
 			"0000_50_cluster-storage-operator_03_credentials_request_azure.yaml",
 			"5b7237c5-45e1-49d6-bc18-a1f62f400748",
 		},
 		{
-			"AzureFilesStorageOperator",
+			"file-csi-driver",
 			"0000_50_cluster-storage-operator_03_credentials_request_azure_file.yaml",
 			"0d7aedc0-15fd-4a67-a412-efad370c947e",
 		},
