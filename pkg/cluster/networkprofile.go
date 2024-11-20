77d1e6901ba80154aab71c6ab6bc879977b0e5e8
Taylor Fahlman <tfahlman@redhat.com>
2024-11-20 13:32:35 -0800

    Update NetworkType in cluster doc if cluster has migrated from SDN to OVN

--- pkg/cluster/networkprofile.go
+++ pkg/cluster/networkprofile.go
@@ -13,6 +13,29 @@ import (
 	"github.com/Azure/ARO-RP/pkg/util/feature"
 )
 
+func (m *manager) reconcileSoftwareDefinedNetwork(ctx context.Context) error {
+	// Clusters that are using SDN will need to migrate to OVN before upgrading to OpenShift 4.17.z
+	// This checks any cluster that is still marked as using SDN in its cluster doc, and updates it
+	// if the on-cluster networkType has been updated to use OVN
+	if m.doc.OpenShiftCluster.Properties.NetworkProfile.SoftwareDefinedNetwork == api.SoftwareDefinedNetworkOpenShiftSDN {
+		network, err := m.configcli.ConfigV1().Networks().Get(ctx, "cluster", metav1.GetOptions{})
+		if err != nil {
+			return err
+		}
+		if network.Spec.NetworkType == string(api.SoftwareDefinedNetworkOVNKubernetes) {
+			m.doc, err = m.db.PatchWithLease(ctx, m.doc.Key, func(doc *api.OpenShiftClusterDocument) error {
+				doc.OpenShiftCluster.Properties.NetworkProfile.SoftwareDefinedNetwork = api.SoftwareDefinedNetworkOVNKubernetes
+				return nil
+			})
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
 // populateMTUSize ensures that every new cluster object has the MTUSize field defined
 func (m *manager) populateMTUSize(ctx context.Context) error {
 	// Get appropriate MTU size
