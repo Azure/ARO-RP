447bf0c3622bc052aca0dfb99fd2956f73b6aebe
Ayato Tokubi <atokubi@redhat.com>
2024-12-10 13:59:59 +0000

    [ARO-11589] Dump keyvault certificate error (#3990)
    * dump keyvault error
    * use spew.Fprintf
    * make NewError private

--- pkg/util/keyvault/keyvault_test.go
+++ pkg/util/keyvault/keyvault_test.go
@@ -6,6 +6,12 @@ package keyvault
 import (
 	"fmt"
 	"testing"
+
+	azkeyvault "github.com/Azure/azure-sdk-for-go/services/keyvault/v7.0/keyvault"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/Azure/ARO-RP/pkg/util/pointerutils"
 )
 
 func TestShortCommonName(t *testing.T) {
@@ -33,6 +39,80 @@ func TestShortCommonName(t *testing.T) {
 			if got != test.wantShortCommonName {
 				t.Error(fmt.Errorf("got != want: %s != %s", got, test.wantShortCommonName))
 			}
+		})
+	}
+}
+
+func TestCheckOperation(t *testing.T) {
+	tests := []struct {
+		name          string
+		operation     *azkeyvault.CertificateOperation
+		wantBool      bool
+		wantError     bool
+		errorContains []string
+	}{
+		{
+			name: "certificate operation is inProgress",
+			operation: &azkeyvault.CertificateOperation{
+				Status: pointerutils.ToPtr("inProgress"),
+			},
+			wantBool:  false,
+			wantError: false,
+		},
+		{
+			name: "certificate operation is completed",
+			operation: &azkeyvault.CertificateOperation{
+				Status: pointerutils.ToPtr("completed"),
+			},
+			wantBool:  true,
+			wantError: false,
+		},
+		{
+			name: "certificate operation is failed",
+			operation: &azkeyvault.CertificateOperation{
+				Status:        pointerutils.ToPtr("failed"),
+				StatusDetails: pointerutils.ToPtr("some error"),
+				Error: &azkeyvault.Error{
+					Code:    pointerutils.ToPtr("some code"),
+					Message: pointerutils.ToPtr("some message"),
+				},
+			},
+			wantBool:      false,
+			wantError:     true,
+			errorContains: []string{"some error", "some code", "some message"},
+		},
+		{
+			name: "certificate operation is failed",
+			operation: &azkeyvault.CertificateOperation{
+				Status:        pointerutils.ToPtr("failed"),
+				StatusDetails: pointerutils.ToPtr("some error"),
+				Error: &azkeyvault.Error{
+					Code:    pointerutils.ToPtr("some code"),
+					Message: pointerutils.ToPtr("some message"),
+					InnerError: &azkeyvault.Error{
+						Code:    pointerutils.ToPtr("some inner code"),
+						Message: pointerutils.ToPtr("some inner message"),
+					},
+				},
+			},
+			wantBool:      false,
+			wantError:     true,
+			errorContains: []string{"some error", "some code", "some message", "some inner code", "some inner message"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			gotBool, err := checkOperation(test.operation)
+			if test.wantError {
+				require.Error(t, err)
+				for _, s := range test.errorContains {
+					require.Contains(t, err.Error(), s)
+				}
+			} else {
+				require.NoError(t, err)
+			}
+			assert.Equal(t, test.wantBool, gotBool)
 		})
 	}
 }
