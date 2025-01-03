14d19959fb53d9f1a4d6c43b71ab0fd1dc6a1245
Amber Brown <ambrown@redhat.com>
2025-01-03 13:28:27 +1100

    remove old mock

--- pkg/metrics/generate.go
+++
@@ -1,8 +0,0 @@
-package metrics
-
-// Copyright (c) Microsoft Corporation.
-// Licensed under the Apache License 2.0.
-
-//go:generate rm -rf ../util/mocks/$GOPACKAGE
-//go:generate mockgen -destination=../util/mocks/$GOPACKAGE/$GOPACKAGE.go github.com/Azure/ARO-RP/pkg/$GOPACKAGE Emitter
-//go:generate goimports -local=github.com/Azure/ARO-RP -e -w ../util/mocks/$GOPACKAGE/$GOPACKAGE.go
