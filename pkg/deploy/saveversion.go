5d4dad0fe6afd71739147f5b7b2bec2d046e4fe1
Tanmay Satam <tsatam@redhat.com>
2025-02-04 10:58:18 -0500

    Switch to using track2 blob sdk for saveversion

--- pkg/deploy/saveversion.go
+++ pkg/deploy/saveversion.go
@@ -4,20 +4,21 @@ package deploy
 // Licensed under the Apache License 2.0.
 
 import (
-	"bytes"
 	"context"
 	"fmt"
-	"net/url"
 	"time"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/service"
 	mgmtstorage "github.com/Azure/azure-sdk-for-go/services/storage/mgmt/2021-09-01/storage"
-	azstorage "github.com/Azure/azure-sdk-for-go/storage"
 	"github.com/Azure/go-autorest/autorest/date"
+
+	"github.com/Azure/ARO-RP/pkg/util/azureclient/azuresdk/azblob"
+	"github.com/Azure/ARO-RP/pkg/util/pointerutils"
 )
 
 // SaveVersion for current location in shared storage account for environment
 func (d *deployer) SaveVersion(ctx context.Context) error {
-	d.log.Printf("saving RP and OCP versions for RP %s deployed in %s to storage account %s", d.version, d.config.Location, *d.config.Configuration.RPVersionStorageAccountName)
+	d.log.Printf("saving RP version %s deployed in %s to storage account %s", d.version, d.config.Location, *d.config.Configuration.RPVersionStorageAccountName)
 	t := time.Now().UTC().Truncate(time.Second)
 	res, err := d.globalaccounts.ListAccountSAS(
 		ctx, *d.config.Configuration.GlobalResourceGroupName, *d.config.Configuration.RPVersionStorageAccountName, mgmtstorage.AccountSasParameters{
@@ -32,29 +33,30 @@ func (d *deployer) SaveVersion(ctx context.Context) error {
 		return err
 	}
 
-	v, err := url.ParseQuery(*res.AccountSasToken)
+	d.log.Infof("instantiating blobs client using SAS token")
+	sasUrl := fmt.Sprintf("https://%s.blob.%s/?%s", *d.config.Configuration.RPVersionStorageAccountName, d.env.Environment().StorageEndpointSuffix, *res.AccountSasToken)
+	blobsClient, err := azblob.NewBlobsClientUsingSAS(sasUrl, d.env.Environment().ArmClientOptions())
 	if err != nil {
+		d.log.Errorf("failure to instantiate blobs client using SAS: %v", err)
 		return err
 	}
 
-	blobClient := azstorage.NewAccountSASClient(
-		*d.config.Configuration.RPVersionStorageAccountName, v, (*d.env.Environment()).Environment).GetBlobService()
-
-	// ensure static web content is enabled
-	props := azstorage.ServiceProperties{
-		StaticWebsite: &azstorage.StaticWebsite{
-			Enabled: true,
-		},
-	}
-	err = blobClient.SetServiceProperties(props)
+	d.log.Infof("ensuring static web content is enabled")
+	_, err = blobsClient.ServiceClient().SetProperties(ctx, &service.SetPropertiesOptions{
+		StaticWebsite: &service.StaticWebsite{Enabled: pointerutils.ToPtr(true)},
+	})
 	if err != nil {
+		d.log.Errorf("failure to update static properties: %v", err)
 		return err
 	}
 
-	// save version of RP which is deployed in this location
-	containerRef := blobClient.GetContainerReference("$web")
-	blobRef := containerRef.GetBlobReference(fmt.Sprintf("rpversion/%s", d.config.Location))
-	blobRef.Properties = azstorage.BlobProperties{ContentType: "application/octet-stream"}
-	blobRef.Metadata = azstorage.BlobMetadata{}
-	return blobRef.CreateBlockBlobFromReader(bytes.NewReader([]byte(d.version)), &azstorage.PutBlobOptions{})
+	d.log.Infof("uploading RP version")
+	blobName := fmt.Sprintf("rpversion/%s", d.config.Location)
+	_, err = blobsClient.UploadBuffer(ctx, "$web", blobName, []byte(d.version), nil)
+	if err != nil {
+		d.log.Errorf("failure to upload version information: %v", err)
+		return err
+	}
+
+	return nil
 }
