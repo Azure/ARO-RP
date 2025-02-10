16fefdb62ff41ad5da4b0feabe7f6a4a613decc4
Amber Brown <ambrown@redhat.com>
2025-02-10 14:03:42 +1100

    initial test code

---
+++ pkg/mimo/steps/cluster/mdsd.go
@@ -0,0 +1,11 @@
+package cluster
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+import "context"
+
+func EnsureMDSDCertificates(ctx context.Context) error {
+	return nil
+
+}
---
+++ pkg/mimo/steps/cluster/mdsd_test.go
@@ -0,0 +1,119 @@
+package cluster
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	. "github.com/onsi/gomega"
+	"go.uber.org/mock/gomock"
+	"golang.org/x/exp/maps"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	configv1 "github.com/openshift/api/config/v1"
+
+	"github.com/Azure/ARO-RP/pkg/api"
+	"github.com/Azure/ARO-RP/pkg/util/clienthelper"
+	mock_env "github.com/Azure/ARO-RP/pkg/util/mocks/env"
+	utilpem "github.com/Azure/ARO-RP/pkg/util/pem"
+	utiltls "github.com/Azure/ARO-RP/pkg/util/tls"
+	testtasks "github.com/Azure/ARO-RP/test/mimo/tasks"
+	testlog "github.com/Azure/ARO-RP/test/util/log"
+)
+
+func TestMDSDRotate(t *testing.T) {
+	ctx := context.Background()
+
+	validKey, validCerts, err := utiltls.GenerateKeyAndCertificate("cert", nil, nil, false, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	encodedKey, err := utilpem.Encode(validKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	encodedCert, err := utilpem.Encode(validCerts[0])
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, tt := range []struct {
+		name    string
+		objects []runtime.Object
+		check   func(clienthelper.Interface, Gomega)
+		wantErr string
+	}{
+		{
+			name:    "secret created (did not exist)",
+			objects: []runtime.Object{},
+			check: func(i clienthelper.Interface, g Gomega) {
+
+				s := &corev1.Secret{}
+				err := i.GetOne(ctx, types.NamespacedName{Name: "cluster", Namespace: "openshift-azure-operator"}, s)
+				g.Expect(err).NotTo(HaveOccurred())
+
+				g.Expect(maps.Keys(s.Data)).To(ContainElements("gcscert.pem", "gcskey.pem"), "MDSD certs")
+				g.Expect(s.Data["gcscert.pem"]).To(Equal(encodedCert))
+				g.Expect(s.Data["gcskey.pem"]).To(Equal(encodedKey))
+
+			},
+		},
+		{
+			name: "secret created",
+			objects: []runtime.Object{
+				&configv1.APIServer{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "cluster",
+					},
+					Spec: configv1.APIServerSpec{},
+				},
+			},
+			check: func(i clienthelper.Interface, g Gomega) {
+			},
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			g := NewWithT(t)
+			controller := gomock.NewController(t)
+			_env := mock_env.NewMockInterface(controller)
+			_env.EXPECT().Domain().AnyTimes().Return("example.com")
+			_env.EXPECT().ClusterGenevaLoggingSecret().Return(validKey, validCerts[0])
+
+			_, log := testlog.New()
+
+			builder := fake.NewClientBuilder().WithRuntimeObjects(tt.objects...)
+			ch := clienthelper.NewWithClient(log, builder.Build())
+			tc := testtasks.NewFakeTestContext(
+				ctx, _env, log, func() time.Time { return time.Unix(100, 0) },
+				testtasks.WithClientHelper(ch),
+				testtasks.WithOpenShiftClusterProperties(clusterUUID, api.OpenShiftClusterProperties{
+					ClusterProfile: api.ClusterProfile{
+						Domain: "something",
+					},
+				}),
+			)
+
+			err := EnsureMDSDCertificates(tc)
+			if tt.wantErr != "" && err != nil {
+				g.Expect(err).To(MatchError(tt.wantErr))
+			} else if tt.wantErr != "" && err == nil {
+				t.Errorf("wanted error %s", tt.wantErr)
+			} else if tt.wantErr == "" {
+				g.Expect(err).ToNot(HaveOccurred())
+			}
+
+			if tt.check != nil {
+				tt.check(ch, g)
+			}
+		})
+	}
+}
