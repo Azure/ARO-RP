348bde6a9e8ba7cea5cdfcd2acbf787a327328a7
Amber Brown <ambrown@redhat.com>
2025-02-21 12:15:43 +1100

    update hook client with new APIs

--- test/util/clienthelper/hookclient.go
+++ test/util/clienthelper/hookclient.go
@@ -8,6 +8,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/watch"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -105,7 +106,7 @@ func (c *HookingClient) WithPrePatchHook(f hookFunc) *HookingClient {
 }
 
 // See [sigs.k8s.io/controller-runtime/pkg/client.Reader.Get]
-func (c *HookingClient) Get(ctx context.Context, key client.ObjectKey, obj client.Object) error {
+func (c *HookingClient) Get(ctx context.Context, key client.ObjectKey, obj client.Object, clientopt ...client.GetOption) error {
 	for _, h := range c.preGetHook {
 		err := h(key, obj)
 		if err != nil {
@@ -113,7 +114,7 @@ func (c *HookingClient) Get(ctx context.Context, key client.ObjectKey, obj clien
 		}
 	}
 
-	err := c.f.Get(ctx, key, obj)
+	err := c.f.Get(ctx, key, obj, clientopt...)
 	if err != nil {
 		return err
 	}
@@ -247,4 +248,16 @@ func (c *HookingClient) RESTMapper() meta.RESTMapper {
 // See [sigs.k8s.io/controller-runtime/pkg/client.Client.Status]
 func (c *HookingClient) Status() client.StatusWriter {
 	return c.f.Status()
+}
+
+func (c *HookingClient) GroupVersionKindFor(obj runtime.Object) (schema.GroupVersionKind, error) {
+	return c.f.GroupVersionKindFor(obj)
+}
+
+func (c *HookingClient) IsObjectNamespaced(obj runtime.Object) (bool, error) {
+	return c.f.IsObjectNamespaced(obj)
+}
+
+func (c *HookingClient) SubResource(subResource string) client.SubResourceClient {
+	return c.f.SubResource(subResource)
 }
