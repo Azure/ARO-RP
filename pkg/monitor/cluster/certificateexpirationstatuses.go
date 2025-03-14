28415b51cf8774db1d89f6739ef29d7f0c9f1e9a
Arris Li <huili@redhat.com>
2025-03-14 20:13:38 +1300

    imports fix

--- pkg/monitor/cluster/certificateexpirationstatuses.go
+++ pkg/monitor/cluster/certificateexpirationstatuses.go
@@ -8,8 +8,11 @@ import (
 	"strings"
 
 	corev1 "k8s.io/api/core/v1"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	operatorv1 "github.com/openshift/api/operator/v1"
 
 	"github.com/Azure/ARO-RP/pkg/api"
 	"github.com/Azure/ARO-RP/pkg/operator"
@@ -18,8 +21,6 @@ import (
 	"github.com/Azure/ARO-RP/pkg/util/dns"
 	"github.com/Azure/ARO-RP/pkg/util/pem"
 	"github.com/Azure/ARO-RP/pkg/util/version"
-	operatorv1 "github.com/openshift/api/operator/v1"
-	kerrors "k8s.io/apimachinery/pkg/api/errors"
 )
 
 // Copyright (c) Microsoft Corporation.
