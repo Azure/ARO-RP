2773529f542144b777041ef872118fe789dedc9f
Kevin O'Brien <keobrien@redhat.com>
2025-03-27 21:53:55 -0700

    kevinobriendotca/ARO-9395-fixssh-refactoring-for-cpms - Imports auto-updated and armnetwork mocks regenerated.

--- pkg/util/mocks/azureclient/azuresdk/armnetwork/armnetwork.go
+++ pkg/util/mocks/azureclient/azuresdk/armnetwork/armnetwork.go
@@ -13,7 +13,9 @@ import (
 	context "context"
 	reflect "reflect"
 
+	runtime "github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
 	armnetwork "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v2"
+	armnetwork0 "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v6"
 	gomock "go.uber.org/mock/gomock"
 )
 
@@ -109,7 +111,7 @@ func (m *MockInterfacesClient) EXPECT() *MockInterfacesClientMockRecorder {
 }
 
 // CreateOrUpdateAndWait mocks base method.
-func (m *MockInterfacesClient) CreateOrUpdateAndWait(ctx context.Context, resourceGroupName, networkInterfaceName string, parameters armnetwork.Interface, options *armnetwork.InterfacesClientBeginCreateOrUpdateOptions) error {
+func (m *MockInterfacesClient) CreateOrUpdateAndWait(ctx context.Context, resourceGroupName, networkInterfaceName string, parameters armnetwork0.Interface, options *armnetwork0.InterfacesClientBeginCreateOrUpdateOptions) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "CreateOrUpdateAndWait", ctx, resourceGroupName, networkInterfaceName, parameters, options)
 	ret0, _ := ret[0].(error)
@@ -123,7 +125,7 @@ func (mr *MockInterfacesClientMockRecorder) CreateOrUpdateAndWait(ctx, resourceG
 }
 
 // DeleteAndWait mocks base method.
-func (m *MockInterfacesClient) DeleteAndWait(ctx context.Context, resourceGroupName, networkInterfaceName string, options *armnetwork.InterfacesClientBeginDeleteOptions) error {
+func (m *MockInterfacesClient) DeleteAndWait(ctx context.Context, resourceGroupName, networkInterfaceName string, options *armnetwork0.InterfacesClientBeginDeleteOptions) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "DeleteAndWait", ctx, resourceGroupName, networkInterfaceName, options)
 	ret0, _ := ret[0].(error)
@@ -137,10 +139,10 @@ func (mr *MockInterfacesClientMockRecorder) DeleteAndWait(ctx, resourceGroupName
 }
 
 // Get mocks base method.
-func (m *MockInterfacesClient) Get(ctx context.Context, resourceGroupName, networkInterfaceName string, options *armnetwork.InterfacesClientGetOptions) (armnetwork.InterfacesClientGetResponse, error) {
+func (m *MockInterfacesClient) Get(ctx context.Context, resourceGroupName, networkInterfaceName string, options *armnetwork0.InterfacesClientGetOptions) (armnetwork0.InterfacesClientGetResponse, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Get", ctx, resourceGroupName, networkInterfaceName, options)
-	ret0, _ := ret[0].(armnetwork.InterfacesClientGetResponse)
+	ret0, _ := ret[0].(armnetwork0.InterfacesClientGetResponse)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
@@ -149,6 +151,20 @@ func (m *MockInterfacesClient) Get(ctx context.Context, resourceGroupName, netwo
 func (mr *MockInterfacesClientMockRecorder) Get(ctx, resourceGroupName, networkInterfaceName, options any) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Get", reflect.TypeOf((*MockInterfacesClient)(nil).Get), ctx, resourceGroupName, networkInterfaceName, options)
+}
+
+// NewListPager mocks base method.
+func (m *MockInterfacesClient) NewListPager(resourceGroupName string, options *armnetwork0.InterfacesClientListOptions) *runtime.Pager[armnetwork0.InterfacesClientListResponse] {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "NewListPager", resourceGroupName, options)
+	ret0, _ := ret[0].(*runtime.Pager[armnetwork0.InterfacesClientListResponse])
+	return ret0
+}
+
+// NewListPager indicates an expected call of NewListPager.
+func (mr *MockInterfacesClientMockRecorder) NewListPager(resourceGroupName, options any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "NewListPager", reflect.TypeOf((*MockInterfacesClient)(nil).NewListPager), resourceGroupName, options)
 }
 
 // MockLoadBalancersClient is a mock of LoadBalancersClient interface.
