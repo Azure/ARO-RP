20da9a58d700947dc3db31efe0723666971d8bfb
Amber Brown <ambrown@redhat.com>
2025-05-26 13:34:51 +1000

    cleanups for v20250725

--- pkg/api/admin/openshiftcluster_validatestatic_test.go
+++ pkg/api/admin/openshiftcluster_validatestatic_test.go
@@ -758,7 +758,7 @@ func TestOpenShiftClusterStaticValidateDelta(t *testing.T) {
 			(&openShiftClusterConverter{}).ToInternal(tt.oc(), current)
 
 			v := &openShiftClusterStaticValidator{}
-			err := v.Static(oc, current, "", "", true, "")
+			err := v.Static(oc, current, "", "", true, api.ArchitectureVersionV2, "")
 			if err == nil {
 				if tt.wantErr != "" {
 					t.Error(err)
