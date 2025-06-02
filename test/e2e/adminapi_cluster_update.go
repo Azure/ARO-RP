133599a87d65d5c7daa36b0ed4ca3ce8e461e9f6
Shubhada <shpaitha@redhat.com>
2025-06-02 14:20:28 -0700

    ARO-4556: make e2e admin update test concurrency-safe (retry on 409, wait for Succeeded)

--- test/e2e/adminapi_cluster_update.go
+++ test/e2e/adminapi_cluster_update.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -19,6 +20,8 @@ var _ = Describe("[Admin API] Cluster admin update action", Serial, func() {
 
 	It("must run cluster update operation on a cluster", func(ctx context.Context) {
 		var oc = &admin.OpenShiftCluster{}
+		var resp *http.Response
+		var err error
 
 		// Wait for the cluster to be in a succeeded state before continuing
 		Eventually(func(g Gomega, ctx context.Context) {
@@ -26,12 +29,35 @@ var _ = Describe("[Admin API] Cluster admin update action", Serial, func() {
 			g.Expect(oc.Properties.ProvisioningState).To(Equal(admin.ProvisioningStateSucceeded))
 		}).WithContext(ctx).WithTimeout(DefaultEventuallyTimeout).Should(Succeed())
 
+		// Trigger the update via RP admin API, retrying on 409 Conflict
 		By("triggering the update via RP admin API")
-		resp, err := adminRequest(ctx, http.MethodPatch, clusterResourceID, nil, true, json.RawMessage("{}"), oc)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(resp.StatusCode).To(Equal(http.StatusOK))
+		for i := 0; i < 3; i++ {
+			// Always get a fresh copy of the cluster so we have the latest ETag
+			g := NewWithT(GinkgoT())
+			oc = adminGetCluster(g, ctx, clusterResourceID)
+
+			resp, err = adminRequest(ctx, http.MethodPatch, clusterResourceID, nil, true, json.RawMessage("{}"), oc)
+			Expect(err).NotTo(HaveOccurred())
+
+			if resp.StatusCode == http.StatusConflict {
+				// Another update is already in flight; wait then retry
+				time.Sleep(5 * time.Second)
+				continue
+			}
+
+			// Expect 200 OK for a successful update
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
+			break
+		}
+
+		// If we have still received 409 on all 3 tries, explicitly fail
+		if resp != nil && resp.StatusCode == http.StatusConflict {
+			Fail("adminRequest returned 409 Conflict on all 3 retries")
+		}
 
 		By("checking provisioning state")
+		g := NewWithT(GinkgoT())
+		oc = adminGetCluster(g, ctx, clusterResourceID)
 		Expect(oc.Properties.ProvisioningState).To(Equal(admin.ProvisioningStateAdminUpdating))
 		Expect(oc.Properties.LastProvisioningState).To(Equal(admin.ProvisioningStateSucceeded))
 
@@ -41,6 +67,7 @@ var _ = Describe("[Admin API] Cluster admin update action", Serial, func() {
 			g.Expect(oc.Properties.ProvisioningState).To(Equal(admin.ProvisioningStateSucceeded))
 		}).WithContext(ctx).WithTimeout(DefaultEventuallyTimeout).Should(Succeed())
 
+		// Ensure there was no admin update error
 		Expect(oc.Properties.LastAdminUpdateError).To(Equal(""))
 	})
 })
