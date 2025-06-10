ce816962a5e6b4a45b443217cc42563b6aaef2b5
Amit Arora <amarora@redhat.com>
2025-06-10 13:49:19 +0530

    HiveManager NIL fix

--- pkg/monitor/cluster/clustersync.go
+++ pkg/monitor/cluster/clustersync.go
@@ -5,6 +5,7 @@ package cluster
 
 import (
 	"context"
+	"regexp"
 
 	"github.com/sirupsen/logrus"
 
@@ -13,57 +14,54 @@ import (
 
 func (mon *Monitor) emitClusterSync(ctx context.Context) error {
 	if mon.hiveClusterManager == nil {
-		// TODO(hive): remove this once we have Hive everywhere.
+		// TODO(hive): remove this once we have HiveManager available everywhere
 		mon.log.Info("skipping: no hive cluster manager")
 		return nil
 	}
 
 	clusterSync, err := mon.hiveClusterManager.GetClusterSync(ctx, mon.doc)
 	if err != nil {
+		mon.log.Errorf("Error in getting the clustersync data: %v", err)
 		return err
 	}
-	if clusterSync == nil {
-		return nil
-	} else {
-		if clusterSync.Status.SyncSets == nil {
-			return nil
-		} else {
+	if clusterSync != nil {
+		if clusterSync.Status.SyncSets != nil {
 			for _, s := range clusterSync.Status.SyncSets {
 				mon.emitGauge("hive.clustersync", 1, map[string]string{
-					"syncType": "SyncSets",
-					"name":     s.Name,
-					"result":   string(s.Result),
+					"name":   s.Name,
+					"status": string(s.Result),
+					"type":   "SyncSets",
+					"reason": cleanString(s.FailureMessage),
 				})
 
 				if mon.hourlyRun {
 					mon.log.WithFields(logrus.Fields{
-						"syncType":           "SyncSets",
 						"name":               s.Name,
-						"result":             string(s.Result),
+						"status":             string(s.Result),
+						"type":               "SyncSets",
 						"firstSuccessTime":   timeToString(s.FirstSuccessTime),
 						"lastTransitionTime": timeToString(&s.LastTransitionTime),
-						"failureMessage":     s.FailureMessage,
+						"reason":             cleanString(s.FailureMessage),
 					}).Print()
 				}
 			}
 		}
-		if clusterSync.Status.SelectorSyncSets == nil {
-			return nil
-		} else {
+		if clusterSync.Status.SelectorSyncSets != nil {
 			for _, s := range clusterSync.Status.SelectorSyncSets {
 				mon.emitGauge("hive.clustersync", 1, map[string]string{
-					"syncType": "SelectorSyncSets",
-					"name":     s.Name,
-					"result":   string(s.Result),
+					"name":   s.Name,
+					"status": string(s.Result),
+					"type":   "SelectorSyncSets",
+					"reason": cleanString(s.FailureMessage),
 				})
 				if mon.hourlyRun {
 					mon.log.WithFields(logrus.Fields{
-						"syncType":           "SelectorSyncSets",
 						"name":               s.Name,
-						"result":             string(s.Result),
+						"status":             string(s.Result),
+						"type":               "SelectorSyncSets",
 						"firstSuccessTime":   timeToString(s.FirstSuccessTime),
 						"lastTransitionTime": timeToString(&s.LastTransitionTime),
-						"failureMessage":     s.FailureMessage,
+						"reason":             cleanString(s.FailureMessage),
 					}).Print()
 				}
 			}
@@ -77,4 +75,12 @@ func timeToString(t *metav1.Time) string {
 		return ""
 	}
 	return t.String()
+}
+
+func cleanString(s string) string {
+	re, err := regexp.Compile(`[^\w\s\-_]|\n`)
+	if err != nil {
+		return s
+	}
+	return re.ReplaceAllString(s, "")
 }
