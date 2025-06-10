82fca2c3c67293a0de3cc60c8ec55a8549f1069f
Amit Arora <amarora@redhat.com>
2025-06-10 13:49:19 +0530

    HiveManager NIL fix

--- pkg/monitor/cluster/clustersync.go
+++ pkg/monitor/cluster/clustersync.go
@@ -12,63 +12,55 @@ import (
 )
 
 func (mon *Monitor) emitClusterSync(ctx context.Context) error {
-	if mon.hiveClusterManager == nil {
-		// TODO(hive): remove this once we have Hive everywhere.
-		mon.log.Info("skipping: no hive cluster manager")
-		return nil
-	}
+	mon.log.Infof("mon.hourlyRun %t:", mon.hourlyRun)
 
 	clusterSync, err := mon.hiveClusterManager.GetClusterSync(ctx, mon.doc)
 	if err != nil {
+		mon.log.Errorf("Error in getting the clustersync data %v", err)
 		return err
 	}
 	if clusterSync == nil {
+		mon.log.Info("Clustersync is NIL")
 		return nil
 	} else {
-		if clusterSync.Status.SyncSets == nil {
-			return nil
-		} else {
-			for _, s := range clusterSync.Status.SyncSets {
-				mon.emitGauge("hive.clustersync", 1, map[string]string{
-					"syncType": "SyncSets",
-					"name":     s.Name,
-					"result":   string(s.Result),
-				})
+		for _, s := range clusterSync.Status.SyncSets {
+			mon.emitGauge("hive.clustersync", 1, map[string]string{
+				"syncType": "SyncSets",
+				"name":     s.Name,
+				"result":   string(s.Result),
+			})
 
-				if mon.hourlyRun {
-					mon.log.WithFields(logrus.Fields{
-						"syncType":           "SyncSets",
-						"name":               s.Name,
-						"result":             string(s.Result),
-						"firstSuccessTime":   timeToString(s.FirstSuccessTime),
-						"lastTransitionTime": timeToString(&s.LastTransitionTime),
-						"failureMessage":     s.FailureMessage,
-					}).Print()
-				}
+			if mon.hourlyRun {
+				mon.log.WithFields(logrus.Fields{
+					"syncType":           "SyncSets",
+					"name":               s.Name,
+					"result":             string(s.Result),
+					"firstSuccessTime":   timeToString(s.FirstSuccessTime),
+					"lastTransitionTime": timeToString(&s.LastTransitionTime),
+					"failureMessage":     s.FailureMessage,
+				}).Print()
 			}
 		}
-		if clusterSync.Status.SelectorSyncSets == nil {
-			return nil
-		} else {
-			for _, s := range clusterSync.Status.SelectorSyncSets {
-				mon.emitGauge("hive.clustersync", 1, map[string]string{
-					"syncType": "SelectorSyncSets",
-					"name":     s.Name,
-					"result":   string(s.Result),
-				})
-				if mon.hourlyRun {
-					mon.log.WithFields(logrus.Fields{
-						"syncType":           "SelectorSyncSets",
-						"name":               s.Name,
-						"result":             string(s.Result),
-						"firstSuccessTime":   timeToString(s.FirstSuccessTime),
-						"lastTransitionTime": timeToString(&s.LastTransitionTime),
-						"failureMessage":     s.FailureMessage,
-					}).Print()
-				}
+		for _, s := range clusterSync.Status.SelectorSyncSets {
+			mon.emitGauge("hive.clustersync", 1, map[string]string{
+				"syncType": "SelectorSyncSets",
+				"name":     s.Name,
+				"result":   string(s.Result),
+			})
+
+			if mon.hourlyRun {
+				mon.log.WithFields(logrus.Fields{
+					"syncType":           "SelectorSyncSets",
+					"name":               s.Name,
+					"result":             string(s.Result),
+					"firstSuccessTime":   timeToString(s.FirstSuccessTime),
+					"lastTransitionTime": timeToString(&s.LastTransitionTime),
+					"failureMessage":     s.FailureMessage,
+				}).Print()
 			}
 		}
 	}
+	mon.log.Info("Syncsets and SelectorSyncets both are NIL")
 	return nil
 }
 
