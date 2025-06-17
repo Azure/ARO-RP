ab8426cc9c3f9411231f98b6a0e41da9afd9a8be
Alex Chvatal <achvatal@redhat.com>
2025-06-17 11:32:04 -0400

    clarify fields that will be empty

--- pkg/frontend/openshiftcluster_putorpatch.go
+++ pkg/frontend/openshiftcluster_putorpatch.go
@@ -230,10 +230,10 @@ func (f *frontend) _putOrPatchOpenShiftCluster(ctx context.Context, log *logrus.
 			// only identity
 			if tempPatchParameters.Identity != nil && len(tempPatchParameters.Identity.UserAssignedIdentities) > 0 {
 				doc.OpenShiftCluster.Identity.UserAssignedIdentities = map[string]api.UserAssignedIdentity{}
-				for k, v := range tempPatchParameters.Identity.UserAssignedIdentities {
+				for k, _ := range tempPatchParameters.Identity.UserAssignedIdentities {
 					doc.OpenShiftCluster.Identity.UserAssignedIdentities[k] = api.UserAssignedIdentity{
-						ClientID:    v.ClientID,
-						PrincipalID: v.PrincipalID,
+						ClientID:    "",
+						PrincipalID: "",
 					}
 				}
 			}
