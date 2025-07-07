f08120d585be4fcdca9ff3c3ab45e36ceef92764
Rajdeep Singh Chauhan <rajdchau@redhat.com>
2025-07-07 13:21:57 -0400

    ARO-17783 make OIDCIssuer field externally readonly

--- pkg/api/admin/openshiftcluster.go
+++ pkg/api/admin/openshiftcluster.go
@@ -151,7 +151,7 @@ type ClusterProfile struct {
 	Version              string               `json:"version,omitempty"`
 	ResourceGroupID      string               `json:"resourceGroupId,omitempty"`
 	FipsValidatedModules FipsValidatedModules `json:"fipsValidatedModules,omitempty"`
-	OIDCIssuer           *OIDCIssuer          `json:"oidcIssuer,omitempty"`
+	OIDCIssuer           *OIDCIssuer          `json:"oidcIssuer,omitempty" swagger:"readOnly"`
 }
 
 // FeatureProfile represents a feature profile.
