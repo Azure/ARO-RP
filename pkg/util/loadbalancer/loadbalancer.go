201508b2d29ada7207cd4e918b5561d9a89afacf
Alfonso Casimiro <21107567+alcasim@users.noreply.github.com>
2025-07-22 12:44:36 +0200

    correct nil check for pointers

--- pkg/util/loadbalancer/loadbalancer.go
+++ pkg/util/loadbalancer/loadbalancer.go
@@ -16,7 +16,7 @@ import (
 func RemoveFrontendIPConfiguration(lb *armnetwork.LoadBalancer, resourceID string) error {
 	newFrontendIPConfig := make([]*armnetwork.FrontendIPConfiguration, 0, len(lb.Properties.FrontendIPConfigurations))
 	for _, fipConfig := range lb.Properties.FrontendIPConfigurations {
-		if strings.EqualFold(*fipConfig.ID, resourceID) {
+		if fipConfig.ID != nil && strings.EqualFold(*fipConfig.ID, resourceID) {
 			if isFrontendIPConfigReferenced(fipConfig) {
 				return fmt.Errorf("frontend IP Configuration %s has external references, remove the external references prior to removing the frontend IP configuration", resourceID)
 			}
@@ -35,20 +35,19 @@ func isFrontendIPConfigReferenced(fipConfig *armnetwork.FrontendIPConfiguration)
 func RemoveHealthProbe(lb *armnetwork.LoadBalancer, resourceID string) error {
 	newProbes := make([]*armnetwork.Probe, 0)
 
-	if lb.Properties.Probes != nil {
-		// Iterate over probes, build a list without the targeted resourceID, if possible
-		for _, probe := range lb.Properties.Probes {
-			if strings.EqualFold(*probe.ID, resourceID) {
-				if probe.Properties.LoadBalancingRules != nil {
-					return api.NewCloudError(http.StatusBadRequest, api.CloudErrorCodeInvalidParameter, "", fmt.Sprintf("Load balancer health probe %s is used by load balancing rules, remove the referencing load balancing rules before removing the health probe", resourceID))
-				}
-				continue
+	// Iterate over probes, build a list without the targeted resourceID, if possible
+	for _, probe := range lb.Properties.Probes {
+		if probe.ID != nil && strings.EqualFold(*probe.ID, resourceID) {
+			if probe.Properties.LoadBalancingRules != nil {
+				return api.NewCloudError(http.StatusBadRequest, api.CloudErrorCodeInvalidParameter, "", fmt.Sprintf("Load balancer health probe %s is used by load balancing rules, remove the referencing load balancing rules before removing the health probe", resourceID))
 			}
-			newProbes = append(newProbes, probe)
+			continue
 		}
-		lb.Properties.Probes = newProbes
-	} else {
-		return api.NewCloudError(http.StatusNotFound, api.CloudErrorCodeInvalidParameter, "", "No health probes found for this loadbalancer")
+		newProbes = append(newProbes, probe)
 	}
+	lb.Properties.Probes = newProbes
+
+	//return api.NewCloudError(http.StatusNotFound, api.CloudErrorCodeInvalidParameter, "", "No health probes found for this loadbalancer")
+
 	return nil
 }
