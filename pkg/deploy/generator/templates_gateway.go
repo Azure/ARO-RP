d054d203ec3614573cce57cfe71d35b7e3634531
Preetish Kumar Tripathi <11624434+preetisht@users.noreply.github.com>
2025-08-13 10:06:03 +0530

    Add IP tags configuration for RP VMSS
    - Add IPTag struct and RpVmssIpTags field to Configuration
    - Add rpVmssIpTags parameter to ARM template with array type
    - Implement IP tags in VMSS PublicIPAddressConfiguration
    - Use conditional ARM logic for backward compatibility
    - Support FirstPartyUsage type with service tag values
    
    Update deployment assets
    
    Trigger new VMSS deployment for IP tags testing - eastus disabled region validation
    
    New commit ID

--- pkg/deploy/generator/templates_gateway.go
+++ pkg/deploy/generator/templates_gateway.go
@@ -34,6 +34,8 @@ func (g *generator) gatewayTemplate() *arm.Template {
 		"gatewayServicePrincipalId",
 		"gatewayVmSize",
 		"gatewayVmssCapacity",
+		"vmssIpTags",
+		"vmssIpTagsDisabledRegions",
 		"keyvaultDNSSuffix",
 		"keyvaultPrefix",
 		"mdmFrontendUrl",
@@ -61,6 +63,12 @@ func (g *generator) gatewayTemplate() *arm.Template {
 		case "gatewayVmssCapacity":
 			p.Type = "int"
 			p.DefaultValue = 3
+		case "vmssIpTags":
+			p.Type = "array"
+			p.DefaultValue = []interface{}{}
+		case "vmssIpTagsDisabledRegions":
+			p.Type = "array"
+			p.DefaultValue = []string{}
 		case "vmssCleanupEnabled":
 			p.Type = "bool"
 			p.DefaultValue = true
