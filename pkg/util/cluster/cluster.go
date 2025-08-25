9a4c6e8ea86bb58aeb29dc0dfc2951dd01895151
Andrew Denton <adenton@redhat.com>
2025-08-25 08:31:24 -0700

    Reorder the cluster deletion for retries to preserve the final failed cluster

--- pkg/util/cluster/cluster.go
+++ pkg/util/cluster/cluster.go
@@ -889,18 +889,24 @@ func (c *Cluster) createCluster(ctx context.Context, vnetResourceGroup, clusterN
 
 	var err error
 	for _, masterVmSize := range c.Config.MasterVMSizes {
+		if err != nil {
+			// If we've already tried and failed to create the cluster, delete
+			// it before retrying. Deleting first ensures that the final failed
+			// cluster remains for diagnostic purposes.
+			err = c.openshiftclusters.DeleteAndWait(ctx, vnetResourceGroup, clusterName)
+			if err != nil {
+				return fmt.Errorf("error deleting cluster after failed creation: %w", err)
+			}
+		}
+
 		oc.Properties.MasterProfile.VMSize = api.VMSize(masterVmSize)
 		c.log.Infof("Creating cluster %s with master VM size %s and worker VM size %s",
 			clusterName, oc.Properties.MasterProfile.VMSize, oc.Properties.WorkerProfiles[0].VMSize)
-		err := c.openshiftclusters.CreateOrUpdateAndWait(ctx, vnetResourceGroup, clusterName, &oc)
+		err = c.openshiftclusters.CreateOrUpdateAndWait(ctx, vnetResourceGroup, clusterName, &oc)
 		if err == nil {
 			break
 		}
 		c.log.WithError(err).Errorf("error creating cluster with master VM size %s, retrying", oc.Properties.MasterProfile.VMSize)
-		err = c.openshiftclusters.DeleteAndWait(ctx, vnetResourceGroup, clusterName)
-		if err != nil {
-			return fmt.Errorf("error deleting cluster after failed creation: %w", err)
-		}
 	}
 	return err
 }
