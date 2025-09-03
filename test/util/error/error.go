42c9ec950b61bf5538db9fce7b18b2e29d350481
Amber Brown <ambrown@redhat.com>
2025-09-03 16:00:25 +1000

    error code from other PR as well

--- test/util/error/error.go
+++ test/util/error/error.go
@@ -4,6 +4,7 @@ package error
 // Licensed under the Apache License 2.0.
 
 import (
+	"errors"
 	"regexp"
 	"slices"
 	"testing"
@@ -55,5 +56,45 @@ func AssertOneOfErrorMessages(t *testing.T, err error, wantMsgs []string) {
 
 	if err != nil && !slices.Contains(wantMsgs, err.Error()) {
 		t.Errorf("got error '%v', but wanted one of these errors: '%v'", err, wantMsgs)
+	}
+}
+
+func AssertErrorIs(t *testing.T, err error, wantError error) {
+	t.Helper()
+	if wantError == nil {
+		AssertErrorMatchesAll(t, err, []error{})
+		return
+	}
+	AssertErrorMatchesAll(t, err, []error{wantError})
+}
+
+// AssertErrorMatchesAll verifies that err contains all of the errors in wantError in its tree.
+func AssertErrorMatchesAll(t *testing.T, err error, wantError []error) {
+	t.Helper()
+
+	if err == nil && len(wantError) == 0 {
+		return
+	} else if err != nil && len(wantError) == 0 {
+		t.Errorf("got unexpected error '%v'", err)
+	} else if err == nil && len(wantError) != 0 {
+		t.Errorf("got unexpected SUCCESS instead of errors '%v'", wantError)
+	} else {
+		errorMatched := false
+		for _, wanted := range wantError {
+			if !errors.Is(err, wanted) {
+				// check the content in case it's just plain error strings
+				if err.Error() == wanted.Error() {
+					errorMatched = true
+				}
+			} else {
+				errorMatched = true
+			}
+		}
+		if !errorMatched {
+			t.Errorf("got error:\n'%v'\n\nwanted one of errors:\n", err)
+			for _, w := range wantError {
+				t.Error(w)
+			}
+		}
 	}
 }
