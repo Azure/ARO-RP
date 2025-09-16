f223a8ab83f58924f3a548ad47862b3cab49fae9
Amber Brown <ambrown@redhat.com>
2025-09-16 18:01:19 +1000

    i don't know why this is wrong

--- pkg/util/version/const.go
+++ pkg/util/version/const.go
@@ -47,13 +47,13 @@ func FluentbitImage(acrDomain string) string {
 // MdmImage contains the location of the MDM container image
 // https://eng.ms/docs/products/geneva/collect/references/linuxcontainers
 func MdmImage(acrDomain string) string {
-	return acrDomain + "/distroless/genevamdm:2.2025.404.1254-77220c-20250406t1133@sha256:8e89bfec19c81398afa0ec51a97d748cc6b7b85cf9440dd1c7ea75b24302fe55"
+	return acrDomain + "/distroless/genevamdm:2.2025.404.1254-77220c-20250406t1133"
 }
 
 // MdsdImage contains the location of the MDSD container image
 // https://eng.ms/docs/products/geneva/collect/references/linuxcontainers
 func MdsdImage(acrDomain string) string {
-	return acrDomain + "/distroless/genevamdsd:mariner_20250225.2@sha256:da59ef7cfe3b0b9b6b453930cc629605cf3528ed11dbb88cdc50a38633198add"
+	return acrDomain + "/distroless/genevamdsd:mariner_20250225.2"
 }
 
 // MUOImage contains the location of the Managed Upgrade Operator container image
