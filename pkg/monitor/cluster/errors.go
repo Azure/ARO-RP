8733d4c49cd53fe282ada0cba4d870ed47b5acc8
Amber Brown <ambrown@redhat.com>
2025-09-22 13:13:45 +1000

    write out the inner errors and include a sentinel for panics

--- pkg/monitor/cluster/errors.go
+++ pkg/monitor/cluster/errors.go
@@ -6,13 +6,15 @@ package cluster
 import (
 	"errors"
 	"fmt"
+
+	"github.com/Azure/ARO-RP/pkg/util/stringutils"
 )
 
 var errFetchClusterVersion = errors.New("error fetching ClusterVersion")
 var errFetchAROOperatorMasterDeployment = errors.New("error fetching ARO Operator master deployment")
 var errListAROOperatorDeployments = errors.New("error listing ARO Operator deployments")
 var errListReplicaSets = errors.New("error listing replicasets")
-var errListNamespaces = errors.New("error")
+var errListNamespaces = errors.New("error listing cluster namespaces")
 
 var errAPIServerHealthzFailure = errors.New("error fetching APIServer healthz endpoint")
 var errAPIServerPingFailure = errors.New("error fetching APIServer healthz ping endpoint")
@@ -23,7 +25,10 @@ type failureToRunClusterCollector struct {
 }
 
 func (e *failureToRunClusterCollector) Error() string {
-	return fmt.Sprintf("failure running cluster collector '%s'", e.collectorName)
+	if e.inner != nil {
+		return fmt.Sprintf("failure running cluster collector '%s':\n%s", e.collectorName, stringutils.IndentLines(e.inner.Error(), "  "))
+	}
+	return fmt.Sprintf("failure running cluster collector '%s': <missing>", e.collectorName)
 }
 
 func (e *failureToRunClusterCollector) Is(err error) bool {
@@ -37,4 +42,21 @@ func (e *failureToRunClusterCollector) Is(err error) bool {
 
 func (e *failureToRunClusterCollector) Unwrap() error {
 	return e.inner
+}
+
+type collectorPanic struct {
+	panicValue any
+}
+
+func (e *collectorPanic) Error() string {
+	return fmt.Sprintf("panic: '%v'", e.panicValue)
+}
+
+func (e *collectorPanic) Is(err error) bool {
+	errCollector, ok := err.(*collectorPanic)
+	if !ok {
+		return false
+	}
+
+	return errCollector.panicValue == e.panicValue
 }
