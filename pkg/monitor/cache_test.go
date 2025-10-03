56569546f41c1fc2237cc081b293eb4c30866cf2
Alfonso Casimiro <21107567+alcasim@users.noreply.github.com>
2025-10-03 09:47:03 +0200

    decoration in new files

--- pkg/monitor/cache_test.go
+++ pkg/monitor/cache_test.go
@@ -1,5 +1,8 @@
 package monitor
 
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
 import (
 	"testing"
 
--- test/database/monitors.go
+++ test/database/monitors.go
@@ -1,5 +1,8 @@
 package database
 
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
 import (
 	"context"
 	"slices"
