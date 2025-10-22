1c1babbf96447f7733dfe71bace6eca94b09a960
Alfonso Casimiro <21107567+alcasim@users.noreply.github.com>
2025-10-22 12:20:50 +0200

    worker can now be tested when updates happen in subscriptions or clusters

--- pkg/monitor/worker_test.go
+++ pkg/monitor/worker_test.go
@@ -50,8 +50,9 @@ func TestExecute(t *testing.T) {
 	assert.True(t, triggeredFail)
 }
 
-func TestChangefeedOperations(t *testing.T) {
+func TestClusterOperationFlow(t *testing.T) {
 	// Setup test environment
+	ctx := context.Background()
 	env := createTestEnvironmentWithLocalCosmos(t)
 	defer env.LocalCosmosCleanup()
 
@@ -59,104 +60,167 @@ func TestChangefeedOperations(t *testing.T) {
 	mon := env.CreateTestMonitor("changefeed")
 
 	// Start changefeed
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	ctxChangeFeed, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
 	stopChan := make(chan struct{})
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	go func() {
-		<-ctx.Done()
+		<-ctxChangeFeed.Done()
 		close(stopChan)
 	}()
 
 	mon.changefeedInterval = time.Second / 2
 	go func() {
-		// Running changefeed loop every second
-		mon.changefeed(ctx, mon.baseLog.WithField("component", "changefeed"), stopChan)
+		mon.changefeed(ctxChangeFeed, mon.baseLog.WithField("component", "changefeed"), stopChan)
 		wg.Done()
 	}()
 
-	type operation struct {
-		name                          string
-		action                        string // "create"
-		clusterProvisioningState      api.ProvisioningState
-		subscriptionProvisioningState api.SubscriptionState
-		expectDocs                    int
-		expectSubs                    int
+	// Create an initial subscription and cluster
+	subDoc := newFakeSubscription()
+	_, err := env.SubscriptionsDB.Create(ctx, subDoc)
+	if err != nil {
+		t.Fatalf("Couldn't create subscription in cosmos: %v", err)
 	}
 
-	operations := []operation{
+	clusterDoc := newFakeCluster(subDoc.ResourceID)
+	clusterDoc.OpenShiftCluster.Properties.ProvisioningState = api.ProvisioningStateCreating
+
+	generatedCluster, err := env.OpenShiftClusterDB.Create(ctx, clusterDoc)
+	if err != nil {
+		t.Fatalf("Couldn't create cluster in cosmos: %v", err)
+	}
+
+	// we'll go over these steps and evaluate if the cache is being populated or not
+	type lifecycleStep struct {
+		name              string
+		clusterState      api.ProvisioningState
+		expectDocsInCache int
+		expectSubsInCache int
+	}
+
+	steps := []lifecycleStep{
 		{
-			name:                          "create first cluster with subscription",
-			action:                        "create",
-			clusterProvisioningState:      api.ProvisioningStateSucceeded,
-			subscriptionProvisioningState: api.SubscriptionStateRegistered,
-			expectDocs:                    1,
-			expectSubs:                    1,
+			name:              "cluster in Creating state - should NOT be in cache",
+			clusterState:      api.ProvisioningStateCreating,
+			expectDocsInCache: 0,
+			expectSubsInCache: 1,
 		},
 		{
-			name:                          "create second cluster with new subscription",
-			action:                        "create",
-			clusterProvisioningState:      api.ProvisioningStateSucceeded,
-			subscriptionProvisioningState: api.SubscriptionStateRegistered,
-			expectDocs:                    2,
-			expectSubs:                    2,
+			name:              "cluster transitions to Succeeded - should appear in cache",
+			clusterState:      api.ProvisioningStateSucceeded,
+			expectDocsInCache: 1,
+			expectSubsInCache: 1,
 		},
 		{
-			name:                          "create cluster in Deleting state - should be ignored",
-			action:                        "create",
-			clusterProvisioningState:      api.ProvisioningStateDeleting,
-			subscriptionProvisioningState: api.SubscriptionStateRegistered,
-			expectDocs:                    2,
-			expectSubs:                    3,
-		}, {
-			name:                          "create cluster in creating state - should be ignored",
-			action:                        "create",
-			clusterProvisioningState:      api.ProvisioningStateCreating,
-			subscriptionProvisioningState: api.SubscriptionStateRegistered,
-			expectDocs:                    2,
-			expectSubs:                    4,
-		}, {
-			name:                          "subscription and cluster in Deleting state - BOTH should be ignored",
-			action:                        "create",
-			clusterProvisioningState:      api.ProvisioningStateDeleting,
-			subscriptionProvisioningState: api.SubscriptionStateDeleted,
-			expectDocs:                    2,
-			expectSubs:                    4,
+			name:              "cluster transitions to Deleting - should disappear from cache",
+			clusterState:      api.ProvisioningStateDeleting,
+			expectDocsInCache: 0,
+			expectSubsInCache: 1,
 		},
 	}
 
-	// Execute operations in sequence
-	for _, op := range operations {
-		t.Run(op.name, func(t *testing.T) {
-			// Create subscription and cluster documents
-			subDoc := newFakeSubscription()
-			subDoc.Subscription.State = op.subscriptionProvisioningState
-			clusterDoc := newFakeCluster(subDoc.ResourceID)
-			clusterDoc.OpenShiftCluster.Properties.ProvisioningState = op.clusterProvisioningState
-
-			switch op.action {
-			case "create":
-				_, err := env.OpenShiftClusterDB.Create(context.Background(), clusterDoc)
+	// Execute lifecycle steps
+	for _, step := range steps {
+		t.Run(step.name, func(t *testing.T) {
+			// Update cluster to the new state (skip for first step since we already created it)
+			if step.clusterState != api.ProvisioningStateCreating {
+				generatedCluster.OpenShiftCluster.Properties.ProvisioningState = step.clusterState
+				generatedCluster, err = env.OpenShiftClusterDB.Update(ctx, generatedCluster)
 				if err != nil {
-					t.Fatalf("Couldn't create cluster doc: %v", err)
-				}
-				_, err = env.SubscriptionsDB.Create(context.Background(), subDoc)
-				if err != nil {
-					t.Fatalf("Couldn't create subscription doc: %v", err)
+					t.Fatalf("Couldn't update cluster in cosmos: %v", err)
 				}
 			}
 
 			// Wait for changefeed to process
-			time.Sleep(2 * time.Second)
+			time.Sleep(time.Second)
 
 			// Validate expected results
-			if len(mon.docs) != op.expectDocs {
-				t.Errorf("%s: expected %d documents in cache, got %d", op.name, op.expectDocs, len(mon.docs))
+			if len(mon.docs) != step.expectDocsInCache {
+				t.Errorf("expected %d clusters in cache, got %d", step.expectDocsInCache, len(mon.docs))
 			}
-			if len(mon.subs) != op.expectSubs {
-				t.Errorf("%s: expected %d subscriptions in cache, got %d", op.name, op.expectSubs, len(mon.subs))
+			if len(mon.subs) != step.expectSubsInCache {
+				t.Errorf("expected %d subscriptions in cache, got %d", step.expectSubsInCache, len(mon.subs))
+			}
+		})
+	}
+}
+
+func TestSubscriptionFlow(t *testing.T) {
+	// Setup test environment
+	ctx := context.Background()
+	env := createTestEnvironmentWithLocalCosmos(t)
+	defer env.LocalCosmosCleanup()
+
+	// Create single monitor for changefeed testing
+	mon := env.CreateTestMonitor("changefeed")
+
+	// Start changefeed
+	ctxChangeFeed, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	stopChan := make(chan struct{})
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		<-ctxChangeFeed.Done()
+		close(stopChan)
+	}()
+
+	mon.changefeedInterval = time.Second / 2
+	go func() {
+		mon.changefeed(ctxChangeFeed, mon.baseLog.WithField("component", "changefeed"), stopChan)
+		wg.Done()
+	}()
+
+	// Create initial subscription
+	subDoc := newFakeSubscription()
+	subDoc.Subscription.State = api.SubscriptionStateRegistered
+
+	generatedSub, err := env.SubscriptionsDB.Create(ctx, subDoc)
+	if err != nil {
+		t.Fatalf("Couldn't create subscription in cosmos: %v", err)
+	}
+
+	// Define subscription lifecycle steps
+	type lifecycleStep struct {
+		name              string
+		subscriptionState api.SubscriptionState
+		expectSubsInCache int
+	}
+
+	steps := []lifecycleStep{
+		{
+			name:              "subscription in Registered state - should be in cache",
+			subscriptionState: api.SubscriptionStateRegistered,
+			expectSubsInCache: 1,
+		},
+		{
+			name:              "subscription transitions to Deleted - should disappear from cache",
+			subscriptionState: api.SubscriptionStateDeleted,
+			expectSubsInCache: 0,
+		},
+	}
+
+	// Execute lifecycle steps
+	for _, step := range steps {
+		t.Run(step.name, func(t *testing.T) {
+			// Update subscription to the new state (skip for first step since we already created it)
+			if step.subscriptionState != api.SubscriptionStateRegistered {
+				generatedSub.Subscription.State = step.subscriptionState
+				generatedSub, err = env.SubscriptionsDB.Update(ctx, generatedSub)
+				if err != nil {
+					t.Fatalf("Couldn't update subscription in cosmos: %v", err)
+				}
+			}
+
+			// Wait for changefeed to process
+			time.Sleep(time.Second)
+
+			// Validate expected results
+			if len(mon.subs) != step.expectSubsInCache {
+				t.Errorf("expected %d subscriptions in cache, got %d", step.expectSubsInCache, len(mon.subs))
 			}
 		})
 	}
