b20500339fd8038d468df34c2d21a5fef209265a
Sudharsan Reddy M H <mhreddy@redhat.com>
2025-11-24 18:05:35 +0530

    Remove metrics changes - keep only logging changes

--- pkg/deploy/generator/scripts/util-services.sh
+++ pkg/deploy/generator/scripts/util-services.sh
@@ -622,9 +622,6 @@ processors:
       - key: \"host\"
         action: insert
         value: \"$(hostname)\"
-      - key: \"environmentType\"
-        action: insert
-        value: \"$ENVIRONMENTTYPE\"
 extensions:
   health_check:
     endpoint: $ipaddress:13133
