6596970cd098a1c6552b8226818be8336486d18e
Amber Brown <ambrown@redhat.com>
2025-12-04 13:17:20 +1100

    remove the explicit dynamic rest mapper where not required and remove the lazy discovery

--- pkg/cluster/install.go
+++ pkg/cluster/install.go
@@ -17,7 +17,6 @@ import (
 	"k8s.io/client-go/kubernetes"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 
 	configclient "github.com/openshift/client-go/config/clientset/versioned"
 	imageregistryclient "github.com/openshift/client-go/imageregistry/clientset/versioned"
@@ -650,14 +649,7 @@ func (m *manager) initializeKubernetesClients(ctx context.Context) error {
 		return err
 	}
 
-	mapper, err := apiutil.NewDynamicRESTMapper(restConfig, apiutil.WithLazyDiscovery)
-	if err != nil {
-		return err
-	}
-
-	client, err := client.New(restConfig, client.Options{
-		Mapper: mapper,
-	})
+	client, err := client.New(restConfig, client.Options{})
 
 	m.ch = clienthelper.NewWithClient(m.log, client)
 	return err
