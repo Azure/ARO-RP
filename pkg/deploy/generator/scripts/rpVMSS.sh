ac419f310682fd49b0eb0d87e2ab82980e51f029
Sudharsan Reddy M H <mhreddy@redhat.com>
2025-12-09 00:31:08 +0530

    Adding environment field for AROClusterLogs

--- pkg/deploy/generator/scripts/rpVMSS.sh
+++ pkg/deploy/generator/scripts/rpVMSS.sh
@@ -109,13 +109,13 @@ main() {
 [FILTER]
 	Name modify
 	Match journald
-	Remove_wildcard _
-	Remove TIMESTAMP
+	Add ENVIRONMENT \${ENVIRONMENT}
 
 [FILTER]
 	Name modify
 	Match journald
-	Add ENVIRONMENT \${ENVIRONMENT}
+	Remove_wildcard _
+	Remove TIMESTAMP
 
 [FILTER]
 	Name rewrite_tag
