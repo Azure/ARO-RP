8d251a622d2b8822b9f4f36a524fc425f62c7fe5
Maitiú Ó Ciaráin <mociarain@redhat.com>
2025-12-16 10:15:20 +0100

    Update pkg/mimo/steps/cluster/acrtoken_checker_test.go
    Co-authored-by: Christophe LACOMBE <141164119+Tof1973@users.noreply.github.com>

--- pkg/mimo/steps/cluster/acrtoken_checker_test.go
+++ pkg/mimo/steps/cluster/acrtoken_checker_test.go
@@ -83,7 +83,7 @@ func TestEnsureACRToken(t *testing.T) {
 							{
 								Name:      publicACR,
 								Username:  user,
-								IssueDate: &startOf20204,
+								IssueDate: &startOf2024,
 							},
 							{
 								Name:      intACR,
