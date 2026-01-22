d131a1fac107bb1b9ab2d1f5ae3c70b19c48b363
Amber Brown <ambrown@redhat.com>
2026-01-22 14:44:36 +1100

    add some az clients

--- pkg/mimo/tasks/networking.go
+++ pkg/mimo/tasks/networking.go
@@ -1,2 +1,16 @@
 package tasks
 
+import (
+	"github.com/Azure/ARO-RP/pkg/api"
+	"github.com/Azure/ARO-RP/pkg/mimo/steps/cluster"
+	"github.com/Azure/ARO-RP/pkg/util/mimo"
+	"github.com/Azure/ARO-RP/pkg/util/steps"
+)
+
+func MigrateInternalLoadBalancerZones(t mimo.TaskContext, doc *api.MaintenanceManifestDocument, oc *api.OpenShiftClusterDocument) error {
+	s := []steps.Step{
+		steps.Action(cluster.MigrateInternalLoadBalancerZonesStep),
+	}
+
+	return run(t, s)
+}
