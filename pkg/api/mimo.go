19b15abfb8fbef885650c67892dec7f50de9e1ef
Amber Brown <ambrown@redhat.com>
2026-02-02 12:57:46 +1100

    initial MIMO scheduler API additions

--- pkg/api/mimo.go
+++ pkg/api/mimo.go
@@ -4,6 +4,8 @@ package api
 // Licensed under the Apache License 2.0.
 
 type MaintenanceManifestState string
+type MaintenanceScheduleState string
+type MaintenanceScheduleSelectorOperator string
 
 const (
 	MaintenanceManifestStatePending         MaintenanceManifestState = "Pending"
@@ -13,6 +15,17 @@ const (
 	MaintenanceManifestStateRetriesExceeded MaintenanceManifestState = "RetriesExceeded"
 	MaintenanceManifestStateTimedOut        MaintenanceManifestState = "TimedOut"
 	MaintenanceManifestStateCancelled       MaintenanceManifestState = "Cancelled"
+)
+
+const (
+	MaintenanceScheduleStateEnabled    MaintenanceScheduleState = "Enabled"
+	MaintenanceScheduleStateProcessing MaintenanceScheduleState = "Processing"
+	MaintenanceManifestStateDisabled   MaintenanceScheduleState = "Disabled"
+	MaintenanceManifestStateInvalid    MaintenanceScheduleState = "Invalid"
+)
+
+const (
+	MaintenanceScheduleSelectorOperatorEq MaintenanceScheduleSelectorOperator = "eq"
 )
 
 type MIMOTaskID string
@@ -32,4 +45,24 @@ type MaintenanceManifest struct {
 	RunAfter int `json:"runAfter,omitempty"`
 	// RunBefore defines the latest that this manifest should start running
 	RunBefore int `json:"runBefore,omitempty"`
+}
+
+type MaintenanceSchedule struct {
+	MissingFields
+
+	State             MaintenanceScheduleState `json:"state,omitempty"`
+	MaintenanceTaskID MIMOTaskID               `json:"maintenanceTaskID,omitempty"`
+
+	Schedule         string `json:"schedule,omitempty"`
+	LookForwardCount int    `json:"lookForwardCount,omitempty"`
+	ScheduleAcross   string `json:"scheduleAcross,omitempty"`
+
+	Selectors []*MaintenanceScheduleSelector
+}
+
+type MaintenanceScheduleSelector struct {
+	Key      string                              `json:"key,omitempty"`
+	Operator MaintenanceScheduleSelectorOperator `json:"operator,omitempty"`
+	Value    string                              `json:"value,omitempty"`
+	Values   []string                            `json:"values,omitempty"`
 }
