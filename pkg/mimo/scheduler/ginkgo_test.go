794fea06a20ee51a4de05351fc6c1634bdd346d6
Amber Brown <ambrown@redhat.com>
2026-02-02 20:16:49 +1100

    scheduler framework, mostly building off the actuator

---
+++ pkg/mimo/scheduler/ginkgo_test.go
@@ -0,0 +1,13 @@
+package scheduler
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Scheduler Suite")
+}
