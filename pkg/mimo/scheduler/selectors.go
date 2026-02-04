85135fa30ac6561bc19651eb479d0c16aaa3c285
Amber Brown <ambrown@redhat.com>
2026-02-04 13:00:21 +1100

    wiring up the caches

---
+++ pkg/mimo/scheduler/selectors.go
@@ -0,0 +1,9 @@
+package scheduler
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+type selectorData struct {
+	ResourceID     string
+	SubscriptionID string
+}
