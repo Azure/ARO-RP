9c6253f0a74b730c30c018e80efe34d693b25f44
Amber Brown <ambrown@redhat.com>
2026-02-06 00:14:34 +1100

    make the selector data struct from the cluster cache and add code to check them against a set of selectors

--- pkg/mimo/scheduler/selectors.go
+++ pkg/mimo/scheduler/selectors.go
@@ -1,13 +1,89 @@
 package scheduler
 
-import "github.com/Azure/ARO-RP/pkg/api"
-
 // Copyright (c) Microsoft Corporation.
 // Licensed under the Apache License 2.0.
 
-type selectorData struct {
-	ResourceID string
+import (
+	"errors"
+	"fmt"
+	"slices"
 
-	SubscriptionID    string
-	SubscriptionState api.SubscriptionState
+	"github.com/sirupsen/logrus"
+
+	"github.com/Azure/ARO-RP/pkg/api"
+)
+
+type SelectorDataType string
+
+const (
+	SelectorDataTypeString SelectorDataType = "string"
+	SelectorDataTypeDate   SelectorDataType = "date"
+)
+
+type SelectorDataKey string
+
+const (
+	SelectorDataKeyResourceID        SelectorDataKey = "resourceID"
+	SelectorDataKeySubscriptionID    SelectorDataKey = "subscriptionID"
+	SelectorDataKeySubscriptionState SelectorDataKey = "subscriptionState"
+)
+
+type selectorData map[SelectorDataKey]string
+
+func (s selectorData) GetType(key string) SelectorDataType {
+	switch SelectorDataKey(key) {
+	case SelectorDataKeyResourceID, SelectorDataKeySubscriptionID, SelectorDataKeySubscriptionState:
+		return SelectorDataTypeString
+	}
+
+	return SelectorDataTypeString
+}
+
+func (s selectorData) GetString(key string) (string, bool) {
+	val, ok := s[SelectorDataKey(key)]
+	return val, ok
+}
+
+func (s selectorData) Matches(log *logrus.Entry, selectors []*api.MaintenanceScheduleSelector) (bool, error) {
+	matches := true
+
+	// Empty selector list never matches
+	if len(selectors) == 0 {
+		return false, errors.New("empty selector list")
+	}
+
+	for _, selector := range selectors {
+		selectorType := s.GetType(selector.Key)
+
+		switch selectorType {
+		case SelectorDataTypeString:
+			selectorVal, exists := s.GetString(selector.Key)
+			if !exists {
+				// selector doesn't match key
+				return false, fmt.Errorf("requested non-existent '%s' selector key", selector.Key)
+			}
+			switch selector.Operator {
+			// string eq
+			case api.MaintenanceScheduleSelectorOperatorEq:
+				if selectorVal != selector.Value {
+					// does not match
+					matches = false
+				}
+
+			// string in/not-in
+			case api.MaintenanceScheduleSelectorOperatorIn, api.MaintenanceScheduleSelectorOperatorNotIn:
+				contains := slices.Contains(selector.Values, selectorVal)
+
+				if (!contains && selector.Operator == api.MaintenanceScheduleSelectorOperatorIn) ||
+					(contains && selector.Operator == api.MaintenanceScheduleSelectorOperatorNotIn) {
+					matches = false
+				}
+			default:
+				return false, fmt.Errorf("unknown selector operator %s", selector.Operator)
+			}
+		default:
+			return false, fmt.Errorf("unhandled type '%s' of key '%s'", selectorType, selector.Key)
+		}
+	}
+	return matches, nil
 }
