7eb9e613224165fee867e9f7350d686f0860e8cd
Maitiu O Ciarain <m.ociarain@gmail.com>
2026-02-06 14:02:32 +0100

    Track down small unnamed instances of the variable

--- pkg/api/admin/openshiftcluster_validatestatic_test.go
+++ pkg/api/admin/openshiftcluster_validatestatic_test.go
@@ -788,7 +788,7 @@ func TestOpenShiftClusterStaticValidateDelta(t *testing.T) {
 			(&openShiftClusterConverter{}).ToInternal(tt.oc(), current)
 
 			v := &openShiftClusterStaticValidator{}
-			err := v.Static(oc, current, "", "", true, api.ArchitectureVersionV2, "")
+			err := v.Static(oc, current, "", "", api.ArchitectureVersionV2, "")
 			if err == nil {
 				if tt.wantErr != "" {
 					t.Error(err)
