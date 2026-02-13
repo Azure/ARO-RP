3dbf9520acdce8b9d636f46f1a022c6525f8c787
Amber Brown <ambrown@redhat.com>
2026-02-13 16:19:11 +1100

    initial admin API portions for mimo scheduler

--- pkg/frontend/frontend.go
+++ pkg/frontend/frontend.go
@@ -55,6 +55,7 @@ type frontendDBs interface {
 	database.DatabaseGroupWithSubscriptions
 	database.DatabaseGroupWithPlatformWorkloadIdentityRoleSets
 	database.DatabaseGroupWithMaintenanceManifests
+	database.DatabaseGroupWithMaintenanceSchedules
 	database.DatabaseGroupWithBilling
 }
 
@@ -315,6 +316,13 @@ func (f *frontend) chiAuthenticatedRoutes(router chi.Router) {
 		r.Route("/maintenancemanifests", func(r chi.Router) {
 			r.Get("/queued", f.getAdminQueuedMaintManifests)
 		})
+
+		r.Route("/maintenanceschedules", func(r chi.Router) {
+			// r.Get("/", f.getAdminMaintManifests)
+			r.Put("/", f.putAdminMaintScheduleCreate)
+			r.Get("/", f.getAdminMaintSchedules)
+		})
+
 		r.Route("/hivesyncset", func(r chi.Router) {
 			r.Get("/", f.listAdminHiveSyncSet)
 			r.Get("/syncsetname/{syncsetname}", f.getAdminHiveSyncSet)
