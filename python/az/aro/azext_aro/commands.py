4572eab6c9c1d960d80c2c8f571c12237b8ddc69
Adam Price <adprice@redhat.com>
2026-02-19 13:50:04 -0500

    chore: remove a FIXME

--- python/az/aro/azext_aro/commands.py
+++ python/az/aro/azext_aro/commands.py
@@ -31,6 +31,5 @@ def load_command_table(loader, _):
 
         g.custom_command('validate', 'aro_validate')
 
-    # FIXME: "identity" doesn't show up in the list of commands in `az aro -h`
     with loader.command_group('aro identity', aro_sdk, client_factory=cf_aro) as g:
         g.custom_command('list-required', 'aro_identity_list_required', supports_no_wait=False)
