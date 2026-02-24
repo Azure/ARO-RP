e5553eaa0152f7361211b7996c9140cb44084342
Amber Brown <ambrown@redhat.com>
2026-02-24 17:20:57 +1100

    clean up how the regex works, ignore seconds

--- pkg/mimo/scheduler/calendar.go
+++ pkg/mimo/scheduler/calendar.go
@@ -4,6 +4,7 @@ package scheduler
 // Licensed under the Apache License 2.0.
 
 import (
+	"errors"
 	"fmt"
 	"regexp"
 	"slices"
@@ -21,29 +22,15 @@ type calendar struct {
 
 	Hour   []int
 	Minute []int
-	Second []int
 }
 
-var calRegex = regexp.MustCompile(`(?:((?:Sun|Mon|Tue|Wed|Thu|Fri|Sat|,)+) )?([0-9,\*]+)-([0-9,\*]+)-([0-9,\*]+) ([0-9,\*]+):([0-9,\*]+):([0-9,\*]+)`)
+var calRegex = regexp.MustCompile(`(?:((?P<weekday>:Sun|Mon|Tue|Wed|Thu|Fri|Sat|,)+) )?(?P<year>[0-9,\*]+)-(?P<month>[0-9,\*]+)-(?P<day>[0-9,\*]+) (?P<hour>[0-9,\*]+):(?P<minute>[0-9,\*]+)(?::(?P<second>[0-9,\*]+))?$`)
 
 func ParseCalendar(s string) (cal calendar, reterr error) {
+	names := calRegex.SubexpNames()
 	p := calRegex.FindStringSubmatch(s)
 	if len(p) == 0 {
 		return calendar{}, fmt.Errorf("failed to match '%s'", s)
-	}
-
-	if len(p) > 1 {
-		p = p[1:]
-	}
-
-	// add the day if it's not on there
-	if len(p) == 6 {
-		p = append([]string{"*"}, p...)
-	}
-
-	if len(p) != 7 {
-		reterr = fmt.Errorf("failed parsing, only got %#v", p)
-		return
 	}
 
 	c := calendar{
@@ -53,115 +40,113 @@ func ParseCalendar(s string) (cal calendar, reterr error) {
 		Day:     []int{},
 		Hour:    []int{},
 		Minute:  []int{},
-		Second:  []int{},
 	}
 
-	// calc weekdays
-	if p[0] != "*" {
-		for weekday := range strings.SplitSeq(p[0], ",") {
-			switch weekday {
-			case "Mon":
-				c.Weekday = append(c.Weekday, time.Monday)
-			case "Tue":
-				c.Weekday = append(c.Weekday, time.Tuesday)
-			case "Wed":
-				c.Weekday = append(c.Weekday, time.Wednesday)
-			case "Thu":
-				c.Weekday = append(c.Weekday, time.Thursday)
-			case "Fri":
-				c.Weekday = append(c.Weekday, time.Friday)
-			case "Sat":
-				c.Weekday = append(c.Weekday, time.Saturday)
-			case "Sun":
-				c.Weekday = append(c.Weekday, time.Sunday)
+	for x := range len(p) {
+		switch names[x] {
+		case "weekday":
+			// calc weekdays -- missing counts as all
+			if p[x] != "*" && p[x] != "" {
+				for weekday := range strings.SplitSeq(p[x], ",") {
+					switch weekday {
+					case "Mon":
+						c.Weekday = append(c.Weekday, time.Monday)
+					case "Tue":
+						c.Weekday = append(c.Weekday, time.Tuesday)
+					case "Wed":
+						c.Weekday = append(c.Weekday, time.Wednesday)
+					case "Thu":
+						c.Weekday = append(c.Weekday, time.Thursday)
+					case "Fri":
+						c.Weekday = append(c.Weekday, time.Friday)
+					case "Sat":
+						c.Weekday = append(c.Weekday, time.Saturday)
+					case "Sun":
+						c.Weekday = append(c.Weekday, time.Sunday)
+					}
+				}
 			}
-		}
-	}
 
-	if p[1] != "*" {
-		for day := range strings.SplitSeq(p[1], ",") {
-			v, err := strconv.Atoi(day)
-			if err != nil {
-				reterr = fmt.Errorf("error parsing year: %w", err)
+		case "year":
+			if p[x] != "*" {
+				for year := range strings.SplitSeq(p[x], ",") {
+					v, err := strconv.Atoi(year)
+					if err != nil {
+						reterr = fmt.Errorf("error parsing year: %w", err)
+						return
+					}
+					if v < 2026 {
+						reterr = fmt.Errorf("error parsing year: %d is out of range (before 2026)", v)
+						return
+					}
+					c.Year = append(c.Year, v)
+				}
+			}
+		case "month":
+			if p[x] != "*" {
+				for month := range strings.SplitSeq(p[x], ",") {
+					v, err := strconv.Atoi(month)
+					if err != nil {
+						reterr = fmt.Errorf("error parsing month: %w", err)
+						return
+					}
+					if v > 12 || v < 1 {
+						reterr = fmt.Errorf("error parsing month: %d is out of range", v)
+						return
+					}
+					c.Month = append(c.Month, v)
+				}
+			}
+		case "day":
+			if p[x] != "*" {
+				for day := range strings.SplitSeq(p[x], ",") {
+					v, err := strconv.Atoi(day)
+					if err != nil {
+						reterr = fmt.Errorf("error parsing day: %w", err)
+						return
+					}
+					if v > 31 || v < 1 {
+						reterr = fmt.Errorf("error parsing day: %d is out of range", v)
+						return
+					}
+					c.Day = append(c.Day, v)
+				}
+			}
+		case "hour":
+			if p[x] != "*" {
+				for hour := range strings.SplitSeq(p[x], ",") {
+					v, err := strconv.Atoi(hour)
+					if err != nil {
+						reterr = fmt.Errorf("error parsing hour: %w", err)
+						return
+					}
+					if v > 23 || v < 0 {
+						reterr = fmt.Errorf("error parsing hour: %d is out of range", v)
+						return
+					}
+					c.Hour = append(c.Hour, v)
+				}
+			}
+		case "minute":
+			if p[x] != "*" {
+				for minute := range strings.SplitSeq(p[x], ",") {
+					v, err := strconv.Atoi(minute)
+					if err != nil {
+						reterr = fmt.Errorf("error parsing minute: %w", err)
+						return
+					}
+					if v > 59 || v < 0 {
+						reterr = fmt.Errorf("error parsing minute: %d is out of range", v)
+						return
+					}
+					c.Minute = append(c.Minute, v)
+				}
+			}
+		case "second":
+			if p[x] != "00" && p[x] != "0" && p[x] != "" {
+				reterr = errors.New("error parsing seconds: per-second granularity is unsupported")
 				return
 			}
-			if v < 2026 {
-				reterr = fmt.Errorf("error parsing year: %d is out of range (before 2026)", v)
-				return
-			}
-			c.Year = append(c.Year, v)
-		}
-	}
-	if p[2] != "*" {
-		for day := range strings.SplitSeq(p[2], ",") {
-			v, err := strconv.Atoi(day)
-			if err != nil {
-				reterr = fmt.Errorf("error parsing month: %w", err)
-				return
-			}
-			if v > 12 || v < 1 {
-				reterr = fmt.Errorf("error parsing month: %d is out of range", v)
-				return
-			}
-			c.Month = append(c.Month, v)
-		}
-	}
-	if p[3] != "*" {
-		for day := range strings.SplitSeq(p[3], ",") {
-			v, err := strconv.Atoi(day)
-			if err != nil {
-				reterr = fmt.Errorf("error parsing day: %w", err)
-				return
-			}
-			if v > 31 || v < 1 {
-				reterr = fmt.Errorf("error parsing day: %d is out of range", v)
-				return
-			}
-			c.Day = append(c.Day, v)
-		}
-	}
-
-	if p[4] != "*" {
-		for day := range strings.SplitSeq(p[4], ",") {
-			v, err := strconv.Atoi(day)
-			if err != nil {
-				reterr = fmt.Errorf("error parsing hour: %w", err)
-				return
-			}
-			if v > 23 || v < 0 {
-				reterr = fmt.Errorf("error parsing hour: %d is out of range", v)
-				return
-			}
-			c.Hour = append(c.Hour, v)
-		}
-	}
-
-	if p[5] != "*" {
-		for day := range strings.SplitSeq(p[5], ",") {
-			v, err := strconv.Atoi(day)
-			if err != nil {
-				reterr = fmt.Errorf("error parsing minute: %w", err)
-				return
-			}
-			if v > 59 || v < 0 {
-				reterr = fmt.Errorf("error parsing minute: %d is out of range", v)
-				return
-			}
-			c.Minute = append(c.Minute, v)
-		}
-	}
-	if p[6] != "*" {
-		for day := range strings.SplitSeq(p[6], ",") {
-			v, err := strconv.Atoi(day)
-			if err != nil {
-				reterr = fmt.Errorf("error parsing seconds: %w", err)
-				return
-			}
-			if v > 59 || v < 0 {
-				reterr = fmt.Errorf("error parsing seconds: %d is out of range", v)
-				return
-			}
-			c.Second = append(c.Second, v)
 		}
 	}
 
@@ -171,7 +156,6 @@ func ParseCalendar(s string) (cal calendar, reterr error) {
 	slices.Sort(c.Day)
 	slices.Sort(c.Hour)
 	slices.Sort(c.Minute)
-	slices.Sort(c.Second)
 
 	return c, nil
 }
@@ -225,11 +209,8 @@ func IsValid(t time.Time, sched calendar) bool {
 		}
 	}
 
-	if len(sched.Second) != 0 {
-		SecondOK := slices.Contains(sched.Second, t.Second())
-		if !SecondOK {
-			return false
-		}
+	if t.Second() != 0 {
+		return false
 	}
 
 	return true
@@ -374,23 +355,9 @@ func Next(now time.Time, sched calendar) (time.Time, bool) {
 			}
 		}
 
-		if len(sched.Second) > 0 {
-			hasSecond := false
-			for _, s := range sched.Second {
-				if s == nextPossible.Second() {
-					hasSecond = true
-					break
-				}
-				if s > nextPossible.Second() {
-					nextPossible = time.Date(nextPossible.Year(), nextPossible.Month(), nextPossible.Day(), nextPossible.Hour(), nextPossible.Minute(), s, 0, nextPossible.Location())
-					hasSecond = true
-					break
-				}
-			}
-			if !hasSecond {
-				nextPossiblePlus := nextPossible.Add(time.Second * 60)
-				nextPossible = time.Date(nextPossiblePlus.Year(), nextPossiblePlus.Month(), nextPossiblePlus.Day(), nextPossiblePlus.Hour(), nextPossiblePlus.Minute(), sched.Second[0], 0, nextPossible.Location())
-			}
+		if nextPossible.Second() != 0 {
+			nextPossiblePlus := nextPossible.Add(time.Second * 60)
+			nextPossible = time.Date(nextPossiblePlus.Year(), nextPossiblePlus.Month(), nextPossiblePlus.Day(), nextPossiblePlus.Hour(), nextPossiblePlus.Minute(), 0, 0, nextPossible.Location())
 		}
 
 		if IsValid(nextPossible, sched) {
