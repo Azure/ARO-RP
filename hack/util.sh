b613acbf1f76cba67868c80351242da97137e54a
Steven Fairchild <sfairchi@redhat.com>
2026-03-11 16:44:48 -0400

    Update hive-generate-config.sh
    * Add additional bash script logging utilities
    * Update default hive hash to latest
    * Update hive-dev-install.sh
        * Simplify
        * Create hive secret completely in memory to remove writing of
          temporary pull secret to filesystem
    
    * Check if util.sh has been already been sourced before attempting to source it.
        * Prevents failures caused by re-assigning readonly variables.
    
    Correct Hive repository url
    Update default hive image tag to use full commits (as used in the new
    repo)

--- hack/util.sh
+++ hack/util.sh
@@ -3,7 +3,43 @@
 # declare -r __hack_util_sourced="true"
 declare -r __hack_util_sourced="true"
 
-[ "${DEBUG:-false}" == true ] && set -x
+[ "${XTRACE:-'false'}" == "true" ] && set -x
+
+## Log level colors
+
+# declare -r COLOR_RED='\e[31m'
+declare -r COLOR_RED='\e[31m'
+# declare -r COLOR_RED_BG='\033[41m'
+declare -r COLOR_RED_BG='\033[41m'
+# declare -r COLOR_GREEN='\e[32m'
+declare -r COLOR_GREEN='\e[32m'
+# declare -r COLOR_YELLOW='\e[33m'
+declare -r COLOR_YELLOW='\e[33m'
+# declare -r COLOR_BLUE='\e[34m'
+# shellcheck disable=SC2034
+declare -r COLOR_BLUE='\e[34m'
+# declare -r COLOR_CYAN='\e[36m'
+declare -r COLOR_CYAN='\e[36m'
+# declare -r COLOR_RESET='\e[0m'
+#
+# Reset color
+declare -r COLOR_RESET='\e[0m'
+
+## Log levels
+
+# declare -ri LOG_LEVEL_CRIT=2
+declare -ri LOG_LEVEL_CRIT=2
+# declare -ri LOG_LEVEL_ERR=3
+declare -ri LOG_LEVEL_ERR=3
+# declare -ri LOG_LEVEL_WARN=4
+declare -ri LOG_LEVEL_WARN=4
+# declare -ri LOG_LEVEL_INFO=6
+declare -ri LOG_LEVEL_INFO=6
+# declare -ri LOG_LEVEL_DEBUG=7
+declare -ri LOG_LEVEL_DEBUG=7
+
+# declare -r LOG_LEVEL="${LOG_LEVEL:-$LOG_LEVEL_INFO}"
+declare -r LOG_LEVEL="${LOG_LEVEL:-$LOG_LEVEL_INFO}"
 
 # log()
 # log is a wrapper for echo that includes the function name
@@ -12,21 +48,51 @@ declare -r __hack_util_sourced="true"
 #   * 1) msg - string
 #   * 2) stack_level - int; optional, defaults to calling function
 log() {
-    local -r msg="${1:-"log message is empty"}"
-    local -r stack_level="${2:-1}"
+    local -r msg_prefix="${1:-}"
+    local -r msg="${2:-}"
+    local -r stack_level="${3:-1}"
 
-    echo "${FUNCNAME[${stack_level}]}: ${msg}"
+    echo -e "${msg_prefix} ${FUNCNAME[${stack_level}]}: ${msg}"
 }
 
-# abort is a wrapper for log followed by exit code 1
+# error()
+error() {
+    if (( LOG_LEVEL_ERR <= LOG_LEVEL )); then
+        log "${COLOR_RED}ERROR${COLOR_RESET}[$(date -Iminutes)]" "$1" 2
+    fi
+}
+
+# warn()
+warn() {
+    if (( LOG_LEVEL_WARN <= LOG_LEVEL )); then
+        log "${COLOR_YELLOW}WARN${COLOR_RESET}[$(date -Iminutes)]" "$1" 2
+    fi
+}
+
+# info()
+info() {
+    if (( LOG_LEVEL_INFO <= LOG_LEVEL )); then
+        log "${COLOR_CYAN}INFO${COLOR_RESET}[$(date -Iminutes)]" "$1" 2
+    fi
+}
+
+debug() {
+    if (( LOG_LEVEL_DEBUG <= LOG_LEVEL )); then
+        log "${COLOR_GREEN}INFO${COLOR_RESET}[$(date -Iminutes)]" "$1" 2
+    fi
+}
+
+# fatal is a wrapper for log followed by exit code 1
 #
 # args:
 #   * 1) message - string; error message passed to log
-abort() {
-    local -ri origin_stacklevel=2
+fatal() {
+    if (( LOG_LEVEL_CRIT <= LOG_LEVEL )); then
+        prefix="${COLOR_RED_BG}FATAL${COLOR_RESET}[$(date -Iminutes)]"
+    fi
 
-    log "${1}" "$origin_stacklevel"
-    log "Exiting"
+    log  "$prefix" "$1" 2
+    log "$prefix" "Exiting"
     exit 1
 }
 
@@ -34,13 +100,38 @@ abort() {
 # Deletes all files and directories provided.
 #
 # args:
-#   * @) tmp - array; files and/or directories to be deleted
+#   * @) tmp - nameref, array; files and/or directories to be deleted
 cleanup() {
-    # shellcheck disable=SC2068
     for tmp in $@; do
         if [[ -d "$tmp" || -f "$tmp" ]]; then
-            log "Deleting $tmp"
+            info "Deleting $tmp"
             rm -fr "$tmp"
         fi
     done
+}
+
+# retry()
+#
+# retry commands
+# args:
+#   * 1) cmd_retry_str - string; Command and all it's options/arguments in a single string
+#   * 2) wait - int; Optional - sleep time, Defaults to 5m
+#   * 3) retries - integer; Optional - Amount of times to retry command, defaults to 5
+retry() {
+    local cmd_retry_str="$1"
+    local wait="${2:-"5m"}"
+    local -ri retries="${3:-5}"
+
+    mapfile -t cmd_retry <<< "$cmd_retry_str"
+
+    for attempt in $(seq 1 $retries); do
+        info "attempt #${attempt} - ${cmd_retry[*]}"
+        # shellcheck disable=SC2068
+        ${cmd_retry[@]} &
+
+        wait -f $! && return 0
+        sleep "$wait"
+    done
+
+    error "${cmd_retry[*]} failed after #$retries attempts"
 }
