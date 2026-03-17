8d290f1cd5b5530eab585f6ece12a001b85bc6e3
Steven Fairchild <sfairchi@redhat.com>
2026-03-17 13:16:02 -0400

    Convert tabs to spaces in hive-dev-install.sh
    Bash uses spaces for indentation. Tabs were erroneously added.

--- hack/hive/hive-dev-install.sh
+++ hack/hive/hive-dev-install.sh
@@ -4,22 +4,22 @@ set -o errexit \
     -o nounset
 
 main() {
-	info "starting"
+    info "starting"
 
-	if [ ! -f "$HIVE_CONFIG/hive-deployment.yaml" ] || [ ! -d "$HIVE_CONFIG/crds" ]; then
+    if [ ! -f "$HIVE_CONFIG/hive-deployment.yaml" ] || [ ! -d "$HIVE_CONFIG/crds" ]; then
         fatal "hive-config is incomplete. Regenerate by running hack/hive/hive-generate-config.sh"
-	fi
+    fi
 
     kubectl="$(which kubectl 2> /dev/null)" \
     || kubectl_install kubectl
 
     info "Creating $HIVE_OPERATOR_NS"
-	$kubectl get namespace "$HIVE_OPERATOR_NS" > /dev/null 2>&1 || $kubectl create namespace "$HIVE_OPERATOR_NS"
+    $kubectl get namespace "$HIVE_OPERATOR_NS" > /dev/null 2>&1 || $kubectl create namespace "$HIVE_OPERATOR_NS"
 
     info "Applying $HIVE_CONFIG/crds"
-	$kubectl apply -f "$HIVE_CONFIG/crds"
+    $kubectl apply -f "$HIVE_CONFIG/crds"
 
-	[ -z "$PULL_SECRET" ] && fatal "global pull secret variable required, please source ./env"
+    [ -z "$PULL_SECRET" ] && fatal "global pull secret variable required, please source ./env"
     info "Generating and applying secret $HIVE_GLOBAL_PULL_SECRET_NAME"
     kubectl create \
             secret \
@@ -32,18 +32,18 @@ main() {
             --dry-run=client \
     | $kubectl apply -f -
 
-	sed "s/HIVE_OPERATOR_NS/$HIVE_OPERATOR_NS/g" "$HIVE_CONFIG/hive-config.yaml" | $kubectl apply -f -
-	$kubectl apply -f "$HIVE_CONFIG/hive-additional-install-log-regexes.yaml"
-	$kubectl apply -f "$HIVE_CONFIG/hive-deployment.yaml"
+    sed "s/HIVE_OPERATOR_NS/$HIVE_OPERATOR_NS/g" "$HIVE_CONFIG/hive-config.yaml" | $kubectl apply -f -
+    $kubectl apply -f "$HIVE_CONFIG/hive-additional-install-log-regexes.yaml"
+    $kubectl apply -f "$HIVE_CONFIG/hive-deployment.yaml"
 
     # shellcheck disable=SC2329
-	kubectl_get() { $kubectl -n "$HIVE_OPERATOR_NS" get "$1"; }
+    kubectl_get() { $kubectl -n "$HIVE_OPERATOR_NS" get "$1"; }
 
     info "Getting $HIVE_DEPLOYMENT_OPERATOR"
     # Ensure the deployment exists before we wait on it
     retry "kubectl_get $HIVE_DEPLOYMENT_OPERATOR" || fatal "$HIVE_DEPLOYMENT_CONTROLLERS failed to create."
     info "Waiting for $HIVE_DEPLOYMENT_OPERATOR to become available."
-	$kubectl wait \
+    $kubectl wait \
         --timeout=5m \
         --for=condition=Available \
         --namespace \
@@ -53,8 +53,8 @@ main() {
     info "Getting $HIVE_DEPLOYMENT_CONTROLLERS"
     # Ensure the deployment exists before we wait on it
     retry "kubectl_get $HIVE_DEPLOYMENT_CONTROLLERS" || fatal "$HIVE_DEPLOYMENT_CONTROLLERS failed to create."
-	info "Waiting for $HIVE_DEPLOYMENT_CONTROLLERS to be available..."
-	$kubectl wait \
+    info "Waiting for $HIVE_DEPLOYMENT_CONTROLLERS to be available..."
+    $kubectl wait \
         --timeout=5m \
         --for=condition=Available \
         --namespace "$HIVE_OPERATOR_NS" \
@@ -69,7 +69,7 @@ kubectl_install() {
     kubectl_stable_url="https://dl.k8s.io/release/$kubectl_stable_version/bin/linux/amd64/kubectl"
 
     info "Downloading $kubectl_stable_url"
-	curl -sLO \
+    curl -sLO \
         --create-dirs \
         --output-dir "$HOME/bin" \
          \
@@ -77,7 +77,7 @@ kubectl_install() {
 
     # shellcheck disable=SC2034
     kubectl_bin="$HOME/bin/kubectl"
-	chmod 755 "$kubectl_bin" || fatal "failed to mark $kubectl_bin as executable."
+    chmod 755 "$kubectl_bin" || fatal "failed to mark $kubectl_bin as executable."
 }
 
 # declare -r source_file_not_found_err="not found. Are you in the ARO-RP repository root?"
