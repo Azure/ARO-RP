2f2713d56755813e2d0486c53b0f41d1c14bea7d
b-lnimmala <b-lnimmala@microsoft.com>
2026-03-18 09:49:54 -0400

    updated the unit tests and structure

--- pkg/frontend/admin_hive_k8s_objects_list.go
+++ pkg/frontend/admin_hive_k8s_objects_list.go
@@ -11,6 +11,13 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/sirupsen/logrus"
 
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
+
 	"github.com/Azure/ARO-RP/pkg/frontend/middleware"
 )
 
@@ -28,50 +35,74 @@ func (f *frontend) adminHiveK8sObjectsList(w http.ResponseWriter, r *http.Reques
 	)
 
 	if name != "" {
-		b, err = f.getHiveK8sObject(ctx, resource, namespace, name)
+		b, err = f.getHiveK8sObject(ctx, log, resource, namespace, name)
 	} else {
-		b, err = f.listHiveK8sObjects(ctx, resource, namespace)
+		b, err = f.listHiveK8sObjects(ctx, log, resource, namespace)
 	}
 
 	adminReply(log, w, nil, b, err)
 }
 
-func (f *frontend) listHiveK8sObjects(ctx context.Context, resource, namespace string) ([]byte, error) {
-	if f.kubeActionsFactory == nil {
-		return nil, fmt.Errorf("kube actions factory not configured")
+// getHiveClient builds a dynamic client pointed at the Hive AKS cluster.
+// used by hiveClusterManager (pkg/hive/manager.go NewFromEnvCLusterManager).
+func (f *frontend) getHiveDynamicClient(ctx context.Context) (dynamic.Interface, meta.RESTMapper, error) {
+	if f.hiveClusterManager == nil {
+		return nil, nil, fmt.Errorf("hive is not enabled")
 	}
 
-	log := logrus.NewEntry(logrus.StandardLogger())
-
-	k, err := f.kubeActionsFactory(log, f.env, nil)
+	hiveShard := 1
+	restConfig, err := f.env.LiveConfig().HiveRestConfig(ctx, hiveShard)
 	if err != nil {
-		return nil, err
+		return nil, nil, fmt.Errorf("failed getting Hive REST config: %w", err)
 	}
 
-	_, err = k.ResolveGVR(resource, "")
+	mapper, err := apiutil.NewDynamicRESTMapper(restConfig, apiutil.WithLazyDiscovery)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	return k.KubeList(ctx, resource, namespace)
+	dyn, err := dynamic.NewForConfig(restConfig)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return dyn, mapper, nil
 }
 
-func (f *frontend) getHiveK8sObject(ctx context.Context, resource, namespace, name string) ([]byte, error) {
-	if f.kubeActionsFactory == nil {
-		return nil, fmt.Errorf("kube actions factory not configured")
-	}
-
-	log := logrus.NewEntry(logrus.StandardLogger())
-
-	k, err := f.kubeActionsFactory(log, f.env, nil)
+func (f *frontend) listHiveK8sObjects(ctx context.Context, log *logrus.Entry, resource, namespace string) ([]byte, error) {
+	dyn, mapper, err := f.getHiveDynamicClient(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	_, err = k.ResolveGVR(resource, "")
+	gvr, err := mapper.ResourceFor(schema.ParseGroupResource(resource).WithVersion(""))
 	if err != nil {
 		return nil, err
 	}
 
-	return k.KubeGet(ctx, resource, namespace, name)
+	ul, err := dyn.Resource(gvr).Namespace(namespace).List(ctx, metav1.ListOptions{Limit: 1000})
+	if err != nil {
+		return nil, err
+	}
+
+	return ul.MarshalJSON()
+}
+
+func (f *frontend) getHiveK8sObject(ctx context.Context, log *logrus.Entry, resource, namespace, name string) ([]byte, error) {
+	dyn, mapper, err := f.getHiveDynamicClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	gvr, err := mapper.ResourceFor(schema.ParseGroupResource(resource).WithVersion(""))
+	if err != nil {
+		return nil, err
+	}
+
+	un, err := dyn.Resource(gvr).Namespace(namespace).Get(ctx, name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	return un.MarshalJSON()
 }
