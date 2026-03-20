8fc49f7434e35de13d20d4679e9da7400b90a14a
chreed <chreed@redhat.com>
2026-03-20 14:33:39 -0400

    ARO-24827 Revert hack scripts to original status

--- hack/get-admin-kubeconfig.sh
+++ hack/get-admin-kubeconfig.sh
@@ -6,8 +6,6 @@ if [[ "$#" -ne 1 ]]; then
 fi
 
 if [[ $CI ]]; then
-    # Build db binary if it doesn't exist
-    [ ! -f ./db ] && go build ./hack/db
     ./db "$1" | jq -r .openShiftCluster.properties.adminKubeconfig | base64 -d | sed -e 's|https://api-int\.|https://api\.|'
 else
     go run ./hack/db "$1" | jq -r .openShiftCluster.properties.adminKubeconfig | base64 -d | sed -e 's|https://api-int\.|https://api\.|'
