e966fd76020f0fdfa30c4535b665406c2d03be6e
Amber Brown <ambrown@redhat.com>
2026-04-14 15:06:09 +1000

    add more tests for the scheduler working

--- pkg/mimo/scheduler/service_test.go
+++ pkg/mimo/scheduler/service_test.go
@@ -5,12 +5,14 @@ package scheduler
 
 import (
 	"context"
+	"strings"
 	"sync"
 	"testing"
 	"time"
 
 	"github.com/go-test/deep"
 	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
 
@@ -112,12 +114,12 @@ func TestSchedulerPolling(t *testing.T) {
 
 			controller := gomock.NewController(nil)
 			_env := mock_env.NewMockInterface(controller)
+			_env.EXPECT().Now().AnyTimes().DoAndReturn(func() time.Time { return time.Date(2026, 1, 1, 0, 0, 0, 0, time.UTC) })
 			hook, log := testlog.LogForTesting(t)
 
 			fixtures := testdatabase.NewFixture()
 
-			now := func() time.Time { return time.Date(2026, 1, 1, 0, 0, 0, 0, time.UTC) }
-			manifests, _ := testdatabase.NewFakeMaintenanceManifests(now)
+			manifests, _ := testdatabase.NewFakeMaintenanceManifests(_env.Now)
 			schedules, _ := testdatabase.NewFakeMaintenanceSchedules()
 			clusters, _ := testdatabase.NewFakeOpenShiftClusters()
 			subscriptions, _ := testdatabase.NewFakeSubscriptions()
@@ -134,9 +136,9 @@ func TestSchedulerPolling(t *testing.T) {
 			require.NoError(err)
 
 			svc := NewService(_env, log, dbs, metrics)
-			svc.now = now
-			svc.workerDelay = func() time.Duration { return 0 * time.Second }
+			svc.workerDelayMax = 0
 			svc.serveHealthz = false
+			svc.emitHeartbeat = false
 			svc.stopping.Store(true)
 
 			newOld, err := svc.poll(ctx, tt.previousLoop)
@@ -177,6 +179,7 @@ func TestSchedulerStoppingWholeProcess(t *testing.T) {
 	m := testmetrics.NewFakeMetricsEmitter(t)
 	controller := gomock.NewController(nil)
 	_env := mock_env.NewMockInterface(controller)
+	_env.EXPECT().Now().AnyTimes().DoAndReturn(func() time.Time { return time.Date(2026, 1, 1, 0, 0, 0, 0, time.UTC) })
 
 	_, log := testlog.LogForTesting(t)
 	fixtures := testdatabase.NewFixture()
@@ -198,9 +201,10 @@ func TestSchedulerStoppingWholeProcess(t *testing.T) {
 	sched := &fakeScheduler{waitOnProcess: waitFor}
 
 	svc := NewService(_env, log, dbs, m)
-	svc.workerDelay = func() time.Duration { return 0 * time.Second }
-	svc.pollTime = 1 * time.Millisecond
-	svc.newScheduler = func(_ env.Interface, _ *logrus.Entry, _ metrics.Emitter, _ getCachedScheduleDocFunc, _ getClustersFunc, _ schedulerDBs, _ func() time.Time) (Scheduler, error) {
+	svc.workerDelayMax = 0
+	svc.interval = time.Millisecond
+	svc.schedulePollInterval = 1 * time.Millisecond
+	svc.newScheduler = func(_ env.Interface, _ *logrus.Entry, _ metrics.Emitter, _ getCachedScheduleDocFunc, _ getClustersFunc, _ schedulerDBs) (Scheduler, error) {
 		return sched, nil
 	}
 
@@ -239,6 +243,7 @@ func TestSchedulerStoppingSingleItem(t *testing.T) {
 	m := testmetrics.NewFakeMetricsEmitter(t)
 	controller := gomock.NewController(nil)
 	_env := mock_env.NewMockInterface(controller)
+	_env.EXPECT().Now().AnyTimes().DoAndReturn(func() time.Time { return time.Date(2026, 1, 1, 0, 0, 0, 0, time.UTC) })
 
 	_, log := testlog.LogForTesting(t)
 	fixtures := testdatabase.NewFixture()
@@ -260,9 +265,10 @@ func TestSchedulerStoppingSingleItem(t *testing.T) {
 	sched := &fakeScheduler{waitOnProcess: waitFor}
 
 	svc := NewService(_env, log, dbs, m)
-	svc.workerDelay = func() time.Duration { return 0 * time.Second }
-	svc.pollTime = 1 * time.Millisecond
-	svc.newScheduler = func(_ env.Interface, _ *logrus.Entry, _ metrics.Emitter, _ getCachedScheduleDocFunc, _ getClustersFunc, _ schedulerDBs, _ func() time.Time) (Scheduler, error) {
+	svc.workerDelayMax = 0
+	svc.interval = time.Millisecond
+	svc.schedulePollInterval = 1 * time.Millisecond
+	svc.newScheduler = func(_ env.Interface, _ *logrus.Entry, _ metrics.Emitter, _ getCachedScheduleDocFunc, _ getClustersFunc, _ schedulerDBs) (Scheduler, error) {
 		return sched, nil
 	}
 
@@ -297,6 +303,233 @@ func TestSchedulerStoppingSingleItem(t *testing.T) {
 			Value: 0,
 		},
 		// This will go to 1 temporarily, but will end up at 0
+		{
+			MetricName: "mimo.scheduler.workers.active.count",
+			Dimensions: map[string]string{},
+			Value:      0,
+		},
+	}...)
+}
+
+func TestSchedulerGoesReady(t *testing.T) {
+	r := require.New(t)
+	ctx := t.Context()
+
+	m := testmetrics.NewFakeMetricsEmitter(t)
+	controller := gomock.NewController(t)
+	_env := mock_env.NewMockInterface(controller)
+	_env.EXPECT().Now().AnyTimes().DoAndReturn(time.Now)
+
+	_, log := testlog.LogForTesting(t)
+	fixtures := testdatabase.NewFixture()
+	schedules, _ := testdatabase.NewFakeMaintenanceSchedules()
+	clusters, _ := testdatabase.NewFakeOpenShiftClusters()
+	subscriptions, _ := testdatabase.NewFakeSubscriptions()
+	poolWorkers, _ := testdatabase.NewFakePoolWorkers(_env.Now)
+	dbs := database.NewDBGroup().
+		WithMaintenanceSchedules(schedules).
+		WithSubscriptions(subscriptions).
+		WithOpenShiftClusters(clusters).
+		WithPoolWorkers(poolWorkers)
+
+	fixtures.AddMaintenanceScheduleDocuments(&api.MaintenanceScheduleDocument{
+		ID: "00000000-0000-0000-0000-000000000000",
+		MaintenanceSchedule: api.MaintenanceSchedule{
+			State: api.MaintenanceScheduleStateEnabled,
+		},
+	})
+
+	// Apply the fixture
+	err := fixtures.WithMaintenanceSchedules(schedules).
+		WithOpenShiftClusters(clusters).
+		WithSubscriptions(subscriptions).
+		Create()
+	r.NoError(err)
+
+	waitFor := &sync.WaitGroup{}
+	sched := &fakeScheduler{waitOnProcess: waitFor}
+	waitFor.Add(1)
+
+	svc := NewService(_env, log, dbs, m)
+	svc.workerDelayMax = 0
+	svc.interval = time.Millisecond
+	svc.schedulePollInterval = 1 * time.Millisecond
+	svc.changefeedInterval = time.Millisecond
+	svc.readinessDelay = time.Millisecond
+	svc.serveHealthz = false
+	svc.emitHeartbeat = false
+	svc.newScheduler = func(_ env.Interface, _ *logrus.Entry, _ metrics.Emitter, _ getCachedScheduleDocFunc, _ getClustersFunc, _ schedulerDBs) (Scheduler, error) {
+		return sched, nil
+	}
+	stop := make(chan struct{})
+	done := make(chan struct{})
+
+	go svc.Run(ctx, stop, done)
+
+	r.EventuallyWithT(func(collect *assert.CollectT) {
+		require.True(collect, svc.checkReady())
+	}, time.Second, time.Millisecond)
+
+	// Wait for at least one run, and then close
+	waitFor.Wait()
+
+	close(stop)
+
+	// Then wait for the worker to stop
+	<-done
+	r.Equal(int32(0), svc.workers.Load())
+
+	m.AssertFloats()
+	m.AssertGauges([]testmetrics.MetricsAssertion[int64]{
+		{
+			MetricName: "changefeed.caches.size",
+			Dimensions: map[string]string{
+				"name": "MaintenanceScheduleDocument",
+			},
+			Value: 1,
+		},
+		// No running workers
+		{
+			MetricName: "mimo.scheduler.workers.active.count",
+			Dimensions: map[string]string{},
+			Value:      0,
+		},
+	}...)
+}
+
+func TestSchedulerServesBucket(t *testing.T) {
+	r := require.New(t)
+	ctx := t.Context()
+
+	m := testmetrics.NewFakeMetricsEmitter(t)
+	controller := gomock.NewController(t)
+	_env := mock_env.NewMockInterface(controller)
+	now := time.Date(2026, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	_env.EXPECT().Now().AnyTimes().DoAndReturn(func() time.Time {
+		now = now.Add(time.Millisecond)
+		return now
+	})
+
+	_, log := testlog.LogForTesting(t)
+	fixtures := testdatabase.NewFixture()
+	checker := testdatabase.NewChecker()
+	manifests, manifestsClient := testdatabase.NewFakeMaintenanceManifests(_env.Now)
+	schedules, _ := testdatabase.NewFakeMaintenanceSchedules()
+	clusters, _ := testdatabase.NewFakeOpenShiftClusters()
+	subscriptions, _ := testdatabase.NewFakeSubscriptions()
+	poolWorkers, _ := testdatabase.NewFakePoolWorkers(_env.Now)
+	dbs := database.NewDBGroup().
+		WithMaintenanceSchedules(schedules).
+		WithSubscriptions(subscriptions).
+		WithOpenShiftClusters(clusters).
+		WithPoolWorkers(poolWorkers).
+		WithMaintenanceManifests(manifests)
+
+	fixtures.AddSubscriptionDocuments(api.ExampleSubscriptionDocument())
+	fixtures.AddOpenShiftClusterDocuments(api.ExampleOpenShiftClusterDocument())
+
+	fixtures.AddMaintenanceScheduleDocuments(&api.MaintenanceScheduleDocument{
+		ID: "00000000-0000-0000-0000-000000000001",
+		MaintenanceSchedule: api.MaintenanceSchedule{
+			State:             api.MaintenanceScheduleStateEnabled,
+			MaintenanceTaskID: api.MIMOTaskID("0"),
+
+			Schedule:         "*-*-* *:15",
+			ScheduleAcross:   "0s",
+			LookForwardCount: 1,
+
+			Selectors: []*api.MaintenanceScheduleSelector{
+				{
+					Key:      string(SelectorDataKeySubscriptionState),
+					Operator: api.MaintenanceScheduleSelectorOperatorEq,
+					Value:    "Registered",
+				},
+			},
+		},
+	})
+
+	checker.AddMaintenanceManifestDocuments(&api.MaintenanceManifestDocument{
+		ID:                "07070707-0707-0707-0707-070707070001",
+		ClusterResourceID: strings.ToLower(api.ExampleOpenShiftClusterDocument().OpenShiftCluster.ID),
+		MaintenanceManifest: api.MaintenanceManifest{
+			State:             api.MaintenanceManifestStatePending,
+			MaintenanceTaskID: "0",
+			CreatedBySchedule: "00000000-0000-0000-0000-000000000001",
+			RunAfter:          time.Date(2026, 1, 1, 0, 15, 0, 0, time.UTC).Unix(),
+			RunBefore:         time.Date(2026, 1, 1, 1, 15, 0, 0, time.UTC).Unix(),
+		},
+	})
+
+	// Apply the fixture
+	err := fixtures.WithMaintenanceSchedules(schedules).
+		WithOpenShiftClusters(clusters).
+		WithSubscriptions(subscriptions).
+		Create()
+	r.NoError(err)
+
+	svc := NewService(_env, log, dbs, m)
+	svc.workerDelayMax = 0
+	svc.interval = 10 * time.Millisecond
+	svc.schedulePollInterval = 1 * time.Millisecond
+	svc.changefeedInterval = time.Millisecond
+	svc.readinessDelay = time.Millisecond
+	svc.serveHealthz = false
+	svc.emitHeartbeat = false
+
+	stop := make(chan struct{})
+	done := make(chan struct{})
+
+	go svc.Run(ctx, stop, done)
+
+	r.EventuallyWithT(func(collect *assert.CollectT) {
+		require.True(collect, svc.checkReady())
+	}, time.Second, time.Millisecond)
+
+	// Wait for our created manifest
+	r.EventuallyWithT(func(collect *assert.CollectT) {
+		require.Empty(collect, checker.CheckMaintenanceManifests(manifestsClient))
+	}, time.Second, time.Millisecond*10)
+
+	// Close it after
+	close(stop)
+	<-done
+	r.Equal(int32(0), svc.workers.Load())
+
+	m.AssertFloats()
+	m.AssertGauges([]testmetrics.MetricsAssertion[int64]{
+		{
+			MetricName: "changefeed.caches.size",
+			Dimensions: map[string]string{
+				"name": "MaintenanceScheduleDocument",
+			},
+			Value: 1,
+		},
+		{
+			MetricName: "changefeed.caches.size",
+			Dimensions: map[string]string{
+				"name": "OpenShiftClusterDocument",
+			},
+			Value: 1,
+		},
+		{
+			MetricName: "changefeed.caches.size",
+			Dimensions: map[string]string{
+				"name": "SubscriptionDocument",
+			},
+			Value: 1,
+		},
+		{
+			MetricName: "mimo.scheduler.manifests.created",
+			Dimensions: map[string]string{
+				"resourceGroup":  "resourcegroup",
+				"resourceId":     strings.ToLower(api.ExampleOpenShiftClusterDocument().OpenShiftCluster.ID),
+				"subscriptionId": api.ExampleSubscriptionDocument().ID,
+				"resourceName":   "resourcename",
+			},
+			Value: 1,
+		},
+		// No running workers
 		{
 			MetricName: "mimo.scheduler.workers.active.count",
 			Dimensions: map[string]string{},
