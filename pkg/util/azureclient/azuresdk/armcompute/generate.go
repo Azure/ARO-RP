94f4fd3470631eb5a98b9663fb3c968b964d07f8
rh-returners <mmancebo@redhat.com>
2026-04-20 16:42:51 +0100

    Fixed wrong path on generate.go

--- pkg/util/azureclient/azuresdk/armcompute/generate.go
+++ pkg/util/azureclient/azuresdk/armcompute/generate.go
@@ -3,7 +3,7 @@ package armcompute
 // Copyright (c) Microsoft Corporation.
 // Licensed under the Apache License 2.0.
 
-//go:generate rm -rf ../../../../util/mocks/$GOPACKAGE
+//go:generate rm -rf ../../../../util/mocks/azureclient/azuresdk/$GOPACKAGE
 //go:generate mockgen -destination=../../../../util/mocks/azureclient/azuresdk/$GOPACKAGE/$GOPACKAGE.go github.com/Azure/ARO-RP/pkg/util/azureclient/azuresdk/$GOPACKAGE ResourceSKUsClient
 //go:generate mockgen -source ./capacityreservationgroups.go -destination=../../../../util/mocks/azureclient/azuresdk/$GOPACKAGE/capacityreservationgroups.go github.com/Azure/ARO-RP/pkg/util/azureclient/azuresdk/$GOPACKAGE CapacityReservationGroupsClient
 //go:generate mockgen -source ./capacityreservations.go -destination=../../../../util/mocks/azureclient/azuresdk/$GOPACKAGE/capacityreservations.go github.com/Azure/ARO-RP/pkg/util/azureclient/azuresdk/$GOPACKAGE CapacityReservationsClient
