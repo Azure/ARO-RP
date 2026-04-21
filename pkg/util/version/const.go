4ddfc49b2f3ffd5490157fc4a8d3035aaa6623a9
LiniSusan <lkurien@redhat.com>
2026-04-21 08:53:39 +0530

    Update mdm and mdsd images

--- pkg/util/version/const.go
+++ pkg/util/version/const.go
@@ -55,13 +55,13 @@ func FluentbitImage(acrDomain string) string {
 // MdmImage contains the location of the MDM container image
 // https://eng.ms/docs/products/geneva/collect/references/linuxcontainers
 func MdmImage(acrDomain string) string {
-	return acrDomain + "/geneva/distroless/mdm:2.202602101834.0-20260211-1@sha256:a581e89744933f30a9f8e2724906fde02e713836fce76afefec7092daa01c064"
+	return acrDomain + "/geneva/distroless/mdm:2.202604071548.0-20260407-1@sha256:390a13ab26a4c90baa9d1a47ef2b502b7ec635840587d89d05120b6952fe680b"
 }
 
 // MdsdImage contains the location of the MDSD container image
 // https://eng.ms/docs/products/geneva/collect/references/linuxcontainers
 func MdsdImage(acrDomain string) string {
-	return acrDomain + "/geneva/distroless/mdsd:1.40.2-20260218-1@sha256:18d5476700adcd40e9ee2bc1cb62a1150ac1b5e696d7bad6adeea4da88c615db"
+	return acrDomain + "/geneva/distroless/mdsd:1.40.3-20260409-1@sha256:1fb51857a0a34e7e7445a91c0a1082d97df235349a66a166a58c86029c80ea89"
 }
 
 // MUOImage contains the location of the Managed Upgrade Operator container image
