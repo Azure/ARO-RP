afdb825baef62b066d65e8e3ecb559c797a5a85d
kimorris27 <kimorris@redhat.com>
2026-04-22 16:05:03 -0500

    Update Swagger generation script and generate CI check to account for the fact that TypeSpec complains about conflicts with examples if you don't delete them before generating the Swagger
    This means that anyone who runs `make generate` will have to understand not commit the changed examples; I know this is a little annoying, but I think it's important to include the TypeSpec -> Swagger generation in `make generate` and the corresponding CI check (`git add *` is not a great practice anyway IMO)

--- hack/api/swagger-from-typespec.sh
+++ hack/api/swagger-from-typespec.sh
@@ -2,6 +2,17 @@
 
 TYPESPEC_IMAGE=$1
 
+# Before generating Swagger and new examples, clear out existing examples to keep TypeSpec from
+# complaining about conflicts
+SPEC_BASE_DIR="api/redhatopenshift/resource-manager/Microsoft.RedHatOpenShift/OpenShiftClusters"
+find "$SPEC_BASE_DIR" -mindepth 2 -maxdepth 2 -type d ! -name examples | sort | while IFS= read -r api_version_dir; do
+    (
+        api_version=$(basename "$api_version_dir")
+        api_version_example_dir="$SPEC_BASE_DIR/examples/$api_version"
+        rm -rf $api_version_example_dir
+    )
+done
+
 # Format TypeSpec
 docker run \
     --platform=${PLATFORM:-linux/$(go env GOARCH)} \
