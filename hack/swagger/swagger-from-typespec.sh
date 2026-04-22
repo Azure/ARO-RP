98f3a0ef36c2c8b55ebff180741ec801d5183734
kimorris27 <kimorris@redhat.com>
2026-04-22 13:57:13 -0500

    Add `make generate-swagger-typespec` and preserve previous `make generate-swagger` as `make generate-swagger-legacy`
    We probably don't need the old one anymore, but preserving it on the off chance that we do; it doesn't hurt anything

---
+++ hack/swagger/swagger-from-typespec.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+
+TYPESPEC_IMAGE=$1
+
+# Format TypeSpec
+docker run \
+    --platform=${PLATFORM:-linux/$(go env GOARCH)} \
+    --rm \
+    -v $PWD/api:/api:z \
+    -w /api \
+    --entrypoint npm \
+    "${TYPESPEC_IMAGE}" run format
+
+# Generate Swagger from TypeSpec
+docker run \
+    --platform=${PLATFORM:-linux/$(go env GOARCH)} \
+    --rm \
+    -v $PWD/api:/api:z \
+    -w /api \
+    --entrypoint npm \
+    "${TYPESPEC_IMAGE}" run compile
+
+# Generate examples from Swagger
+SPEC_BASE_DIR="api/redhatopenshift/resource-manager/Microsoft.RedHatOpenShift/OpenShiftClusters"
+find "$SPEC_BASE_DIR" -mindepth 2 -maxdepth 2 -type d ! -name examples | sort | while IFS= read -r api_version_dir; do
+    (
+        api_version=$(basename "$api_version_dir")
+        api_version_example_dir="$SPEC_BASE_DIR/examples/$api_version"
+        mkdir -p $api_version_example_dir
+        docker run \
+            --platform=${PLATFORM:-linux/$(go env GOARCH)} \
+            --rm \
+            -v $PWD/api:/api:z \
+            -w "/$api_version_dir" \
+            --entrypoint oav \
+            "${TYPESPEC_IMAGE}" generate-examples redhatopenshift.json
+        mv $api_version_dir/examples/* $api_version_example_dir/
+        rm -rf $api_version_dir/examples
+    )
+done
