f51403b8484a3e802f6239a1c2ae97e6f0355453
kimorris27 <kimorris@redhat.com>
2026-04-29 10:16:03 -0500

    Treat path as a fixed pattern rather than interpreting as a regex per Copilot's suggestion

--- hack/ci-utils/isClean.sh
+++ hack/ci-utils/isClean.sh
@@ -2,7 +2,7 @@
 
 set -xe
 IGNORED_PATH="api/redhatopenshift/resource-manager/Microsoft.RedHatOpenShift/OpenShiftClusters/examples/"
-CHANGES=$(git status --short | grep -v "$IGNORED_PATH" || true)
+CHANGES=$(git status --short | grep -F -v "$IGNORED_PATH" || true)
 
 if [[ -n "$CHANGES" ]]
 then
