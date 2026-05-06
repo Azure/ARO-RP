f127f355cf0066cc848218371eb0fcf16715fc56
b-lnimmala <b-lnimmala@microsoft.com>
2026-05-06 12:21:03 -0400

    ARO-3837: upadte the poc to use track-2 sdk

--- hack/encryptionathost-poc/main.go
+++ hack/encryptionathost-poc/main.go
@@ -6,58 +6,26 @@ package main
 import (
 	"context"
 	"fmt"
-	"net/http"
 	"os"
 
-	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/adal"
-	"github.com/Azure/go-autorest/autorest/azure"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armfeatures"
 )
 
-type featureResult struct {
-	Properties *featureResultProperties `json:"properties"`
-}
-
-type featureResultProperties struct {
-	State string `json:"state"`
-}
-
-func validateEncryptionAtHostFeature(subscriptionID string, authorizer autorest.Authorizer) error {
-	client := autorest.NewClientWithUserAgent("")
-	client.Authorizer = authorizer
-
-	url := fmt.Sprintf(
-		"https://management.azure.com/subscriptions/%s/providers/Microsoft.Features/providers/Microsoft.Compute/features/EncryptionAtHost",
-		subscriptionID)
-
-	req, err := autorest.CreatePreparer(
-		autorest.AsGet(),
-		autorest.WithBaseURL(url),
-		autorest.WithQueryParameters(map[string]interface{}{
-			"api-version": "2021-07-01",
-		}),
-	).Prepare((&http.Request{}).WithContext(context.Background()))
+func validateEncryptionAtHostFeature(ctx context.Context, subscriptionID string, featuresClient *armfeatures.Client) error {
+	// Get the feature registration status
+	resp, err := featuresClient.Get(ctx, "Microsoft.Compute", "EncryptionAtHost", nil)
 	if err != nil {
-		return fmt.Errorf("failed to prepare request: %v", err)
+		return fmt.Errorf("failed to get feature: %w", err)
 	}
 
-	resp, err := autorest.SendWithSender(client, req)
-	if err != nil {
-		return fmt.Errorf("failed to send request: %v", err)
+	if resp.Properties == nil || resp.Properties.State == nil {
+		return fmt.Errorf("Microsoft.Compute/EncryptionAtHost feature has no state for subscription %s", subscriptionID)
 	}
 
-	var result featureResult
-	err = autorest.Respond(resp,
-		azure.WithErrorUnlessStatusCode(http.StatusOK),
-		autorest.ByUnmarshallingJSON(&result),
-		autorest.ByClosing())
-	if err != nil {
-		return fmt.Errorf("failed to read response: %v", err)
-	}
-
-	if result.Properties == nil ||
-		result.Properties.State != "Registered" {
-		return fmt.Errorf("Microsoft.Compute/EncryptionAtHost is not registered for subscription %s", subscriptionID)
+	if *resp.Properties.State != "Registered" {
+		return fmt.Errorf("Microsoft.Compute/EncryptionAtHost is not registered for subscription %s (current state: %s)",
+			subscriptionID, *resp.Properties.State)
 	}
 
 	return nil
@@ -70,26 +38,22 @@ func main() {
 	registeredSubscriptionID := os.Getenv("SUBSCRIPTION_ID")
 	notRegisteredSubscriptionID := os.Getenv("SUBSCRIPTION_ID_NOT_REGISTERED")
 
-	oauthConfig, err := adal.NewOAuthConfig(
-		azure.PublicCloud.ActiveDirectoryEndpoint, tenantID)
+	// Create credential using track-2 SDK
+	cred, err := azidentity.NewClientSecretCredential(tenantID, clientID, clientSecret, nil)
 	if err != nil {
-		panic(err)
+		panic(fmt.Sprintf("failed to create credential: %v", err))
 	}
 
-	token, err := adal.NewServicePrincipalToken(
-		*oauthConfig,
-		clientID,
-		clientSecret,
-		azure.PublicCloud.ResourceManagerEndpoint)
-	if err != nil {
-		panic(err)
-	}
-
-	authorizer := autorest.NewBearerAuthorizer(token)
+	ctx := context.Background()
 
 	// SUCCESS SCENARIO - feature IS registered
 	fmt.Println("=== SCENARIO 1: Feature IS registered ===")
-	err = validateEncryptionAtHostFeature(registeredSubscriptionID, authorizer)
+	featuresClient1, err := armfeatures.NewClient(registeredSubscriptionID, cred, nil)
+	if err != nil {
+		panic(fmt.Sprintf("failed to create features client: %v", err))
+	}
+
+	err = validateEncryptionAtHostFeature(ctx, registeredSubscriptionID, featuresClient1)
 	if err != nil {
 		fmt.Printf("FAIL - %v\n", err)
 	} else {
@@ -100,7 +64,12 @@ func main() {
 
 	// FAILURE SCENARIO - feature is NOT registered
 	fmt.Println("=== SCENARIO 2: Feature is NOT registered ===")
-	err = validateEncryptionAtHostFeature(notRegisteredSubscriptionID, authorizer)
+	featuresClient2, err := armfeatures.NewClient(notRegisteredSubscriptionID, cred, nil)
+	if err != nil {
+		panic(fmt.Sprintf("failed to create features client: %v", err))
+	}
+
+	err = validateEncryptionAtHostFeature(ctx, notRegisteredSubscriptionID, featuresClient2)
 	if err != nil {
 		fmt.Printf("SUCCESS - Correctly caught unregistered feature: %v\n", err)
 	} else {
