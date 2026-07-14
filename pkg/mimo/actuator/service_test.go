22e454b126987c854fb1d8e9c7e31b4c2637fec4
Amber Brown <ambrown@redhat.com>
2026-07-14 16:27:08 +1000

    switch out the ginkgo test here for a normal one, and expand the testing to catch if bucketing doesn't function

--- pkg/mimo/actuator/service_test.go
+++ pkg/mimo/actuator/service_test.go
@@ -12,7 +12,6 @@ import (
 	"testing"
 	"time"
 
-	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	"github.com/go-test/deep"
@@ -24,9 +23,7 @@ import (
 	"github.com/Azure/ARO-RP/pkg/api"
 	"github.com/Azure/ARO-RP/pkg/api/util/uuid"
 	"github.com/Azure/ARO-RP/pkg/database"
-	"github.com/Azure/ARO-RP/pkg/database/cosmosdb"
 	"github.com/Azure/ARO-RP/pkg/env"
-	"github.com/Azure/ARO-RP/pkg/metrics"
 	"github.com/Azure/ARO-RP/pkg/mimo/tasks"
 	"github.com/Azure/ARO-RP/pkg/util/mimo"
 	mock_env "github.com/Azure/ARO-RP/pkg/util/mocks/env"
@@ -200,242 +197,214 @@ func TestActuatorPolling(t *testing.T) {
 	}
 }
 
-var _ = Describe("MIMO Actuator Service", Ordered, func() {
-	var fixtures *testdatabase.Fixture
-	var checker *testdatabase.Checker
-	var manifests database.MaintenanceManifests
-	var manifestsClient *cosmosdb.FakeMaintenanceManifestDocumentClient
-	var clusters database.OpenShiftClusters
-	var subscriptions database.Subscriptions
-	var m metrics.Emitter
+func TestActuatorPerformsMaintenance(t *testing.T) {
+	r := require.New(t)
+	ctx := t.Context()
 
-	var svc *service
+	m := testmetrics.NewFakeMetricsEmitter(t)
+	controller := gomock.NewController(t)
+	_env := mock_env.NewMockInterface(controller)
+	_env.EXPECT().Now().AnyTimes().DoAndReturn(func() time.Time {
+		return time.Unix(120, 0)
+	})
 
-	var ctx context.Context
-	var cancel context.CancelFunc
-
-	var log *logrus.Entry
-	var _env *mock_env.MockInterface
-
-	var controller *gomock.Controller
+	_, log := testlog.LogForTesting(t)
+	ourUUID := uuid.DefaultGenerator.Generate()
+	fixtures := testdatabase.NewFixture()
+	checker := testdatabase.NewChecker()
+	manifests, manifestsClient := testdatabase.NewFakeMaintenanceManifests(_env.Now)
+	clusters, _ := testdatabase.NewFakeOpenShiftClusters()
+	subscriptions, _ := testdatabase.NewFakeSubscriptions()
+	poolWorkers, _ := testdatabase.NewFakePoolWorkers(_env.Now, ourUUID)
+	dbs := database.NewDBGroup().
+		WithMaintenanceManifests(manifests).
+		WithSubscriptions(subscriptions).
+		WithOpenShiftClusters(clusters).
+		WithPoolWorkers(poolWorkers)
 
 	mockSubID := "00000000-0000-0000-0000-000000000000"
+	fixtures.AddSubscriptionDocuments(
+		&api.SubscriptionDocument{
+			ID: mockSubID,
+		},
+	)
 	clusterResourceID := fmt.Sprintf("/subscriptions/%s/resourcegroups/resourceGroup/providers/Microsoft.RedHatOpenShift/openShiftClusters/resourceName", mockSubID)
+	fixtures.AddOpenShiftClusterDocuments(
+		&api.OpenShiftClusterDocument{
+			Key:    strings.ToLower(clusterResourceID),
+			Bucket: 1,
+			OpenShiftCluster: &api.OpenShiftCluster{
+				ID: clusterResourceID,
+				Properties: api.OpenShiftClusterProperties{
+					ProvisioningState: api.ProvisioningStateSucceeded,
+					NetworkProfile: api.NetworkProfile{
+						PodCIDR: "0.0.0.0/32",
+					},
+				},
+			},
+		},
+		// Cluster that will not be served because we are only looking at
+		// bucket 1
+		&api.OpenShiftClusterDocument{
+			Key:    strings.ToLower(clusterResourceID + "ignored"),
+			Bucket: 2,
+			OpenShiftCluster: &api.OpenShiftCluster{
+				ID: clusterResourceID + "ignored",
+				Properties: api.OpenShiftClusterProperties{
+					ProvisioningState: api.ProvisioningStateSucceeded,
+					NetworkProfile: api.NetworkProfile{
+						PodCIDR: "0.0.0.0/32",
+					},
+				},
+			},
+		},
+	)
 
-	AfterAll(func() {
-		if cancel != nil {
-			cancel()
-		}
-
-		if controller != nil {
-			controller.Finish()
-		}
+	fixtures.AddPoolWorkerDocuments(&api.PoolWorkerDocument{
+		ID:         string(api.PoolWorkerTypeMIMOActuator),
+		WorkerType: api.PoolWorkerTypeMIMOActuator,
+		PoolWorker: &api.PoolWorker{
+			// We only have bucket 1, the unowned cluster will not be served
+			Buckets: []string{"other", ourUUID, "other", "other"},
+		},
+		LeaseOwner:   "other",
+		LeaseExpires: 9999999999999,
 	})
 
-	BeforeAll(func() {
-		controller = gomock.NewController(nil)
-		_env = mock_env.NewMockInterface(controller)
-		_env.EXPECT().Now().AnyTimes().DoAndReturn(func() time.Time {
-			return time.Unix(120, 0)
-		})
-		ctx, cancel = context.WithCancel(context.Background())
+	manifestID := manifests.NewUUID()
+	manifestID2 := manifests.NewUUID()
+	manifestID3 := manifests.NewUUID()
 
-		log = logrus.NewEntry(&logrus.Logger{
-			Out:       GinkgoWriter,
-			Formatter: new(logrus.TextFormatter),
-			Hooks:     make(logrus.LevelHooks),
-			Level:     logrus.DebugLevel,
-		})
+	fixtures.AddMaintenanceManifestDocuments(
+		&api.MaintenanceManifestDocument{
+			ID:                manifestID,
+			ClusterResourceID: strings.ToLower(clusterResourceID),
+			MaintenanceManifest: api.MaintenanceManifest{
+				State:     api.MaintenanceManifestStatePending,
+				RunBefore: 60,
+				RunAfter:  0,
+			},
+		},
+		&api.MaintenanceManifestDocument{
+			ID:                manifestID2,
+			ClusterResourceID: strings.ToLower(clusterResourceID),
+			MaintenanceManifest: api.MaintenanceManifest{
+				State:             api.MaintenanceManifestStatePending,
+				RunBefore:         300,
+				RunAfter:          0,
+				MaintenanceTaskID: "0000-0000-0001",
+			},
+		},
+		// A manifest for a cluster that is not served by our bucket allocation
+		&api.MaintenanceManifestDocument{
+			ID:                manifestID3,
+			ClusterResourceID: strings.ToLower(clusterResourceID + "ignored"),
+			MaintenanceManifest: api.MaintenanceManifest{
+				State:             api.MaintenanceManifestStatePending,
+				RunBefore:         300,
+				RunAfter:          0,
+				MaintenanceTaskID: "0000-0000-0001",
+			},
+		},
+	)
 
-		fixtures = testdatabase.NewFixture()
-		checker = testdatabase.NewChecker()
+	// Apply the fixture
+	err := fixtures.
+		WithOpenShiftClusters(clusters).
+		WithSubscriptions(subscriptions).
+		WithMaintenanceManifests(manifests).
+		WithPoolWorkers(poolWorkers).
+		Create()
+	r.NoError(err)
+
+	svc := NewService(_env, log, nil, dbs, m)
+	svc.workerMaxStartupDelay = 0
+	svc.taskPollTime = time.Millisecond
+	svc.changefeedInterval = time.Millisecond
+	svc.readinessDelay = time.Millisecond
+	svc.serveHealthz = false
+	svc.emitHeartbeat = false
+
+	svc.SetMaintenanceTasks(map[api.MIMOTaskID]tasks.MaintenanceTask{
+		"0000-0000-0001": func(th mimo.TaskContext, mmd *api.MaintenanceManifestDocument, oscd *api.OpenShiftClusterDocument) error {
+			// Only the ClusterResourceID is available to the bucket
+			// worker, so make sure this is the full document
+			r.Equal(oscd.OpenShiftCluster.Properties.NetworkProfile.PodCIDR, "0.0.0.0/32")
+
+			// once we've run this task, stop the worker
+			svc.stopping.Store(true)
+			th.SetResultMessage("ok")
+			return nil
+		},
 	})
 
-	BeforeEach(func() {
-		m = newfakeMetricsEmitter()
+	stop := make(chan struct{})
+	done := make(chan struct{})
 
-		manifests, manifestsClient = testdatabase.NewFakeMaintenanceManifests(_env.Now)
-		clusters, _ = testdatabase.NewFakeOpenShiftClusters()
-		subscriptions, _ = testdatabase.NewFakeSubscriptions()
-		dbg := database.NewDBGroup().WithMaintenanceManifests(manifests).WithOpenShiftClusters(clusters).WithSubscriptions(subscriptions)
+	go svc.Run(ctx, stop, done)
 
-		svc = NewService(_env, log, nil, dbg, m)
-		svc.workerMaxStartupDelay = time.Second * 0
-		svc.serveHealthz = false
-		svc.b.SetBuckets([]int{1})
-	})
+	// Wait for it to finish
+	<-done
+	r.Equal(int32(0), svc.workerCount.Load())
 
-	JustBeforeEach(func() {
-		err := fixtures.WithOpenShiftClusters(clusters).WithMaintenanceManifests(manifests).WithSubscriptions(subscriptions).Create()
-		Expect(err).ToNot(HaveOccurred())
-	})
+	checker.AddMaintenanceManifestDocuments(
+		&api.MaintenanceManifestDocument{
+			ID:                manifestID,
+			ClusterResourceID: strings.ToLower(clusterResourceID),
+			MaintenanceManifest: api.MaintenanceManifest{
+				State:      api.MaintenanceManifestStateTimedOut,
+				StatusText: "timed out at 1970-01-01 00:02:00 +0000 UTC",
+				RunBefore:  60,
+				RunAfter:   0,
+			},
+		},
+		&api.MaintenanceManifestDocument{
+			ID:                manifestID2,
+			Dequeues:          1,
+			ClusterResourceID: strings.ToLower(clusterResourceID),
+			MaintenanceManifest: api.MaintenanceManifest{
+				State:             api.MaintenanceManifestStateCompleted,
+				StatusText:        "ok",
+				RunBefore:         300,
+				RunAfter:          0,
+				MaintenanceTaskID: "0000-0000-0001",
+			},
+		},
+		// manifest will not be served
+		&api.MaintenanceManifestDocument{
+			ID:                manifestID3,
+			ClusterResourceID: strings.ToLower(clusterResourceID + "ignored"),
+			MaintenanceManifest: api.MaintenanceManifest{
+				State:             api.MaintenanceManifestStatePending,
+				RunBefore:         300,
+				RunAfter:          0,
+				MaintenanceTaskID: "0000-0000-0001",
+			},
+		},
+	)
 
-	When("maintenance needs to occur", func() {
-		var manifestID string
+	errs := checker.CheckMaintenanceManifests(manifestsClient)
+	r.Nil(errs, fmt.Sprintf("%v", errs))
 
-		BeforeEach(func() {
-			fixtures.Clear()
-			fixtures.AddSubscriptionDocuments(
-				&api.SubscriptionDocument{
-					ID: mockSubID,
-				},
-			)
-			fixtures.AddOpenShiftClusterDocuments(
-				&api.OpenShiftClusterDocument{
-					Key:    strings.ToLower(clusterResourceID),
-					Bucket: 1,
-					OpenShiftCluster: &api.OpenShiftCluster{
-						ID: clusterResourceID,
-						Properties: api.OpenShiftClusterProperties{
-							ProvisioningState: api.ProvisioningStateSucceeded,
-							NetworkProfile: api.NetworkProfile{
-								PodCIDR: "0.0.0.0/32",
-							},
-						},
-					},
-				},
-				// Cluster that will not be served because we are only looking at
-				// bucket 1
-				&api.OpenShiftClusterDocument{
-					Key:    strings.ToLower(clusterResourceID + "ignored"),
-					Bucket: 2,
-					OpenShiftCluster: &api.OpenShiftCluster{
-						ID: clusterResourceID + "ignored",
-						Properties: api.OpenShiftClusterProperties{
-							ProvisioningState: api.ProvisioningStateSucceeded,
-							NetworkProfile: api.NetworkProfile{
-								PodCIDR: "0.0.0.0/32",
-							},
-						},
-					},
-				},
-			)
+	m.AssertFloats()
+	m.AssertGauges([]testmetrics.MetricsAssertion[int64]{
+		{
+			MetricName: "changefeed.caches.size",
+			Dimensions: map[string]string{
+				"name": "OpenShiftClusterDocument",
+			},
+			Value: 2,
+		},
+		// No running workers
+		{
+			MetricName: "mimo.actuator.workers.active.count",
+			Dimensions: map[string]string{},
+			Value:      0,
+		},
+	}...)
+}
 
-			manifestID = manifests.NewUUID()
-			manifestID2 := manifests.NewUUID()
-			manifestID3 := manifests.NewUUID()
-
-			fixtures.AddMaintenanceManifestDocuments(
-				&api.MaintenanceManifestDocument{
-					ID:                manifestID,
-					ClusterResourceID: strings.ToLower(clusterResourceID),
-					MaintenanceManifest: api.MaintenanceManifest{
-						State:     api.MaintenanceManifestStatePending,
-						RunBefore: 60,
-						RunAfter:  0,
-					},
-				},
-				&api.MaintenanceManifestDocument{
-					ID:                manifestID2,
-					ClusterResourceID: strings.ToLower(clusterResourceID),
-					MaintenanceManifest: api.MaintenanceManifest{
-						State:             api.MaintenanceManifestStatePending,
-						RunBefore:         300,
-						RunAfter:          0,
-						MaintenanceTaskID: "0000-0000-0001",
-					},
-				},
-				// A manifest for a cluster that is not served by our bucket allocation
-				&api.MaintenanceManifestDocument{
-					ID:                manifestID3,
-					ClusterResourceID: strings.ToLower(clusterResourceID + "ignored"),
-					MaintenanceManifest: api.MaintenanceManifest{
-						State:             api.MaintenanceManifestStatePending,
-						RunBefore:         300,
-						RunAfter:          0,
-						MaintenanceTaskID: "0000-0000-0001",
-					},
-				},
-			)
-
-			checker.Clear()
-			checker.AddMaintenanceManifestDocuments(
-				&api.MaintenanceManifestDocument{
-					ID:                manifestID,
-					ClusterResourceID: strings.ToLower(clusterResourceID),
-					MaintenanceManifest: api.MaintenanceManifest{
-						State:      api.MaintenanceManifestStateTimedOut,
-						StatusText: "timed out at 1970-01-01 00:02:00 +0000 UTC",
-						RunBefore:  60,
-						RunAfter:   0,
-					},
-				},
-				&api.MaintenanceManifestDocument{
-					ID:                manifestID2,
-					Dequeues:          1,
-					ClusterResourceID: strings.ToLower(clusterResourceID),
-					MaintenanceManifest: api.MaintenanceManifest{
-						State:             api.MaintenanceManifestStateCompleted,
-						StatusText:        "ok",
-						RunBefore:         300,
-						RunAfter:          0,
-						MaintenanceTaskID: "0000-0000-0001",
-					},
-				},
-				// manifest will not be served
-				&api.MaintenanceManifestDocument{
-					ID:                manifestID3,
-					ClusterResourceID: strings.ToLower(clusterResourceID + "ignored"),
-					MaintenanceManifest: api.MaintenanceManifest{
-						State:             api.MaintenanceManifestStatePending,
-						RunBefore:         300,
-						RunAfter:          0,
-						MaintenanceTaskID: "0000-0000-0001",
-					},
-				},
-			)
-		})
-
-		It("expires them", func() {
-			svc.taskPollTime = time.Millisecond
-
-			svc.SetMaintenanceTasks(map[api.MIMOTaskID]tasks.MaintenanceTask{
-				"0000-0000-0001": func(th mimo.TaskContext, mmd *api.MaintenanceManifestDocument, oscd *api.OpenShiftClusterDocument) error {
-					// once we've run this task, stop the worker
-					svc.stopping.Store(true)
-					th.SetResultMessage("ok")
-					return nil
-				},
-			})
-
-			_, err := svc.poll(ctx, nil)
-			Expect(err).ToNot(HaveOccurred())
-
-			// Wait for all of the workers to have stopped
-			svc.b.WaitForWorkerCompletion()
-
-			errs := checker.CheckMaintenanceManifests(manifestsClient)
-			Expect(errs).To(BeNil(), fmt.Sprintf("%v", errs))
-		})
-
-		It("loads the full cluster document", func() {
-			svc.taskPollTime = time.Millisecond
-
-			svc.SetMaintenanceTasks(map[api.MIMOTaskID]tasks.MaintenanceTask{
-				"0000-0000-0001": func(th mimo.TaskContext, mmd *api.MaintenanceManifestDocument, oscd *api.OpenShiftClusterDocument) error {
-					// Only the ClusterResourceID is available to the bucket
-					// worker, so make sure this is the full document
-					Expect(oscd.OpenShiftCluster.Properties.NetworkProfile.PodCIDR).To(Equal("0.0.0.0/32"))
-
-					svc.stopping.Store(true)
-					th.SetResultMessage("ok")
-					return nil
-				},
-			})
-
-			_, err := svc.poll(ctx, nil)
-			Expect(err).ToNot(HaveOccurred())
-
-			// Wait for all of the workers to have stopped
-			svc.b.WaitForWorkerCompletion()
-
-			errs := checker.CheckMaintenanceManifests(manifestsClient)
-			Expect(errs).To(BeNil(), fmt.Sprintf("%v", errs))
-		})
-	})
-})
-
-func TestActuatorGoesReady(t *testing.T) {
+func TestActuatorGoesReadyEvenIfNoWork(t *testing.T) {
 	r := require.New(t)
 	ctx := t.Context()
 
@@ -481,17 +450,7 @@ func TestActuatorGoesReady(t *testing.T) {
 		Create()
 	r.NoError(err)
 
-	waitFor := &sync.WaitGroup{}
-	act := &fakeActuator{waitOnProcess: waitFor, whileRunning: func() {
-		// Verify the worker metric is incremented during the runtime
-		m.AssertSingleGauge(testmetrics.MetricsAssertion[int64]{
-			MetricName: "mimo.actuator.workers.active.count",
-			Dimensions: map[string]string{},
-			Value:      1,
-		})
-	}}
-	waitFor.Add(1)
-
+	act := &fakeActuator{}
 	svc := NewService(_env, log, nil, dbs, m)
 	svc.workerMaxStartupDelay = 0
 	svc.taskPollTime = time.Millisecond
@@ -516,11 +475,10 @@ func TestActuatorGoesReady(t *testing.T) {
 		require.True(collect, svc.checkReady())
 	}, time.Second, time.Millisecond)
 
-	// Wait for at least one run, and then close
-	waitFor.Wait()
+	// Once it goes ready, stop the service
 	close(stop)
 
-	// Then wait for the worker to stop
+	// Make sure no workers are running
 	<-done
 	r.Equal(int32(0), svc.workerCount.Load())
 
