3228b921d35dd1fb951443a6d7c982a5333a647b
kimorris27 <kimorris@redhat.com>
2026-07-22 13:54:08 -0500

    `make client-generate`

--- python/client/azure/mgmt/redhatopenshift/operations/__init__.py
+++ python/client/azure/mgmt/redhatopenshift/operations/__init__.py
@@ -12,21 +12,22 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from ._patch import *  # pylint: disable=unused-wildcard-import
 
-from ._client import RedHatOpenShiftClient  # type: ignore
-from ._version import VERSION
+from ._operations import Operations  # type: ignore
+from ._operations import OpenShiftVersionsOperations  # type: ignore
+from ._operations import PlatformWorkloadIdentityRoleSetsOperations  # type: ignore
+from ._operations import OpenShiftClustersOperations  # type: ignore
+from ._operations import PlatformWorkloadIdentityRoleSetOperations  # type: ignore
 
-__version__ = VERSION
-
-try:
-    from ._patch import __all__ as _patch_all
-    from ._patch import *
-except ImportError:
-    _patch_all = []
+from ._patch import __all__ as _patch_all
+from ._patch import *
 from ._patch import patch_sdk as _patch_sdk
 
 __all__ = [
-    "RedHatOpenShiftClient",
+    "Operations",
+    "OpenShiftVersionsOperations",
+    "PlatformWorkloadIdentityRoleSetsOperations",
+    "OpenShiftClustersOperations",
+    "PlatformWorkloadIdentityRoleSetOperations",
 ]
 __all__.extend([p for p in _patch_all if p not in __all__])  # pyright: ignore
-
 _patch_sdk()
