0710085a9483f8bfb9b2bf15a8fb205281126962
Amber Brown <ambrown@redhat.com>
2026-07-22 22:04:25 +1000

    Revert "ARO-28189: prevent ingress visibility corruption" (#4988)

--- pkg/frontend/openshiftcluster_putorpatch.go
+++ pkg/frontend/openshiftcluster_putorpatch.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/sirupsen/logrus"
@@ -159,7 +160,15 @@ func (f *frontend) _putOrPatchOpenShiftCluster(ctx context.Context, log *logrus.
 		}
 	}
 
-	var ext any
+	// If Put or Patch is executed we will enrich document with cluster data.
+	if !isCreate {
+		timeoutCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+		defer cancel()
+
+		f.clusterEnricher.Enrich(timeoutCtx, log, doc.OpenShiftCluster)
+	}
+
+	var ext interface{}
 	switch putOrPatchClusterParameters.method {
 	// In case of PUT we will take customer request payload and store into database
 	// Our base structure for unmarshal is skeleton document with values we
@@ -186,8 +195,8 @@ func (f *frontend) _putOrPatchOpenShiftCluster(ctx context.Context, log *logrus.
 
 		ext = putOrPatchClusterParameters.converter.ToExternal(document)
 
-	// In case of PATCH we take current cluster document and use it as base
-	// for unmarshal. So customer can
+	// In case of PATCH we take current cluster document, which is enriched
+	// from the cluster and use it as base for unmarshal. So customer can
 	// provide single field json to be updated in the database.
 	// Patch should be used for updating individual fields of the document.
 	case http.MethodPatch:
@@ -435,7 +444,7 @@ func validateIdentityTenantID(cluster *api.OpenShiftCluster, identityTenantID st
 	return nil
 }
 
-func (f *frontend) ValidateNewCluster(ctx context.Context, subscription *api.SubscriptionDocument, cluster *api.OpenShiftCluster, staticValidator api.OpenShiftClusterStaticValidator, ext any, path string) error {
+func (f *frontend) ValidateNewCluster(ctx context.Context, subscription *api.SubscriptionDocument, cluster *api.OpenShiftCluster, staticValidator api.OpenShiftClusterStaticValidator, ext interface{}, path string) error {
 	err := staticValidator.Static(ext, nil, f.env.Location(), f.env.Domain(), f.env.FeatureIsSet(env.FeatureRequireD2sWorkers), version.InstallArchitectureVersion, path)
 	if err != nil {
 		return err
--- pkg/util/clusterdata/ingress_profile_test.go
+++ pkg/util/clusterdata/ingress_profile_test.go
@@ -31,64 +31,13 @@ func TestIngressProfilesEnricherTask(t *testing.T) {
 	owningIngressLabel := "ingresscontroller.operator.openshift.io/owning-ingresscontroller"
 	for _, tt := range []struct {
 		name        string
-		oc          *api.OpenShiftCluster
 		operatorcli operatorclient.Interface
 		kubecli     kubernetes.Interface
 		wantOc      *api.OpenShiftCluster
 		wantErr     string
 	}{
 		{
-			name: "visibility remains unknown when endpoint publishing strategy is missing",
-			operatorcli: operatorfake.NewSimpleClientset(
-				&operatorv1.IngressController{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "default",
-						Namespace: oioNamespace,
-					},
-				},
-			),
-			kubecli: fake.NewSimpleClientset(&corev1.Service{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "router-default",
-					Namespace: oiNamespace,
-					Labels: map[string]string{
-						"app":              "router",
-						owningIngressLabel: "default",
-					},
-				},
-				Status: corev1.ServiceStatus{
-					LoadBalancer: corev1.LoadBalancerStatus{
-						Ingress: []corev1.LoadBalancerIngress{
-							{
-								IP: "x.x.x.x",
-							},
-						},
-					},
-				},
-			}),
-			wantOc: &api.OpenShiftCluster{
-				Properties: api.OpenShiftClusterProperties{
-					IngressProfiles: []api.IngressProfile{
-						{
-							Name: "default",
-							IP:   "x.x.x.x",
-						},
-					},
-				},
-			},
-		},
-		{
-			name: "stored visibility is preserved when live visibility is unknown",
-			oc: &api.OpenShiftCluster{
-				Properties: api.OpenShiftClusterProperties{
-					IngressProfiles: []api.IngressProfile{
-						{
-							Name:       "default",
-							Visibility: api.VisibilityPrivate,
-						},
-					},
-				},
-			},
+			name: "default simplest case of ingress profile found",
 			operatorcli: operatorfake.NewSimpleClientset(
 				&operatorv1.IngressController{
 					ObjectMeta: metav1.ObjectMeta{
@@ -121,7 +70,7 @@ func TestIngressProfilesEnricherTask(t *testing.T) {
 					IngressProfiles: []api.IngressProfile{
 						{
 							Name:       "default",
-							Visibility: api.VisibilityPrivate,
+							Visibility: api.VisibilityPublic,
 							IP:         "x.x.x.x",
 						},
 					},
@@ -130,64 +79,6 @@ func TestIngressProfilesEnricherTask(t *testing.T) {
 		},
 		{
 			name: "private ingress profile found",
-			operatorcli: operatorfake.NewSimpleClientset(
-				&operatorv1.IngressController{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "private",
-						Namespace: oioNamespace,
-					},
-					Spec: operatorv1.IngressControllerSpec{
-						EndpointPublishingStrategy: &operatorv1.EndpointPublishingStrategy{
-							LoadBalancer: &operatorv1.LoadBalancerStrategy{
-								Scope: operatorv1.InternalLoadBalancer,
-							},
-						},
-					},
-				},
-			),
-			kubecli: fake.NewSimpleClientset(&corev1.Service{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "router-private",
-					Namespace: oiNamespace,
-					Labels: map[string]string{
-						"app":              "router",
-						owningIngressLabel: "private",
-					},
-				},
-				Status: corev1.ServiceStatus{
-					LoadBalancer: corev1.LoadBalancerStatus{
-						Ingress: []corev1.LoadBalancerIngress{
-							{
-								IP: "x.x.x.x",
-							},
-						},
-					},
-				},
-			}),
-			wantOc: &api.OpenShiftCluster{
-				Properties: api.OpenShiftClusterProperties{
-					IngressProfiles: []api.IngressProfile{
-						{
-							Name:       "private",
-							Visibility: api.VisibilityPrivate,
-							IP:         "x.x.x.x",
-						},
-					},
-				},
-			},
-		},
-		{
-			name: "live visibility overrides stored visibility when known",
-			oc: &api.OpenShiftCluster{
-				Properties: api.OpenShiftClusterProperties{
-					IngressProfiles: []api.IngressProfile{
-						{
-							Name:       "private",
-							Visibility: api.VisibilityPublic,
-						},
-					},
-				},
-			},
 			operatorcli: operatorfake.NewSimpleClientset(
 				&operatorv1.IngressController{
 					ObjectMeta: metav1.ObjectMeta{
@@ -414,10 +305,7 @@ func TestIngressProfilesEnricherTask(t *testing.T) {
 		},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
-			oc := tt.oc
-			if oc == nil {
-				oc = &api.OpenShiftCluster{}
-			}
+			oc := &api.OpenShiftCluster{}
 			e := ingressProfileEnricher{}
 
 			clients := clients{
@@ -428,154 +316,9 @@ func TestIngressProfilesEnricherTask(t *testing.T) {
 			if (err == nil && tt.wantErr != "") || (err != nil && err.Error() != tt.wantErr) {
 				t.Errorf("wanted err to be %s but got %s", err, tt.wantErr)
 			}
-			if !reflect.DeepEqual(oc.Properties, tt.wantOc.Properties) {
-				t.Error(cmp.Diff(oc.Properties, tt.wantOc.Properties))
+			if !reflect.DeepEqual(oc, tt.wantOc) {
+				t.Error(cmp.Diff(oc, tt.wantOc))
 			}
 		})
-	}
-}
-
-func TestVisibilityFromEndpointPublishingStrategy(t *testing.T) {
-	for _, tt := range []struct {
-		name           string
-		strategy       *operatorv1.EndpointPublishingStrategy
-		wantVisibility api.Visibility
-		wantOK         bool
-	}{
-		{
-			name:           "internal load balancer resolves to private",
-			strategy:       endpointPublishingStrategy(operatorv1.InternalLoadBalancer),
-			wantVisibility: api.VisibilityPrivate,
-			wantOK:         true,
-		},
-		{
-			name:           "external load balancer resolves to public",
-			strategy:       endpointPublishingStrategy(operatorv1.ExternalLoadBalancer),
-			wantVisibility: api.VisibilityPublic,
-			wantOK:         true,
-		},
-		{
-			name:     "nil strategy remains unknown",
-			strategy: nil,
-		},
-		{
-			name:     "missing load balancer remains unknown",
-			strategy: &operatorv1.EndpointPublishingStrategy{},
-		},
-		{
-			name:     "unexpected scope remains unknown",
-			strategy: endpointPublishingStrategy(operatorv1.LoadBalancerScope("Unexpected")),
-		},
-	} {
-		t.Run(tt.name, func(t *testing.T) {
-			gotVisibility, gotOK := visibilityFromEndpointPublishingStrategy(tt.strategy)
-			if gotVisibility != tt.wantVisibility {
-				t.Fatalf("got visibility %q, want %q", gotVisibility, tt.wantVisibility)
-			}
-			if gotOK != tt.wantOK {
-				t.Fatalf("got ok %t, want %t", gotOK, tt.wantOK)
-			}
-		})
-	}
-}
-
-func TestIngressProfileVisibility(t *testing.T) {
-	log := logrus.NewEntry(logrus.StandardLogger())
-
-	for _, tt := range []struct {
-		name              string
-		ingressController operatorv1.IngressController
-		wantVisibility    api.Visibility
-	}{
-		{
-			name: "status internal load balancer resolves to private when spec is nil",
-			ingressController: operatorv1.IngressController{
-				Status: operatorv1.IngressControllerStatus{
-					EndpointPublishingStrategy: endpointPublishingStrategy(operatorv1.InternalLoadBalancer),
-				},
-			},
-			wantVisibility: api.VisibilityPrivate,
-		},
-		{
-			name: "status external load balancer resolves to public when spec is nil",
-			ingressController: operatorv1.IngressController{
-				Status: operatorv1.IngressControllerStatus{
-					EndpointPublishingStrategy: endpointPublishingStrategy(operatorv1.ExternalLoadBalancer),
-				},
-			},
-			wantVisibility: api.VisibilityPublic,
-		},
-		{
-			name: "spec internal load balancer resolves to private when status is nil",
-			ingressController: operatorv1.IngressController{
-				Spec: operatorv1.IngressControllerSpec{
-					EndpointPublishingStrategy: endpointPublishingStrategy(operatorv1.InternalLoadBalancer),
-				},
-			},
-			wantVisibility: api.VisibilityPrivate,
-		},
-		{
-			name: "spec external load balancer resolves to public when status is nil",
-			ingressController: operatorv1.IngressController{
-				Spec: operatorv1.IngressControllerSpec{
-					EndpointPublishingStrategy: endpointPublishingStrategy(operatorv1.ExternalLoadBalancer),
-				},
-			},
-			wantVisibility: api.VisibilityPublic,
-		},
-		{
-			name:              "visibility remains unknown when status and spec are nil",
-			ingressController: operatorv1.IngressController{},
-		},
-		{
-			name: "status takes precedence over spec when both resolve",
-			ingressController: operatorv1.IngressController{
-				Spec: operatorv1.IngressControllerSpec{
-					EndpointPublishingStrategy: endpointPublishingStrategy(operatorv1.ExternalLoadBalancer),
-				},
-				Status: operatorv1.IngressControllerStatus{
-					EndpointPublishingStrategy: endpointPublishingStrategy(operatorv1.InternalLoadBalancer),
-				},
-			},
-			wantVisibility: api.VisibilityPrivate,
-		},
-		{
-			name: "status without load balancer falls back to spec",
-			ingressController: operatorv1.IngressController{
-				Spec: operatorv1.IngressControllerSpec{
-					EndpointPublishingStrategy: endpointPublishingStrategy(operatorv1.ExternalLoadBalancer),
-				},
-				Status: operatorv1.IngressControllerStatus{
-					EndpointPublishingStrategy: &operatorv1.EndpointPublishingStrategy{},
-				},
-			},
-			wantVisibility: api.VisibilityPublic,
-		},
-		{
-			name: "visibility remains unknown when both status and spec are unusable",
-			ingressController: operatorv1.IngressController{
-				Spec: operatorv1.IngressControllerSpec{
-					EndpointPublishingStrategy: &operatorv1.EndpointPublishingStrategy{},
-				},
-				Status: operatorv1.IngressControllerStatus{
-					EndpointPublishingStrategy: endpointPublishingStrategy(operatorv1.LoadBalancerScope("Unexpected")),
-				},
-			},
-		},
-	} {
-		t.Run(tt.name, func(t *testing.T) {
-			gotVisibility := ingressProfileVisibility(log, "test", tt.ingressController)
-			if gotVisibility != tt.wantVisibility {
-				t.Fatalf("got visibility %q, want %q", gotVisibility, tt.wantVisibility)
-			}
-		})
-	}
-}
-
-func endpointPublishingStrategy(scope operatorv1.LoadBalancerScope) *operatorv1.EndpointPublishingStrategy {
-	return &operatorv1.EndpointPublishingStrategy{
-		LoadBalancer: &operatorv1.LoadBalancerStrategy{
-			Scope: scope,
-		},
 	}
 }
