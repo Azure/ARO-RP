9c543da70cf8ecc56ec03a4dfd1064c9bc615293
kimorris27 <kimorris@redhat.com>
2026-07-27 09:55:48 -0500

    Revert `isClean.sh` back to its pre-TypeSpec contents since we no longer need to ignore any paths

--- hack/ci-utils/isClean.sh
+++ hack/ci-utils/isClean.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
 set -xe
-IGNORED_PATH=""
-CHANGES=$(git status --short)
-if [[ -n "$IGNORED_PATH" ]]; then
-    CHANGES=$(printf "%s\n" "$CHANGES" | grep -F -v "$IGNORED_PATH" || true)
-fi
-
-if [[ -n "$CHANGES" ]]
+if [[ ! -z "$(git status -s)" ]]
 then
     echo "there are some modified files"
-	echo "$CHANGES"
-    exit 1
+    git status
+    exit 1 
 fi
-
-
