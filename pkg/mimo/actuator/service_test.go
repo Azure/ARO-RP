02828ddef0b586a7bc37faf65d1527a9bbe3bbf4
Amber Brown <ambrown@redhat.com>
2026-07-30 14:44:02 +1000

    fix copilot nit

--- pkg/mimo/actuator/service_test.go
+++ pkg/mimo/actuator/service_test.go
@@ -8,6 +8,7 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
@@ -430,7 +431,9 @@ func TestActuatorGoesReadyEvenIfNoWork(t *testing.T) {
 		Create()
 	r.NoError(err)
 
-	act := &fakeActuator{}
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	act := &fakeActuator{waitOnProcess: wg}
 	svc := NewService(_env, log, nil, dbs, m)
 	svc.workerMaxStartupDelay = 0
 	svc.taskPollTime = time.Millisecond
@@ -453,8 +456,10 @@ func TestActuatorGoesReadyEvenIfNoWork(t *testing.T) {
 
 	r.EventuallyWithT(func(collect *assert.CollectT) {
 		require.True(collect, svc.checkReady())
-		require.True(collect, act.hasRan)
 	}, time.Second, time.Millisecond)
+
+	// Let the Actuator poll process run once
+	act.waitOnProcess.Wait()
 
 	// Once it goes ready, stop the service
 	close(stop)
