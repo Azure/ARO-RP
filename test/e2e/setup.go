4c92073bcfe509ccca8694414c82f389b6bb18b2
Rachel Weber <rawo@microsoft.com>
2026-08-12 11:41:18 -0700

    Allowing E2E test in internal tenant

--- test/e2e/setup.go
+++ test/e2e/setup.go
@@ -301,9 +301,17 @@ func resourceIDFromEnv() string {
 		_env.SubscriptionID(), vnetResourceGroup, clusterName)
 }
 
-func newClientSet(ctx context.Context) (*clientSet, error) {
+func newE2ETokenCredential() (azcore.TokenCredential, error) {
+	if os.Getenv("AZURE_TOKEN_CREDENTIALS") != "" {
+		return _env.Environment().NewTokenCredential()
+	}
+
 	options := _env.Environment().EnvironmentCredentialOptions()
-	tokenCredential, err := azidentity.NewEnvironmentCredential(options)
+	return azidentity.NewEnvironmentCredential(options)
+}
+
+func newClientSet(ctx context.Context) (*clientSet, error) {
+	tokenCredential, err := newE2ETokenCredential()
 	if err != nil {
 		return nil, err
 	}
@@ -577,10 +585,7 @@ func newClientSet(ctx context.Context) (*clientSet, error) {
 
 func setupE2EInfrastructure(ctx context.Context) error {
 	if err := env.ValidateVars(
-		"AZURE_CLIENT_ID",
-		"AZURE_CLIENT_SECRET",
 		"AZURE_SUBSCRIPTION_ID",
-		"AZURE_TENANT_ID",
 		"CLUSTER",
 		"LOCATION"); err != nil {
 		return err
@@ -600,8 +605,7 @@ func setupE2EInfrastructure(ctx context.Context) error {
 	}
 
 	// Build a bare‐bones Azure SDK client for OpenshiftClusters
-	credOptions := _env.Environment().EnvironmentCredentialOptions()
-	tokenCred, err := azidentity.NewEnvironmentCredential(credOptions)
+	tokenCred, err := newE2ETokenCredential()
 	if err != nil {
 		return err
 	}
